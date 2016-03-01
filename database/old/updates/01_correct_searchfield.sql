6adedbec8cd879be443249a006ecb66cbe92b06d
Stefan Heinemann <stefan.heinemann@codedump.ch>
2016-03-01 15:44:05 +0100

    Removed obsolete db stuff

--- database/old/updates/01_correct_searchfield.sql
+++
@@ -1,7 +0,0 @@
--- Correcting the Search field parzellennumber in BK 
--- Fixes #10719
-
-
-UPDATE "R_SEARCH_FILTER" 
-SET  "LABEL" = 'Parzellennummer'
-WHERE "R_SEARCH_FILTER_ID" = 46;
--- database/old/updates/02_change_company_to_organisation.sql
+++
@@ -1,6 +0,0 @@
--- Change the question "company" to "organisation"
-
-
-UPDATE "QUESTION"
-SET "NAME" = 'Organisation'
-WHERE "QUESTION_ID" IN (221, 222, 223);
