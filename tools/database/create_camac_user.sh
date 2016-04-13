9ae42c3d5cd1a113df8cf717885528933db1c0a5
Stefan Heinemann <stefan.heinemann@codedump.ch>
2016-04-13 16:46:03 +0200

    Improved the test-server deployment process and similar tasks

--- tools/database/create_camac_user.sh
+++ tools/database/create_camac_user.sh
@@ -5,14 +5,19 @@ source /u01/app/oracle/product/11.2.0/xe/bin/oracle_env.sh
 sqlplus system/oracle << EOF
 
 CREATE USER camac IDENTIFIED BY camac;
-grant connect to camac;
-grant create table to camac;
-grant create view to camac;
-grant create procedure to camac;
-grant create sequence to camac;
-grant create trigger to camac;
-grant resource to camac;
-grant create materialized view to camac;
-grant CREATE any directory to camac;
+grant all privileges to camac;
 
 EOF
+
+#grant CONNECT to camac;
+#grant CREATE TABLE to camac;
+#grant CREATE VIEW to camac;
+#grant CREATE PROCEDURE to camac;
+#grant CREATE SEQUENCE to camac;
+#grant CREATE TRIGGER to camac;
+#grant RESOURCE to camac;
+#grant CREATE MATERIALIZED view to camac;
+#grant CREATE any directory to camac;
+#grant CREATE SYNONYM to camac;
+#
+#EOF
--- tools/database/insert_base_structure.sh
+++ tools/database/insert_base_structure.sh
@@ -3,11 +3,8 @@
 source /u01/app/oracle/product/11.2.0/xe/bin/oracle_env.sh
 
 sqlplus system/oracle << EOF
-
 CREATE OR REPLACE DIRECTORY strucdir AS '/var/local/database/structure_dumps/';
-
 GRANT read,write ON DIRECTORY strucdir TO camac;
-
 EOF
 
 impdp camac/camac DIRECTORY=strucdir DUMPFILE=latest SCHEMAS=camac 
