85a7502bb9d1b868d893a299e692df3253f4e46e
Christian Zosel <christian.zosel@adfinis-sygroup.ch>
2016-08-23 16:06:55 +0200

    Add 'invoiced' field to billing & bill generation

--- database/structure_updates/2016-08-22-billing.sql
+++ database/structure_updates/2016-08-22-billing.sql
@@ -1,7 +1,8 @@
 alter table billing_entry add "CREATED" DATE;
 alter table billing_entry add "AMOUNT_TYPE" NUMBER(1,0) DEFAULT 0 NOT NULL;
 alter table billing_entry add "TYPE" NUMBER(1,0) DEFAULT 0 NOT NULL;
-alter table billing_entry add "REASON" VARCHAR2(300) DEFAULT 0 NOT NULL;
+alter table billing_entry add "REASON" VARCHAR2(300);
+alter table billing_entry add "INVOICED" NUMBER(1,0) DEFAULT 0 NOT NULL;
 
 
 --- THE CONFIG TABLE
@@ -20,3 +21,6 @@ CREATE TABLE BILLING_CONFIG
 
 
 CREATE SEQUENCE BILLING_CONFIG_SEQ INCREMENT BY 1 START WITH 1 MAXVALUE 9000000 MINVALUE 1 ORDER;
+
+INSERT INTO "CAMAC"."AIR_ACTION" (AVAILABLE_INSTANCE_RESOURCE_ID, ACTION_NAME, HIDDEN) VALUES ('billing', 'complete', '1')
+
