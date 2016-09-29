bcfb561832838487c049a73499d75df17d6eed92
Stefan Heinemann <stefan.heinemann@adfinis-sygroup.ch>
2016-09-29 14:14:29 +0200

    Host hey checking disabled

--- .wait-for-ssh.sh
+++ .wait-for-ssh.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 
-ssh $1
+ssh $1 -p $p2 --o StrictHostKeyChecking=no
 while test $? -gt 0
 do
    sleep 1
     echo "Waiting for ssh..."
-   ssh $1
+   ssh $1 -p $p2 -o StrictHostKeyChecking=no
 done
 
