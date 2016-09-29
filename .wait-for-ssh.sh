5e67807398c5d791b471383dbb85af861a447f7e
Stefan Heinemann <stefan.heinemann@adfinis-sygroup.ch>
2016-09-29 14:21:55 +0200

    sshpass

--- .wait-for-ssh.sh
+++ .wait-for-ssh.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 
-ssh $1 -p $2 -o StrictHostKeyChecking=no
+sshpass -p "admin" ssh $1 -p $2 -o StrictHostKeyChecking=no
 while test $? -gt 0
 do
    sleep 1
-    echo "Waiting for ssh..."
-   ssh $1 -p $2 -o StrictHostKeyChecking=no
+   echo "Waiting for ssh..."
+   sshpass -p "admin" ssh $1 -p $2 -o StrictHostKeyChecking=no
 done
 
