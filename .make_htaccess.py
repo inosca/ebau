53ea2dc24f9d3e6d70f0c6fa8a77e8842fb2c45f
Stefan Heinemann <stefan.heinemann@adfinis-sygroup.ch>
2016-10-18 13:51:59 +0200

    Reorganised htaccess and configuration managment

---
+++ .make_htaccess.py
@@ -0,0 +1,31 @@
+import sys
+
+with open("tools/deploy/htaccess_tmpl", "r") as tmpl_file:
+    tmpl = tmpl_file.read()
+
+
+if len(sys.argv) > 1:
+
+    ENV = sys.argv[1]
+    if ENV == 'dev':
+        env = 'development'
+
+    elif ENV == 'prod':
+        env = 'production'
+
+    elif ENV == 'stage':
+        env = 'staging'
+
+        with open("tools/deploy/test-server-htaccess") as test_htaccess:
+            tmpl += "\n\n"
+            tmpl += test_htaccess.read()
+    elif ENV == 'test':
+        env = 'testing'
+
+else:
+    env = 'development'
+
+tmpl = tmpl.replace('{{ENV}}', env)
+
+with open("camac/public/.htaccess", "w") as htaccess:
+    htaccess.write(tmpl)
