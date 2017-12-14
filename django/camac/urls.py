7a6eb03114cc01f93c89ecd04c7eb2dc55daae04
Oliver Sauder <oliver.sauder@adfinis-sygroup.ch>
2017-12-14 15:27:36 +0100

    Remove not needed settings

--- django/camac/urls.py
+++ django/camac/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import include, url
-from django.contrib import admin
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
     url(r'^api-token-auth/', obtain_jwt_token),
     url(r'^api-token-refresh/', refresh_jwt_token),
     url(r'^api/v1/', include('camac.core.urls')),
