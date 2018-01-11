5f9b3ada34948893e6c9e3b289c8acde326671d7
Oliver Sauder <oliver.sauder@adfinis-sygroup.ch>
2018-01-11 09:17:53 +0100

    Add missing coverage to relations module

---
+++ django/camac/tests/test_relations.py
@@ -0,0 +1,29 @@
+import pytest
+from django.contrib.auth import get_user_model
+from rest_framework import exceptions
+
+from camac import relations
+
+
+def test_form_data_related_field_valid_pk(admin_user):
+    field = relations.FormDataResourceReleatedField(
+        queryset=get_user_model().objects.all()
+    )
+    user = field.to_internal_value(admin_user.pk)
+    assert user == admin_user
+
+
+def test_form_data_related_field_inexistent_pk(db):
+    field = relations.FormDataResourceReleatedField(
+        queryset=get_user_model().objects.all()
+    )
+    with pytest.raises(exceptions.ValidationError):
+        field.to_internal_value(10)
+
+
+def test_form_data_related_field_invalid_pk(db):
+    field = relations.FormDataResourceReleatedField(
+        queryset=get_user_model().objects.all()
+    )
+    with pytest.raises(exceptions.ValidationError):
+        field.to_internal_value({'invalid'})
