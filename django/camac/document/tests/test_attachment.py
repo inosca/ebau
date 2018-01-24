2f5f8c62a6e08f9c909ecbc6ae82679061a34ea7
Oliver Sauder <oliver.sauder@adfinis-sygroup.ch>
2018-01-24 12:22:27 +0100

    Validate instance on attachment whether it is valid according to role

--- django/camac/document/tests/test_attachment.py
+++ django/camac/document/tests/test_attachment.py
@@ -33,38 +33,137 @@ def test_attachment_list(admin_client, attachment,
         assert json['data'][0]['id'] == str(attachment.pk)
 
 
-@pytest.mark.parametrize("role__name,instance__user", [
-    ('Applicant', LazyFixture('admin_user')),
-])
 @pytest.mark.parametrize(
-    "filename,mime_type,attachment_section_group_acl__mode,status_code", [
+    "filename,mime_type,role__name,instance__user,instance_locations__location,activation__service,attachment_section_group_acl__mode,status_code", [  # noqa: E501
+        # applicant creates valid pdf attachment on a instance of its own in a
+        # attachment section with admin permissions
         (
-            'multiple-pages.pdf',
-            'application/pdf',
-            models.ADMIN_PERMISSION,
-            status.HTTP_201_CREATED
+            'multiple-pages.pdf',  # filename
+            'application/pdf',  # mime_type
+            'Applicant',  # role__name
+            LazyFixture('admin_user'),  # instance__user
+            LazyFixture('location'),  # instance_locations__location
+            LazyFixture('service'),  # activation__service
+            models.ADMIN_PERMISSION,  # attachment_section_group_acl__mode
+            status.HTTP_201_CREATED,  # status_code
         ),
+        # user with role Municipality creates valid jpg attachment on an
+        # instance of its location in a attachment section with admin
+        # permissions
         (
             'test-thumbnail.jpg',
             'image/jpeg',
+            'Municipality',
+            LazyFixture('user'),
+            LazyFixture('location'),
+            LazyFixture('service'),
             models.ADMIN_PERMISSION,
             status.HTTP_201_CREATED
         ),
+        # user with role Service creates valid jpg attachment on an
+        # instance which is assigned to user in an activation in
+        # an attachment section with admin permissions
+        (
+            'test-thumbnail.jpg',
+            'image/jpeg',
+            'Service',
+            LazyFixture('user'),
+            LazyFixture(lambda location_factory: location_factory()),
+            LazyFixture('service'),
+            models.ADMIN_PERMISSION,
+            status.HTTP_201_CREATED
+        ),
+        # user with role Canton creates valid jpg attachment on any
+        # instance with attachment section with admin permissions
+        (
+            'test-thumbnail.jpg',
+            'image/jpeg',
+            'Canton',
+            LazyFixture('user'),
+            LazyFixture(lambda location_factory: location_factory()),
+            LazyFixture('service'),
+            models.ADMIN_PERMISSION,
+            status.HTTP_201_CREATED,
+        ),
+        # user with role Applicant tries to create invalid gif attachment
+        # on its own instance with attachment section with admin permissions
         (
             'invalid-attachment.gif',
             'image/gif',
+            'Applicant',
+            LazyFixture('admin_user'),
+            LazyFixture('location'),
+            LazyFixture('service'),
             models.ADMIN_PERMISSION,
             status.HTTP_400_BAD_REQUEST
         ),
+        # user with role Applicant tries to create valid pdf attachment
+        # on instance which doesn't belong to him with attachment section
+        # with admin permissions
+        (
+            'multiple-pages.pdf',
+            'application/pdf',
+            'Applicant',
+            LazyFixture('user'),
+            LazyFixture('location'),
+            LazyFixture('service'),
+            models.ADMIN_PERMISSION,
+            status.HTTP_400_BAD_REQUEST,
+        ),
+        # user with role Applicant tries to create valid jpg attachment
+        # on its own instance with attachment section with only read rights
         (
             'test-thumbnail.jpg',
             'image/jpeg',
+            'Applicant',
+            LazyFixture('admin_user'),
+            LazyFixture('location'),
+            LazyFixture('service'),
             models.READ_PERMISSION,
+            status.HTTP_400_BAD_REQUEST
+        ),
+        # user with role Municipality tries to create valid jpg attachment
+        # on instance of a different location with attachment section
+        # with admin rights
+        (
+            'test-thumbnail.jpg',
+            'image/jpeg',
+            'Municipality',
+            LazyFixture('user'),
+            LazyFixture(lambda location_factory: location_factory()),
+            LazyFixture('service'),
+            models.ADMIN_PERMISSION,
+            status.HTTP_400_BAD_REQUEST,
+        ),
+        # user with role Service tries to create valid jpg attachment
+        # on instance without activation with attachment section
+        # with admin rights
+        (
+            'test-thumbnail.jpg',
+            'image/jpeg',
+            'Service',
+            LazyFixture('admin_user'),
+            LazyFixture('location'),
+            LazyFixture(lambda service_factory: service_factory()),
+            models.ADMIN_PERMISSION,
+            status.HTTP_400_BAD_REQUEST,
+        ),
+        # user with role Unknown tries to create valid jpg attachment
+        # on its own instance with an attachment section with admin permissions
+        (
+            'test-thumbnail.jpg',
+            'image/jpeg',
+            'Unknown',
+            LazyFixture('admin_user'),
+            LazyFixture('location'),
+            LazyFixture('service'),
+            models.ADMIN_PERMISSION,
             status.HTTP_400_BAD_REQUEST
         ),
     ]
 )
 def test_attachment_create(admin_client, instance, attachment_section,
+                           activation, instance_locations,
                            attachment_section_group_acl, mime_type, filename,
                            status_code):
     url = reverse('attachment-list')
