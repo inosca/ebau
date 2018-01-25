3c56bb830cb31d0f45f724eb8cce9f7b8a4b07e0
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2018-01-25 08:37:48 +0100

    Ember CI

--- ember/testem.js
+++ ember/testem.js
@@ -2,15 +2,15 @@ module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
   launch_in_ci: [
-    'Chromium'
+    'Chrome'
   ],
   launch_in_dev: [],
   browser_args: {
-    Chromium: {
+    Chrome: {
       mode: 'ci',
       args: [
         // --no-sandbox is needed when running Chrome inside a container
-        process.env.TRAVIS ? '--no-sandbox' : null,
+        process.env.ENV === 'docker' ? '--no-sandbox' : null,
 
         '--disable-gpu',
         '--headless',
@@ -19,4 +19,4 @@ module.exports = {
       ].filter(Boolean)
     }
   }
-};
+}
