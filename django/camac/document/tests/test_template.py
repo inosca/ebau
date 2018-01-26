123909dd6b08c929b4a285639b1433e65719531a
Oliver Sauder <oliver.sauder@adfinis-sygroup.ch>
2018-01-26 17:15:21 +0100

    Add template merge conversion possibility

--- django/camac/document/tests/test_template.py
+++ django/camac/document/tests/test_template.py
@@ -37,19 +37,51 @@ def test_template_detail(admin_client, template):
     assert response.status_code == status.HTTP_200_OK
 
 
-@pytest.mark.parametrize("role__name,instance__user,status_code", [
-    ('Canton', LazyFixture('user'), status.HTTP_200_OK),
-    # service is not assigned to instance so not allowed to build document
-    ('Service', LazyFixture('user'), status.HTTP_400_BAD_REQUEST),
+@pytest.mark.parametrize(
+    "role__name,template__path,instance__user,status_code,to_type", [
+        (
+            'Canton',
+            django_file('template.docx'),
+            LazyFixture('user'),
+            status.HTTP_200_OK,
+            'docx'
+        ),
+        (
+            'Canton',
+            django_file('template.docx'),
+            LazyFixture('user'),
+            status.HTTP_200_OK,
+            'pdf'
+        ),
+        (
+            'Canton',
+            django_file('template.docx'),
+            LazyFixture('user'),
+            status.HTTP_400_BAD_REQUEST,
+            'invalid'
+        ),
+        # service is not assigned to instance so not allowed to build document
+        (
+            'Service',
+            django_file('template.docx'),
+            LazyFixture('user'),
+            status.HTTP_400_BAD_REQUEST,
+            'docx'
+        ),
+    ]
+)
+@pytest.mark.parametrize("form_field__name", [
+    'testname',
 ])
-@pytest.mark.parametrize("template__path,form_field__name", [
-    (django_file('template.docx'), 'testname'),
-])
-def test_template_merge(admin_client, template, instance,
+def test_template_merge(admin_client, template, instance, to_type,
                         instance_locations, form_field, status_code):
+
     url = reverse('template-merge', args=[template.pk])
-    response = admin_client.get(url, data={'instance': instance.pk})
+    response = admin_client.get(url, data={
+        'instance': instance.pk,
+        'type': to_type,
+    })
     assert response.status_code == status_code
-    if status_code == status.HTTP_200_OK:
+    if status_code == status.HTTP_200_OK and to_type == 'docx':
         docx = MailMerge(io.BytesIO(response.content))
         assert len(docx.get_merge_fields()) == 0
