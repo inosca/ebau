d59cd0af5f29b3cfec5bf578c8ed819abc91f005
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2018-03-02 10:53:24 +0100

    Improve form components

--- ember/tests/unit/instances/new/index/controller-test.js
+++ ember/tests/unit/instances/new/index/controller-test.js
@@ -1,12 +1,12 @@
 import { module, test } from 'qunit'
 import { setupTest } from 'ember-qunit'
 
-module('Unit | Controller | instances/new/type', function(hooks) {
+module('Unit | Controller | instances/new/index', function(hooks) {
   setupTest(hooks)
 
   // Replace this with your real tests.
   test('it exists', function(assert) {
-    let controller = this.owner.lookup('controller:instances/new/type')
+    let controller = this.owner.lookup('controller:instances/new/index')
     assert.ok(controller)
   })
 })
