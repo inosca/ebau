0fa38caaf9d54cc1a8ef64f79f5c38e5cafef4d4
Oliver Sauder <oliver.sauder@adfinis-sygroup.ch>
2018-03-12 09:28:51 +0100

    Add instance state end point

--- django/camac/instance/urls.py
+++ django/camac/instance/urls.py
@@ -8,6 +8,7 @@ r = SimpleRouter(trailing_slash=False)
 r.register(r'forms', views.FormView, 'form')
 r.register(r'instances', views.InstanceView, 'instance')
 r.register(r'form-fields', views.FormFieldView, 'form-field')
+r.register(r'instance-states', views.InstanceStateView, 'instance-state')
 
 urlpatterns = [
     url(
