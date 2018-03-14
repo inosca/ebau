e5c3638eda2e0a8f9304db58b658f36d15210384
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2018-03-14 10:10:50 +0100

    Add 404 page

--- ember/app/router.js
+++ ember/app/router.js
@@ -8,6 +8,7 @@ const Router = EmberRouter.extend({
 
 Router.map(function() {
   this.route('login')
+  this.route('notfound', { path: '/*path' })
 
   this.route('protected', { path: '/' }, function() {
     this.route('index', { path: '/', resetNamespace: true })
