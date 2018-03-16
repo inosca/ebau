2a4b8a9151a1fea52ba4481a49e9bc6e56809d68
Oliver Sauder <oliver.sauder@adfinis-sygroup.ch>
2018-03-16 10:42:19 +0100

    Add end points for activation and circulation

---
+++ django/camac/circulation/models.py
@@ -0,0 +1,3 @@
+# current circulation models are in core as they are to interconnected with php
+# specific database tables such as `InstanceResource` and `Resource`.
+# new models of circulation may be added here.
