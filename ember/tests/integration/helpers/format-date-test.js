f0feb3be0a4aa7578f2edded0ae91f3ef7acb68b
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2018-03-20 09:32:47 +0100

    Use Date.toLocaleString for formatting dates

--- ember/tests/integration/helpers/format-date-test.js
+++ ember/tests/integration/helpers/format-date-test.js
@@ -7,19 +7,19 @@ module('Integration | Helper | format-date', function(hooks) {
   setupRenderingTest(hooks)
 
   test('it formats the date correctly', async function(assert) {
-    this.set('date', new Date(2018, 3, 19, 9, 50))
+    this.set('date', new Date(2018, 2, 19, 9, 50))
 
     await render(hbs`{{format-date date}}`)
 
-    assert.dom(this.element).hasText('19.03.2018 09:50')
+    assert.dom(this.element).hasText('19.03.2018, 09:50')
   })
 
   test('it zeropads the parts correctly', async function(assert) {
-    this.set('date', new Date(2018, 1, 1, 1, 1))
+    this.set('date', new Date(2018, 0, 1, 1, 1))
 
     await render(hbs`{{format-date date}}`)
 
-    assert.dom(this.element).hasText('01.01.2018 01:01')
+    assert.dom(this.element).hasText('01.01.2018, 01:01')
   })
 
   test('it ignores incorrect values', async function(assert) {
