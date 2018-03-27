be460449a036d6931f0ac27c8f28062ba0428b5e
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2018-03-27 16:17:55 +0200

    WIP: GWR

--- ember/app/instances/edit/grundinformationen/gwr/route.js
+++ ember/app/instances/edit/grundinformationen/gwr/route.js
@@ -1,3 +1,14 @@
 import Route from '@ember/routing/route'
+import { inject as service } from '@ember/service'
+import { v4 } from 'ember-uuid'
 
-export default Route.extend({})
+export default Route.extend({
+  questionStore: service(),
+
+  model() {
+    return this.get('questionStore.find').perform(
+      'gwr',
+      this.modelFor('instances.edit').id
+    )
+  }
+})
