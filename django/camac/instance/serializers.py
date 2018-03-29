31f8d7cd68764a8fd6efa746fa972ef8817a173f
Oliver Sauder <oliver.sauder@adfinis-sygroup.ch>
2018-03-29 13:53:45 +0200

    Move instance submit validation to InstanceSubmitSerializer

--- django/camac/instance/serializers.py
+++ django/camac/instance/serializers.py
@@ -1,9 +1,11 @@
+from django.db.models import Max
 from django.utils import timezone
 from django.utils.translation import gettext as _
 from rest_framework import exceptions
 from rest_framework_json_api import serializers
 
-from camac.user.relations import GroupResourceRelatedField
+from camac.user.relations import (FormDataResourceRelatedField,
+                                  GroupResourceRelatedField)
 from camac.user.serializers import CurrentGroupDefault
 
 from . import models
@@ -101,6 +103,51 @@ class InstanceSerializer(serializers.ModelSerializer):
         read_only_fields = (
             'identifier',
         )
+
+
+class InstanceSubmitSerializer(InstanceSerializer):
+    instance_state = FormDataResourceRelatedField(
+        queryset=models.InstanceState.objects
+    )
+    previous_instance_state = FormDataResourceRelatedField(
+        queryset=models.InstanceState.objects
+    )
+
+    def generate_identifier(self):
+        """
+        Build identifier for instance.
+
+        Format:
+        two last digits of communal location number
+        year in two digits
+        unique sequence
+
+        Example: 11-18-001
+        """
+        identifier = self.instance.identifier
+        if not identifier:
+            location_nr = self.instance.location.communal_federal_number[-2:]
+            year = timezone.now().strftime('%y')
+
+            max_identifier = models.Instance.objects.filter(
+                identifier__startswith='{0}-{1}-'.format(location_nr, year)
+            ).aggregate(max_identifier=Max(
+                'identifier'))['max_identifier'] or '00-00-000'
+            sequence = int(max_identifier[-3:])
+
+            identifier = '{0}-{1}-{2}'.format(
+                location_nr,
+                timezone.now().strftime('%y'),
+                str(sequence + 1).zfill(3))
+
+        return identifier
+
+    def validate(self, data):
+        if self.instance.location is None:
+            raise exceptions.ValidationError(_('No location assigned.'))
+
+        data['identifier'] = self.generate_identifier()
+        return data
 
 
 class FormFieldSerializer(serializers.ModelSerializer):
