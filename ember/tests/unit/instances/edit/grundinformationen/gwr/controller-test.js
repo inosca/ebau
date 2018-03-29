ac5840654af874bdb895c85790bf2059e09e6eb9
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2018-03-29 09:24:59 +0200

    WIP: GWR

---
+++ ember/tests/unit/instances/edit/grundinformationen/gwr/controller-test.js
@@ -0,0 +1,12 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+
+module('Unit | Controller | instances/edit/grundinformationen/gwr', function(hooks) {
+  setupTest(hooks);
+
+  // Replace this with your real tests.
+  test('it exists', function(assert) {
+    let controller = this.owner.lookup('controller:instances/edit/grundinformationen/gwr');
+    assert.ok(controller);
+  });
+});
