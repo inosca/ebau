18e537cf45281512b56e3a660a0eb6d80fdf9ce6
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2018-04-03 15:50:13 +0200

    WIP: GWR

---
+++ ember/app/validations/building.js
@@ -0,0 +1,15 @@
+import { validatePresence } from 'ember-changeset-validations/validators'
+
+export default {
+  adresse: validatePresence(true),
+  'vers-nr': validatePresence(true),
+  'kataster-nr': validatePresence(true),
+  kategorie: validatePresence(true),
+  heizungsart: validatePresence(true),
+  'energietrager-heizung': validatePresence(true),
+  'energietrager-warmwasser': validatePresence(true),
+  leistung: validatePresence(true),
+  geschosse: validatePresence(true),
+  wohnraume: validatePresence(true),
+  wohnungen: validatePresence(true)
+}
