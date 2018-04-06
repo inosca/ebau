56d14e59ca3455516a9ebc581bed57e34ee38f39
Oliver Sauder <oliver.sauder@adfinis-sygroup.ch>
2018-04-06 17:01:56 +0200

    Add service end point to retrieve services
    Applicant is not allowed to see different services

---
+++ django/camac/instance/tests/test_service.py
@@ -0,0 +1,19 @@
+import pytest
+from django.urls import reverse
+from rest_framework import status
+
+
+@pytest.mark.parametrize("role__name,size", [
+    ('Applicant', 0),
+    ('Service', 1),
+    ('Canton', 1),
+    ('Municipality', 1),
+])
+def test_service_list(admin_client, service, size):
+    url = reverse('service-list')
+
+    response = admin_client.get(url)
+    assert response.status_code == status.HTTP_200_OK
+
+    json = response.json()
+    assert len(json['data']) == size
