eeeaf590f622b4cd9fa1d588fcefe8cce093eb0b
Oliver Sauder <os@esite.ch>
2018-04-12 11:51:47 +0200

    Add base multi value filter supporting different lookup expressions
    This is needed to implement name on attahcment with startswith lookup.

--- django/camac/document/filters.py
+++ django/camac/document/filters.py
@@ -1,13 +1,18 @@
 from django_filters.rest_framework import FilterSet
 
+from camac.filters import CharMultiValueFilter
+
 from . import models
 
 
 class AttachmentFilterSet(FilterSet):
+    name = CharMultiValueFilter(lookup_expr='startswith')
+
     class Meta:
         model = models.Attachment
         fields = (
             'instance',
             'user',
+            'name',
             'attachment_section',
         )
