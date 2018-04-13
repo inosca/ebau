b767e16a5bb03a8b860552f69fcfb9fb6c6bd2bc
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2018-04-13 16:03:13 +0200

    Fix minor performance issue

--- ember/app/models/instance.js
+++ ember/app/models/instance.js
@@ -3,7 +3,6 @@ import attr from 'ember-data/attr'
 import { belongsTo, hasMany } from 'ember-data/relationships'
 
 export default Model.extend({
-  name: attr('string'),
   identifier: attr('string'),
   creationDate: attr('date'),
   location: belongsTo('location'),
