630347ab895f6050eec9a2ce8062a0e97ade0648
Oliver Sauder <oliver.sauder@adfinis-sygroup.ch>
2018-04-13 12:14:02 +0200

    Do instance validation of notification template merge in serializer

--- django/camac/notification/serializers.py
+++ django/camac/notification/serializers.py
@@ -1,7 +1,11 @@
+from collections import namedtuple
+
 import jinja2
 from rest_framework import exceptions
 from rest_framework_json_api import serializers
 
+from camac.instance.mixins import InstanceEditableMixin
+from camac.instance.models import Instance
 from camac.instance.serializers import InstanceMergeSerializer
 
 from . import models
@@ -18,9 +22,13 @@ class NotificationTemplateSerializer(serializers.ModelSerializer):
         )
 
 
-class NotificationTemplateMergeSerializer(serializers.Serializer):
-    subject = serializers.SerializerMethodField()
-    body = serializers.SerializerMethodField()
+class NotificationTemplateMergeSerializer(InstanceEditableMixin,
+                                          serializers.Serializer):
+    instance = serializers.ResourceRelatedField(
+        queryset=Instance.objects.all()
+    )
+    subject = serializers.CharField(required=False)
+    body = serializers.CharField(required=False)
 
     def _merge(self, value, instance):
         try:
@@ -30,13 +38,31 @@ class NotificationTemplateMergeSerializer(serializers.Serializer):
         except jinja2.TemplateError as e:
             raise exceptions.ValidationError(str(e))
 
-    def get_subject(self, notification_template):
-        return self._merge(notification_template.subject,
-                           notification_template.instance)
+    def validate(self, data):
+        notification_template = self.context['view'].get_object()
+        instance = data['instance']
 
-    def get_body(self, notification_template):
-        return self._merge(notification_template.body,
-                           notification_template.instance)
+        data['subject'] = self._merge(
+            data.get('subject', notification_template.subject),
+            instance
+        )
+        data['body'] = self._merge(
+            data.get('body', notification_template.body),
+            instance
+        )
+        data['pk'] = '{0}-{1}'.format(
+            notification_template.pk, instance.pk
+        )
+
+        return data
+
+    def create(self, validated_data):
+        NotificationTemplateMerge = namedtuple(
+            'NotificationTemplateMerge', validated_data.keys()
+        )
+        obj = NotificationTemplateMerge(**validated_data)
+
+        return obj
 
     class Meta:
         resource_name = 'notification-template-merges'
