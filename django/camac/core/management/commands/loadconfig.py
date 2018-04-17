29ccc0b7e5d86290636ebf4e8bd5a3d652278562
Oliver Sauder <oliver.sauder@adfinis-sygroup.ch>
2018-04-17 17:09:38 +0200

    Flush tables which are pure config durig loadconfig

--- django/camac/core/management/commands/loadconfig.py
+++ django/camac/core/management/commands/loadconfig.py
@@ -1,7 +1,10 @@
+from django.apps import apps
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
+
+from camac.core.management.commands.dumpconfig import pure_config_models
 
 
 class Command(BaseCommand):
@@ -19,12 +22,18 @@ class Command(BaseCommand):
             model.objects.get(pk=1)
         except model.DoesNotExist:
             fixtures.append(settings.APPLICATION_DIR('init.json'))
+            if settings.ENV != 'production':
+                # load test data in dev setup
+                fixtures.append(settings.APPLICATION_DIR('data.json'))
 
         # default application config
         fixtures.append(settings.APPLICATION_DIR('config.json'))
 
-        # TODO: add user adsy to groups so it is ready for testing
-        # on fresh database
+        print("Flushing 'pure' config models")
+        for model_name in pure_config_models:
+            (app_label, model_name) = model_name.split(".")
+            model = apps.get_model(app_label=app_label, model_name=model_name)
+            model.objects.all().delete()
 
         self.stdout.write('Loading config {0}'.format(', '.join(fixtures)))
         call_command('loaddata', *fixtures, **options)
