a8bb4a5e53cd1bb4552c44390abb0b68a18dba29
Oliver Sauder <oliver.sauder@adfinis-sygroup.ch>
2018-04-19 10:16:39 +0200

    Clear cache when starting php container

--- .clear_cache.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# we need to wait and kill the nc process, since the memcache server
-# keeps the connection open
-
-echo "flush_all" | nc localhost 11211 &  PID=$! && sleep 2 && kill $PID
