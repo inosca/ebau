33f6060c8fc7e11c7124b20e04ee7ca4de2eaeed
Oliver Sauder <oliver.sauder@adfinis-sygroup.ch>
2018-04-27 17:17:36 +0200

    Optional notification when document is uploaded to attachment section

--- django/camac/document/factories.py
+++ django/camac/document/factories.py
@@ -2,6 +2,7 @@ from factory import Faker, SubFactory
 from factory.django import DjangoModelFactory, ImageField
 
 from camac.instance.factories import InstanceFactory
+from camac.notification.factories import NotificationTemplateFactory
 from camac.user.factories import GroupFactory, RoleFactory, UserFactory
 
 from . import models
@@ -10,6 +11,8 @@ from . import models
 class AttachmentSectionFactory(DjangoModelFactory):
     name = Faker('name')
     sort = Faker('pyint')
+    notification_template = SubFactory(NotificationTemplateFactory)
+    recipient_types = ['municipality']
 
     class Meta:
         model = models.AttachmentSection
