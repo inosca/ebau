94642e63024f1963bc35297567a1cf99b1edc0bb
Oliver Sauder <oliver.sauder@adfinis-sygroup.ch>
2018-04-27 15:40:57 +0200

    Add placeholders for activations and billing entries

--- django/camac/core/factories.py
+++ django/camac/core/factories.py
@@ -39,8 +39,35 @@ class ActivationFactory(DjangoModelFactory):
     circulation_state = SubFactory(CirculationStateFactory)
     user = SubFactory(UserFactory)
     start_date = Faker('past_datetime', tzinfo=pytz.UTC)
+    end_date = Faker('future_datetime', tzinfo=pytz.UTC)
     deadline_date = Faker('future_datetime', tzinfo=pytz.UTC)
+    reason = Faker('text', max_nb_chars=50)
     version = 1
 
     class Meta:
         model = models.Activation
+
+
+class BillingAccountFactory(DjangoModelFactory):
+    name = Faker('name')
+    account_number = '0000'
+    department = Faker('name')
+    predefined = 0
+
+    class Meta:
+        model = models.BillingAccount
+
+
+class BillingEntryFactory(DjangoModelFactory):
+    amount = Faker('pyfloat', left_digits=3, right_digits=2, positive=True)
+    billing_account = SubFactory(BillingAccountFactory)
+    user = SubFactory(UserFactory)
+    instance = SubFactory(InstanceFactory)
+    service = SubFactory(ServiceFactory)
+    created = Faker('past_datetime', tzinfo=pytz.UTC)
+    amount_type = 0
+    type = 0
+    invoiced = 1
+
+    class Meta:
+        model = models.BillingEntry
