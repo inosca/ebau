93e13779d00cf30e8adb2e8a13748a4bb106b967
Patrick Winter <patrick.winter@adfinis-sygroup.ch>
2018-06-29 16:13:29 +0200

    Fix broken migration dependency

--- django/camac/core/migrations/0011_kt_uri_billing_amount_view.py
+++ django/camac/core/migrations/0011_kt_uri_billing_amount_view.py
@@ -8,7 +8,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0010_auto_20180628_1310.py'),
+        ('core', '0010_auto_20180628_1310'),
     ]
 
     operations = [
