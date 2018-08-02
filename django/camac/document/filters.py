258988e5f460445d4b2fb9a6842557a09196eb3f
Adrian Wittwer <adrian.wittwer@adfinis-sygroup.ch>
2018-08-02 16:17:40 +0200

    add global_template filter to TemplateView

--- django/camac/document/filters.py
+++ django/camac/document/filters.py
@@ -1,6 +1,6 @@
 from django_filters.rest_framework import FilterSet
 
-from camac.filters import CharMultiValueFilter
+from camac.filters import CharMultiValueFilter, NumberFilter
 
 from . import models
 
@@ -10,4 +10,19 @@ class AttachmentFilterSet(FilterSet):
 
     class Meta:
         model = models.Attachment
-        fields = ("instance", "user", "name", "attachment_section")
+        fields = (
+            'instance',
+            'user',
+            'name',
+            'attachment_section',
+        )
+
+
+class TemplateFilterSet(FilterSet):
+    global_template = NumberFilter(field_name='group', lookup_expr='isnull')
+
+    class Meta:
+        model = models.Template
+        fields = (
+            'global_template',
+        )
