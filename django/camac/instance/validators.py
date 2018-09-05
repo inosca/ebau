243245d46d4eee8b6bb4b874945905bf17af7c95
Oliver Sauder <oliver.sauder@adfinis-sygroup.ch>
2018-09-05 10:29:24 +0200

    Mark a question only as active when all dependend questions are active too

--- django/camac/instance/validators.py
+++ django/camac/instance/validators.py
@@ -5,6 +5,7 @@ import inflection
 from django.conf import settings
 from django.utils.translation import gettext as _
 from pyjexl.jexl import JEXL
+from pyjexl.parser import Transform
 from rest_framework import exceptions
 
 from camac.document.models import Attachment
@@ -30,6 +31,7 @@ class FormDataValidator(object):
         self.jexl = JEXL()
         self.jexl.add_transform("value", lambda name: self.fields.get(name))
         self.jexl.add_transform("mapby", lambda arr, key: [obj[key] for obj in arr])
+        self.active_question_cache = {}
 
     def _validate_question_radio(self, question, question_def, value):
         if value not in question_def["config"]["options"]:
@@ -96,18 +98,68 @@ class FormDataValidator(object):
                     row.get(column["name"]),
                 )
 
+    def _get_expression_questions(self, expression, questions=[]):
+        """Get all questions referenced in expression."""
+        # copy as list is not immutable
+        questions = list(questions)
+
+        if expression is None:
+            return questions
+
+        if isinstance(expression, Transform):
+            if expression.name == "value":
+                questions.append(expression.subject.value)
+        else:
+            left = getattr(expression, "left", None)
+            right = getattr(expression, "right", None)
+
+            if left and right:
+                return self._get_expression_questions(
+                    left, questions
+                ) + self._get_expression_questions(right, questions)
+
+        return questions
+
+    def _check_questions_active(self, questions):
+        for question in questions:
+            if not self._check_question_active(
+                question, self.forms_def["questions"][question]
+            ):
+                return False
+
+        return True
+
+    def _check_question_active(self, question, question_def):
+        """Question is only active when all dependend questions are active as well."""
+
+        if question in self.active_question_cache:
+            return self.active_question_cache[question]
+
+        expression = question_def.get("active-expression")
+        if expression is None:
+            return True
+
+        dep_questions = self._get_expression_questions(self.jexl.parse(expression))
+        active = False
+        try:
+            active = (
+                expression is None
+                or self._check_questions_active(dep_questions)
+                and self.jexl.evaluate(expression)
+            )
+        except TypeError:
+            # A TypeError is raised if a question is not filled. It then tries
+            # to e.g compare None < 250 which can't work
+            pass
+
+        self.active_question_cache[question] = active
+        return active
+
     def _check_question_required(self, question, question_def):
         if not question_def["required"]:
             return False
 
-        expression = question_def.get("active-expression", None)
-
-        try:
-            return expression is None or self.jexl.evaluate(expression)
-        except TypeError:
-            # A TypeError is raised if a question is not filled. It then tries
-            # to e.g compare None < 250 which can't work
-            return False
+        return self._check_question_active(question, question_def)
 
     def _validate_question(self, question, question_def, value):
         required = self._check_question_required(question, question_def)
