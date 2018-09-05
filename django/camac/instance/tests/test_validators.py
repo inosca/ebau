5a7143491676393c163c62ab986b6ef8634a6571
Oliver Sauder <oliver.sauder@adfinis-sygroup.ch>
2018-09-05 10:29:24 +0200

    Mark a question only as active when all dependend questions are active too

--- django/camac/instance/tests/test_validators.py
+++ django/camac/instance/tests/test_validators.py
@@ -4,7 +4,11 @@ from rest_framework import exceptions
 from camac.instance import validators
 
 
-@pytest.mark.django_db
+@pytest.fixture
+def validator(db, instance):
+    return validators.FormDataValidator(instance)
+
+
 @pytest.mark.parametrize(
     "question,value",
     [
@@ -15,13 +19,27 @@ from camac.instance import validators
         ("dokument-parzellen", None),
     ],
 )
-def test_form_data_validator_validate_question_type(instance, question, value):
-    validator = validators.FormDataValidator(instance)
+def test_form_data_validator_validate_question_type(validator, question, value):
     question_def = validator.forms_def["questions"][question]
-
     validate_method = getattr(
         validator, "_validate_question_{0}".format(question_def["type"])
     )
 
     with pytest.raises(exceptions.ValidationError):
         validate_method(question, question_def, value)
+
+
+@pytest.mark.parametrize(
+    "expression,questions",
+    [
+        (
+            "'anlagen-mit-erheblichen-schadstoffemissionen'|value == 'Ja'",
+            ["anlagen-mit-erheblichen-schadstoffemissionen"],
+        ),
+        (None, []),
+    ],
+)
+def test_get_expression_questions(validator, expression, questions):
+    parsed_expression = expression and validator.jexl.parse(expression)
+    dep_questions = validator._get_expression_questions(parsed_expression)
+    assert dep_questions == questions
