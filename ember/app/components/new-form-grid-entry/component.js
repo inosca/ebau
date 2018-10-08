6515c592143aabccfe3be366329c4ad42e41bc86
Adrian Wittwer <adrian.wittwer@adfinis-sygroup.ch>
2018-10-08 13:49:33 +0200

    directly call the onSetForm function

--- ember/app/components/new-form-grid-entry/component.js
+++ ember/app/components/new-form-grid-entry/component.js
@@ -1,11 +1,5 @@
 import Component from "@ember/component";
 
 export default Component.extend({
-  classNames: ["uk-width-1-1"],
-
-  actions: {
-    setForm(form) {
-      this.onSetForm(form);
-    }
-  }
+  classNames: ["uk-width-1-1"]
 });
