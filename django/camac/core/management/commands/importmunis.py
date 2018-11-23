8217c39daaaead329c797c55adf49fcafdefea93
Patrick Winter <patrick.winter@adfinis-sygroup.ch>
2018-11-23 14:23:57 +0100

    Migrate import django commands from camac-be

---
+++ django/camac/core/management/commands/importmunis.py
@@ -0,0 +1,85 @@
+from camac.core.dataimport import (ImportCommand, create_or_update_group,
+                                   create_or_update_service)
+from camac.core.models import Role, ServiceGroup
+
+ROLE_LEITUNG_LEITBEHOERDE = 3
+ROLE_SACHBEARBEITER_LEITBEHOERDE = 20004
+ROLE_READONLY_LEITBEHOERDE = 20003
+
+ROLE_LEITUNG_BAUKONTROLLE = 5
+ROLE_SACHBEARBEITER_BAUKONTROLLE = 20005
+ROLE_READONLY_BAUKONTROLLE = 20006
+
+ROLE_LEITUNG_FACHSTELLE = 4
+ROLE_UNTERFACHSTELLE = 20000
+
+# service_group: (id, prefix)
+SERVICE_GROUP_LEITBEHOERDE_GEMEINDE = {
+    "service_group": ServiceGroup.objects.get(pk=2),
+    "name": "Leitbehörde",
+    "groups": [
+        ("Leitung", ROLE_LEITUNG_LEITBEHOERDE),
+        ("Sachbearbeiter", ROLE_SACHBEARBEITER_LEITBEHOERDE),
+        ("Einsichtsberechtigte", ROLE_READONLY_LEITBEHOERDE)
+    ]
+}
+SERVICE_GROUP_BAUKONTROLLE = {
+    "service_group": ServiceGroup.objects.get(pk=3),
+    "name": "Baukontrolle",
+    "groups": [
+        ("Leitung", ROLE_LEITUNG_BAUKONTROLLE),
+        ("Sachbearbeiter", ROLE_SACHBEARBEITER_BAUKONTROLLE),
+        ("Einsichtsberechtigte", ROLE_READONLY_BAUKONTROLLE)
+    ]
+}
+
+
+class Command(ImportCommand):
+    help = """
+    (Bern): Import municipalities from csv file. For every entry, the following
+    database entries are created/updated:
+
+    * Groups "Leitung {name}", "Sachbearbeiter {name}" and
+      "Einsichtsberechtigte {name}"
+    * Service (Organisation) "{name}"
+    """
+
+    def import_data(self, reader):
+        _import_data(reader)
+
+
+def _import_data(data):
+    service = None
+    for row in data:
+        if row.get('Gemeinde'):
+            for sg in [
+                SERVICE_GROUP_BAUKONTROLLE, SERVICE_GROUP_LEITBEHOERDE_GEMEINDE
+            ]:
+                service = create_or_update_service(
+                    row, sg["name"], defaults={"service_group": sg["service_group"]}
+                )
+                for role in sg["groups"]:
+                    create_or_update_group(
+                        row,
+                        "{0} {1}".format(role[0], sg["name"]),
+                        defaults={
+                            "role": Role.objects.get(pk=role[1]),
+                            "service": service,
+                        }
+                    )
+        else:
+            subservice = create_or_update_service(
+                row, "Unterfachstelle", defaults={
+                    "service_group":
+                        SERVICE_GROUP_LEITBEHOERDE_GEMEINDE["service_group"],
+                    "service_parent": service
+                }
+            )
+            create_or_update_group(
+                row,
+                "Unterfachstelle",
+                defaults={
+                    "role": Role.objects.get(pk=ROLE_UNTERFACHSTELLE),
+                    "service": subservice
+                },
+            )
