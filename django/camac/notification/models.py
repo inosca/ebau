bfb3723b60b13dfb2c7e3dd468c4a1b57c3ecfe3
Patrick Winter <patrick.winter@adfinis-sygroup.ch>
2018-11-29 09:55:30 +0100

    Reformat python files using black

--- django/camac/notification/models.py
+++ django/camac/notification/models.py
@@ -3,11 +3,7 @@ from django.db import models
 
 class NotificationTemplate(models.Model):
     purpose = models.CharField(
-        db_column="PURPOSE",
-        max_length=100,
-        db_index=True,
-        blank=True,
-        null=True
+        db_column="PURPOSE", max_length=100, db_index=True, blank=True, null=True
     )
     subject = models.TextField(db_column="SUBJECT", blank=True, null=True)
     body = models.TextField(db_column="BODY", blank=True, null=True)
@@ -19,10 +15,7 @@ class NotificationTemplate(models.Model):
 
 class NotificationTemplateT(models.Model):
     template = models.ForeignKey(
-        NotificationTemplate,
-        models.CASCADE,
-        db_column="TEMPLATE_ID",
-        related_name="+",
+        NotificationTemplate, models.CASCADE, db_column="TEMPLATE_ID", related_name="+"
     )
     language = models.CharField(db_column="LANGUAGE", max_length=2)
     purpose = models.CharField(db_column="PURPOSE", max_length=100)
