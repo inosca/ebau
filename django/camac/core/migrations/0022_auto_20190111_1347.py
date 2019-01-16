756edb3f7e29e7cd9481877f0d4b7e20a316c076
Patrick Winter <patrick.winter@adfinis-sygroup.ch>
2019-01-16 13:28:07 +0100

    Fix django migration conflict

--- django/camac/core/migrations/0022_auto_20190111_1347.py
+++ django/camac/core/migrations/0022_auto_20190111_1347.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0020_commissionassignment_creator_group'),
+        ('core', '0021_commissionassignment_creator_group'),
     ]
 
     operations = [
