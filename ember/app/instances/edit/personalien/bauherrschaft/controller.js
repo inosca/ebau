466e5563e9511d4b7e8bd0cb410d59d87124099f
Yelin Zhang <yelin.zhang@adfinis-sygroup.ch>
2019-01-29 10:59:21 +0100

    Add a transfer button for personalien

---
+++ ember/app/instances/edit/personalien/bauherrschaft/controller.js
@@ -0,0 +1,18 @@
+import Controller from "@ember/controller";
+import { inject as service } from "@ember/service";
+
+export default Controller.extend({
+  questionStore: service("question-store"),
+
+  actions: {
+    async transfer() {
+      let question = await this.questionStore.peek(
+        "bauherrschaft",
+        this.get("model.instance.id")
+      );
+      question.set("model.value", this.value);
+      question.set("value", this.value);
+      await this.get("questionStore.saveQuestion").perform(question);
+    }
+  }
+});
--- ember/app/instances/edit/personalien/bauherrschaft/route.js
+++ ember/app/instances/edit/personalien/bauherrschaft/route.js
@@ -1,3 +1,16 @@
 import Route from "@ember/routing/route";
+import { inject as service } from "@ember/service";
 
-export default Route.extend({});
+export default Route.extend({
+  questionStore: service("question-store"),
+
+  setupController(controller, model) {
+    this._super(...arguments);
+    controller.set(
+      "value",
+      this.questionStore
+        .peek("grundeigentumerschaft", model.instance.get("id"))
+        .get("value")
+    );
+  }
+});
