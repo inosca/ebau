1310debf6593db1b9b2aaf8ed7ffacfdb75a3ab9
Yelin Zhang <yelin.zhang@adfinis-sygroup.ch>
2019-01-30 14:12:04 +0100

    Change transfer button to copyQuestionValue

--- ember/app/instances/edit/personalien/projektverfasser-planer/controller.js
+++ ember/app/instances/edit/personalien/projektverfasser-planer/controller.js
@@ -1,17 +1,25 @@
 import Controller from "@ember/controller";
 import { inject as service } from "@ember/service";
+import { computed } from "@ember/object";
+import { mapBy } from "@ember/object/computed";
 
 export default Controller.extend({
   questionStore: service("question-store"),
 
+  fields: mapBy("model.instance.fields", "name"),
+
+  questionActive: computed("model.instance.fields.@each", function() {
+    return this.fields.includes("bauherrschaft");
+  }),
+
   actions: {
-    async transfer() {
+    async copyQuestionValue() {
       let question = await this.questionStore.peek(
         "projektverfasser-planer",
         this.get("model.instance.id")
       );
-      question.set("model.value", this.value);
-      question.set("value", this.value);
+      question.set("model.value", this.bauherrschaftValue);
+      question.set("value", this.bauherrschaftValue);
       await this.get("questionStore.saveQuestion").perform(question);
     }
   }
