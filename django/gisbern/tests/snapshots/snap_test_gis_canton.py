d95c5383db66eae0fa2068796b7e75cc04574e81
Christian Zosel <christian@zosel.ch>
2019-02-18 20:05:57 +0100

    Add vcrpy-based tests

--- django/gisbern/tests/snapshots/snap_test_gis_canton.py
+++ django/gisbern/tests/snapshots/snap_test_gis_canton.py
@@ -4,17 +4,16 @@ from __future__ import unicode_literals
 
 from snapshottest import Snapshot
 
+
 snapshots = Snapshot()
 
-snapshots['test_get_data 1'] = {
+snapshots['test_gis_canton[CH643546955207] 1'] = {
     'data': {
         'archäologisches_objekt': False,
         'bauinventar': False,
-        'belasteter_standort': True,
+        'belasteter_standort': False,
         'besonderer_landschaftsschutz': False,
         'gebiet_mit_naturkatastrophen': False,
-        'gewässerschutz': 'übriger Bereich üB',
-        'naturschutzgebiet': True,
-        'überbauungsordnung': 'Teilzonenplan Moorlandschaft 336 Amsoldingen'
+        'naturschutzgebiet': False
     }
 }
