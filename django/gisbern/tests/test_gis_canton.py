478d3952be7e0954b10c7a3445922743abbe6bae
Christian Zosel <christian@zosel.ch>
2019-02-19 11:13:12 +0100

    Fix vcrpy usage

--- django/gisbern/tests/test_gis_canton.py
+++ django/gisbern/tests/test_gis_canton.py
@@ -2,9 +2,16 @@ import pytest
 import vcr
 from django.urls import reverse
 
+my_vcr = vcr.VCR(
+    # add "body" to default match_on because we're passing the
+    # egrid number in the body.
+    match_on=["method", "scheme", "host", "port", "path", "query", "body"],
+    cassette_library_dir="gisbern/tests/cassettes",
+)
 
-@vcr.use_cassette()
-@pytest.mark.parametrize("egrid", ["CH643546955207"])
+
+@pytest.mark.parametrize("egrid", ["CH643546955207", "CH673533354667", "doesntexist"])
 def test_gis_canton(egrid, client, snapshot):
-    response = client.get(reverse("egrid", kwargs={"egrid": egrid}))
-    snapshot.assert_match(response.json())
+    with my_vcr.use_cassette(egrid + ".yml"):
+        response = client.get(reverse("egrid", kwargs={"egrid": egrid}))
+        snapshot.assert_match(response.json())
