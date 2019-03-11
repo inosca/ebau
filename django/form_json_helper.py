891ca068605678436f3410ed227b3dd51cfb58be
Yelin Zhang <yelin.zhang@adfinis-sygroup.ch>
2019-03-11 08:26:02 +0100

    Change form_json_helper to be more readable

--- django/form_json_helper.py
+++ django/form_json_helper.py
@@ -6,49 +6,42 @@ PATH = "kt_schwyz/form.json"
 
 
 def importText():
-    try:
-        with open(PATH) as f:
-            data = json.load(f)
+    with open(PATH) as f:
+        data = json.load(f)
 
-        changed = False
-        reader = csv.reader(sys.stdin)
+    changed = False
+    reader = csv.reader(sys.stdin)
 
-        for row in reader:
-            if len(row) == 2:
-                try:
-                    if len(row[1]) > 1 and data["questions"][row[0]]["hint"] != row[1]:
-                        data["questions"][row[0]]["hint"] = row[1]
-                        changed = True
-                except KeyError as e:
-                    print("Question {0} does not exist, skipping it.".format(e))
+    for row in reader:
+        if len(row) == 2:
+            try:
+                label, hint = row
+                if len(hint) > 1 and data["questions"][label]["hint"] != hint:
+                    data["questions"][label]["hint"] = hint
+                    changed = True
+            except KeyError as e:
+                print("Question {0} does not exist, skipping it.".format(e))
 
-        if changed:
-            with open(PATH, "w") as f:
-                json.dump(data, f, ensure_ascii=False, indent=2)
-
-    except Exception as e:
-        sys.exit("Unexpected Error occured: {0}".format(e))
+    if changed:
+        with open(PATH, "w") as f:
+            json.dump(data, f, ensure_ascii=False, indent=2)
 
 
 def exportForm():
-    try:
-        with open(PATH) as f:
-            data = json.load(f)
-        questions = data["questions"]
+    with open(PATH) as f:
+        data = json.load(f)
+    questions = data["questions"]
 
-        form_export = open("form-export.csv", "w")
-        writer = csv.writer(form_export)
+    form_export = open("form-export.csv", "w")
+    writer = csv.writer(form_export)
 
-        for k, v in questions.items():
-            try:
-                writer.writerow([k, v["label"], v["hint"]])
-            except KeyError as e:
-                print("Question {0} does not have a {1}, skipping it.".format(k, e))
+    for k, v in questions.items():
+        try:
+            writer.writerow([k, v["label"], v["hint"]])
+        except KeyError as e:
+            print("Question {0} does not have a {1}, skipping it.".format(k, e))
 
-        form_export.close()
-
-    except Exception as e:
-        sys.exit("Unexpected Error occured: {0}".format(e))
+    form_export.close()
 
 
 if sys.argv[1] == "import":
