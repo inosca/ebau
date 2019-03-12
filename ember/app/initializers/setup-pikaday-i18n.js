503e05e5676fa6263259674be530f4a70ab64ba0
Yelin Zhang <yelin.zhang@adfinis-sygroup.ch>
2019-03-12 10:26:48 +0100

    Add localization and fix datepicker

---
+++ ember/app/initializers/setup-pikaday-i18n.js
@@ -0,0 +1,18 @@
+import EmberObject from "@ember/object";
+import moment from "moment";
+
+export default {
+  name: "setup-pikaday-i18n",
+  initialize: function(application) {
+    var i18n = EmberObject.extend({
+      previousMonth: "Vorheriger Monat",
+      nextMonth: "Nächster Monat",
+      months: moment.localeData()._months,
+      weekdays: moment.localeData()._weekdays,
+      weekdaysShort: moment.localeData()._weekdaysShort
+    });
+
+    application.register("pikaday-i18n:main", i18n, { singleton: true });
+    application.inject("component:pikaday-input", "i18n", "pikaday-i18n:main");
+  }
+};
