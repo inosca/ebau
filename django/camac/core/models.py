703fa3fa440f62a83469a08c27ecd06fc462a981
Patrick Winter <patrick.winter@adfinis-sygroup.ch>
2019-03-14 14:02:56 +0100

    Add boilerplate of new calumalist resource

--- django/camac/core/models.py
+++ django/camac/core/models.py
@@ -3596,6 +3596,15 @@ class RSimpleList(models.Model):
         db_table = "R_SIMPLE_LIST"
 
 
+class RCalumaList(models.Model):
+    resource_id = models.AutoField(db_column="RESOURCE_ID", primary_key=True)
+    instance_states = models.CharField(db_column="INSTANCE_STATES", max_length=400)
+
+    class Meta:
+        managed = True
+        db_table = "R_CALUMA_LIST"
+
+
 class RUserAcl(models.Model):
     id = models.AutoField(db_column="ID", primary_key=True)
     resource = models.ForeignKey(
