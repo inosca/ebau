c83f9cde48480494310d78aa6fd3a49c4186029c
Christian Zosel <christian@zosel.ch>
2019-04-07 20:23:03 +0200

    Setup ember-caluma

--- ember-caluma-portal/app/app.js
+++ ember-caluma-portal/app/app.js
@@ -1,12 +1,20 @@
-import Application from '@ember/application';
-import Resolver from './resolver';
-import loadInitializers from 'ember-load-initializers';
-import config from './config/environment';
+import Application from "@ember/application";
+import Resolver from "./resolver";
+import loadInitializers from "ember-load-initializers";
+import config from "./config/environment";
 
+/* eslint-disable ember/avoid-leaking-state-in-ember-objects */
 const App = Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
-  Resolver
+  Resolver,
+  engines: {
+    emberCaluma: {
+      dependencies: {
+        services: ["apollo", "notification", "router", "intl", "caluma-options"]
+      }
+    }
+  }
 });
 
 loadInitializers(App, config.modulePrefix);
