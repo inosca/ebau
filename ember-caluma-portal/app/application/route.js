d64a55ba8679c3b09c3b48939954334db3d83557
Christian Zosel <christian@zosel.ch>
2019-04-07 14:30:37 +0200

    Setup basics with authentication

---
+++ ember-caluma-portal/app/application/route.js
@@ -0,0 +1,4 @@
+import Route from "@ember/routing/route";
+import OIDCApplicationRouteMixin from "ember-simple-auth-oidc/mixins/oidc-application-route-mixin";
+
+export default Route.extend(OIDCApplicationRouteMixin, {});
