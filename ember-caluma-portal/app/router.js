7519d5b0b64c18af4cc5bbe3bc8d1670a42aa425
Christian Zosel <christian@zosel.ch>
2019-04-07 14:30:37 +0200

    Setup basics with authentication

--- ember-caluma-portal/app/router.js
+++ ember-caluma-portal/app/router.js
@@ -1,12 +1,19 @@
-import EmberRouter from '@ember/routing/router';
-import config from './config/environment';
+import EmberRouter from "@ember/routing/router";
+import config from "./config/environment";
 
 const Router = EmberRouter.extend({
   location: config.locationType,
   rootURL: config.rootURL
 });
 
+const resetNamespace = true;
+
 Router.map(function() {
+  this.route("login");
+  this.route("logout");
+  this.route("protected", { path: "/" }, function() {
+    this.route("index", { path: "/", resetNamespace });
+  });
 });
 
 export default Router;
