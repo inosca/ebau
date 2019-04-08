f7c169886601b9b49fc9a54842fffbff2cb5ec8e
Christian Zosel <christian@zosel.ch>
2019-04-08 09:39:30 +0200

    Initialize workflow, create case, WIP on list view

--- ember-caluma-portal/app/instances/new/controller.js
+++ ember-caluma-portal/app/instances/new/controller.js
@@ -1,16 +1,32 @@
 import Controller from "@ember/controller";
 import { task } from "ember-concurrency";
 import { info1, info2 } from "ember-caluma-portal/instances/new/info";
+import { inject as service } from "@ember/service";
+import mutation from "ember-caluma-portal/gql/mutations/start-case";
 
 export default Controller.extend({
+  apollo: service(),
+
   infoCol1: info1,
   infoCol2: info2,
 
   selectedForm: null,
 
   save: task(function*() {
-    const form = this.get("selectedForm");
+    const caseId = yield this.get("apollo").mutate(
+      {
+        mutation,
+        variables: {
+          input: {
+            workflow: "building-permit",
+            form: "baugesuch" // TODO make dynamic once forms are configured
+          }
+        }
+      },
+      "startCase.case.id"
+    );
 
-    // TODO
+    console.log(caseId);
+    yield this.transitionToRoute("instances.edit", caseId);
   }).drop()
 });
