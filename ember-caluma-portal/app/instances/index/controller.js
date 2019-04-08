e03db571645cf0f04aa24d30486c63d17d0460d4
Christian Zosel <christian@zosel.ch>
2019-04-08 11:12:11 +0200

    Instance list view

--- ember-caluma-portal/app/instances/index/controller.js
+++ ember-caluma-portal/app/instances/index/controller.js
@@ -2,13 +2,26 @@ import Controller from "@ember/controller";
 import { inject as service } from "@ember/service";
 import { task } from "ember-concurrency";
 import allCasesQuery from "ember-caluma-portal/gql/queries/all-cases";
+import QueryParams from "ember-parachute";
 
-export default Controller.extend({
+export const queryParams = new QueryParams({
+  sort: {
+    defaultValue: "-creation_date",
+    refresh: true,
+    replace: true
+  },
+  identifier: {
+    defaultValue: "",
+    refresh: true,
+    replace: true
+  }
+});
+
+export default Controller.extend(queryParams.Mixin, {
   apollo: service(),
   notification: service(),
 
   setup() {
-    console.log("setup");
     this.data.perform();
   },
 
