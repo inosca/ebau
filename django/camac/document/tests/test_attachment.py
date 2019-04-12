c07e2e8377ecbac572dfb3b4cf0be97a267826d5
Yelinz <yelin.zhang@adfinis-sygroup.ch>
2019-04-12 14:14:51 +0200

    Fix apidocument module to show the service of each attachment

--- django/camac/document/tests/test_attachment.py
+++ django/camac/document/tests/test_attachment.py
@@ -14,11 +14,11 @@ from .data import django_file
 @pytest.mark.parametrize(
     "role__name,instance__user,num_queries",
     [
-        ("Applicant", LazyFixture("admin_user"), 6),
-        ("Reader", LazyFixture("user"), 6),
-        ("Canton", LazyFixture("user"), 6),
-        ("Municipality", LazyFixture("user"), 6),
-        ("Service", LazyFixture("user"), 6),
+        ("Applicant", LazyFixture("admin_user"), 7),
+        ("Reader", LazyFixture("user"), 7),
+        ("Canton", LazyFixture("user"), 7),
+        ("Municipality", LazyFixture("user"), 7),
+        ("Service", LazyFixture("user"), 7),
     ],
 )
 def test_attachment_list(
