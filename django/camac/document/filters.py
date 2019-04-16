f761a8bf3200a9cf9266c36ef60a201e316ea55a
Yelinz <yelin.zhang@adfinis-sygroup.ch>
2019-04-16 13:04:43 +0200

    Rewrite to log download in download method

--- django/camac/document/filters.py
+++ django/camac/document/filters.py
@@ -1,4 +1,4 @@
-from django_filters.rest_framework import FilterSet
+from django_filters.rest_framework import DateTimeFilter, FilterSet
 
 from camac.filters import CharMultiValueFilter, NumberFilter
 
@@ -19,3 +19,20 @@ class TemplateFilterSet(FilterSet):
     class Meta:
         model = models.Template
         fields = ("global_template",)
+
+
+class AttachmentDownloadHistoryFilterSet(FilterSet):
+    name = CharMultiValueFilter(lookup_expr="startswith")
+    download_date_after = DateTimeFilter(field_name="date_time", lookup_expr="gte")
+    download_date_before = DateTimeFilter(field_name="date_time", lookup_expr="lte")
+
+    class Meta:
+        model = models.AttachmentDownloadHistory
+        fields = (
+            "date_time",
+            "keycloak_id",
+            "name",
+            "attachment",
+            "group",
+            "group__role",
+        )
---
+++ django/camac/document/tests/test_attachment_download_history.py
@@ -0,0 +1,46 @@
+import pytest
+from django.urls import reverse
+from pytest_factoryboy import LazyFixture
+from rest_framework import status
+
+from .data import django_file
+
+
+def test_attachment_download_history_list(
+    admin_client, attachment_download_history_factory
+):
+    adhl = attachment_download_history_factory.create_batch(2)
+    url = reverse("attachment-download-history-list")
+    response = admin_client.get(url, {"attachment": adhl[0].attachment.pk})
+
+    assert response.status_code == status.HTTP_200_OK
+    json = response.json()
+    print(json)
+    assert len(json["data"]) == 1
+    assert json["data"][0]["relationships"]["attachment"]["data"]["id"] == str(
+        adhl[0].attachment.pk
+    )
+
+
+@pytest.mark.parametrize(
+    "role__name,instance__user,attachment__path",
+    [("Applicant", LazyFixture("admin_user"), django_file("multiple-pages.pdf"))],
+)
+def test_attachment_download_history_create(
+    admin_client, attachment_attachment_sections, attachment_section_group_acl
+):
+    download_url = reverse(
+        "attachment-download", args=[attachment_attachment_sections.attachment.path]
+    )
+    download_response = admin_client.get(download_url)
+    assert download_response.status_code == status.HTTP_200_OK
+
+    histroy_url = reverse("attachment-download-history-list")
+    history_response = admin_client.get(histroy_url)
+
+    assert history_response.status_code == status.HTTP_200_OK
+    json = history_response.json()
+    assert len(json["data"]) == 1
+    assert json["data"][0]["relationships"]["attachment"]["data"]["id"] == str(
+        attachment_attachment_sections.attachment.pk
+    )
