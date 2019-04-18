2aae0f706b81dffecec877bcacdc87dd02e3fb95
Christian Zosel <christian@zosel.ch>
2019-04-18 14:24:29 +0200

    Fix built of ember-caluma-portal container

--- ember-caluma-portal/ember-cli-build.js
+++ ember-caluma-portal/ember-cli-build.js
@@ -1,10 +1,13 @@
-'use strict';
+"use strict";
 
-const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const EmberApp = require("ember-cli/lib/broccoli/ember-app");
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    "ember-cli-babel": {
+      includePolyfill: true
+    },
+    SRI: { enabled: false }
   });
 
   // Use `app.import` to add additional libraries to the generated
