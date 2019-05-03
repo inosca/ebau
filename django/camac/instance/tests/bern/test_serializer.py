228902d39381a5ffefa1aaca4dd01dc62cbdda04
David Vogt <david.vogt@adfinis-sygroup.ch>
2019-05-03 15:56:23 +0200

    Test improvements

--- django/camac/instance/tests/bern/test_serializer.py
+++ django/camac/instance/tests/bern/test_serializer.py
@@ -1,12 +1,74 @@
 import json
 
+import pytest
 from django.urls import reverse
 from rest_framework import status
 
 from camac.instance.models import Instance
 
+RESP_CASE_INCOMPLETE = {
+    "data": {
+        "node": {
+            "id": "Q2FzZToxODBlMGQxNy0zZmZkLTQ1ZDMtYTU1MC1kMjVjNGVhODIxNDU=",
+            "meta": {},
+            "workflow": {"id": "V29ya2Zsb3c6YnVpbGRpbmctcGVybWl0"},
+            "workItems": {
+                "edges": [
+                    {"node": {"status": "WORKING", "task": {"slug": "fill-form"}}}
+                ]
+            },
+        }
+    }
+}
+RESP_CASE_ALREADY_ASSIGNED = {
+    "data": {
+        "node": {
+            "id": "Q2FzZToxODBlMGQxNy0zZmZkLTQ1ZDMtYTU1MC1kMjVjNGVhODIxNDU=",
+            "meta": {"camac-instance-id": 9999},
+            "workflow": {"id": "V29ya2Zsb3c6YnVpbGRpbmctcGVybWl0"},
+            "workItems": {
+                "edges": [
+                    {"node": {"status": "COMPLETED", "task": {"slug": "fill-form"}}}
+                ]
+            },
+        }
+    }
+}
+RESP_CASE_COMPLETED = {
+    "data": {
+        "node": {
+            "id": "Q2FzZToxODBlMGQxNy0zZmZkLTQ1ZDMtYTU1MC1kMjVjNGVhODIxNDU=",
+            "meta": {},
+            "workflow": {"id": "V29ya2Zsb3c6YnVpbGRpbmctcGVybWl0"},
+            "workItems": {
+                "edges": [
+                    {"node": {"status": "COMPLETED", "task": {"slug": "fill-form"}}}
+                ]
+            },
+        }
+    }
+}
 
-def test_create_instance(db, admin_client, mocker, instance_state, form, snapshot):
+
+@pytest.mark.freeze_time("2019-05-02")
+@pytest.mark.parametrize(
+    "work_item_resp,expected_resp",
+    [
+        (RESP_CASE_COMPLETED, status.HTTP_201_CREATED),
+        (RESP_CASE_INCOMPLETE, status.HTTP_400_BAD_REQUEST),
+        (RESP_CASE_ALREADY_ASSIGNED, status.HTTP_400_BAD_REQUEST),
+    ],
+)
+def test_create_instance(
+    db,
+    admin_client,
+    mocker,
+    instance_state,
+    form,
+    snapshot,
+    work_item_resp,
+    expected_resp,
+):
     recorded_requests = []
 
     def last_inst_id():
@@ -14,28 +76,7 @@ def test_create_instance(db, admin_client, mocker, instance_state, form, snapsho
 
     mock_responses = [
         # first response: NG asks caluma for data about our case
-        mocker.MagicMock(
-            json=lambda: {
-                "data": {
-                    "node": {
-                        "id": "Q2FzZToxODBlMGQxNy0zZmZkLTQ1ZDMtYTU1MC1kMjVjNGVhODIxNDU=",
-                        "meta": {},
-                        "workflow": {"id": "V29ya2Zsb3c6YnVpbGRpbmctcGVybWl0"},
-                        "workItems": {
-                            "edges": [
-                                {
-                                    "node": {
-                                        "status": "COMPLETED",
-                                        "task": {"slug": "fill-form"},
-                                    }
-                                }
-                            ]
-                        },
-                    }
-                }
-            },
-            status_code=status.HTTP_200_OK,
-        ),
+        mocker.MagicMock(json=lambda: work_item_resp, status_code=status.HTTP_200_OK),
         # second response: NG updates case with instance id
         mocker.MagicMock(
             json=lambda: {
@@ -79,7 +120,11 @@ def test_create_instance(db, admin_client, mocker, instance_state, form, snapsho
             }
         },
     )
-    assert create_resp.status_code == status.HTTP_201_CREATED, create_resp.content
+    assert create_resp.status_code == expected_resp, create_resp.content
+
+    if expected_resp == status.HTTP_400_BAD_REQUEST:
+        # in this case, we don't need to test the rest of the procedure
+        return
 
     # make sure meta is updated correctly
     assert json.loads(
