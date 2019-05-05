26709fca0a701e907a6db9675dea21bd4dbc1b1e
Christian Zosel <christian@zosel.ch>
2019-05-05 20:29:52 +0200

    fix(build): always build for ie11

--- ember-caluma-portal/config/targets.js
+++ ember-caluma-portal/config/targets.js
@@ -3,15 +3,9 @@
 const browsers = [
   "last 2 Chrome versions",
   "last 2 Firefox versions",
-  "last 2 Safari versions"
+  "last 2 Safari versions",
+  "ie 11"
 ];
-
-const isCI = !!process.env.CI;
-const isProduction = process.env.EMBER_ENV === "production";
-
-if (isCI || isProduction) {
-  browsers.push("ie 11");
-}
 
 module.exports = {
   browsers
