03a898a890f1fe304e2159c2b44deaf618892a09
Florian Mäder <florian.maeder@adfinis-sygroup.ch>
2019-05-15 15:03:20 +0200

    feat: add more columns to case list

--- ember-caluma-portal/app/instances/index/controller.js
+++ ember-caluma-portal/app/instances/index/controller.js
@@ -6,6 +6,10 @@ import allCasesQuery from "ember-caluma-portal/gql/queries/all-cases";
 import QueryParams from "ember-parachute";
 
 const SLUG_MUNICIPALITY = "gemeinde";
+const SLUG_DESCRIPTION = "beschreibung-bauvorhaben";
+const SLUG_ADDRESS_STREET = "strasse-flurname";
+const SLUG_ADDRESS_NR = "nr";
+const SLUG_ADDRESS_LOCALITY = "ort-grundstueck";
 
 export const queryParams = new QueryParams({
   sort: {
@@ -50,16 +54,74 @@ export default Controller.extend(queryParams.Mixin, {
     return this.getWithDefault("data.lastSuccessful.value", [])
       .map(edge => edge.node.document.answers.edges
         .filter(edge => edge.node.__typename === "FormAnswer")
-        .map(edge => edge.node.formValue.answers.edges.map(edge => {
-          const answer = edge.node.formValue.answers.edges.find(edge =>
-            edge.node.question.slug === SLUG_MUNICIPALITY
-          );
-          return answer && answer.node.stringValue;
-        })[0])[0]
+        .map(edge => edge.node.formValue.answers.edges
+          .map(edge => {
+            const answer = edge.node.formValue.answers.edges.find(edge =>
+              edge.node.question.slug === SLUG_MUNICIPALITY
+            );
+            return answer && answer.node.stringValue;
+          })
+          .filter(answer => answer !== undefined)[0]
+        )
+        .filter(answer => answer !== undefined)[0]
       );
   }),
 
-  applicants: computed("data.lastSuccessful.value", function() {
-    const cases = this.getWithDefault("data.lastSuccessful.value", []);
+  descriptions: computed("data.lastSuccessful.value", function() {
+    return this.getWithDefault("data.lastSuccessful.value", [])
+      .map(edge => edge.node.document.answers.edges
+        .filter(edge => edge.node.__typename === "FormAnswer")
+        .map(edge => edge.node.formValue.answers.edges
+          .map(edge => {
+            const answer = edge.node.formValue.answers.edges.find(edge =>
+              edge.node.question.slug === SLUG_DESCRIPTION
+            );
+            return answer && answer.node.stringValue;
+          })
+          .filter(answer => answer !== undefined)[0]
+        )
+        .filter(answer => answer !== undefined)[0]
+      );
+  }),
+
+  addresses: computed("data.lastSuccessful.value", function() {
+    return this.getWithDefault("data.lastSuccessful.value", [])
+      .map(edge => edge.node.document.answers.edges
+        .filter(edge => edge.node.__typename === "FormAnswer")
+        .map(edge => edge.node.formValue.answers.edges
+          .map(edge => {
+            const answer_street = edge.node.formValue.answers.edges.find(edge =>
+              edge.node.question.slug === SLUG_ADDRESS_STREET
+            );
+            const answer_nr = edge.node.formValue.answers.edges.find(edge =>
+              edge.node.question.slug === SLUG_ADDRESS_NR
+            );
+            const answer_locality = edge.node.formValue.answers.edges.find(edge =>
+              edge.node.question.slug === SLUG_ADDRESS_LOCALITY
+            );
+
+            const value_street = answer_street && answer_street.node.stringValue;
+            const value_nr = answer_nr && answer_nr.node.stringValue;
+            const value_locality = answer_locality && answer_locality.node.stringValue;
+
+            let value_concatenated;
+            if (value_street && value_nr) {
+              value_concatenated = `${value_street} ${value_nr}`;
+            } else if (answer_street || answer_nr) {
+              value_concatenated = value_street || value_nr;
+            }
+
+            if (value_locality && (value_street || value_nr)) {
+              value_concatenated += `, ${value_locality}`;
+            } else if (value_locality) {
+              value_concatenated = value_locality;
+            }
+
+            return value_concatenated;
+          })
+          .filter(answer => answer !== undefined)[0]
+        )
+        .filter(answer => answer !== undefined)[0]
+      );
   })
 });
