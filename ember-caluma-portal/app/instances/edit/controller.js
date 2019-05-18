b319a2a009c5649280c8354d08cb20855e952406
Christian Zosel <christian@zosel.ch>
2019-05-18 16:40:50 +0200

    Fix instance submit

--- ember-caluma-portal/app/instances/edit/controller.js
+++ ember-caluma-portal/app/instances/edit/controller.js
@@ -1,5 +1,7 @@
 import Controller from "@ember/controller";
 
+import { computed } from "@ember/object";
+
 export default Controller.extend({
   queryParams: ["section", "subSection", "group", "role"],
   section: null,
@@ -7,5 +9,16 @@ export default Controller.extend({
   group: null,
   role: null,
 
-  isEmbedded: window !== window.top
+  isEmbedded: window !== window.top,
+
+  headers: computed("group", "role", function() {
+    const headers = {};
+    if (this.get("group")) {
+      headers["X-CAMAC-GROUP"] = this.get("group");
+    }
+    if (this.get("role")) {
+      headers["X-CAMAC-GROUP"] = this.get("role");
+    }
+    return headers;
+  })
 });
