ebbeddaac310591c3f00a865dcead3be3eff5dde
David Vogt <david.vogt@adfinis-sygroup.ch>
2019-05-21 15:04:16 +0200

    Move from vcrpy to pytest-vcr
    Same module, but controllable via commandline arguments

--- django/gisbern/tests/snapshots/snap_test_gis_canton.py
+++ django/gisbern/tests/snapshots/snap_test_gis_canton.py
@@ -7,18 +7,17 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
-snapshots['test_gis_canton[doesntexist] 1'] = {
-    'errors': 'No multisurface found'
-}
-
 snapshots['test_gis_canton[CH643546955207] 1'] = {
     'data': {
         'ARCHINV_FUNDST': False,
         'BALISKBS_KBS': True,
+        'BAUINV_BAUINV_VW': True,
         'GK5_SY': False,
+        'GSK25_GSK_VW': True,
+        'GSKT_BEZEICH_DE': 'übriger Bereich üB',
         'NSG_NSGP': True,
-        'UZP_LSG_VW': True,
-        'UZP_UEO_VW': 'Teilzonenplan Moorlandschaft 336 Amsoldingen'
+        'UZP_LSG_VW': False,
+        'UZP_UEO_VW': 'Teilzonenplan Moorlandschaft Nr. 336 Amsoldingen'
     }
 }
 
@@ -26,10 +25,13 @@ snapshots['test_gis_canton[CH673533354667] 1'] = {
     'data': {
         'ARCHINV_FUNDST': False,
         'BALISKBS_KBS': True,
+        'BAUINV_BAUINV_VW': False,
         'GK5_SY': True,
+        'GSK25_GSK_VW': True,
+        'GSKT_BEZEICH_DE': 'Gewässerschutzbereich Au',
         'NSG_NSGP': False,
         'UZP_BAU_VW': [
-            'Gewerbezone G2'
+            'Zone für öffentliche Nutzung'
         ],
         'UZP_LSG_VW': False,
         'UZP_UEO_VW': 'Chräjeninsel'
@@ -40,11 +42,18 @@ snapshots['test_gis_canton[CH851446093521] 1'] = {
     'data': {
         'ARCHINV_FUNDST': True,
         'BALISKBS_KBS': False,
+        'BAUINV_BAUINV_VW': True,
         'GK5_SY': False,
+        'GSK25_GSK_VW': True,
+        'GSKT_BEZEICH_DE': 'Gewässerschutzbereich Ao',
         'NSG_NSGP': False,
         'UZP_BAU_VW': [
-            'Wohnzone 1 E1 '
+            'Wohnzone 2 E2 '
         ],
         'UZP_LSG_VW': False
     }
+}
+
+snapshots['test_gis_canton[doesntexist] 1'] = {
+    'errors': 'No multisurface found'
 }
--- django/gisbern/tests/test_gis_canton.py
+++ django/gisbern/tests/test_gis_canton.py
@@ -1,20 +1,22 @@
 import pytest
-import vcr
 from django.urls import reverse
 
-my_vcr = vcr.VCR(
-    # add "body" to default match_on because we're passing the
-    # egrid number in the body.
-    match_on=["method", "scheme", "host", "port", "path", "query", "body"],
-    cassette_library_dir="gisbern/tests/cassettes",
-    decode_compressed_response=True,
-)
+
+@pytest.fixture(scope="module")
+def vcr_config():
+    return {
+        # add "body" to default match_on because we're passing the
+        # egrid number in the body.
+        "match_on": ["method", "scheme", "host", "port", "path", "query", "body"],
+        "cassette_library_dir": "gisbern/tests/cassettes",
+        "decode_compressed_response": True,
+    }
 
 
 @pytest.mark.parametrize(
     "egrid", ["CH643546955207", "CH673533354667", "CH851446093521", "doesntexist"]
 )
+@pytest.mark.vcr()
 def test_gis_canton(egrid, client, snapshot):
-    with my_vcr.use_cassette(egrid + ".yml"):
-        response = client.get(reverse("egrid", kwargs={"egrid": egrid}))
-        snapshot.assert_match(response.json())
+    response = client.get(reverse("egrid", kwargs={"egrid": egrid}))
+    snapshot.assert_match(response.json())
