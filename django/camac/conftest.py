4940d77b953cda19c47402d8581ca4f294ba1f1f
David Vogt <david.vogt@adfinis-sygroup.ch>
2019-05-21 15:16:57 +0200

    Rename instance states fixture to "bern" prefix
    The names and IDs will differ across projects, and we're using it in
    Bern-only tests. If other cantons require a similar fixture, we'll need
    a custom fixture anyway, so might make it clear in the first place

--- django/camac/conftest.py
+++ django/camac/conftest.py
@@ -59,7 +59,7 @@ def admin_client(db, admin_user):
 
 
 @pytest.fixture
-def instance_states(db, instance_state_factory):
+def bern_instance_states(db, instance_state_factory):
     new, _ = instance_models.InstanceState.objects.get_or_create(pk=1)
     new.trans.create(name="Neu", language="de")
     subm, _ = instance_models.InstanceState.objects.get_or_create(pk=20000)
