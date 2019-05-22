bc321046af29540141c1ffdd5e4cc81d523e04f2
Christian Zosel <christian@zosel.ch>
2019-05-22 15:08:53 +0200

    replace ember-ajax with ember-fetch

--- ember-caluma-portal/app/components/be-submit-instance/component.js
+++ ember-caluma-portal/app/components/be-submit-instance/component.js
@@ -1,3 +1,4 @@
+import fetch from "fetch";
 import Component from "@ember/component";
 import { task } from "ember-concurrency";
 import { inject as service } from "@ember/service";
@@ -44,35 +45,33 @@ export default Component.extend({
         );
       }
       // submit instance in CAMAC
-      yield this.ajax.request(
-        `/api/v1/instances/${instanceId}?group=${GROUP_APPLICANT}`,
-        {
-          method: "PATCH",
+      yield fetch(`/api/v1/instances/${instanceId}?group=${GROUP_APPLICANT}`, {
+        method: "PATCH",
+        body: JSON.stringify({
           data: {
-            data: {
-              type: "instances",
-              id: instanceId,
-              attributes: {
-                "caluma-case-id": caseId
-              },
-              relationships: {
-                "instance-state": {
-                  data: {
-                    id: INSTANCE_STATE_SUBMITTED,
-                    type: "instance-states"
-                  }
+            type: "instances",
+            id: instanceId,
+            attributes: {
+              "caluma-case-id": caseId
+            },
+            relationships: {
+              "instance-state": {
+                data: {
+                  id: INSTANCE_STATE_SUBMITTED,
+                  type: "instance-states"
                 }
               }
             }
-          },
-          headers: {
-            Authorization: `Bearer ${this.get(
-              "session.data.authenticated.access_token"
-            )}`,
-            "content-type": "application/vnd.api+json"
           }
+        }),
+        headers: {
+          authorization: `Bearer ${this.get(
+            "session.data.authenticated.access_token"
+          )}`,
+          accept: "application/vnd.api+json",
+          "content-type": "application/vnd.api+json"
         }
-      );
+      });
 
       this.notification.success("Das Gesuch wurde erfolgreich eingereicht");
 
