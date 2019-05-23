c360d3465d29eefc1d13bb2c6e5336cd17f4d60b
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2019-05-23 17:27:40 +0200

    refactor: extract fetch into a service which handles headers

---
+++ ember-caluma-portal/app/services/fetch.js
@@ -0,0 +1,28 @@
+import Service, { inject as service } from "@ember/service";
+import fetch from "fetch";
+import { reads } from "@ember/object/computed";
+import { computed } from "@ember/object";
+
+export default Service.extend({
+  session: service(),
+
+  token: reads("session.data.authenticated.access_token"),
+
+  headers: computed("token", function() {
+    return {
+      authorization: `Bearer ${this.token}`,
+      accept: "application/vnd.api+json",
+      "content-type": "application/vnd.api+json"
+    };
+  }),
+
+  fetch(resource, init = {}) {
+    init.headers = Object.assign(this.headers, init.headers);
+
+    Object.keys(init.headers).forEach(
+      k => init.headers[k] === undefined && delete init.headers[k]
+    );
+
+    return fetch(resource, init);
+  }
+});
