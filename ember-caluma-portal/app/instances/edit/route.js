716558937d6cd8c423e1059c0b52d1cb50d0723d
Christian Zosel <christian@zosel.ch>
2019-05-28 09:23:59 +0200

    fix(be-form): send proper x-camac headers for GetCase call
    Live bug fixing during customer meeting ftw

--- ember-caluma-portal/app/instances/edit/route.js
+++ ember-caluma-portal/app/instances/edit/route.js
@@ -1,36 +1,14 @@
 import Route from "@ember/routing/route";
-import { inject as service } from "@ember/service";
-import { task } from "ember-concurrency";
-import getCaseQuery from "ember-caluma-portal/gql/queries/get-case";
 
 export default Route.extend({
-  apollo: service(),
-  notification: service(),
-  intl: service(),
-
   model({ case_id }) {
     return {
-      data: this.get("data").perform(case_id),
       caseId: case_id
     };
   },
 
-  data: task(function*(caseId) {
-    try {
-      return yield this.get("apollo").watchQuery(
-        {
-          query: getCaseQuery,
-          fetchPolicy: "cache-and-network",
-          variables: { caseId: parseInt(caseId) }
-        },
-        "allCases.edges"
-      );
-    } catch (e) {
-      // eslint-disable-next-line no-console
-      console.error(e);
-      this.get("notification").danger(
-        this.get("intl").t("global.loadingError")
-      );
-    }
-  }).restartable()
+  setupController(controller, model) {
+    this._super(controller, model);
+    controller.data.perform(model.caseId);
+  }
 });
