a345d901edd32ba53cd133f5df0702792680b214
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2019-06-12 11:12:22 +0200

    feat(portal): enforce prettier

--- ember-caluma-portal/tests/unit/login/route-test.js
+++ ember-caluma-portal/tests/unit/login/route-test.js
@@ -1,11 +1,11 @@
-import { module, test } from 'qunit';
-import { setupTest } from 'ember-qunit';
+import { module, test } from "qunit";
+import { setupTest } from "ember-qunit";
 
-module('Unit | Route | login', function(hooks) {
+module("Unit | Route | login", function(hooks) {
   setupTest(hooks);
 
-  test('it exists', function(assert) {
-    let route = this.owner.lookup('route:login');
+  test("it exists", function(assert) {
+    let route = this.owner.lookup("route:login");
     assert.ok(route);
   });
 });
