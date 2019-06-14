64f8e096a9db945543b23637de087873ab4d0d29
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2019-06-14 16:18:13 +0200

    feat(portal): add tab in form view for showing feedback

--- ember-caluma-portal/app/instances/edit/controller.js
+++ ember-caluma-portal/app/instances/edit/controller.js
@@ -1,14 +1,34 @@
 import Controller from "@ember/controller";
-
-import { computed } from "@ember/object";
-import { task } from "ember-concurrency";
-import getCaseQuery from "ember-caluma-portal/gql/queries/get-case";
+import { computed, defineProperty } from "@ember/object";
+import { reads } from "@ember/object/computed";
 import { inject as service } from "@ember/service";
+import { getOwner } from "@ember/application";
+import { task } from "ember-concurrency";
+
+import Attachment from "ember-caluma-portal/lib/attachment";
+import getCaseQuery from "ember-caluma-portal/gql/queries/get-case";
+
+const SECTION = 3;
+
+const FeedbackAttachment = Attachment.extend({
+  documentStore: service(),
+
+  init() {
+    this._super(...arguments);
+
+    defineProperty(
+      this,
+      "document",
+      reads(`documentStore.documents.${this.documentUuid}`)
+    );
+  }
+});
 
 export default Controller.extend({
   apollo: service(),
   notification: service(),
   intl: service(),
+  fetch: service(),
 
   queryParams: ["section", "subSection", "group", "role"],
   section: null,
@@ -43,5 +63,34 @@ export default Controller.extend({
         this.get("intl").t("global.loadingError")
       );
     }
+  }).restartable(),
+
+  feedbackData: task(function*(caseId) {
+    const response = yield this.fetch.fetch(
+      `/api/v1/attachments?attachment_sections=${SECTION}&instance=${caseId}`
+    );
+
+    yield this.data.last;
+
+    const documentId = yield this.apollo.query(
+      {
+        query: getCaseQuery,
+        fetchPolicy: "cache-only",
+        variables: { caseId: parseInt(caseId) },
+        context: { headers: this.headers }
+      },
+      "allCases.edges.firstObject.node.document.id"
+    );
+
+    const { data } = yield response.json();
+
+    return data.map(attachment =>
+      FeedbackAttachment.create(
+        getOwner(this).ownerInjection(),
+        Object.assign(attachment, {
+          documentUuid: atob(documentId).split(":")[1]
+        })
+      )
+    );
   }).restartable()
 });
