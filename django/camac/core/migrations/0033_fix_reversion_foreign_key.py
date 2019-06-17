65c8a0843ce4c48244346a7c1c957bf88d01b831
David Vogt <david.vogt@adfinis-sygroup.ch>
2019-06-17 12:53:09 +0200

    fix migration dependency

--- django/camac/core/migrations/0033_fix_reversion_foreign_key.py
+++ django/camac/core/migrations/0033_fix_reversion_foreign_key.py
@@ -7,7 +7,10 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
 
-    dependencies = [("core", "0032_multilingual_pages_answerlist")]
+    dependencies = [
+        ("core", "0032_multilingual_pages_answerlist"),
+        ("reversion", "0001_squashed_0004_auto_20160611_1202"),
+    ]
 
     operations = [
         migrations.RunSQL(
