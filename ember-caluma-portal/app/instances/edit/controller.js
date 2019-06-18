e7954ed7a85d1af342ec20a326e3f07c767aa1a3
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2019-06-18 15:48:04 +0200

    feat(portal): move all embed logic into own route

--- ember-caluma-portal/app/instances/edit/controller.js
+++ ember-caluma-portal/app/instances/edit/controller.js
@@ -1,33 +1,12 @@
 import Controller from "@ember/controller";
-import { computed, defineProperty } from "@ember/object";
-import { reads } from "@ember/object/computed";
+import { computed } from "@ember/object";
 import { inject as service } from "@ember/service";
-import { getOwner } from "@ember/application";
 import { task } from "ember-concurrency";
 
-import Attachment from "ember-caluma-portal/lib/attachment";
 import getCaseQuery from "ember-caluma-portal/gql/queries/get-case";
-
-const SECTION = 3;
-
-const FeedbackAttachment = Attachment.extend({
-  documentStore: service(),
-
-  init() {
-    this._super(...arguments);
-
-    defineProperty(
-      this,
-      "document",
-      reads(`documentStore.documents.${this.documentUuid}`)
-    );
-  }
-});
 
 export default Controller.extend({
   apollo: service(),
-  notification: service(),
-  intl: service(),
   fetch: service(),
 
   queryParams: ["section", "subSection", "group", "role"],
@@ -36,8 +15,6 @@ export default Controller.extend({
   group: null,
   role: null,
 
-  isEmbedded: window !== window.top,
-
   headers: computed("group", "role", function() {
     return {
       ...(this.group ? { "X-CAMAC-GROUP": this.group } : {}),
@@ -45,29 +22,21 @@ export default Controller.extend({
     };
   }),
 
-  data: task(function*(caseId) {
-    try {
-      return yield this.get("apollo").watchQuery(
-        {
-          query: getCaseQuery,
-          fetchPolicy: "cache-and-network",
-          variables: { caseId: parseInt(caseId) },
-          context: { headers: this.headers }
-        },
-        "allCases.edges"
-      );
-    } catch (e) {
-      // eslint-disable-next-line no-console
-      console.error(e);
-      this.get("notification").danger(
-        this.get("intl").t("global.loadingError")
-      );
-    }
+  data: task(function*() {
+    return yield this.apollo.watchQuery(
+      {
+        query: getCaseQuery,
+        fetchPolicy: "cache-and-network",
+        variables: { caseId: this.model },
+        context: { headers: this.headers }
+      },
+      "allCases.edges.firstObject.node"
+    );
   }).restartable(),
 
-  instanceState: task(function*(caseId) {
+  instanceState: task(function*() {
     const response = yield this.fetch.fetch(
-      `/api/v1/instances/${caseId}?include=instance_state`
+      `/api/v1/instances/${this.model}?include=instance_state`
     );
 
     const { included, data: instance } = yield response.json();
@@ -77,45 +46,5 @@ export default Controller.extend({
         obj.type === "instance-states" &&
         obj.id === instance.relationships["instance-state"].data.id
     );
-  }).restartable(),
-
-  feedbackData: task(function*(caseId) {
-    const response = yield this.fetch.fetch(
-      `/api/v1/attachments?attachment_sections=${SECTION}&instance=${caseId}`
-    );
-
-    yield this.data.last;
-
-    const documentId = yield this.apollo.query(
-      {
-        query: getCaseQuery,
-        fetchPolicy: "cache-only",
-        variables: { caseId: parseInt(caseId) },
-        context: { headers: this.headers }
-      },
-      "allCases.edges.firstObject.node.document.id"
-    );
-
-    const { data } = yield response.json();
-
-    return data.map(attachment =>
-      FeedbackAttachment.create(
-        getOwner(this).ownerInjection(),
-        Object.assign(attachment, {
-          documentUuid: atob(documentId).split(":")[1]
-        })
-      )
-    );
-  }).restartable(),
-
-  disabled: computed(
-    "isEmbedded",
-    "instanceState.lastSuccessful.value.attributes.name",
-    function() {
-      return (
-        this.isEmbedded ||
-        this.get("instanceState.lastSuccessful.value.attributes.name") !== "Neu"
-      );
-    }
-  )
+  }).restartable()
 });
