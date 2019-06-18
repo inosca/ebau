5bc56e0d3a204f4c8ecef0f301d089d3f1701737
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2019-06-18 15:48:04 +0200

    feat(portal): move all embed logic into own route

---
+++ ember-caluma-portal/app/instances/edit/embed/controller.js
@@ -0,0 +1,18 @@
+import Controller from "@ember/controller";
+import { inject as controller } from "@ember/controller";
+import { reads } from "@ember/object/computed";
+import { computed } from "@ember/object";
+
+const EDITABLE_INSTANCE_STATE_NAMES = ["In Korrektur"];
+
+export default Controller.extend({
+  editController: controller("instances.edit"),
+  data: reads("editController.data"),
+  instanceState: reads("editController.instanceState"),
+
+  disabled: computed("instanceState.lastSuccessful.value", function() {
+    return !EDITABLE_INSTANCE_STATE_NAMES.includes(
+      this.get("instanceState.lastSuccessful.value.attributes.name")
+    );
+  })
+});
---
+++ ember-caluma-portal/tests/unit/instances/edit/embed/controller-test.js
@@ -0,0 +1,12 @@
+import { module, test } from "qunit";
+import { setupTest } from "ember-qunit";
+
+module("Unit | Controller | instances/edit/embed", function(hooks) {
+  setupTest(hooks);
+
+  // Replace this with your real tests.
+  test("it exists", function(assert) {
+    let controller = this.owner.lookup("controller:instances/edit/embed");
+    assert.ok(controller);
+  });
+});
