94ffaad0b976992e41b70b6082b5709fadf07e12
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2019-07-16 10:59:20 +0200

    feat(be-templates): add page for managing templates

--- ember-caluma-portal/app/services/apollo.js
+++ ember-caluma-portal/app/services/apollo.js
@@ -10,42 +10,36 @@ export default ApolloService.extend(CalumaApolloServiceMixin, {
   session: service(),
   router: service(),
 
+  token: reads("session.data.authenticated.access_token"),
   group: reads("router.currentRoute.queryParams.group"),
   role: reads("router.currentRoute.queryParams.role"),
 
-  link: computed(
-    "session.data.authenticated.access_token",
-    "group",
-    "role",
-    function() {
-      const httpLink = this._super(...arguments);
+  link: computed("token", "group", "role", function() {
+    const httpLink = this._super(...arguments);
 
-      const middleware = setContext(async (request, context) => {
-        const token = this.get("session.data.authenticated.access_token");
-
-        context.headers = Object.assign({}, context.headers, {
-          authorization: `Bearer ${token}`,
-          ...(this.group ? { "X-CAMAC-GROUP": this.group } : {}),
-          ...(this.role ? { "X-CAMAC-ROLE": this.role } : {})
-        });
-        return context;
+    const middleware = setContext(async (request, context) => {
+      context.headers = Object.assign({}, context.headers, {
+        authorization: `Bearer ${this.token}`,
+        ...(this.group ? { "x-camac-group": this.group } : {}),
+        ...(this.role ? { "x-camac-role": this.role } : {})
       });
+      return context;
+    });
 
-      const afterware = onError(error => {
-        const { networkError, graphQLErrors } = error;
+    const afterware = onError(error => {
+      const { networkError, graphQLErrors } = error;
 
-        if (
-          (graphQLErrors &&
-            graphQLErrors.some(({ message }) =>
-              /^401 Client Error/.test(message)
-            )) ||
-          (networkError && networkError.statusCode === 401)
-        ) {
-          this.router.transitionTo("logout");
-        }
-      });
+      if (
+        (graphQLErrors &&
+          graphQLErrors.some(({ message }) =>
+            /^401 Client Error/.test(message)
+          )) ||
+        (networkError && networkError.statusCode === 401)
+      ) {
+        this.router.transitionTo("logout");
+      }
+    });
 
-      return middleware.concat(afterware).concat(httpLink);
-    }
-  )
+    return middleware.concat(afterware).concat(httpLink);
+  })
 });
