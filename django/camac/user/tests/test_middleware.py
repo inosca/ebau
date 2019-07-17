b6d6c65f4fcf88f6d985076871d09518973d1e42
Oliver Sauder <os@esite.ch>
2019-07-17 12:25:31 +0200

    Allow group to be passed on with header X_CAMAC_GROUP
    query param group overrules provided header

--- django/camac/user/tests/test_middleware.py
+++ django/camac/user/tests/test_middleware.py
@@ -26,3 +26,12 @@ def test_get_group_param(rf, admin_user, user_group_factory):
 
     group = middleware.get_group(request)
     assert group == new_group
+
+
+def test_get_group_header(rf, admin_user, user_group_factory):
+    new_group = user_group_factory(user=admin_user).group
+    request = rf.get("/", HTTP_X_CAMAC_GROUP=new_group.pk)
+    request.user = admin_user
+
+    group = middleware.get_group(request)
+    assert group == new_group
