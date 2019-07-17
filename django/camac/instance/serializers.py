40bff223462b42a53bbc92733a1f594f59626a66
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2019-07-17 15:23:10 +0200

    feat(be-api): drop usage of caluma cases and add custom action for submitting

--- django/camac/instance/serializers.py
+++ django/camac/instance/serializers.py
@@ -166,9 +166,25 @@ class CalumaInstanceSerializer(InstanceSerializer):
         ).first(),
     )
 
-    caluma_case_id = serializers.CharField(required=False)
+    caluma_form = serializers.CharField(required=False)
 
     public_status = serializers.SerializerMethodField()
+
+    def _caluma_query(self, query, variables):
+        response = requests.post(
+            settings.CALUMA_URL,
+            json={"query": query, "variables": variables},
+            headers={
+                "Authorization": get_authorization_header(self.context["request"])
+            },
+        )
+
+        if response.status_code != 200 or response.json().get(
+            "errors"
+        ):  # pragma: no cover
+            raise exceptions.ValidationError("Error while querying caluma")
+
+        return response
 
     def get_public_status(self, instance):
         STATUS_MAP = {
@@ -197,95 +213,37 @@ class CalumaInstanceSerializer(InstanceSerializer):
         )
 
     def validate_instance_state(self, value):
-        if not self.instance:  # pragma: no cover
-            request_logger.info("Creating new instance, overriding %s" % value)
-            return models.InstanceState.objects.get(name="new")
-        return value
-
-    def _is_submit(self, data):
-        if self.instance:
-            old_version = models.Instance.objects.get(pk=self.instance.pk)
-            return (
-                old_version.instance_state_id != data.get("instance_state").pk
-                and old_version.instance_state.name in ["new", "rejected"]
-                and data.get("instance_state").name == "subm"
-            )
+        return models.InstanceState.objects.get(name="new")
 
     def validate(self, data):
-        request_logger.info(f"validating instance {data.keys()}")
-        case_id = data.get("caluma_case_id")
-
-        # Fetch case data and meta information. Validate that the case doesn't
-        # have another instance assigned already, and at the same time store
-        # the data we need to update the case later on.
-        request_logger.info("Fetching Caluma case info to validate instance creation")
-        caluma_resp = requests.post(
-            settings.CALUMA_URL,
-            json={
-                "query": """
-                    query ($case_id: ID!) {
-                      node(id:$case_id) {
-                        ... on Case {
-                          id
-                          meta
-                          workflow {
-                            id
-                          }
-                          document {
-                            id
-                            answers(questions: ["gemeinde"]) {
-                              edges {
-                                node {
-                                  id
-                                  question {
-                                    slug
-                                  }
-                                  ... on StringAnswer {
-                                    stringValue: value
-                                  }
-                                }
-                              }
+        form = data.get("caluma_form")
+        caluma_resp = self._caluma_query(
+            """
+                query GetMainForm($slug: String!) {
+                    allForms(slug: $slug, metaValue: [{key: "is-main-form", value: true}]) {
+                        edges {
+                            node {
+                                slug
+                                meta
                             }
-                          }
                         }
-                      }
                     }
-                """,
-                "variables": {"case_id": case_id},
-            },
-            headers={
-                "Authorization": get_authorization_header(self.context["request"])
-            },
+                }
+            """,
+            {"slug": form},
         )
-        if "errors" in caluma_resp.json():  # pragma: no cover
+
+        if (
+            len(caluma_resp.json()["data"]["allForms"]["edges"]) != 1
+        ):  # pragma: no cover
             raise exceptions.ValidationError(
-                f"Caluma API call returned errors {caluma_resp.json()['errors']}"
+                f"Passed caluma form is not a main form: {form}"
             )
-
-        data["caluma_case_data"] = caluma_resp.json()["data"]["node"]
-        request_logger.info("Caluma case information: %s", data["caluma_case_data"])
-
-        if not self._is_submit(data):
-            case_meta = data["caluma_case_data"]["meta"]
-            if "camac-instance-id" in case_meta:  # pragma: no cover
-                # Already linked. this should not be, as we just
-                # created a new Camac instance for a caluma case that
-                # has already an instance assigned
-                raise exceptions.ValidationError(
-                    f"Caluma case already has an instance id "
-                    f"assigned: {case_meta['camac-instance-id']}"
-                )
-        else:
-            # TODO ask caluma if case is actually valid
-            pass
 
         return data
 
     def create(self, validated_data):  # pragma: no cover
-        case_id = validated_data.pop("caluma_case_id")
-        case_data = validated_data.pop("caluma_case_data")
-        case_meta = case_data["meta"]
-
+        form = validated_data.pop("caluma_form")
         created = super().create(validated_data)
 
         created.involved_applicants.create(
@@ -294,111 +252,39 @@ class CalumaInstanceSerializer(InstanceSerializer):
             created=timezone.now(),
         )
 
-        # Now, add instance id to case
-        case_meta["camac-instance-id"] = created.pk
-
-        caluma_resp = self._save_case(case_id, case_meta, case_data["workflow"]["id"])
-        if caluma_resp.status_code not in (200, 201):  # pragma: no cover
-            raise exceptions.ValidationError("Error while linking case and instance")
+        self._caluma_query(
+            """
+                mutation CreateDocument($input: SaveDocumentInput!) {
+                    saveDocument(input: $input) {
+                    document {
+                        id
+                        meta
+                    }
+                    }
+                }
+            """,
+            {
+                "input": {
+                    "form": form,
+                    "meta": json.dumps({"camac-instance-id": created.instance_id}),
+                }
+            },
+        )
 
         return created
 
-    def _save_case(self, case_id, case_meta, workflow_id):
-        caluma_resp = requests.post(
-            settings.CALUMA_URL,
-            json={
-                "query": """
-                       mutation save_instance_id ($input: SaveCaseInput!) {
-                         saveCase (input: $input) {
-                           case {
-                             id
-                             meta
-                           }
-                         }
-                       }
-                """,
-                "variables": {
-                    "input": {
-                        "id": case_id,
-                        "meta": json.dumps(case_meta),
-                        "workflow": workflow_id,
-                    }
-                },
-            },
-            headers={
-                "Authorization": get_authorization_header(self.context["request"])
-            },
-        )
-        return caluma_resp
+    class Meta(InstanceSerializer.Meta):
+        fields = InstanceSerializer.Meta.fields + ("caluma_form", "public_status")
+        read_only_fields = InstanceSerializer.Meta.read_only_fields + ("public_status",)
 
-    @transaction.atomic
-    def update(self, instance, validated_data):
-        request_logger.info("Updating instance %s" % instance.pk)
 
-        if not self._is_submit(validated_data):
-            raise exceptions.ValidationError(
-                f"Updating cases is only allowed for submitting"
-            )
+class CalumaInstanceSubmitSerializer(CalumaInstanceSerializer):
+    instance_state = FormDataResourceRelatedField(queryset=models.InstanceState.objects)
+    previous_instance_state = FormDataResourceRelatedField(
+        queryset=models.InstanceState.objects
+    )
 
-        validated_data["modification_date"] = timezone.now()
-
-        if instance.instance_state.name == "rejected":
-            self.instance.instance_state = instance.previous_instance_state
-        else:
-            self.instance.instance_state = models.InstanceState.objects.get(name="subm")
-
-        service_id = None
-        try:
-            first_answer = validated_data.get("caluma_case_data")["document"][
-                "answers"
-            ]["edges"][0]["node"]
-            service_id = int(first_answer["stringValue"])
-        except (KeyError, IndexError):  # pragma: no cover
-            raise exceptions.ValidationError("Did not find municipality in Caluma")
-
-        InstanceService.objects.get_or_create(
-            instance=self.instance,
-            service_id=service_id,
-            active=1,
-            defaults={"activation_date": None},
-        )
-
-        self.instance.save()
-
-        self._set_submit_date(validated_data)
-
-        # send out emails upon submission
-        for notification_config in settings.APPLICATION["NOTIFICATIONS"]["SUBMIT"]:
-            self.notify_submit(**notification_config)
-
-        return instance
-
-    def _set_submit_date(self, validated_data):
-        case_data = validated_data["caluma_case_data"]
-
-        if "submit-date" in case_data["meta"]:  # pragma: no cover
-            # instance was already submitted, this is probably a re-submit
-            # after correction.
-            return
-
-        # Set submit date in Camac first...
-        subm_question = Question.objects.get(pk=SUBMIT_DATE_QUESTION_ID)
-        subm_ans, _ = Answer.objects.get_or_create(
-            instance=self.instance,
-            question=subm_question,
-            item=1,
-            chapter_id=SUBMIT_DATE_CHAPTER,
-            # CAMAC date is formatted in "dd.mm.yyyy"
-            defaults={"answer": timezone.now().strftime("%d.%m.%Y")},
-        )
-        new_meta = {
-            **case_data["meta"],
-            # Caluma date is formatted yyyy-mm-dd so it can be sorted
-            "submit-date": timezone.now().strftime("%Y-%m-%d"),
-        }
-        self._save_case(case_data["id"], new_meta, case_data["workflow"]["id"])
-
-    def notify_submit(self, template_id, recipient_types):
+    def _notify_submit(self, template_id, recipient_types):
         """Send notification email."""
 
         # fake jsonapi request for notification serializer
@@ -425,9 +311,178 @@ class CalumaInstanceSerializer(InstanceSerializer):
 
         mail_serializer.create(mail_serializer.validated_data)
 
-    class Meta(InstanceSerializer.Meta):
-        fields = InstanceSerializer.Meta.fields + ("caluma_case_id", "public_status")
-        read_only_fields = InstanceSerializer.Meta.read_only_fields + ("public_status",)
+    def _set_submit_date(self, validated_data):
+        document = validated_data["caluma_document"]
+
+        if "submit-date" in document["meta"]:  # pragma: no cover
+            # instance was already submitted, this is probably a re-submit
+            # after correction.
+            return
+
+        # Set submit date in Camac first...
+        Answer.objects.get_or_create(
+            instance=self.instance,
+            question_id=SUBMIT_DATE_QUESTION_ID,
+            item=1,
+            chapter_id=SUBMIT_DATE_CHAPTER,
+            # CAMAC date is formatted in "dd.mm.yyyy"
+            defaults={"answer": timezone.now().strftime("%d.%m.%Y")},
+        )
+
+        new_meta = {
+            **document["meta"],
+            # Caluma date is formatted yyyy-mm-dd so it can be sorted
+            "submit-date": timezone.now().strftime("%Y-%m-%d"),
+        }
+
+        self._caluma_query(
+            """
+                mutation SaveDocumentMeta($input: SaveDocumentInput!) {
+                    saveDocument(input: $input) {
+                        clientMutationId
+                    }
+                }
+            """,
+            {
+                "input": {
+                    "id": document["id"],
+                    "form": document["form"]["slug"],
+                    "meta": json.dumps(new_meta),
+                }
+            },
+        )
+
+    def validate(self, data):
+        caluma_resp = self._caluma_query(
+            """
+                query GetDocument($instanceId: GenericScalar!) {
+                    allDocuments(metaValue: [{key: "camac-instance-id", value: $instanceId}]) {
+                        edges {
+                                node {
+                                    id
+                                    meta
+                                    form {
+                                        slug
+                                        meta
+                                    }
+                                    answers(questions: ["gemeinde"]) {
+                                        edges {
+                                            node {
+                                            id
+                                            question {
+                                                slug
+                                            }
+                                            ...on StringAnswer {
+                                                value
+                                            }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                    }
+                }
+            """,
+            {"instanceId": self.instance.pk},
+        )
+
+        documents = caluma_resp.json()["data"]["allDocuments"]["edges"]
+
+        data["caluma_document"] = next(
+            iter(
+                [
+                    document["node"]
+                    for document in (documents)
+                    if document["node"]["form"]["meta"].get("is-main-form", False)
+                ]
+            ),
+            None,
+        )
+
+        if not data["caluma_document"]:  # pragma: no cover
+            raise exceptions.ValidationError(
+                f"Could not find caluma main document for instance {self.instance.pk}"
+            )
+
+        data["caluma_municipality"] = next(
+            iter(
+                [
+                    answer["node"]["value"]
+                    for answer in data["caluma_document"]["answers"]["edges"]
+                    if answer["node"]["question"]["slug"] == "gemeinde"
+                ]
+            ),
+            None,
+        )
+
+        if not data["caluma_municipality"]:  # pragma: no cover
+            raise exceptions.ValidationError(
+                f"Could not find municipality in caluma document for instance {self.instance.pk}"
+            )
+
+        # TODO: reenable this when caluma document validity is fixed
+        # validity_resp = self._caluma_query(
+        #     """
+        #         query GetDocumentValidity($id: ID!) {
+        #             documentValidity(id: $id) {
+        #                 edges {
+        #                     node {
+        #                         id
+        #                         isValid
+        #                         errors {
+        #                             slug
+        #                             errorMsg
+        #                         }
+        #                     }
+        #                 }
+        #             }
+        #         }
+        #     """,
+        #     {"id": document["id"]},
+        # )
+
+        # validity = validity_resp.json()
+        # if validity["errors"] or not validity["edges"]["node"][0]["isValid"]:
+        #     errors = ", ".join(
+        #         map(lambda e: e["message"], validity["errors"])
+        #         or map(lambda e: e["errorMsg"], validity["edges"]["node"][0]["errors"])
+        #     )
+
+        #     raise exceptions.ValidationError(
+        #         f"Error while validating caluma document: {errors}"
+        #     )
+
+        return data
+
+    @transaction.atomic
+    def update(self, instance, validated_data):
+        request_logger.info(f"Submitting instance {instance.pk}")
+
+        previous_instance_state = instance.instance_state
+
+        instance.previous_instance_state = previous_instance_state
+        instance.instance_state = (
+            models.InstanceState.objects.get(name="subm")
+            if instance.instance_state.name == "new"
+            else previous_instance_state
+        )
+
+        instance.save()
+
+        InstanceService.objects.get_or_create(
+            instance=self.instance,
+            service_id=int(validated_data.get("caluma_municipality")),
+            active=1,
+            defaults={"activation_date": None},
+        )
+
+        self._set_submit_date(validated_data)
+
+        # send out emails upon submission
+        for notification_config in settings.APPLICATION["NOTIFICATIONS"]["SUBMIT"]:
+            self._notify_submit(**notification_config)
+
+        return instance
 
 
 class InstanceResponsibilitySerializer(
--- django/camac/instance/tests/snapshots/snap_test_instance_caluma.py
+++ django/camac/instance/tests/snapshots/snap_test_instance_caluma.py
@@ -82,3 +82,61 @@ snapshots['test_create_instance[work_item_resp0-201-new] 1'] = [
         }
     )
 ]
+
+snapshots['test_create_instance[new] 1'] = [
+    (
+        'http://caluma:8000/graphql/',
+        (
+        ),
+        {
+            'headers': {
+                'Authorization': b''
+            },
+            'json': {
+                'query': '''
+                query GetMainForm($slug: String!) {
+                    allForms(slug: $slug, metaValue: [{key: "is-main-form", value: true}]) {
+                        edges {
+                            node {
+                                slug
+                                meta
+                            }
+                        }
+                    }
+                }
+            ''',
+                'variables': {
+                    'slug': 'test'
+                }
+            }
+        }
+    ),
+    (
+        'http://caluma:8000/graphql/',
+        (
+        ),
+        {
+            'headers': {
+                'Authorization': b''
+            },
+            'json': {
+                'query': '''
+                mutation CreateDocument($input: SaveDocumentInput!) {
+                    saveDocument(input: $input) {
+                    document {
+                        id
+                        meta
+                    }
+                    }
+                }
+            ''',
+                'variables': {
+                    'input': {
+                        'form': 'test',
+                        'meta': '{"camac-instance-id": "XXX"}'
+                    }
+                }
+            }
+        }
+    )
+]
