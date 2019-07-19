0fc5538213de513dbaed101b49d897f0d6b8b6cf
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2019-07-19 15:06:12 +0200

    feat(be-portal): fetch caluma data sources from camac API

--- django/camac/user/filters.py
+++ django/camac/user/filters.py
@@ -1,4 +1,4 @@
-from django_filters.rest_framework import FilterSet
+from django_filters.rest_framework import BooleanFilter, FilterSet
 
 from . import models
 
@@ -7,3 +7,13 @@ class LocationFilterSet(FilterSet):
     class Meta:
         model = models.Location
         fields = ("name", "communal_federal_number")
+
+
+class ServiceFilterSet(FilterSet):
+    has_parent = BooleanFilter(
+        field_name="service_parent", lookup_expr="isnull", exclude=True
+    )
+
+    class Meta:
+        model = models.Service
+        fields = ("service_group", "has_parent")
