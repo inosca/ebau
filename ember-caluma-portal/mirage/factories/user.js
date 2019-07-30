fae0a4c77e026977dbcb239acbd43db551133f05
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2019-07-30 10:04:04 +0200

    chore(tests): add mirage api for testing the applicants feature

---
+++ ember-caluma-portal/mirage/factories/user.js
@@ -0,0 +1,8 @@
+import { Factory } from "ember-cli-mirage";
+import faker from "faker";
+
+export default Factory.extend({
+  surname: () => faker.name.firstName(),
+  name: () => faker.name.lastName(),
+  username: () => faker.random.uuid()
+});
---
+++ ember-caluma-portal/mirage/models/applicant.js
@@ -0,0 +1,7 @@
+import { Model, belongsTo } from "ember-cli-mirage";
+
+export default Model.extend({
+  user: belongsTo("user"),
+  invitee: belongsTo("user"),
+  instance: belongsTo("instance")
+});
