9d7481f7e9dc2d914c069b5d7f863ed97a4c5e12
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2019-08-23 11:04:33 +0200

    chore(be-portal): disable multilang for the production server

--- ember-caluma-portal/app/protected/controller.js
+++ ember-caluma-portal/app/protected/controller.js
@@ -1,6 +1,7 @@
 import Controller from "@ember/controller";
 import { inject as service } from "@ember/service";
 import config from "../config/environment";
+import { computed } from "@ember/object";
 
 const { environment, languages, fallbackLanguage } = config;
 
@@ -11,8 +12,15 @@ export default Controller.extend({
   languages,
   environment,
 
+  enableMultilang: computed(() => {
+    // TODO: remove this if the french translations are approved
+    return !/ebau.apps.be.ch$/.test(window.location.host);
+  }),
+
   actions: {
     setLanguage(language) {
+      if (!this.enableMultilang) return;
+
       if (languages.includes(language)) {
         localStorage.setItem("language", language);
 
