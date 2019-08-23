4357523a9ffd63e988e8c3fafe57e7387d52bc10
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2019-08-23 10:48:00 +0200

    chore(be-portal): minor multilang improvements
    * Use french logo if FR is the primary locale
    * Always use DE as fallback language
    * Remove unused assets

--- ember-caluma-portal/app/protected/controller.js
+++ ember-caluma-portal/app/protected/controller.js
@@ -2,22 +2,25 @@ import Controller from "@ember/controller";
 import { inject as service } from "@ember/service";
 import config from "../config/environment";
 
-const { environment, languages } = config;
+const { environment, languages, fallbackLanguage } = config;
 
 export default Controller.extend({
-  init() {
-    this._super(...arguments);
-
-    this.languages = ["de", "fr"];
-  },
   intl: service(),
   session: service(),
+
+  languages,
   environment,
+
   actions: {
     setLanguage(language) {
       if (languages.includes(language)) {
         localStorage.setItem("language", language);
-        this.intl.setLocale(localStorage.getItem("language"));
+
+        this.intl.setLocale([
+          localStorage.getItem("language"),
+          fallbackLanguage
+        ]);
+
         window.location.reload(true);
       }
     }
