inosca/ebau

commit 62749dc108fcad519415b8222efb00cc58c6ef67

feat(django): add new endpoint for services for the public and add back the old endpoint
Adds a new read only end point for services which only shows the name and adds the old endpoint back
to support editing of services

django/camac/user/filters.py
@@ -12,7 +12,7 @@ class LocationFilterSet(FilterSet):
         fields = ("name", "communal_federal_number")
 
 
-class ServiceFilterSet(FilterSet):
+class PublicServiceFilterSet(FilterSet):
     service_id = NumberMultiValueFilter()
     has_parent = BooleanFilter(
         field_name="service_parent", lookup_expr="isnull", exclude=True

django/camac/user/views.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.contrib.auth import get_user_model
 from rest_framework import generics, viewsets
 
@@ -37,26 +36,37 @@ class UserView(viewsets.ReadOnlyModelViewSet):
 
 
 class ServiceView(viewsets.ModelViewSet):
-    filterset_class = filters.ServiceFilterSet
     serializer_class = serializers.ServiceSerializer
     queryset = models.Service.objects.all()
-
-    def get_serializer_class(self):
-        application_name = settings.APPLICATION_NAME
-
-        if application_name == "kt_schwyz":
-            return serializers.SchwyzServiceSerializer
-
-        return serializers.ServiceSerializer
-
-    def get_queryset(self):
-        return models.Service.objects.filter(disabled=False).prefetch_related("groups")
 
     def has_destroy_permission(self):
         return False
 
     def has_object_update_permission(self, obj):
         return obj == self.request.group.service
+
+    @permission_aware
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.none()
+
+    def get_queryset_for_service(self):
+        return super().get_queryset()
+
+    def get_queryset_for_canton(self):
+        return super().get_queryset()
+
+    def get_queryset_for_municipality(self):
+        return super().get_queryset()
+
+
+class PublicServiceView(viewsets.ReadOnlyModelViewSet):
+    filterset_class = filters.PublicServiceFilterSet
+    serializer_class = serializers.PublicServiceSerializer
+    queryset = models.Service.objects.all()
+
+    def get_queryset(self):
+        return models.Service.objects.filter(disabled=False).prefetch_related("groups")
 
 
 class MeView(generics.RetrieveAPIView):

caluma/extensions/data_sources.py
@@ -16,7 +16,7 @@ class Municipalities(BaseDataSource):
     @data_source_cache(timeout=3600)
     def get_data(self, info):
         response = requests.get(
-            f"{camac_api}/api/v1/services?has_parent=0&service_group={SERVICE_GROUP_MUNICIPALITY}",
+            f"{camac_api}/api/v1/public-services?has_parent=0&service_group={SERVICE_GROUP_MUNICIPALITY}",
             headers={"Authorization": info.context.META.get("HTTP_AUTHORIZATION")},
         )
 
@@ -37,7 +37,7 @@ class Services(BaseDataSource):
     @data_source_cache(timeout=3600)
     def get_data(self, info):
         response = requests.get(
-            f"{camac_api}/api/v1/services?service_group={SERVICE_GROUP_SERVICE}",
+            f"{camac_api}/api/v1/public-services?service_group={SERVICE_GROUP_SERVICE}",
             headers={"Authorization": info.context.META.get("HTTP_AUTHORIZATION")},
         )
 

django/camac/document/tests/test_attachment.py
@@ -14,11 +14,11 @@ from .data import django_file
 @pytest.mark.parametrize(
     "role__name,instance__user,num_queries",
     [
-        ("Applicant", LazyFixture("admin_user"), 9),
-        ("Reader", LazyFixture("user"), 9),
-        ("Canton", LazyFixture("user"), 9),
-        ("Municipality", LazyFixture("user"), 9),
-        ("Service", LazyFixture("user"), 9),
+        ("Applicant", LazyFixture("admin_user"), 8),
+        ("Reader", LazyFixture("user"), 8),
+        ("Canton", LazyFixture("user"), 8),
+        ("Municipality", LazyFixture("user"), 8),
+        ("Service", LazyFixture("user"), 8),
     ],
 )
 def test_attachment_list(

django/camac/user/serializers.py
@@ -73,40 +73,15 @@ class LocationSerializer(serializers.ModelSerializer):
         fields = ("name", "communal_federal_number")
 
 
-class SchwyzServiceSerializer(MultilingualSerializer, serializers.ModelSerializer):
-    class Meta:
-        model = models.Service
-        fields = ("name", "email", "notification")
-        read_only_fields = ("name",)
-
-
 class ServiceSerializer(MultilingualSerializer, serializers.ModelSerializer):
-    email = serializers.SerializerMethodField()
-    notification = serializers.SerializerMethodField()
-
-    def _has_full_read_permission(self, service):
-        """
-        Check whether the user of the request is allowed to read all properties.
-
-        The user should only be able to read all properties if he belongs to
-        a group which has access to the requested service.
-        """
-        request = self.context["request"]
-
-        if not hasattr(request, "user_services"):
-            request.user_services = request.user.groups.values_list(
-                "service", flat=True
-            )
-
-        return service.pk in request.user_services
-
-    def get_email(self, service):
-        return service.email if self._has_full_read_permission(service) else None
-
-    def get_notification(self, service):
-        return service.notification if self._has_full_read_permission(service) else None
-
     class Meta:
         model = models.Service
         fields = ("name", "email", "notification")
         read_only_fields = ("name",)
+
+
+class PublicServiceSerializer(MultilingualSerializer, serializers.ModelSerializer):
+    class Meta:
+        model = models.Service
+        fields = ("name",)
+        resource_name = "public-service"

django/camac/user/tests/test_public_service.py
@@ -1,0 +1,25 @@
+import pytest
+from django.urls import reverse
+from rest_framework import status
+
+
+def test_public_service_list(admin_client, service, service_factory):
+    service_factory()
+
+    response = admin_client.get(reverse("publicservice-list"))
+
+    assert response.status_code == status.HTTP_200_OK
+
+    data = response.json()["data"]
+    assert len(data) == 2
+
+
+@pytest.mark.parametrize(
+    "service_t__name,service_t__language", [("je ne sais pas", "fr")]
+)
+def test_public_service_list_multilingual(admin_client, service_t, multilang):
+    url = reverse("publicservice-list")
+
+    response = admin_client.get(url, HTTP_ACCEPT_LANGUAGE=service_t.language)
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["data"][0]["attributes"]["name"] == service_t.name

django/camac/user/tests/test_service.py
@@ -3,69 +3,63 @@ from django.urls import reverse
 from rest_framework import status
 
 
-def find_service(id, data):
-    return next(filter(lambda i: int(i["id"]) == int(id), data))
+@pytest.mark.parametrize(
+    "role__name,size",
+    [("Applicant", 0), ("Service", 1), ("Canton", 1), ("Municipality", 1)],
+)
+def test_service_list(admin_client, service, size):
+    url = reverse("service-list")
 
-
-def test_service_list(admin_client, service, service_factory):
-    foreign_service = service_factory()
-
-    response = admin_client.get(reverse("service-list"))
-
+    response = admin_client.get(url)
     assert response.status_code == status.HTTP_200_OK
-
-    data = response.json()["data"]
-    assert len(data) == 2
-
-    assert find_service(service.pk, data)["attributes"]["email"] == service.email
-    assert find_service(foreign_service.pk, data)["attributes"]["email"] is None
-
-    assert find_service(foreign_service.pk, data)["attributes"]["notification"] is None
-    assert (
-        find_service(service.pk, data)["attributes"]["notification"]
-        == service.notification
-    )
-
-
-def test_service_update(admin_client, service, service_factory):
-    foreign_service = service_factory()
-
-    response = admin_client.patch(reverse("service-detail", args=[service.pk]))
-    response_foreign = admin_client.patch(
-        reverse("service-detail", args=[foreign_service.pk])
-    )
-
-    assert response.status_code == status.HTTP_200_OK
-    assert response_foreign.status_code == status.HTTP_403_FORBIDDEN
+    json = response.json()
+    assert len(json["data"]) == size
+    if size > 0:
+        assert json["data"][0]["attributes"]["name"] == service.name
 
 
 @pytest.mark.parametrize(
     "role__name,status_code",
     [
+        ("Applicant", status.HTTP_404_NOT_FOUND),
         ("Municipality", status.HTTP_200_OK),
         ("Canton", status.HTTP_200_OK),
         ("Service", status.HTTP_200_OK),
     ],
 )
-def test_schwyz_service_update(admin_client, settings, service, status_code):
-    settings.APPLICATION_NAME = "kt_schwyz"
+def test_service_update(admin_client, service, status_code):
     url = reverse("service-detail", args=[service.pk])
     response = admin_client.patch(url)
     assert response.status_code == status_code
 
 
-def test_service_delete(admin_client, service):
+@pytest.mark.parametrize(
+    "role__name,status_code",
+    [
+        ("Applicant", status.HTTP_403_FORBIDDEN),
+        ("Municipality", status.HTTP_403_FORBIDDEN),
+        ("Canton", status.HTTP_403_FORBIDDEN),
+        ("Service", status.HTTP_403_FORBIDDEN),
+    ],
+)
+def test_service_delete(admin_client, service, status_code):
     url = reverse("service-detail", args=[service.pk])
     response = admin_client.delete(url)
-    assert response.status_code == status.HTTP_403_FORBIDDEN
+    assert response.status_code == status_code
 
 
 @pytest.mark.parametrize(
     "service_t__name,service_t__language", [("je ne sais pas", "fr")]
 )
-def test_service_list_multilingual(admin_client, service_t, multilang):
+@pytest.mark.parametrize(
+    "role_t__name,size", [("Applicant", 0), ("Canton", 1), ("Service", 1)]
+)
+def test_service_list_multilingual(admin_client, service_t, size, multilang):
     url = reverse("service-list")
 
     response = admin_client.get(url, HTTP_ACCEPT_LANGUAGE=service_t.language)
     assert response.status_code == status.HTTP_200_OK
-    assert response.json()["data"][0]["attributes"]["name"] == service_t.name
+    json = response.json()
+    assert len(json["data"]) == size
+    if size > 0:
+        assert json["data"][0]["attributes"]["name"] == service_t.name

django/camac/user/urls.py
@@ -8,6 +8,7 @@ r = SimpleRouter(trailing_slash=False)
 r.register(r"locations", views.LocationView)
 r.register(r"roles", views.RoleView)
 r.register(r"services", views.ServiceView)
+r.register(r"public-services", views.PublicServiceView, basename="publicservice")
 r.register(r"users", views.UserView)
 
 urlpatterns = [url(r"^me", views.MeView.as_view(), name="me")]

commit c978d56243bb2664b9e348ad448887c443f867f7

fix(services): add service_id filter on services API
This fixes a regression introduced in 62749dc108fcad519415b8222efb00cc58c6ef67

django/camac/user/filters.py
@@ -13,14 +13,21 @@ class LocationFilterSet(FilterSet):
 
 
 class PublicServiceFilterSet(FilterSet):
-    service_id = NumberMultiValueFilter()
     has_parent = BooleanFilter(
         field_name="service_parent", lookup_expr="isnull", exclude=True
     )
 
     class Meta:
         model = models.Service
-        fields = ("service_group", "has_parent", "service_id")
+        fields = ("service_group", "has_parent")
+
+
+class ServiceFilterSet(FilterSet):
+    service_id = NumberMultiValueFilter()
+
+    class Meta:
+        model = models.Service
+        fields = ("service_id",)
 
 
 class UserFilterSet(FilterSet):

django/camac/user/views.py
@@ -36,6 +36,7 @@ class UserView(viewsets.ReadOnlyModelViewSet):
 
 
 class ServiceView(viewsets.ModelViewSet):
+    filterset_class = filters.ServiceFilterSet
     serializer_class = serializers.ServiceSerializer
     queryset = models.Service.objects.all()
 