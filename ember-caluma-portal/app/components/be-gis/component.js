0f226433f3287879070fceec11ac2e7908eae080
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2019-10-11 13:29:56 +0200

    fix(be-gis): fix map component to work with the new/old API

--- ember-caluma-portal/app/components/be-gis/component.js
+++ ember-caluma-portal/app/components/be-gis/component.js
@@ -301,7 +301,7 @@ export default Component.extend(ComponentQueryManager, {
       let projectStatus = coords.keyvalue[project_status_index];
 
       // Keep the value only if the status is "valid"
-      if (projectStatus === "0") {
+      if (["0", "gültig", "valable"].includes(projectStatus)) {
         let parcel_number = "";
         let baurecht_number = "";
 
