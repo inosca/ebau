a430ae31e2b01abac7622e531240bb789954ef36
Patrick Winter <patrick.winter@adfinis-sygroup.ch>
2019-10-23 15:25:13 +0200

    Revert "Merge branch 'fix_migration' into 'master'"
    This reverts merge request !1883

--- django/camac/applicants/models.py
+++ django/camac/applicants/models.py
@@ -22,7 +22,7 @@ class Applicant(models.Model):
         null=True,
     )
     created = models.DateTimeField(db_column="CREATED", auto_now=True)
-    email = models.EmailField(blank=True, null=True)
+    email = models.EmailField()
 
     class Meta:
         managed = True
