fe873f852eae8dcbc45987ab029de060fb631b78
David Vogt <david.vogt@adfinis-sygroup.ch>
2019-10-30 09:38:13 +0100

    feat(ech0211): create "status notification" on SB1, SB2

--- django/camac/echbern/signals.py
+++ django/camac/echbern/signals.py
@@ -1,3 +1,6 @@
 import django.dispatch
 
 instance_submitted = django.dispatch.Signal(providing_args=["instance", "group_pk"])
+
+sb1_submitted = django.dispatch.Signal(providing_args=["instance", "group_pk"])
+sb2_submitted = django.dispatch.Signal(providing_args=["instance", "group_pk"])
