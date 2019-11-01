8618e4d5ddd5f015ce10f0880351ae741d928fd6
Fabio Ambauen <fabio.ambauen@adfinis-sygroup.ch>
2019-11-01 10:02:21 +0100

    chore(ech): add pretty_print method to Message model
    This commit adds a convenience method `pretty_print()` to the `Message`
    model. This method can be handy when developing.

--- django/camac/echbern/models.py
+++ django/camac/echbern/models.py
@@ -1,6 +1,7 @@
 from uuid import uuid4
 
 from django.db import models
+from lxml import etree
 
 
 class Message(models.Model):
@@ -8,6 +9,15 @@ class Message(models.Model):
     body = models.TextField(help_text="XML body")
     created_at = models.DateTimeField(auto_now_add=True)
     receiver = models.ForeignKey("user.service", on_delete=models.PROTECT)
+
+    def pretty_print(self):  # pragma: no cover
+        """
+        Pretty print the XML body.
+
+        This is a convenience method for testing.
+        """
+        root = etree.fromstring(self.body)
+        print(etree.tostring(root, pretty_print=True).decode())
 
     class Meta:
         managed = True
