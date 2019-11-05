50c8872a4ec66f1afc64e6596a5ce66373980361
David Vogt <david.vogt@adfinis-sygroup.ch>
2019-11-05 17:51:10 +0100

    feat(ech-0211): send notification to ng api when "nachforderung" is uploaded
    NG API has a admin-client only endpoint that can be used to trigger an eCH-0211
    event when subsequent data is submitted by the requestor.

--- caluma/extensions/validations.py
+++ caluma/extensions/validations.py
@@ -5,8 +5,22 @@
 # Currently, this is only a sketch of what needs to be done, and is only here
 # so it doesn't get forgotten.
 
+
+import requests
+
 from caluma.core.validations import BaseValidation, validation_for
+from caluma.form.models import Document
 from caluma.form.schema import SaveDocumentAnswer
+
+from . import common
+
+
+def _send_notification_to_ng(instance_id, event_name, info):
+    requests.post(
+        f"{common.CAMAC_NG_URL}/ech/v1/event/{instance_id}/{event_name}",
+        headers={"Authorization": f"Bearer {common.get_admin_token()}"},
+    )
+    pass
 
 
 class CustomValidation(BaseValidation):
@@ -14,4 +28,14 @@ class CustomValidation(BaseValidation):
     def validate_answer_mutation(self, mutation, data, info):
         # TODO (validation SaveAnswer mutation): check if "gesuchsteller" email
         # question. If yes, validate email exists in NG user database
+
+        if (
+            data["question"].slug == "nfd-tabelle-status"
+            and data["value"] == "nfd-tabelle-status-beantwortet"
+        ):
+            main_doc = Document.objects.get(pk=data["document"].family)
+            print(main_doc.meta)
+            _send_notification_to_ng(
+                main_doc.meta["camac-instance-id"], "FileSubsequently", info
+            )
         return data
