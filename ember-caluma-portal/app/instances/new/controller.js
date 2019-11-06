80bf875daa4e75b04f552d9247bb8cd6d0df5581
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2019-11-06 10:52:56 +0100

    chore(be-portal): update dependencies including caluma
    * Update caluma container to 3.0.0
    * Update ember-caluma to 3.0.0
    * Update ember to latest LTS version 3.12
    * Update remaining portal dependencies
    * Refactor deprecated usage of query manager mixins to macro
    * Remove deprecated usage of `ember-cli-htmlbars-inline-precompile`

--- ember-caluma-portal/app/instances/new/controller.js
+++ ember-caluma-portal/app/instances/new/controller.js
@@ -1,12 +1,14 @@
 import Controller from "@ember/controller";
 import { task } from "ember-concurrency";
 import { inject as service } from "@ember/service";
-import { ObjectQueryManager } from "ember-apollo-client";
+import { queryManager } from "ember-apollo-client";
 
 import getRootFormsQuery from "ember-caluma-portal/gql/queries/get-root-forms";
 
-export default Controller.extend(ObjectQueryManager, {
+export default Controller.extend({
   fetch: service(),
+
+  apollo: queryManager(),
 
   selectedForm: null,
 
