b221290963b1623737243d49fb0cee43efccd16b
Fabio Ambauen <fabio.ambauen@adfinis-sygroup.ch>
2019-11-11 08:51:17 +0100

    fic(ech): fix typo

--- django/camac/echbern/models.py
+++ django/camac/echbern/models.py
@@ -8,7 +8,7 @@ class Message(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     body = models.TextField(help_text="XML body")
     created_at = models.DateTimeField(auto_now_add=True)
-    receiver = models.ForeignKey("user.service", on_delete=models.PROTECT)
+    receiver = models.ForeignKey("user.Service", on_delete=models.PROTECT)
 
     def pretty_print(self):  # pragma: no cover
         """
