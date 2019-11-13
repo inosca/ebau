00eb411244b2c99c54bc432f6a51e099ea0a4a0c
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2019-11-13 16:14:25 +0100

    feat(be-portal): refactor and improve claims workflow

---
+++ ember-caluma-portal/app/components/be-claims-form/edit/component.js
@@ -0,0 +1,154 @@
+import Component from "@ember/component";
+import { computed, action } from "@ember/object";
+import { task, dropTask } from "ember-concurrency-decorators";
+import { inject as service } from "@ember/service";
+import { all } from "rsvp";
+import moment from "moment";
+import { NFD_ATTACHMENT_SECTION } from "ember-caluma-portal/components/be-claims-form/component";
+
+const NOTIFICATION_TEMPLATE_ID = 32;
+const ALLOWED_MIMETYPES = ["image/png", "image/jpeg", "application/pdf"];
+
+export default class BeClaimsFormEditComponent extends Component {
+  @service notification;
+  @service intl;
+  @service fetch;
+  @service store;
+
+  init() {
+    super.init(...arguments);
+
+    this.setProperties({
+      files: [],
+      file: null,
+      selectedTags: [],
+      allowedMimetypes: ALLOWED_MIMETYPES
+    });
+  }
+
+  @computed("claim.comment.answer.value.length", "files.length")
+  get canSubmit() {
+    return this.get("claim.comment.answer.value.length") || this.files.length;
+  }
+
+  @action
+  addFile(file) {
+    if (!ALLOWED_MIMETYPES.includes(file.blob.type)) {
+      this.notification.danger(this.intl.t("documents.wrongMimeType"));
+
+      return;
+    }
+
+    this.set("file", file);
+  }
+
+  @action
+  confirmFile() {
+    this.setProperties({
+      files: [
+        ...this.files,
+        {
+          file: this.file,
+          tags: this.selectedTags
+        }
+      ],
+      file: null,
+      selectedTags: []
+    });
+  }
+
+  @dropTask
+  *submit() {
+    if (!this.canSubmit) return;
+
+    try {
+      yield all(
+        this.files.map(async ({ file, tags }) => {
+          await this.uploadFile.perform(file, tags);
+        })
+      );
+
+      yield this.updateClaim.perform();
+      yield this.notifyMunicipality.perform();
+
+      this.onCancel();
+    } catch (error) {
+      this.notification.danger(this.intl.t("claims.error"));
+    }
+  }
+
+  @task
+  *uploadFile(file, tags) {
+    const formData = new FormData();
+
+    formData.append("instance", this.instanceId);
+    formData.append("attachment_sections", NFD_ATTACHMENT_SECTION);
+    formData.append("path", file.blob, file.name);
+    formData.append(
+      "context",
+      JSON.stringify({
+        tags: tags.map(({ slug }) => slug),
+        claimId: this.claim.id
+      })
+    );
+
+    const response = yield this.fetch.fetch("/api/v1/attachments", {
+      method: "post",
+      body: formData,
+      headers: {
+        "content-type": undefined
+      }
+    });
+
+    if (!response.ok) {
+      throw new Error(yield response.json());
+    }
+
+    const { data } = yield response.json();
+
+    this.store.push(this.store.normalize("attachment", data));
+
+    this.claim.notifyPropertyChange("attachments");
+  }
+
+  @task
+  *updateClaim() {
+    this.set("claim.status.answer.value", "nfd-tabelle-status-beantwortet");
+    this.set("claim.answered.answer.value", moment().format("YYYY-MM-DD"));
+
+    yield all(
+      [this.claim.answered, this.claim.status, this.claim.comment].map(
+        async field => {
+          await field.validate.perform();
+          await field.save.perform();
+        }
+      )
+    );
+  }
+
+  @task
+  *notifyMunicipality() {
+    yield this.fetch.fetch(
+      `/api/v1/notification-templates/${NOTIFICATION_TEMPLATE_ID}/sendmail`,
+      {
+        method: "post",
+        body: JSON.stringify({
+          data: {
+            type: "notification-template-sendmails",
+            attributes: {
+              "recipient-types": ["leitbehoerde"]
+            },
+            relationships: {
+              instance: {
+                data: {
+                  type: "instances",
+                  id: this.instanceId
+                }
+              }
+            }
+          }
+        })
+      }
+    );
+  }
+}
