49caab05e1a649a882cca83f549d0a4f7f94ef1e
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2019-11-13 16:14:25 +0100

    feat(be-portal): refactor and improve claims workflow

--- ember-caluma-portal/tests/integration/components/be-claims-form/edit/component-test.js
+++ ember-caluma-portal/tests/integration/components/be-claims-form/edit/component-test.js
@@ -1,24 +1,24 @@
 import { module, skip } from "qunit";
 import { setupRenderingTest } from "ember-qunit";
 import { render } from "@ember/test-helpers";
-import { hbs } from "ember-cli-htmlbars";
+import hbs from "htmlbars-inline-precompile";
 
-module("Integration | Component | be-claims-documents", function(hooks) {
+module("Integration | Component | be-claims-form/edit", function(hooks) {
   setupRenderingTest(hooks);
 
   skip("it renders", async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`{{be-claims-documents}}`);
+    await render(hbs`{{be-claims-form/edit}}`);
 
     assert.equal(this.element.textContent.trim(), "");
 
     // Template block usage:
     await render(hbs`
-      {{#be-claims-documents}}
+      {{#be-claims-form/edit}}
         template block text
-      {{/be-claims-documents}}
+      {{/be-claims-form/edit}}
     `);
 
     assert.equal(this.element.textContent.trim(), "template block text");
