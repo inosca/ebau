c9c5b5241f2e2995a43718682369119578567fa2
Fabio Ambauen <fabio.ambauen@adfinis-sygroup.ch>
2019-11-14 14:17:13 +0100

    feat(ech): task mailing
    This commit implements sending mails for tasks triggered by eCH message.
    This implementation provides you with the same potential race conditions
    as the one currently in php.
    
    Getting rid of them here means first we need to get rid of them in php.

--- django/camac/echbern/signals.py
+++ django/camac/echbern/signals.py
@@ -1,12 +1,18 @@
 import django.dispatch
 
-instance_submitted = django.dispatch.Signal(providing_args=["instance", "group_pk"])
+instance_submitted = django.dispatch.Signal(
+    providing_args=["instance", "user_pk", "group_pk"]
+)
 
-sb1_submitted = django.dispatch.Signal(providing_args=["instance", "group_pk"])
-sb2_submitted = django.dispatch.Signal(providing_args=["instance", "group_pk"])
+sb1_submitted = django.dispatch.Signal(
+    providing_args=["instance", "user_pk", "group_pk"]
+)
+sb2_submitted = django.dispatch.Signal(
+    providing_args=["instance", "user_pk", "group_pk"]
+)
 
-task_send = django.dispatch.Signal(providing_args=["instance", "group_pk"])
+task_send = django.dispatch.Signal(providing_args=["instance", "user_pk", "group_pk"])
 
 accompanying_report_send = django.dispatch.Signal(
-    providing_args=["instance", "group_pk", "attachments"]
+    providing_args=["instance", "user_pk", "group_pk", "attachments"]
 )
