624da1aa5e5d1fa6dd35b9c153b207ad5d6d8965
Fabio Ambauen <fabio.ambauen@adfinis-sygroup.ch>
2019-11-25 11:55:11 +0100

    feat(ech): add NoticeKindOfProceedingsSendHandler
    This commit handles incoming `noticeKindOfProceedings` events.

--- django/camac/echbern/signals.py
+++ django/camac/echbern/signals.py
@@ -11,6 +11,10 @@ sb2_submitted = django.dispatch.Signal(
     providing_args=["instance", "user_pk", "group_pk"]
 )
 
+circulation_started = django.dispatch.Signal(
+    providing_args=["instance", "user_pk", "group_pk"]
+)
+
 task_send = django.dispatch.Signal(providing_args=["instance", "user_pk", "group_pk"])
 
 accompanying_report_send = django.dispatch.Signal(
