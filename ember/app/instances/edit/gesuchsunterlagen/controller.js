82970be5e52d16dff87ad720175dafe01a1cb0fe
Yelinz <yelin.zhang@adfinis-sygroup.ch>
2019-12-02 08:49:30 +0100

    feat(ember): add info pop up to gesuchsunterlagen
    update ember-uikit version

---
+++ ember/app/instances/edit/gesuchsunterlagen/controller.js
@@ -0,0 +1,9 @@
+import Controller from "@ember/controller";
+
+export default Controller.extend({
+  actions: {
+    hideModal() {
+      localStorage.setItem("hideDocumentInfo", this.hideDocumentInfo);
+    }
+  }
+});
