ccd4e337645e53de9b0b93685a7fb0c6efa208af
Yelinz <yelin.zhang@adfinis-sygroup.ch>
2019-12-20 13:21:21 +0100

    fix(django): add override fields to template merge

--- django/camac/settings.py
+++ django/camac/settings.py
@@ -145,6 +145,12 @@ APPLICATIONS = {
             "PUBLICATION": 15,
             "START_CIRC": 44,
         },
+        "QUESTIONS_WITH_OVERRIDE": [
+            "bezeichnung",
+            "bauherrschaft",
+            "projektverfasser-planer",
+            "grundeigentumerschaft",
+        ],
     },
     "kt_bern": {
         "ROLE_PERMISSIONS": {
