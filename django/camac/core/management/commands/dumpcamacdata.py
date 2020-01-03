62dfcdc4bcde41189771b4fd784562ca2b9f8b3b
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2020-01-03 12:49:41 +0100

    fix(dev): fix development setup
    * Move caluma config to django app
      * Adjust loadconfig to load caluma data and config
      * Adjust dumpdata and dumpconfig to dump caluma data and config
    * Reduce make commands to `flush` and `loadconfig`
    * Fix `flush` command
    * Fix docker-compose dependencies
    * Update README

--- django/camac/core/management/commands/dumpcamacdata.py
+++ django/camac/core/management/commands/dumpcamacdata.py
@@ -8,7 +8,9 @@ from django.core.management.base import BaseCommand
 from .dumpconfig import (
     models_managed_by_customer,
     models_referencing_data,
+    models_referencing_data_caluma,
     pure_config_models,
+    pure_config_models_caluma,
 )
 
 
@@ -16,40 +18,38 @@ class Command(BaseCommand):
     help = "Output the camac data of the database as a fixture of the " "given format."
 
     def add_arguments(self, parser):
-        parser.add_argument("--output", type=str, help="Output")
-
-    def handle(self, *app_labels, **options):
-        options["indent"] = 2
-
-        exclude_models = [*pure_config_models, *models_referencing_data]
-
-        # respect customer specific excludes
-        exclude_models = [
-            m
-            for m in [*pure_config_models, *models_referencing_data]
-            if m not in models_managed_by_customer[settings.APPLICATION_NAME]
-        ]
-
-        options["exclude"] = exclude_models
-
-        # apps which include data models
-        apps = (
-            "circulation",
-            "core",
-            "document",
-            "instance",
-            "notification",
-            "user",
-            "applicants",
+        parser.add_argument(
+            "--output",
+            type=str,
+            default=settings.APPLICATION_DIR("data-caluma.json"),
+            help="Output file for camac data",
+        )
+        parser.add_argument(
+            "--output-caluma",
+            dest="output_caluma",
+            type=str,
+            default=settings.APPLICATION_DIR("data-caluma.json"),
+            help="Output file for caluma data",
         )
 
-        try:
-            output = options.pop("output")
-        except KeyError:  # pragma: no cover
-            output = settings.APPLICATION_DIR("data.json")
+        parser.add_argument(
+            "--caluma",
+            dest="caluma",
+            action="store_true",
+            help="Dump caluma data as well",
+        )
+        parser.add_argument(
+            "--no-caluma",
+            dest="caluma",
+            action="store_false",
+            help="Don't dump caluma data",
+        )
+
+        parser.set_defaults(caluma=settings.APPLICATION.get("FORM_BACKEND") == "caluma")
+
+    def dump_data(self, apps, exclude, output):
         tmp_output = io.StringIO()
-        options["stdout"] = tmp_output
-        call_command("dumpdata", *apps, **options)
+        call_command("dumpdata", *apps, stdout=tmp_output, indent=2, exclude=exclude)
         tmp_output.seek(0)
         data = json.load(tmp_output)
         data = sorted(data, key=lambda k: (k["model"], k["pk"]))
@@ -57,3 +57,32 @@ class Command(BaseCommand):
         with open(output, "w") as f:
             json.dump(data, f, indent=2, sort_keys=True)
             f.flush()
+
+    def handle(self, *app_labels, **options):
+        self.dump_data(
+            # apps which include data models
+            (
+                "circulation",
+                "core",
+                "document",
+                "instance",
+                "notification",
+                "user",
+                "applicants",
+            ),
+            # respect customer specific excludes
+            [
+                m
+                for m in pure_config_models + models_referencing_data
+                if m not in models_managed_by_customer[settings.APPLICATION_NAME]
+            ],
+            options["output"],
+        )
+
+        if options["caluma"]:
+            self.dump_data(
+                # apps which include data models
+                ("form",),
+                pure_config_models_caluma + models_referencing_data_caluma,
+                options["output_caluma"],
+            )
