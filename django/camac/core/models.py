b68a3851f7bfd01565fe2dec7d0e00448fe2f014
Wiktork <wiktor.kepczynski@adfinis-sygroup.ch>
2020-01-22 15:13:53 +0100

    feat(document-merge): added new placeholders for publication module refactor(documente-merge): use dataCamac.LANGUAGE in locale() in {{TODAY}}-placeholder

--- django/camac/core/models.py
+++ django/camac/core/models.py
@@ -3224,6 +3224,9 @@ class Publication(models.Model):
     start = models.DateField(db_column="START_DATE")
     end = models.DateField(db_column="END_DATE")
     text = models.TextField(db_column="TEXT")
+    publication_anzeiger_1 = models.DateField(db_column="PUBLICATION_DATE_1_ANZEIGER")
+    publication_anzeiger_2 = models.DateField(db_column="PUBLICATION_DATE_2_ANZEIGER")
+    publication_amtsblatt = models.DateField(db_column="PUBLICATION_DATE_AMTSBLATT")
 
     class Meta:
         managed = True
