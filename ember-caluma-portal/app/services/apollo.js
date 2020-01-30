59f151bbd731d4ed36c08112dc271df52515f797
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2020-01-30 09:35:37 +0100

    chore(be-portal): update ember-simple-auth-oidc

--- ember-caluma-portal/app/services/apollo.js
+++ ember-caluma-portal/app/services/apollo.js
@@ -1,43 +1,28 @@
-import { reads } from "@ember/object/computed";
 import { inject as service } from "@ember/service";
 import { setContext } from "apollo-link-context";
 import { onError } from "apollo-link-error";
 import ApolloService from "ember-apollo-client/services/apollo";
 import CalumaApolloServiceMixin from "ember-caluma/mixins/caluma-apollo-service-mixin";
 
-export default ApolloService.extend(CalumaApolloServiceMixin, {
-  session: service(),
-  router: service(),
-
-  token: reads("session.data.authenticated.access_token"),
-  language: reads("session.language"),
-  group: reads("session.group"),
+export default class CustomApolloService extends ApolloService.extend(
+  CalumaApolloServiceMixin
+) {
+  @service session;
 
   link(...args) {
-    const httpLink = this._super(...args);
+    const httpLink = super.link(...args);
 
-    const middleware = setContext(async (request, context) => ({
+    const middleware = setContext(async (_, context) => ({
       ...context,
-      headers: {
-        ...context.headers,
-        authorization: `Bearer ${this.token}`,
-        "accept-language": this.language,
-        ...(this.group ? { "x-camac-group": this.group } : {})
-      }
+      headers: { ...context.headers, ...this.session.headers }
     }));
 
     const afterware = onError(error => {
-      const { networkError } = error;
-
-      if (
-        networkError &&
-        networkError.statusCode === 401 &&
-        this.session.isAuthenticated
-      ) {
-        this.session.invalidate();
+      if (error.networkError.statusCode === 401) {
+        this.session.handleUnauthorized();
       }
     });
 
     return middleware.concat(afterware).concat(httpLink);
   }
-});
+}
