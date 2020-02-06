ca83cf8a9f1136bbd0b91729873e43d8f82af01d
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2020-02-06 16:44:44 +0100

    fix(caluma): use the camac group determination function in visibilities

--- django/camac/caluma/extensions/visibilities.py
+++ django/camac/caluma/extensions/visibilities.py
@@ -1,13 +1,16 @@
+from base64 import b64decode
+from json import loads
+
 from caluma.caluma_core.visibilities import BaseVisibility, filter_queryset_for
 from caluma.caluma_form import models as form_models, schema as form_schema
-from caluma.caluma_user.models import AnonymousUser
 from django.db.models import F, OuterRef, Q, Subquery
 
 from camac.constants.kt_bern import DASHBOARD_FORM_SLUG
 from camac.instance.filters import CalumaInstanceFilterSet
 from camac.instance.mixins import InstanceQuerysetMixin
 from camac.instance.models import Instance
-from camac.user.models import Group, User, UserGroup
+from camac.user.middleware import get_group
+from camac.user.models import User
 from camac.utils import filters
 
 
@@ -56,16 +59,34 @@ class CustomVisibility(BaseVisibility, InstanceQuerysetMixin):
             )
         )
 
-    def _get_group_from_query_params(self, info):
-        group_pk = info.context.META.get("HTTP_X_CAMAC_GROUP", None)
-        if not group_pk:
-            return None
-        return Group.objects.get(pk=group_pk)
-
     def get_base_queryset(self):
         """Overridden from InstanceQuerysetMixin to avoid the super().get_queryset() call."""
         instance_state_expr = self._get_instance_filter_expr("instance_state")
         return Instance.objects.all().select_related(instance_state_expr)
+
+    def _parse_token(self, token):
+        data = token.split(b".")[1]
+        data += b"=" * (-len(data) % 4)
+
+        return loads(b64decode(data))
+
+    def _get_camac_user(self, info):
+        try:
+            return User.objects.get(username=info.context.user.username)
+        except User.DoesNotExist:
+            return None
+
+    def _get_camac_group(self, info):
+        oidc_user = info.context.user
+
+        info.context.user = self.user
+        info.context.auth = self._parse_token(oidc_user.token)
+
+        group = get_group(info.context)
+
+        info.context.user = oidc_user
+
+        return group
 
     def _all_visible_instances(self, info):
         """Fetch visible camac instances and cache the result.
@@ -76,26 +97,20 @@ class CustomVisibility(BaseVisibility, InstanceQuerysetMixin):
         Return a list of instance identifiers.
         """
         result = getattr(info.context, "_visibility_instances_cache", None)
-        if result is not None:
+        if result is not None:  # pragma: no cover
             return result
 
-        user = info.context.user
-        if isinstance(user, AnonymousUser):
-            return Instance.queryset.none()
+        self.user = self._get_camac_user(info)
 
-        user = User.objects.get(username=user.username)
-        self.user = user.pk
+        if not self.user:
+            return Instance.objects.none()
 
-        self.group = self._get_group_from_query_params(info)
-        if not self.group:
-            self.group = UserGroup.objects.get(user=user, default_group=True).group
+        self.group = self._get_camac_group(info)
 
-        qs = self.get_queryset(self.group)
+        filtered = CalumaInstanceFilterSet(
+            data=filters(info), queryset=self.get_queryset(self.group)
+        )
 
-        filtered = CalumaInstanceFilterSet(data=filters(info), queryset=qs)
-
-        qs = filtered.qs
-
-        instance_ids = list(qs.values_list("pk", flat=True))
+        instance_ids = list(filtered.qs.values_list("pk", flat=True))
         setattr(info.context, "_visibility_instances_cache", instance_ids)
         return instance_ids
