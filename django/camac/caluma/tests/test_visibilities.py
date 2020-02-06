30dff55addc462f21eb2bc7d074314ce85404586
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2020-02-06 16:44:44 +0100

    fix(caluma): use the camac group determination function in visibilities

--- django/camac/caluma/tests/test_visibilities.py
+++ django/camac/caluma/tests/test_visibilities.py
@@ -1,27 +1,92 @@
-import pytest
+import functools
+from base64 import b64encode
+from json import dumps
 
-from camac.caluma.extensions.visibilities import CustomVisibility
+import pytest
+from caluma.caluma_user.models import OIDCUser
+from caluma.schema import schema
+
+
+@pytest.fixture
+def caluma_admin_user(settings, admin_user):
+    token_body = b64encode(dumps({"aud": admin_user.groups.first().name}).encode())
+
+    return OIDCUser(
+        b".".join([b"some", token_body, b"token"]), {"sub": admin_user.username}
+    )
+
+
+@pytest.fixture
+def caluma_admin_request(rf, caluma_admin_user):
+    request = rf.get("/graphql")
+    request.user = caluma_admin_user
+    return request
+
+
+@pytest.fixture
+def caluma_admin_schema_executor(caluma_admin_request):
+    return functools.partial(
+        schema.execute, context=caluma_admin_request, middleware=[]
+    )
 
 
 @pytest.mark.parametrize(
     "role__name,expected_count", [("Support", 3), ("Service", 1), ("Applicant", 0)]
 )
-def test_visibilities(
+def test_document_visibility(
     db,
     role,
     expected_count,
     instance_factory,
-    group_factory,
     activation_factory,
-    admin_info,
+    admin_user,
+    document_factory,
+    caluma_admin_schema_executor,
 ):
-    group = group_factory(pk=5, role=role)
-    admin_info.context.META["HTTP_X_CAMAC_GROUP"] = "5"
+    group = admin_user.groups.first()
+
     instance = instance_factory(group=group)
     activation_factory(circulation__instance=instance, service=group.service)
-    instance_factory(group=group)
-    instance_factory()
 
-    v = CustomVisibility()
-    qs = v._all_visible_instances(admin_info)
-    assert len(qs) == expected_count
+    for instance in [instance, instance_factory(group=group), instance_factory()]:
+        document_factory(meta={"camac-instance-id": instance.pk})
+
+    result = caluma_admin_schema_executor(
+        """
+        query {
+            allDocuments {
+                edges {
+                    node {
+                        id
+                    }
+                }
+            }
+        }
+    """
+    )
+
+    assert not result.errors
+    assert len(result.data["allDocuments"]["edges"]) == expected_count
+
+
+def test_document_visibility_no_camac_user(
+    db, caluma_admin_request, caluma_admin_schema_executor
+):
+    caluma_admin_request.user = OIDCUser("sometoken", {"sub": "inexistentusername"})
+
+    result = caluma_admin_schema_executor(
+        """
+        query {
+            allDocuments {
+                edges {
+                    node {
+                        id
+                    }
+                }
+            }
+        }
+    """
+    )
+
+    assert not result.errors
+    assert len(result.data["allDocuments"]["edges"]) == 0
