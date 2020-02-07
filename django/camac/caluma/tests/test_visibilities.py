9728ce167696596b025ed95b13830354879153d9
Christian Zosel <christian@zosel.ch>
2020-02-07 17:42:53 +0100

    fix(visibility): regression in caluma filters

--- django/camac/caluma/tests/test_visibilities.py
+++ django/camac/caluma/tests/test_visibilities.py
@@ -8,12 +8,14 @@ from caluma.schema import schema
 
 
 @pytest.fixture
-def caluma_admin_user(settings, admin_user):
+def token(admin_user):
     token_body = b64encode(dumps({"aud": admin_user.groups.first().name}).encode())
+    return b".".join([b"some", token_body, b"token"])
 
-    return OIDCUser(
-        b".".join([b"some", token_body, b"token"]), {"sub": admin_user.username}
-    )
+
+@pytest.fixture
+def caluma_admin_user(settings, token, admin_user):
+    return OIDCUser(token, {"sub": admin_user.username})
 
 
 @pytest.fixture
@@ -70,9 +72,9 @@ def test_document_visibility(
 
 
 def test_document_visibility_no_camac_user(
-    db, caluma_admin_request, caluma_admin_schema_executor
+    db, caluma_admin_request, caluma_admin_schema_executor, token
 ):
-    caluma_admin_request.user = OIDCUser("sometoken", {"sub": "inexistentusername"})
+    caluma_admin_request.user = OIDCUser(token, {"sub": "inexistentusername"})
 
     result = caluma_admin_schema_executor(
         """
@@ -90,3 +92,57 @@ def test_document_visibility_no_camac_user(
 
     assert not result.errors
     assert len(result.data["allDocuments"]["edges"]) == 0
+
+
+@pytest.mark.parametrize("role__name", ["Support"])
+def test_document_visibility_filter(
+    db,
+    rf,
+    role,
+    instance_factory,
+    activation_factory,
+    admin_user,
+    caluma_admin_user,
+    document_factory,
+    circulation_state,
+    circulation_state_factory,
+):
+    group = admin_user.groups.first()
+
+    instance1 = instance_factory(group=group)
+    activation_factory(
+        circulation__instance=instance1,
+        service=group.service,
+        circulation_state=circulation_state,
+    )
+
+    instance2 = instance_factory(group=group)
+    activation_factory(
+        circulation__instance=instance2,
+        service=group.service,
+        circulation_state=circulation_state_factory(),
+    )
+
+    for instance in [instance1, instance2]:
+        document_factory(meta={"camac-instance-id": instance.pk})
+
+    request = rf.get(
+        "/graphql",
+        **{"HTTP_X_CAMAC_FILTERS": f"circulation_state={circulation_state.pk}"},
+    )
+    request.user = caluma_admin_user
+    query = """
+        query {
+            allDocuments {
+                edges {
+                    node {
+                        id
+                    }
+                }
+            }
+        }
+    """
+    result = schema.execute(query, context=request, middleware=[])
+
+    assert not result.errors
+    assert len(result.data["allDocuments"]["edges"]) == 1
