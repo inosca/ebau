79a40bad0d2bfe943fd0c6c7a7a91d02cec9f397
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2020-03-31 14:22:51 +0200

    feat(be): add module for editing the own organisation

---
+++ ember-camac-ng/mirage/factories/service.js
@@ -0,0 +1,20 @@
+import { Factory } from "ember-cli-mirage";
+import faker from "faker";
+
+export default Factory.extend({
+  name: () => faker.company.companyName(),
+  description: () => faker.lorem.sentence(),
+  phone: () =>
+    [
+      "+41",
+      faker.random.number({ min: 10, max: 99 }),
+      faker.random.number({ min: 100, max: 999 }),
+      faker.random.number({ min: 10, max: 99 }),
+      faker.random.number({ min: 10, max: 99 })
+    ].join(" "),
+  zip: () => faker.random.number({ min: 1000, max: 9999 }),
+  city: () => faker.address.city(),
+  address: () => faker.address.streetAddress(),
+  email: () => faker.internet.email(),
+  website: () => faker.internet.url()
+});
---
+++ ember-camac-ng/tests/unit/ui/organisation/controller-test.js
@@ -0,0 +1,12 @@
+import { setupTest } from "ember-qunit";
+import { module, test } from "qunit";
+
+module("Unit | Controller | organisation", function(hooks) {
+  setupTest(hooks);
+
+  // Replace this with your real tests.
+  test("it exists", function(assert) {
+    const controller = this.owner.lookup("controller:organisation");
+    assert.ok(controller);
+  });
+});
