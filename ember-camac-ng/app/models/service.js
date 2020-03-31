d0f0e16b46f983a66cffcf2490ff0b89b32b3d62
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2020-03-31 14:22:51 +0200

    feat(be): add module for editing the own organisation

---
+++ ember-camac-ng/app/models/service.js
@@ -0,0 +1,12 @@
+import Model, { attr } from "@ember-data/model";
+
+export default class ServiceModel extends Model {
+  @attr("string") name;
+  @attr("string") description;
+  @attr("string") phone;
+  @attr("string") zip;
+  @attr("string") city;
+  @attr("string") address;
+  @attr("string") email;
+  @attr("string") website;
+}
