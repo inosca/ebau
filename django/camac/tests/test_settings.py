8f831398696d88344376cc826f749700367b7805
Stefan Borer <stefan.borer@adfinis-sygroup.ch>
2020-04-03 09:45:07 +0200

    fix(kt_bern): correct suggestions, remove unused questions
    Add test for suggestions.
    Some suggestions were referencing unused questions.
    We correct them to the right questions and drop the old questions from
    the config.

--- django/camac/tests/test_settings.py
+++ django/camac/tests/test_settings.py
@@ -1,3 +1,5 @@
+import json
+
 import environ
 import pytest
 
@@ -13,3 +15,24 @@ def test_admins():
 def test_invalid_admins():
     with pytest.raises(environ.ImproperlyConfigured):
         settings.parse_admins(["Test Example <test@example.com"])
+
+
+@pytest.mark.parametrize("app_name", ["kt_bern"])
+def test_verify_proposal_config(app_name, settings):
+    caluma_config_file = settings.ROOT_DIR + app_name + "config-caluma.json"
+    caluma_config = json.load(open(caluma_config_file, "r"))
+
+    proposal_config = settings.APPLICATIONS.get(app_name, {}).get("SUGGESTIONS", [])
+
+    questionoptions_caluma_config = {
+        (obj["fields"]["question"], obj["fields"]["option"])
+        for obj in caluma_config
+        if obj["model"] == "caluma_form.questionoption"
+    }
+
+    questionoptions_settings = {
+        (question, option) for question, option, _ in proposal_config
+    }
+
+    # all values must be in the config-caluma question.options
+    assert questionoptions_settings - questionoptions_caluma_config == set()
