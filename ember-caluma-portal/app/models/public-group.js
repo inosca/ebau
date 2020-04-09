539cb21897f24fa4445ed9f81c64533aee144ccc
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2020-04-09 09:32:49 +0200

    fix(be-portal): fix paper permissions

--- ember-caluma-portal/app/models/public-group.js
+++ ember-caluma-portal/app/models/public-group.js
@@ -1,7 +1,22 @@
 import Model, { attr, belongsTo } from "@ember-data/model";
+import { get } from "@ember/object";
+
+import config from "../config/environment";
 
 export default class PublicGroup extends Model {
   @attr("string") name;
   @belongsTo("public-role") role;
   @belongsTo("public-service") service;
+
+  get canCreatePaper() {
+    const roleId = parseInt(get(this, "role.id"));
+    const serviceGroupId = parseInt(get(this, "service.serviceGroup.id"));
+
+    return (
+      config.ebau.paperInstances.allowedGroups.roles.includes(roleId) &&
+      config.ebau.paperInstances.allowedGroups.serviceGroups.includes(
+        serviceGroupId
+      )
+    );
+  }
 }
