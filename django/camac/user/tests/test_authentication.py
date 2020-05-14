b5a00c44aa1d6112175852a12d47b634d0c3aab0
Patrick Winter <patrick.winter@adfinis-sygroup.ch>
2020-05-14 15:44:51 +0200

    test(authentication-ur): test that user is updated with keycloak data
    If OIDC_BOOTSTRAP_BY_EMAIL is set to true, then new keycloak users will be
    machted to to existing Camac user by their email address.

--- django/camac/user/tests/test_authentication.py
+++ django/camac/user/tests/test_authentication.py
@@ -141,3 +141,38 @@ def test_authenticate_header(rf, settings):
     request = rf.request()
     header = JSONWebTokenKeycloakAuthentication().authenticate_header(request)
     assert settings.KEYCLOAK_REALM in header
+
+
+def test_authenticate_bootstrap_by_mail(
+    rf,
+    mocker,
+    application_settings,
+    user_factory,
+):
+    token_value = {
+        "sub": "existing-already",
+        "email": "existing-guy@example.com",
+        "family_name": "Existing",
+        "given_name": "Guy",
+    },
+
+    # The user with the same email should be updated with keycloak information
+    user_factory(email=token_value['email'])
+
+    application_settings['OIDC_BOOTSTRAP_BY_EMAIL'] = True
+
+    decode_token = mocker.patch("keycloak.KeycloakOpenID.decode_token")
+    decode_token.return_value = token_value
+    mocker.patch("keycloak.KeycloakOpenID.certs")
+
+    userinfo = mocker.patch("keycloak.KeycloakOpenID.userinfo")
+    userinfo.return_value = token_value
+
+    request = rf.request(HTTP_AUTHORIZATION="Bearer some_token")
+    user, token = JSONWebTokenKeycloakAuthentication().authenticate(request)
+
+    assert user.username == token['sub']
+    assert user.name == token['family_name']
+    assert user.surname == token['given_name']
+    assert user.groups.count() == 0
+    assert decode_token.return_value == token
