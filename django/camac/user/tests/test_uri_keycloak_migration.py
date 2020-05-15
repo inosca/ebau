07bd7a24bce554cbfb12efc9f69ed6e22609768c
Patrick Winter <you@example.com>
2020-05-15 10:11:29 +0200

    feat(keycloak-ur): add portal user to applicant group on first login

--- django/camac/user/tests/test_uri_keycloak_migration.py
+++ django/camac/user/tests/test_uri_keycloak_migration.py
@@ -1,5 +1,5 @@
 """
-The tests in this module cover the migration logic required to migrate 
+The tests in this module cover the migration logic required to migrate
 Kanton Uri's identity management from camac to keycloak.
 """
 import pytest
@@ -46,18 +46,25 @@ def test_authenticate_bootstrap_by_mail(
     assert decode_token.return_value == token
 
 
-def test_authenticate_iweb_merge(
+def test_migrate_portal_user(
     rf,
     mocker,
     settings,
     application_settings,
     instance_portal_factory,
-    instance_factory
+    instance_factory,
+    group_factory
 ):
     """When an existing applicant logs in for the first time via keycloak all their
     instances should be transfered from the portal user to their newly created
     camac user stub.
     """
+
+
+    application_settings['URI_MIGRATE_PORTAL_USER'] = True
+
+    applicant_group = group_factory()
+    application_settings['APPLICANT_GROUP_ID'] = applicant_group.pk
 
     iweb_profile_id = "d12_1023"
 
@@ -83,16 +90,19 @@ def test_authenticate_iweb_merge(
     request = rf.request(HTTP_AUTHORIZATION="Bearer some_token")
     user, token = JSONWebTokenKeycloakAuthentication().authenticate(request)
 
-    # The instance user has been changed.
     instance.refresh_from_db()
-
-    # Migration status has been changed
     instance_portal.refresh_from_db()
 
+    # Ensure that the user is now owner of his submitted instances
     assert instance.user == user
     assert instance_portal.migrated == True
+
+    # Ensure the user object attrubtes were updated
     assert user.username == token['sub']
     assert user.name == token['family_name']
     assert user.surname == token['given_name']
-    assert user.groups.count() == 0
-    assert decode_token.return_value == token
+
+    # Ensure that the user was added to the applicant group
+    groups = user.groups.all()
+    assert len(groups)  == 1
+    assert groups[0] == applicant_group
