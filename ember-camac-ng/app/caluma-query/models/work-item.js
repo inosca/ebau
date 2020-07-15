3ba0fdb0a2793cc6d0ca810e083d08b59dc6d20b
Yelinz <yelin.zhang@adfinis.com>
2020-07-15 17:08:27 +0200

    feat(workitemlist): parse instances and create filters

--- ember-camac-ng/app/caluma-query/models/work-item.js
+++ ember-camac-ng/app/caluma-query/models/work-item.js
@@ -13,6 +13,25 @@ export default class CustomWorkItemModel extends WorkItemModel {
     return users;
   }
 
+  get instance() {
+    return this.store.peekRecord(
+      "instance",
+      this.raw.case.meta["camac-instance-id"]
+    );
+  }
+
+  get instanceIdentifier() {
+    return this.instance?.identifier || "-";
+  }
+
+  get instanceName() {
+    return this.instance?.name || this.raw.case.document.form.name;
+  }
+
+  get instanceLink() {
+    return `/index/redirect-to-instance-resource/instance-id/${this.raw.case.meta["camac-instance-id"]}`;
+  }
+
   get deadlineCss() {
     const remainingDays = moment().diff(this.raw.deadline, "days");
 
@@ -24,4 +43,28 @@ export default class CustomWorkItemModel extends WorkItemModel {
 
     return "";
   }
+
+  static fragment = `{
+    createdAt
+    createdByUser
+    createdByGroup
+    closedAt
+    closedByUser
+    closedByGroup
+    status
+    meta
+    addressedGroups
+    controllingGroups
+    assignedUsers
+    name
+    deadline
+    case {
+      meta
+      document {
+        form {
+          name
+        }
+      }
+    }
+  }`;
 }
