94c5ff9ba029557c64d50ef6ae7ea5fcf24d769e
Yelinz <yelin.zhang@adfinis.com>
2020-07-15 17:08:27 +0200

    feat(workitemlist): parse instances and create filters

--- ember-camac-ng/app/ui/workitem-list/controller.js
+++ ember-camac-ng/app/ui/workitem-list/controller.js
@@ -1,4 +1,5 @@
 import Controller from "@ember/controller";
+import { action, set } from "@ember/object";
 import { inject as service } from "@ember/service";
 import { tracked } from "@glimmer/tracking";
 import calumaQuery from "ember-caluma/caluma-query";
@@ -9,12 +10,22 @@ import saveWorkItem from "../../gql/mutations/save-workitem";
 
 async function processAll(workitems) {
   let users = [];
+  let instances = [];
+
   workitems.forEach(workitem => {
     users = [...new Set([...users, ...workitem.assignedUsers])];
+    instances = [
+      ...new Set([...instances, workitem.case.meta["camac-instance-id"]])
+    ];
   });
+
+  if (instances.length) {
+    this.store.query("instance", { id: instances.join(","), include: "form" });
+  }
   if (users.length) {
     await this.store.query("user", { id: users.join(",") });
   }
+
   return workitems;
 }
 
@@ -42,14 +53,30 @@ export default class WorkitemListController extends Controller {
   })
   workItemsQuery;
 
-  get entries() {
-    return (this.workItemsQuery.value || []).sortBy("deadline").reverse();
-  }
-
   @dropTask
   *fetchWorkitems() {
+    const filter = [];
+
+    if (this.filters.responsible === "own") {
+      filter.push({ assignedUsers: [this.shoebox.content.userId] });
+    } else {
+      filter.push({ assignedUsers: [] });
+    }
+
+    if (this.filters.status === "closed") {
+      filter.push({ status: "READY", invert: true });
+    } else {
+      filter.push({ status: "READY" });
+    }
+
+    if (this.filters.type === "control") {
+      filter.push({ controllingGroups: [this.shoebox.content.groupId] });
+    } else {
+      filter.push({ addressedGroups: [this.shoebox.content.groupId] });
+    }
+
     yield this.workItemsQuery.fetch({
-      filter: this.filter,
+      filter,
       order: this.order
     });
   }
@@ -67,5 +94,11 @@ export default class WorkitemListController extends Controller {
     } catch (error) {
       this.notifications.error(this.intl.t("workitemlist.saveError"));
     }
+  }
+
+  @action
+  updateFilter(type, value) {
+    set(this, `filters.${type}`, value);
+    this.fetchWorkitems.perform();
   }
 }
