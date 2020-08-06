1c42741b30023cc05a46a4fac0a0def938a23647
Yelinz <yelin.zhang@adfinis.com>
2020-08-06 16:06:56 +0200

    feat: rename module workitemlist to workitem

--- ember-camac-ng/tests/unit/ui/work-item-list-all/route-test.js
+++ ember-camac-ng/tests/unit/ui/work-item-list-all/route-test.js
@@ -1,11 +1,11 @@
 import { setupTest } from "ember-qunit";
 import { module, test } from "qunit";
 
-module("Unit | Route | workitem-list", function(hooks) {
+module("Unit | Route | work-item-list-all", function(hooks) {
   setupTest(hooks);
 
   test("it exists", function(assert) {
-    const route = this.owner.lookup("route:workitem-list");
+    const route = this.owner.lookup("route:work-item-list-all");
     assert.ok(route);
   });
 });
