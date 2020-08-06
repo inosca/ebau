ead3a057b1168ae2aa199941acdd1af201870988
Yelinz <yelin.zhang@adfinis.com>
2020-08-06 16:06:56 +0200

    feat: rename module workitemlist to workitem

---
+++ ember-camac-ng/app/ui/work-item-list-instance/route.js
@@ -0,0 +1,7 @@
+import Route from '@ember/routing/route';
+
+export default class WorkItemListInstanceRoute extends Route {
+    model({ id }) {
+        return id;
+      }
+}
--- ember-camac-ng/tests/unit/ui/work-item-list-all/controller-test.js
+++ ember-camac-ng/tests/unit/ui/work-item-list-all/controller-test.js
@@ -1,12 +1,12 @@
 import { setupTest } from "ember-qunit";
 import { module, test } from "qunit";
 
-module("Unit | Controller | workitem-list", function(hooks) {
+module("Unit | Controller | work-item-list-all", function(hooks) {
   setupTest(hooks);
 
   // Replace this with your real tests.
   test("it exists", function(assert) {
-    const controller = this.owner.lookup("controller:workitem-list");
+    const controller = this.owner.lookup("controller:work-item-list-all");
     assert.ok(controller);
   });
 });
