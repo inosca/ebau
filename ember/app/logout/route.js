03aed5201319f916ace3854a83c2a23ea0a1e795
Adrian Wittwer <adrian.wittwer@adfinis.com>
2020-08-18 16:29:07 +0200

    chore(oidc): update ember-simple-auth-oidc to v3.0.0

--- ember/app/logout/route.js
+++ ember/app/logout/route.js
@@ -1,11 +1,15 @@
 import Route from "@ember/routing/route";
 import { inject as service } from "@ember/service";
+import Ember from "ember";
 
 export default class LogoutRoute extends Route {
   @service session;
 
   constructor(...args) {
     super(...args);
-    this.session.invalidate();
+
+    if (!Ember.testing) {
+      this.session.singleLogout();
+    }
   }
 }
