b8f2bdfb841461c6d59309435ae9ee47f700557c
Yelinz <yelin.zhang@adfinis.com>
2020-09-01 18:30:28 +0200

    fix: rename ember work item route

---
+++ ember-camac-ng/tests/unit/ui/work-items/instance/new/route-test.js
@@ -0,0 +1,11 @@
+import { setupTest } from "ember-qunit";
+import { module, test } from "qunit";
+
+module("Unit | Route | work-item.new", function(hooks) {
+  setupTest(hooks);
+
+  test("it exists", function(assert) {
+    const route = this.owner.lookup("route:work-items.instance.new");
+    assert.ok(route);
+  });
+});
