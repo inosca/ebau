0f2321f57d174e3030307a89de339ea103781cae
Jonas Metzener <jonas.metzener@adfinis.com>
2020-09-30 16:12:57 +0200

    chore(be): make migration failsafe

--- django/camac/instance/migrations/0026_fix_rejected.py
+++ django/camac/instance/migrations/0026_fix_rejected.py
@@ -1,14 +1,26 @@
+from logging import getLogger
+
 from caluma.caluma_user.models import BaseUser
 from caluma.caluma_workflow.api import suspend_case
 from caluma.caluma_workflow.models import Case
+from django.conf import settings
 from django.db import migrations
+
+log = getLogger(__name__)
 
 
 def fix_rejected_instances(apps, schema_editor):
+    if settings.APPLICATION_NAME != "kt_bern":
+        return
+
     Instance = apps.get_model("instance", "Instance")
 
     for instance in Instance.objects.filter(instance_state__name="rejected"):
-        case = Case.objects.get(**{"meta__camac-instance-id": instance.pk})
+        try:
+            case = Case.objects.get(**{"meta__camac-instance-id": instance.pk})
+        except Case.DoesNotExist:
+            log.warning(f"No case for instance {instance.pk} found -- skipping")
+            continue
 
         if case.status != Case.STATUS_SUSPENDED:
             suspend_case(case, BaseUser())
