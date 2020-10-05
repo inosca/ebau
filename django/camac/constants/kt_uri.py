ec8b126cc8ec074bdc73ca9c2fafeeafc95e6717
Christian Zosel <christian@zosel.ch>
2020-10-05 10:25:37 +0200

    fix(ur): cqi config vor "vorhaben"

--- django/camac/constants/kt_uri.py
+++ django/camac/constants/kt_uri.py
@@ -27,7 +27,7 @@ INSTANCE_STATES_HIDDEN_FOR_KOOR = [INSTANCE_STATE_NEW, INSTANCE_STATE_NEW_PORTAL
 
 # Question identifiers (Chapter/Question/Item) for various information that we need
 # Format: List of 3-tuples to implement fallback
-CQI_FOR_VORHABEN = [(21, 97, 1)]  # TODO is this the right answer?
+CQI_FOR_VORHABEN = [(21, 98, 1)]
 CQI_FOR_PARZELLE = [(21, 91, 1), (101, 91, 1), (102, 91, 1)]
 CQI_FOR_GESUCHSTELLER = [(1, 23, 1)]
 CQI_FOR_NFD_COMPLETION_DATE = (41, 243, 1)
