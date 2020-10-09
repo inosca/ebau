03b59ba5d905924653547b182c27db64b3a8bd6c
Jonas Cosandey <jonas.cosandey@adfinis.com>
2020-10-09 14:27:14 +0200

    feat(search): add a search view to the cases list view

--- ember-camac-ng/app/decorators.js
+++ ember-camac-ng/app/decorators.js
@@ -1,4 +1,5 @@
 import { getOwner } from "@ember/application";
+import { tracked } from "@glimmer/tracking";
 
 export function loadingTask(target, property, desc) {
   const gen = desc.value;
@@ -20,4 +21,44 @@ export function loadingTask(target, property, desc) {
   return desc;
 }
 
-export default { loadingTask };
+export function objectFromQueryParams(...fields) {
+  // There is no easy way of putting the fileds directly into the queryParams array.
+  // So you need to set the array yourself.
+  return function(target, property, desc) {
+    fields.forEach(field => {
+      Object.defineProperty(
+        target,
+        field,
+        tracked(target, field, {
+          value: target[field],
+          enumerable: true,
+          configurable: true,
+          writable: true,
+          initializer: null
+        })
+      );
+    });
+
+    delete desc.writable;
+    delete desc.configurable;
+    delete desc.initializer;
+    delete desc.enumerable;
+
+    desc.get = function() {
+      return Object.assign(
+        ...fields.map(field => ({
+          [field]: this[field]
+        }))
+      );
+    };
+    desc.set = function(object) {
+      fields.forEach(key => {
+        this[key] = object[key];
+      });
+    };
+
+    return desc;
+  };
+}
+
+export default { loadingTask, objectFromQueryParams };
---
+++ ember-camac-ng/app/ui/components/case-table/component.js
@@ -0,0 +1,164 @@
+import { action } from "@ember/object";
+import { inject as service } from "@ember/service";
+import { htmlSafe } from "@ember/template";
+import Component from "@glimmer/component";
+import calumaQuery from "ember-caluma/caluma-query";
+import { allCases } from "ember-caluma/caluma-query/queries";
+
+export default class CaseTableComponent extends Component {
+  @service store;
+  @service intl;
+  @service shoebox;
+
+  @calumaQuery({ query: allCases, options: "options" }) casesQuery;
+
+  get options() {
+    return {
+      pageSize: 15,
+      processNew: cases => this.processNew(cases)
+    };
+  }
+
+  get gqlFilter() {
+    const filter = this.args.filter;
+    const availableFilterSet = {
+      buildingPermitType: {
+        hasAnswer: [
+          {
+            question: "building-permit-type",
+            value: filter.buildingPermitType
+          }
+        ]
+      },
+      dossierNumber: {
+        metaValue: [
+          {
+            key: "dossier-number",
+            value: filter.dossierNumber
+          }
+        ]
+      },
+      proposalDescription: {
+        hasAnswer: [
+          {
+            question: "proposal-description",
+            lookup: "ICONTAINS",
+            value: filter.proposalDescription
+          }
+        ]
+      },
+      createdBefore: { createdBefore: filter.createdBefore },
+      createdAfter: { createdAfter: filter.createdAfter }
+    };
+
+    return Object.entries(filter)
+      .filter(
+        ([key, value]) => Boolean(value) && Boolean(availableFilterSet[key])
+      )
+      .map(([key]) => availableFilterSet[key]);
+  }
+
+  get paginationInfo() {
+    return htmlSafe(
+      this.intl.t("global.paginationInfo", {
+        count: this.casesQuery.value.length,
+        total: this.casesQuery.totalCount
+      })
+    );
+  }
+
+  async processNew(cases) {
+    const instanceIds = cases.map(_case => _case.meta["camac-instance-id"]);
+
+    await this.store.query("instance", {
+      instance_id: instanceIds.join(","),
+      include: "instance_state,user"
+    });
+    return cases;
+  }
+
+  get tableHeaders() {
+    // TODO camac_legacy: Remove this in the future
+    switch (this.shoebox.role) {
+      case "municipality":
+        return [
+          "instanceId",
+          "dossierNr",
+          "form",
+          "municipality",
+          "user",
+          "applicant",
+          "intent",
+          "street",
+          "instanceState"
+        ];
+
+      case "coordination":
+        return [
+          "instanceId",
+          "dossierNr",
+          "coordination",
+          "coordinationShort",
+          "form",
+          "municipality",
+          "user",
+          "applicant",
+          "intent",
+          "street",
+          "instanceState"
+        ];
+      case "service":
+        return [
+          "instanceId",
+          "dossierNr",
+          "coordination",
+          "coordinationShort",
+          "form",
+          "municipality",
+          "applicant",
+          "intent",
+          "street",
+          "reason",
+          "caseStatus"
+        ];
+
+      default:
+        return [
+          "dossierNr",
+          "municipality",
+          "applicant",
+          "intent",
+          "street",
+          "parcel"
+        ];
+    }
+  }
+
+  @action
+  setup() {
+    this.casesQuery.fetch({
+      order: [{ meta: "camac-instance-id" }],
+      filter: this.gqlFilter,
+      queryOptions: {
+        context: {
+          headers: {
+            "x-camac-filters": `instance_state=${this.args.filter
+              .instanceState ?? ""}&location=${this.args.filter.municipality}`
+          }
+        }
+      }
+    });
+  }
+
+  @action
+  loadNextPage() {
+    this.casesQuery.fetchMore();
+  }
+
+  @action
+  redirectToCase(caseRecord) {
+    location.assign(
+      `/index/redirect-to-instance-resource/instance-id/${caseRecord.instanceId}/`
+    );
+  }
+}
