cd2f89cc44a32912250df9b86f1aa13da4aed05d
Christian Zosel <christian@zosel.ch>
2020-10-29 12:57:56 +0100

    feat(camac-api): add new filters for activation api

--- django/camac/circulation/tests/test_circulation.py
+++ django/camac/circulation/tests/test_circulation.py
@@ -20,10 +20,10 @@ def get_activation_work_item(case, activation_id):
 @pytest.mark.parametrize(
     "role__name,instance__user,num_queries",
     [
-        ("Applicant", LazyFixture("admin_user"), 10),
-        ("Canton", LazyFixture("user"), 10),
-        ("Municipality", LazyFixture("user"), 9),
-        ("Service", LazyFixture("user"), 9),
+        ("Applicant", LazyFixture("admin_user"), 11),
+        ("Canton", LazyFixture("user"), 11),
+        ("Municipality", LazyFixture("user"), 10),
+        ("Service", LazyFixture("user"), 10),
     ],
 )
 def test_circulation_list(
