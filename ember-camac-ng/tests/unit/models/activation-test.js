25ed37f4a2cc3568938ff29461d55a52a9d29468
Jonas Cosandey <jonas.cosandey@adfinis.com>
2020-10-30 17:16:59 +0100

    feat(dashboard): move dashboard into component

--- ember-camac-ng/tests/unit/models/activation-test.js
+++ ember-camac-ng/tests/unit/models/activation-test.js
@@ -1,13 +1,13 @@
-import { module, test } from 'qunit';
-import { setupTest } from 'ember-qunit';
+import { setupTest } from "ember-qunit";
+import { module, test } from "qunit";
 
-module('Unit | Model | activation', function(hooks) {
+module("Unit | Model | activation", function (hooks) {
   setupTest(hooks);
 
   // Replace this with your real tests.
-  test('it exists', function(assert) {
-    let store = this.owner.lookup('service:store');
-    let model = store.createRecord('activation', {});
+  test("it exists", function (assert) {
+    const store = this.owner.lookup("service:store");
+    const model = store.createRecord("activation", {});
     assert.ok(model);
   });
 });
