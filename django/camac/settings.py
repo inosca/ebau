5a730bc468903f7c9a796120671d94ea72792587
Jonas Lehmann <jonas.lehmann@adfinis.com>
2020-11-03 09:38:38 +0100

    feat(be): add two new decision types
    Add the new decision tpyes `Bewilligungspflichtig` and
    `Nicht Bewilligungspflichtig` for the `Entscheid über
    die Baubewilligungspflicht` form.

--- django/camac/settings.py
+++ django/camac/settings.py
@@ -503,7 +503,9 @@ APPLICATIONS = {
                 ],
             },
             "vorabklaerung-einfach": {
-                "forms": ["vorabklaerung-einfach"],
+                "forms": [
+                    "vorabklaerung-einfach",
+                ],
                 "template": "1-level",
                 "allgemeine_info": "allgemeine-informationen-vorabklaerung-form",
                 "givenName": "vorname-gesuchstellerin-vorabklaerung",
@@ -548,6 +550,40 @@ APPLICATIONS = {
             },
             "baupolizeiliches-verfahren": {
                 "forms": ["baupolizeiliches-verfahren"],
+                "template": "2-level",
+                "allgemeine_info": "allgemeine-angaben-kurz-formular",
+                "personalien": "personalien-kurz-formular",
+                "people_sources": {
+                    "personalien-gesuchstellerin": {
+                        "familyName": "name-gesuchstellerin",
+                        "givenName": "vorname-gesuchstellerin",
+                    },
+                    "personalien-vertreterin-mit-vollmacht": {
+                        "familyName": "name-vertreterin",
+                        "givenName": "vorname-vertreterin",
+                    },
+                    "personalien-grundeigentumerin": {
+                        "familyName": "name-grundeigentuemerin",
+                        "givenName": "vorname-grundeigentuemerin",
+                    },
+                    "personalien-gebaudeeigentumerin": {
+                        "familyName": "name-gebaeudeeigentuemerin",
+                        "givenName": "vorname-gebaeudeeigentuemerin",
+                    },
+                    "personalien-projektverfasserin": {
+                        "familyName": "name-projektverfasserin",
+                        "givenName": "vorname-projektverfasserin",
+                    },
+                },
+                "exclude_slugs": [
+                    "papierdossier",
+                    "projektaenderung",
+                    "8-freigabequittung",
+                    "karte",
+                ],
+            },
+            "klaerung-baubewilligungspflicht": {
+                "forms": ["klaerung-baubewilligungspflicht"],
                 "template": "2-level",
                 "allgemeine_info": "allgemeine-angaben-kurz-formular",
                 "personalien": "personalien-kurz-formular",
