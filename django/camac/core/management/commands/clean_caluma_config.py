29ea63cb5332ae71d1aa118e5a15d68ea0c21fdb
Jonas Metzener <jonas.metzener@adfinis.com>
2020-11-12 09:52:27 +0100

    feat(dev): improve clean config command
    - Add -c / --cascade option for deleting related data as well
    - Clean the config recursively (e.g if a form was deleted the questions
    in it should be checked too)

--- django/camac/core/management/commands/clean_caluma_config.py
+++ django/camac/core/management/commands/clean_caluma_config.py
@@ -1,4 +1,4 @@
-from caluma.caluma_form.models import Form, Option, Question
+from caluma.caluma_form.models import Answer, Document, Form, Option, Question
 from django.core.management.base import BaseCommand
 from django.db import transaction
 from django.db.models import Count
@@ -11,20 +11,82 @@ class Command(BaseCommand):
         parser.add_argument(
             "-d", "--dry", action="store_true", help="Dry run only, no changes"
         )
+        parser.add_argument(
+            "-c",
+            "--cascade",
+            dest="cascade",
+            action="store_true",
+            help="Cascade answers and documents of removed questions and forms",
+        )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.questions = []
+        self.answers = []
+        self.options = []
+        self.forms = []
+        self.documents = []
 
     @transaction.atomic
     def handle(self, *args, **options):
-        dry = options["dry"]
+        self.cascade = options["cascade"]
+
         sid = transaction.savepoint()
 
+        while any([self.clean_questions(), self.clean_options(), self.clean_forms()]):
+            pass
+
+        self.log((self.questions, "questions"), (self.answers, "answers"))
+        self.log((self.options, "options"))
+        self.log((self.forms, "forms"), (self.documents, "documents"))
+
+        if options["dry"]:
+            transaction.savepoint_rollback(sid)
+        else:
+            transaction.savepoint_commit(sid)
+
+    def log(self, blueprint_tuple, data_tuple=None):
+        blueprint_pks, blueprint_name = blueprint_tuple
+
+        if data_tuple:
+            data_pks, data_name = data_tuple
+
+        if self.cascade and data_tuple:
+            text = f"Deleted {len(blueprint_pks)} {blueprint_name} and {len(data_pks)} related {data_name}:"
+        else:
+            text = f"Deleted {len(blueprint_pks)} {blueprint_name}:"
+
+        self.stdout.write("")
+        self.stdout.write(self.style.SUCCESS(text))
+
+        if len(blueprint_pks):
+            self.stdout.write("")
+            self.stdout.write("\n".join([f"- {pk}" for pk in blueprint_pks]))
+
+    def clean_questions(self):
         questions = Question.objects.annotate(count=Count("forms__pk")).filter(count=0)
-        self._log(questions, "questions")
+        retval = questions.exists()
+        self.questions.extend(questions.values_list("pk", flat=True))
+
+        if self.cascade:
+            answers = Answer.objects.filter(question__in=questions)
+            self.answers.extend(answers.values_list("pk", flat=True))
+            answers.delete()
+
         questions.delete()
 
+        return retval
+
+    def clean_options(self):
         options = Option.objects.annotate(count=Count("questions__pk")).filter(count=0)
-        self._log(options, "options")
+        retval = options.exists()
+        self.options.extend(options.values_list("pk", flat=True))
         options.delete()
 
+        return retval
+
+    def clean_forms(self):
         row_forms = Question.objects.filter(row_form__isnull=False).values_list(
             "row_form_id", flat=True
         )
@@ -42,21 +104,14 @@ class Command(BaseCommand):
             )
             .filter(workflow_count=0, task_count=0)
         )
-        self._log(forms, "forms")
+        retval = forms.exists()
+        self.forms.extend(forms.values_list("pk", flat=True))
+
+        if self.cascade:
+            documents = Document.objects.filter(form__in=forms)
+            self.documents.extend(documents.values_list("pk", flat=True))
+            documents.delete()
+
         forms.delete()
 
-        if dry:
-            transaction.savepoint_rollback(sid)
-        else:
-            transaction.savepoint_commit(sid)
-
-    def _log(self, objects, name):
-        print(f"{objects.count()} {name} will be deleted:\n")
-        print(
-            "".join(
-                [
-                    f"- {pk}\n"
-                    for pk in objects.order_by("pk").values_list("pk", flat=True)
-                ]
-            )
-        )
+        return retval
