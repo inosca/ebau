945aabe244e7743e7d65e86953b742a7610bb4b8
Yelinz <yelin.zhang@adfinis.com>
2020-11-16 14:00:44 +0100

    feat(ember-camac-ng): add pagination to history module

--- ember-camac-ng/app/ui/history/controller.js
+++ ember-camac-ng/app/ui/history/controller.js
@@ -1,17 +1,35 @@
 import Controller from "@ember/controller";
+import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
+import { tracked } from "@glimmer/tracking";
 import { dropTask, lastValue } from "ember-concurrency-decorators";
 
 export default class HistoryController extends Controller {
   @service store;
 
-  @lastValue("fetchEntries") entries;
+  page = 1;
+  @tracked _entries = [];
+
+  get entries() {
+    return this._entries.concat(this.newEntries?.toArray());
+  }
+
+  @lastValue("fetchEntries") newEntries;
   @dropTask
   *fetchEntries() {
     return yield this.store.query("history-entry", {
       instance: this.model.id,
       include: "user",
       sort: "-created_at",
+      "page[number]": this.page,
+      "page[size]": 20,
     });
+  }
+
+  @action
+  loadMore() {
+    this.page += 1;
+    this._entries = this.entries;
+    this.fetchEntries.perform();
   }
 }
