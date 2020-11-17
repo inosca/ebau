46e279ebae4dbb6c72c2d31263f62feeef52d500
Jonas Metzener <jonas.metzener@adfinis.com>
2020-11-17 13:36:35 +0100

    fix(be): allow RSTAs to select disabled municipalities

--- django/camac/caluma/extensions/data_sources.py
+++ django/camac/caluma/extensions/data_sources.py
@@ -1,27 +1,55 @@
 from caluma.caluma_data_source.data_sources import BaseDataSource
 from caluma.caluma_data_source.utils import data_source_cache
+from django.core.cache import cache
+from django.utils.translation import gettext as _
 
+from camac.constants.kt_bern import SERVICE_GROUP_RSTA
 from camac.user.models import Service
+
+
+def get_municipality_label(service):
+    name = service.get_name().replace("Leitbehörde ", "")
+
+    if service.disabled:
+        suffix = _("not activated")
+
+        return f"{name} ({suffix})"
+
+    return name
 
 
 class Municipalities(BaseDataSource):
     info = "List of municipalities from Camac"
 
-    @data_source_cache(timeout=3600)
-    def get_data(self, info):
-        services = Service.objects.filter(
-            service_parent__isnull=True,
-            service_group__name="municipality",
-            disabled=False,
+    def get_data(self, user):
+        cache_key = f"data_source_{type(self).__name__}"
+        is_rsta = (
+            hasattr(user, "group")
+            and Service.objects.filter(
+                pk=user.group, service_group_id=SERVICE_GROUP_RSTA
+            ).exists()
         )
-        data = sorted(
+
+        if is_rsta:
+            cache_key += "_rsta"
+            filters = {}
+        else:
+            filters = {"disabled": False}
+
+        return cache.get_or_set(cache_key, lambda: self._get_data(filters), 3600)
+
+    def _get_data(self, filters):
+        services = Service.objects.filter(
+            service_parent__isnull=True, service_group__name="municipality", **filters
+        )
+
+        return sorted(
             [
-                [service.pk, service.get_name().replace("Leitbehörde ", "")]
+                [service.pk, get_municipality_label(service)]
                 for service in services.iterator()
             ],
             key=lambda x: x[1].casefold(),
         )
-        return data
 
 
 class Services(BaseDataSource):
--- django/camac/caluma/tests/test_data_sources.py
+++ django/camac/caluma/tests/test_data_sources.py
@@ -1,21 +1,52 @@
 import pytest
 
+from camac.constants.kt_bern import SERVICE_GROUP_RSTA
+
 from ..extensions.data_sources import Municipalities, Services
 
 
 @pytest.mark.parametrize(
-    "test_class,service_group_name,expected",
+    "test_class,service_group_name,expected,is_rsta",
     [
-        (Municipalities, "municipality", [[1, "service1"], [2, "service2"]]),
-        (Services, "service", [["1", "service1"], ["2", "service2"], ["-1", "Andere"]]),
+        (
+            Municipalities,
+            "municipality",
+            [[1, "service1"], [2, "service2 (nicht aktiviert)"]],
+            True,
+        ),
+        (Municipalities, "municipality", [[1, "service1"]], False),
+        (
+            Services,
+            "service",
+            [["1", "service1"], ["-1", "Andere"]],
+            True,
+        ),
+        (Services, "service", [["1", "service1"], ["-1", "Andere"]], False),
     ],
 )
-def test_data_sources(db, service_factory, test_class, service_group_name, expected):
-    service1 = service_factory(
-        pk=1, name="service1", disabled=False, service_group__name=service_group_name
-    )
+def test_data_sources(
+    db,
+    service_factory,
+    service_group_factory,
+    test_class,
+    service_group_name,
+    expected,
+    is_rsta,
+):
+    if is_rsta:
+        sg = service_group_factory(pk=SERVICE_GROUP_RSTA, name=service_group_name)
+        service1 = service_factory(
+            pk=1, name="service1", disabled=False, service_group=sg
+        )
+    else:
+        service1 = service_factory(
+            pk=1,
+            name="service1",
+            disabled=False,
+            service_group__name=service_group_name,
+        )
     service_factory(
-        pk=2, name="service2", disabled=False, service_group=service1.service_group
+        pk=2, name="service2", disabled=True, service_group=service1.service_group
     )
     service_factory(pk=3, name="service3", disabled=False, service_group__pk=666)
 
