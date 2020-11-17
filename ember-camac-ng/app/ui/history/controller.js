bb35ba430bfddcd1be030697d5f948ea28bd697f
Yelinz <yelin.zhang@adfinis.com>
2020-11-17 16:52:09 +0100

    refactor(history): improve code

--- ember-camac-ng/app/ui/history/controller.js
+++ ember-camac-ng/app/ui/history/controller.js
@@ -2,34 +2,32 @@ import Controller from "@ember/controller";
 import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
 import { tracked } from "@glimmer/tracking";
-import { dropTask, lastValue } from "ember-concurrency-decorators";
+import { dropTask } from "ember-concurrency-decorators";
 
 export default class HistoryController extends Controller {
   @service store;
 
   page = 1;
-  @tracked _entries = [];
+  @tracked entries = [];
+  @tracked hasNextPage = false;
 
-  get entries() {
-    return this._entries.concat(this.newEntries?.toArray());
-  }
-
-  @lastValue("fetchEntries") newEntries;
   @dropTask
   *fetchEntries() {
-    return yield this.store.query("history-entry", {
+    const entries = yield this.store.query("history-entry", {
       instance: this.model.id,
       include: "user",
       sort: "-created_at",
       "page[number]": this.page,
       "page[size]": 20,
     });
+
+    this.hasNextPage = Boolean(entries.links.next);
+    this.entries = [...this.entries, ...entries.toArray()];
   }
 
   @action
   loadMore() {
     this.page += 1;
-    this._entries = this.entries;
     this.fetchEntries.perform();
   }
 }
