706699be7d50c990a87d4e110b1a54a96723bbc7
Jonas Metzener <jonas.metzener@adfinis.com>
2020-12-03 15:22:14 +0100

    refactor(php): set ebau number with an API call
    This refactoring moves the logic of assigning an ebau number into the
    django API. This is necessary for a new module that helps the support by
    allowing changing of an ebau number. The validation of the ebau number
    is duplicated in PHP to keep the existing UX.

--- django/camac/core/utils.py
+++ django/camac/core/utils.py
@@ -1,9 +1,14 @@
+from typing import Callable
+
 from django.db.models import IntegerField
 from django.db.models.functions import Cast, Substr
 from django.utils import timezone
 
 from camac.constants.kt_bern import CHAPTER_EBAU_NR, QUESTION_EBAU_NR
-from camac.instance.models import Instance
+from camac.core.models import HistoryActionConfig
+from camac.core.translations import get_translations
+from camac.instance.models import HistoryEntry, HistoryEntryT, Instance
+from camac.user.models import User
 
 from .models import Answer
 
@@ -45,3 +50,41 @@ def save_ebau_nr_answer(instance: Instance, ebau_nr: str):
         item=1,
         defaults={"answer": ebau_nr},
     )
+
+
+def create_history_entry(
+    instance: Instance,
+    user: User,
+    text: str,
+    text_data: Callable[[str], dict] = lambda language: {},
+    history_type: str = HistoryActionConfig.HISTORY_TYPE_STATUS,
+) -> None:
+    """
+    Create a multilingual history entry for an instance.
+
+    The parameters `instance`, `user` and `text` are required. Optionally it
+    accepts a function `text_data` that takes the language as parameter and
+    should return a dictionary of data that will be formatted into the text.
+    Also, the `history_type` can be passed if it's anything other than a
+    status change.
+
+    >>> create_history_entry(
+    ...     instance,
+    ...     user,
+    ...     gettext_noop("Your text"),
+    ...     text_data_function,
+    ...     history_type
+    ... )
+    """
+
+    history = HistoryEntry.objects.create(
+        instance=instance,
+        created_at=timezone.now(),
+        user=user,
+        history_type=history_type,
+    )
+
+    for (language, text) in get_translations(text):
+        HistoryEntryT.objects.create(
+            history_entry=history, title=text % text_data(language), language=language
+        )
