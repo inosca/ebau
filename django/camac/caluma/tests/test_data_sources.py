86178338f143e0ded77d7f62971933d1fb484bee
Jonas Lehmann <jonas.lehmann@adfinis.com>
2020-12-07 17:05:06 +0100

    feat(be): change dynamic data source 'services'
    Show all municipalities and rstas in the `Durchgeführte
    Vorabklärungen` dropdown instead of sub-services.

--- django/camac/caluma/tests/test_data_sources.py
+++ django/camac/caluma/tests/test_data_sources.py
@@ -2,28 +2,28 @@ from collections import namedtuple
 
 import pytest
 
-from camac.constants.kt_bern import SERVICE_GROUP_RSTA
-
 from ..extensions.data_sources import Municipalities, Services
 
 
 @pytest.mark.parametrize(
-    "test_class,service_group_name,expected,is_rsta",
+    "test_class,expected,is_rsta",
     [
+        (Municipalities, [[1, "service1"]], False),
         (
             Municipalities,
-            "municipality",
-            [[1, "service1"], [2, "service2 (nicht aktiviert)"]],
+            [[2, "service2 (nicht aktiviert)"]],
             True,
         ),
-        (Municipalities, "municipality", [[1, "service1"]], False),
         (
             Services,
-            "service",
-            [["1", "service1"], ["-1", "Andere"]],
+            [["1", "service1"], ["3", "service3"], ["4", "service4"], ["-1", "Andere"]],
             True,
         ),
-        (Services, "service", [["1", "service1"], ["-1", "Andere"]], False),
+        (
+            Services,
+            [["1", "service1"], ["3", "service3"], ["4", "service4"], ["-1", "Andere"]],
+            False,
+        ),
     ],
 )
 def test_data_sources(
@@ -31,26 +31,31 @@ def test_data_sources(
     service_factory,
     service_group_factory,
     test_class,
-    service_group_name,
     expected,
     is_rsta,
 ):
     if is_rsta:
-        sg = service_group_factory(pk=SERVICE_GROUP_RSTA, name=service_group_name)
         service1 = service_factory(
-            pk=1, name="service1", disabled=False, service_group=sg
+            pk=1, name="service1", disabled=False, service_group__name="district"
         )
+
     else:
         service1 = service_factory(
             pk=1,
             name="service1",
             disabled=False,
-            service_group__name=service_group_name,
+            service_group__name="municipality",
         )
+
     service_factory(
-        pk=2, name="service2", disabled=True, service_group=service1.service_group
+        pk=2, name="service2", disabled=True, service_group__name="municipality"
     )
-    service_factory(pk=3, name="service3", disabled=False, service_group__pk=666)
+    service_factory(
+        pk=3, name="service3", disabled=False, service_group__name="district"
+    )
+    service_factory(
+        pk=4, name="service4", disabled=False, service_group__name="service"
+    )
 
     User = namedtuple("OIDCUser", "group")
     user = User(group=service1.pk)
