e3579dfa3458d97a1d33cc5a96b9bce3cde305d5
Jonas Metzener <jonas.metzener@adfinis.com>
2020-12-07 16:12:30 +0100

    fix(ember): always use queryManager instead of apollo service

--- ember-camac-ng/app/ui/work-items/instance/new/controller.js
+++ ember-camac-ng/app/ui/work-items/instance/new/controller.js
@@ -2,6 +2,7 @@ import Controller from "@ember/controller";
 import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
 import { tracked } from "@glimmer/tracking";
+import { queryManager } from "ember-apollo-client";
 import { dropTask } from "ember-concurrency-decorators";
 import moment from "moment";
 
@@ -21,8 +22,9 @@ class NewWorkItem {
 }
 
 export default class WorkItemNewController extends Controller {
+  @queryManager apollo;
+
   @service store;
-  @service apollo;
   @service notifications;
   @service intl;
   @service shoebox;
