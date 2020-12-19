5d074836ef66e5da7e2f41afef514bdc288f016a
Jonas Metzener <jonas.metzener@adfinis.com>
2020-12-19 17:42:39 +0100

    chore(lint): fix flake8 linting

--- django/camac/caluma/extensions/events/__init__.py
+++ django/camac/caluma/extensions/events/__init__.py
@@ -2,5 +2,11 @@
 # automatically without having to define each file as caluma event receiver
 # module
 
-# noqa: F401
-from . import audit, circulation, decision, ebau_number, general, simple_workflow
+from . import (  # noqa: F401
+    audit,
+    circulation,
+    decision,
+    ebau_number,
+    general,
+    simple_workflow,
+)
