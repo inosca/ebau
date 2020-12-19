f5ca2e0c44c1cef40ecab05804b2b6d48b978bb0
Jonas Metzener <jonas.metzener@adfinis.com>
2020-12-19 15:39:58 +0100

    feat(be): move more workflow logic from config to caluma events
    This moves the very simple logic (instance state change, ech message and
    history entry) of the following actions into the caluma event layer:
    
    - Start decision
    - Skip circulation
    - Reopen circulation
    - Complete procedure

--- django/camac/caluma/extensions/events/__init__.py
+++ django/camac/caluma/extensions/events/__init__.py
@@ -1,4 +1,6 @@
 # Import all modules in this folder so the event receivers are registered
 # automatically without having to define each file as caluma event receiver
 # module
-from . import audit, circulation, decision, ebau_number, general  # noqa
+
+# noqa: F401
+from . import audit, circulation, decision, ebau_number, general, simple_workflow
