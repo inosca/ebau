b4e2b00541eb041484a5fb54d54587ded5822c12
Jonas Metzener <jonas.metzener@adfinis.com>
2021-01-21 16:24:14 +0100

    feat(be): allow managing of applicants for support role

--- ember-caluma-portal/app/abilities/dashboard.js
+++ ember-caluma-portal/app/abilities/dashboard.js
@@ -1,14 +1,9 @@
-import { computed } from "@ember/object";
+import { reads } from "@ember/object/computed";
 import { inject as service } from "@ember/service";
 import { Ability } from "ember-can";
-
-import config from "../config/environment";
 
 export default class DashboardAbility extends Ability {
   @service session;
 
-  @computed("session.group")
-  get canEdit() {
-    return config.ebau.supportGroups.includes(parseInt(this.session.group));
-  }
+  @reads("session.isSupport") canEdit;
 }
