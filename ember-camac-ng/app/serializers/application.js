b807c769626830c4196ae37fc65b2e394015e3f2
Christian Zosel <christian@zosel.ch>
2021-02-02 11:43:46 +0100

    fix(ember-camac-ng): render form disabled if not editable

--- ember-camac-ng/app/serializers/application.js
+++ ember-camac-ng/app/serializers/application.js
@@ -1,3 +1,12 @@
 import JSONAPISerializer from "@ember-data/serializer/json-api";
 
-export default class ApplicationSerializer extends JSONAPISerializer {}
+export default class ApplicationSerializer extends JSONAPISerializer {
+  normalizeSingleResponse(...args) {
+    const [, , payload] = args;
+
+    // write the object's meta field to attributes
+    payload.data.attributes.meta = payload.data.meta || {};
+
+    return super.normalizeSingleResponse(...args);
+  }
+}
