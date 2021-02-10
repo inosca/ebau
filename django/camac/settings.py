6996e0c7067b3f6de9a69b6d12c306caf4a3f620
Jonas Metzener <jonas.metzener@adfinis.com>
2021-02-10 10:29:11 +0100

    feat(be): configure workflow and permissions for publication

--- django/camac/settings.py
+++ django/camac/settings.py
@@ -453,7 +453,14 @@ APPLICATIONS = {
             "surname",
         ],
         "CALUMA": {
-            "FORM_PERMISSIONS": ["main", "sb1", "sb2", "nfd", "dossierpruefung"],
+            "FORM_PERMISSIONS": [
+                "main",
+                "sb1",
+                "sb2",
+                "nfd",
+                "dossierpruefung",
+                "publikation",
+            ],
             "CIRCULATION_WORKFLOW": "circulation",
             "CIRCULATION_TASK": "circulation",
             "CIRCULATION_FORM": "circulation",
@@ -531,14 +538,21 @@ APPLICATIONS = {
                 "init-circulation": {"cancel": ["skip-circulation"]},
                 "start-circulation": {"cancel": ["check-activation", "start-decision"]},
                 "decision": {
-                    "skip": ["audit", "publication"],
-                    "cancel": ["reopen-circulation", "create-manual-workitems"],
+                    "skip": ["audit", "publication", "fill-publication"],
+                    "cancel": [
+                        "reopen-circulation",
+                        "create-manual-workitems",
+                        "create-publication",
+                    ],
                     "complete": ["nfd"],
                 },
                 "reopen-circulation": {"cancel": ["decision"]},
                 "complete": {
-                    "skip": ["check-sb1", "check-sb2"],
-                    "cancel": ["create-manual-workitems"],
+                    "skip": ["check-sb1", "check-sb2", "fill-publication"],
+                    "cancel": [
+                        "create-manual-workitems",
+                        "create-publication",
+                    ],
                 },
             },
             "HAS_PROJECT_CHANGE": True,
