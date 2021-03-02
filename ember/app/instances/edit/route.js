a7b141104230bc7a4f5abc9be9f6f6d4aedd3aeb
yelinz <yelin.zhang@adfinis.com>
2021-03-02 14:19:16 +0100

    fix(ember/php): apply linting to changes

--- ember/app/instances/edit/route.js
+++ ember/app/instances/edit/route.js
@@ -43,13 +43,16 @@ export default Route.extend({
   async setupController(controller, model) {
     this._super(controller, model);
 
-    // Set instanceTransformation on the controller to determine if the instance was transformed 
+    // Set instanceTransformation on the controller to determine if the instance was transformed
     const meta = this.questionStore.peek("meta", model.instance.id);
     if (meta && meta.value) {
       const formId = JSON.parse(meta.value).formChange.id;
       if (formId) {
-        const form = await this.store.findRecord("form", formId)
-        this.controllerFor("instances.edit").set('instanceTransformation', form.description);
+        const form = await this.store.findRecord("form", formId);
+        this.controllerFor("instances.edit").set(
+          "instanceTransformation",
+          form.description
+        );
       }
     }
   },
@@ -79,7 +82,7 @@ export default Route.extend({
       // have too much unnecessary data in the memory
       this.store.unloadAll();
       this.questionStore.clear();
-      this.controllerFor("instances.edit").set('instanceTransformation', null)
+      this.controllerFor("instances.edit").set("instanceTransformation", null);
     }
   },
 });
