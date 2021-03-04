0a69245660021bd871737e518d1dff4dc083ce45
Jonas Cosandey <jonas.cosandey@adfinis.com>
2021-03-04 15:16:38 +0100

    feat(gwr-data-import): replace the now unneeded api config with the service

--- ember-camac-ng/app/ui/gwr-data-import/service.js
+++ ember-camac-ng/app/ui/gwr-data-import/service.js
@@ -1,3 +1,12 @@
-import Service from "@ember/service";
+import Service, { inject as service } from "@ember/service";
 
-export default class GwrDataImportService extends Service {}
+export default class GwrDataImportService extends Service {
+  @service fetch;
+  async fetchProject(instanceId) {
+    const data = await this.fetch.fetch(
+      `/api/v1/instances/${instanceId}/gwr_data`
+    );
+    const json = await data.json();
+    return json.data;
+  }
+}
