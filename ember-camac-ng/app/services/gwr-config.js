5b1b5cbb5a66f40a4c9d7d045fe5024de583a4dc
Jonas Cosandey <jonas.cosandey@adfinis.com>
2021-03-08 09:55:20 +0100

    fix(ebau-login): read credentials from config service

--- ember-camac-ng/app/services/gwr-config.js
+++ ember-camac-ng/app/services/gwr-config.js
@@ -6,4 +6,8 @@ export default class GwrConfigService extends Service {
   cantonAbbreviation = "SZ";
   constructionSurveyDept = 134200;
   gwrAPI = "/housing-stat/regbl/api/ech0216/2";
+
+  username = "1342zoselc";
+  password = "%u1N89DqX4";
+  wsk_id = 351388;
 }
