294fb0e47460dbfada2055bf3e52a368f65dd529
Jonas Metzener <jonas.metzener@adfinis.com>
2021-03-22 13:38:51 +0100

    chore(portal): update ember to LTS 3.24

--- ember-caluma-portal/.eslintrc.js
+++ ember-caluma-portal/.eslintrc.js
@@ -5,6 +5,12 @@ module.exports = {
   rules: {
     "ember/no-mixins": "warn",
     "ember/no-get": "warn",
+    "ember/no-computed-properties-in-native-classes": "warn",
+    "ember/classic-decorator-no-classic-methods": "warn",
+    "ember/require-tagless-components": "warn",
+    "ember/no-classic-classes": "warn",
+    "ember/no-classic-components": "warn",
+    "ember/no-component-lifecycle-hooks": "warn",
   },
   settings: {
     "import/internal-regex": "^ember-caluma-portal/",
