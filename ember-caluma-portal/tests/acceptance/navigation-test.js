e22dbae01c34cc4befcd0975609a66a0b93745c3
Jonas Metzener <jonas.metzener@adfinis.com>
2021-03-23 11:05:44 +0100

    fix(portal): fix tests

---
+++ ember-caluma-portal/tests/acceptance/navigation-test.js
@@ -0,0 +1,79 @@
+import { visit, click, waitFor } from "@ember/test-helpers";
+import { setupMirage } from "ember-cli-mirage/test-support";
+import { setupIntl } from "ember-intl/test-support";
+import { setupApplicationTest } from "ember-qunit";
+import { authenticateSession } from "ember-simple-auth/test-support";
+import { module, test } from "qunit";
+
+module("Acceptance | navigation", function (hooks) {
+  setupApplicationTest(hooks);
+  setupMirage(hooks);
+  setupIntl(hooks, "de");
+
+  hooks.beforeEach(async function () {
+    this.server.create("user", { name: "John", surname: "Doe" });
+    await authenticateSession({ access_token: "123qweasdyxc" });
+  });
+
+  test("it renders a group switcher", async function (assert) {
+    assert.expect(4);
+
+    this.server.create("public-group", { name: "Test Group" });
+
+    await visit("/");
+
+    assert
+      .dom(".uk-navbar-right ul > li:nth-of-type(1) > a")
+      .hasText("Doe John");
+
+    await click(".uk-navbar-right ul > li:nth-of-type(1) > a");
+
+    assert.dom(".uk-dropdown ul > li.uk-active").hasText("Gesuchsteller/in");
+
+    await click(".uk-dropdown ul > li:nth-of-type(3) > a");
+
+    assert
+      .dom(".uk-navbar-right ul > li:nth-of-type(1) > a")
+      .hasText("Doe John (Test Group)");
+
+    await click(".uk-dropdown ul > li:nth-of-type(1) > a");
+
+    assert
+      .dom(".uk-navbar-right ul > li:nth-of-type(1) > a")
+      .hasText("Doe John");
+  });
+
+  test("it renders a link to the internal section", async function (assert) {
+    assert.expect(2);
+
+    const { id: instanceId } = this.server.create("instance", {
+      meta: { permissions: { main: ["read"] } },
+      calumaForm: "baugesuch",
+    });
+    const form = this.server.create("form", { slug: "baugesuch" });
+    const document = this.server.create("document", { form });
+    this.server.create("case", {
+      meta: { "camac-instance-id": instanceId },
+      document,
+    });
+
+    const { id: groupId } = this.server.create("public-group");
+    this.owner.lookup("service:session").set("data.group", groupId);
+
+    await visit("/");
+
+    assert
+      .dom("a.be-navbar-internal-link")
+      .hasAttribute("href", "http://camac-ng.local");
+
+    await visit(`/instances/${instanceId}`);
+    await waitFor("a.be-navbar-internal-link");
+
+    assert
+      .dom("a.be-navbar-internal-link")
+      .hasAttribute(
+        "href",
+        "http://camac-ng.local/index/redirect-to-instance-resource/instance-id/1"
+      );
+  });
+});
--- ember-caluma-portal/tests/integration/components/be-navbar/component-test.js
+++ ember-caluma-portal/tests/integration/components/be-navbar/component-test.js
@@ -1,6 +1,4 @@
-import Service from "@ember/service";
 import { render, settled, click } from "@ember/test-helpers";
-import { tracked } from "@glimmer/component";
 import { setupMirage } from "ember-cli-mirage/test-support";
 import { setupIntl } from "ember-intl/test-support";
 import { setupRenderingTest } from "ember-qunit";
@@ -27,13 +25,6 @@ module("Integration | Component | be-navbar", function (hooks) {
 
   hooks.beforeEach(async function () {
     this.server.create("user", { name: "John", surname: "Doe" });
-
-    this.owner.register(
-      "service:router",
-      class MockService extends Service {
-        @tracked currentRoute = { name: "dummy", parent: null, params: {} };
-      }
-    );
   });
 
   hooks.afterEach(async function () {
@@ -84,76 +75,5 @@ module("Integration | Component | be-navbar", function (hooks) {
     assert.dom(".uk-navbar-right ul > li:nth-of-type(2)").hasClass("uk-active");
     await click(".uk-navbar-right ul > li:nth-of-type(3) > a");
     assert.dom(".uk-navbar-right ul > li:nth-of-type(3)").hasClass("uk-active");
-  });
-
-  test("it renders a group switcher", async function (assert) {
-    assert.expect(4);
-
-    this.server.create("public-group", { name: "Test Group" });
-
-    await render(hbs`<BeNavbar />`);
-
-    assert
-      .dom(".uk-navbar-right ul > li:nth-of-type(1) > a")
-      .hasText("Doe John");
-
-    await click(".uk-navbar-right ul > li:nth-of-type(1) > a");
-
-    assert.dom(".uk-dropdown ul > li.uk-active").hasText("t:nav.applicant:()");
-
-    await click(".uk-dropdown ul > li:nth-of-type(3) > a");
-
-    assert
-      .dom(".uk-navbar-right ul > li:nth-of-type(1) > a")
-      .hasText("Doe John (Test Group)");
-
-    await click(".uk-dropdown ul > li:nth-of-type(1) > a");
-
-    assert
-      .dom(".uk-navbar-right ul > li:nth-of-type(1) > a")
-      .hasText("Doe John");
-  });
-
-  test("it renders a link to the internal section", async function (assert) {
-    assert.expect(3);
-
-    const { id: groupId } = this.server.create("public-group");
-    const editRoute = {
-      name: "instances.edit",
-      parent: null,
-      params: { instance: 1 },
-    };
-
-    this.owner.lookup("service:session").set("group", groupId);
-
-    await render(hbs`<BeNavbar />`);
-
-    assert
-      .dom("a.be-navbar-internal-link")
-      .hasAttribute("href", "http://camac-ng.local");
-
-    this.owner.lookup("service:router").set("currentRoute", editRoute);
-    await settled();
-
-    assert
-      .dom("a.be-navbar-internal-link")
-      .hasAttribute(
-        "href",
-        "http://camac-ng.local/index/redirect-to-instance-resource/instance-id/1"
-      );
-
-    this.owner.lookup("service:router").set("currentRoute", {
-      name: "instances.edit.form",
-      parent: editRoute,
-      params: { form: "baugesuch" },
-    });
-    await settled();
-
-    assert
-      .dom("a.be-navbar-internal-link")
-      .hasAttribute(
-        "href",
-        "http://camac-ng.local/index/redirect-to-instance-resource/instance-id/1"
-      );
   });
 });
