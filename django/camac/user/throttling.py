b5f8c86ee439935d37f307090a88e1071617bd8d
Jonas Metzener <jonas.metzener@adfinis.com>
2021-03-29 15:51:07 +0200

    feat(ech): implement throttling for the ech message endpoint

---
+++ django/camac/user/throttling.py
@@ -0,0 +1,9 @@
+from rest_framework.throttling import ScopedRateThrottle
+
+
+class GroupScopedRateThrottle(ScopedRateThrottle):
+    def get_cache_key(self, request, view):
+        return self.cache_format % {
+            "scope": self.scope,
+            "ident": request.group.pk if request.group else self.get_indent(request),
+        }
