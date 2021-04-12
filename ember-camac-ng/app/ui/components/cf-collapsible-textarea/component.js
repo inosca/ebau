b6f0cfd93e3d4e360e81451a05ea51c9c5fa7318
yelinz <yelin.zhang@adfinis.com>
2021-04-12 16:58:51 +0200

    fix(ember-camac-ng): use ember for collapsible

--- ember-camac-ng/app/ui/components/cf-collapsible-textarea/component.js
+++ ember-camac-ng/app/ui/components/cf-collapsible-textarea/component.js
@@ -1,9 +1,19 @@
 import { action } from "@ember/object";
 import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
 
 export default class CfCollapsibleTextareaComponent extends Component {
+  @tracked collapsed = true;
+
   @action
   save({ target: { value } }) {
     this.args.onSave(value);
+  }
+
+  @action
+  toggleCollapsible(e) {
+    e.preventDefault();
+
+    this.collapsed = !this.collapsed;
   }
 }
