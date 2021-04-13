7e6fa16097a1cbe0757212d4548ca4b02d8e0d19
Jonas Metzener <jonas.metzener@adfinis.com>
2021-04-13 17:28:13 +0200

    fix(caluma-portal): refactor flaky can helper to new component

--- ember-caluma-portal/app/instances/new/route.js
+++ ember-caluma-portal/app/instances/new/route.js
@@ -1,8 +1,3 @@
 import Route from "@ember/routing/route";
 
-import { can } from "ember-caluma-portal/-private/decorators";
-
-@can("create instance")
-class InstancesNewRoute extends Route {}
-
-export default InstancesNewRoute;
+export default class InstancesNewRoute extends Route {}
