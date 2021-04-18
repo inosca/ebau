c59acae54e2737fa42d4d12fccd861a36110e0a0
Christian Zosel <christian@zosel.ch>
2021-04-18 11:43:19 +0200

    feat(tooling): introduce yarn workspaces (try 2)

--- ember-camac-ng/config/targets.js
+++ ember-camac-ng/config/targets.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 const isProduction = process.env.EMBER_ENV === "production";
 
--- ember-camac-ng/tests/test-helper.js
+++ ember-camac-ng/tests/test-helper.js
@@ -1,9 +1,10 @@
-import Application from 'camac-ng/app';
-import config from 'camac-ng/config/environment';
-import * as QUnit from 'qunit';
-import { setApplication } from '@ember/test-helpers';
-import { setup } from 'qunit-dom';
-import { start } from 'ember-qunit';
+import { setApplication } from "@ember/test-helpers";
+import { start } from "ember-qunit";
+import * as QUnit from "qunit";
+import { setup } from "qunit-dom";
+
+import Application from "camac-ng/app";
+import config from "camac-ng/config/environment";
 
 setApplication(Application.create(config.APP));
 
