561a14945bb3bd90d4b055aefe79327d2be5352b
Christian Zosel <christian@zosel.ch>
2021-04-18 11:43:19 +0200

    feat(tooling): introduce yarn workspaces (try 2)

--- ember-camac-ng/.eslintrc.js
+++ ember-camac-ng/.eslintrc.js
@@ -1,11 +1,11 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  extends: ['@adfinis-sygroup/eslint-config/ember-app', 'plugin:ember/octane'],
+  extends: "@adfinis-sygroup/eslint-config/ember-app",
   rules: {
-    'ember/no-mixins': 'warn',
+    "ember/no-mixins": "warn",
   },
   settings: {
-    'import/internal-regex': '^camac-ng/',
+    "import/internal-regex": "^camac-ng/",
   },
 };
