365fae7a93e0e345d9dd462cb2eac0bf6a835a33
Jonas Cosandey <jonas.cosandey@adfinis.com>
2021-04-22 13:25:02 +0200

    fix(gwr): move config and rename config property

--- ember-camac-ng/app/services/gwr-config.js
+++ ember-camac-ng/app/services/gwr-config.js
@@ -1,12 +1,25 @@
-import Service from "@ember/service";
+import Service, { inject as service } from "@ember/service";
 
 export default class GwrConfigService extends Service {
+  @service session;
+  @service shoebox;
+
   municipalityId = 1342;
-  municipalityName = "Galgenen";
   cantonAbbreviation = "SZ";
-  constructionSurveyDept = 134200;
   gwrAPI = "/housing-stat/regbl/api/ech0216/2";
 
   username = "1342zoselc";
   password = "%u1N89DqX4";
+
+  get constructionSurveyDeptNumber() {
+    return `${this.municipalityId}00`;
+  }
+
+  get authToken() {
+    return this.session.data.authenticated.access_token;
+  }
+
+  get camacGroup() {
+    return this.shoebox.content.groupId;
+  }
 }
