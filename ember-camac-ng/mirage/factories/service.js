80d18859d4a05fae767cca3d1c7467ee3073b7c8
Jonas Metzener <jonas.metzener@adfinis.com>
2021-04-29 12:02:12 +0200

    fix(portal): debounce template rendering of loading or notfound component

--- ember-camac-ng/mirage/factories/service.js
+++ ember-camac-ng/mirage/factories/service.js
@@ -7,12 +7,12 @@ export default Factory.extend({
   phone: () =>
     [
       "+41",
-      faker.random.number({ min: 10, max: 99 }),
-      faker.random.number({ min: 100, max: 999 }),
-      faker.random.number({ min: 10, max: 99 }),
-      faker.random.number({ min: 10, max: 99 }),
+      faker.datatype.number({ min: 10, max: 99 }),
+      faker.datatype.number({ min: 100, max: 999 }),
+      faker.datatype.number({ min: 10, max: 99 }),
+      faker.datatype.number({ min: 10, max: 99 }),
     ].join(" "),
-  zip: () => faker.random.number({ min: 1000, max: 9999 }),
+  zip: () => faker.datatype.number({ min: 1000, max: 9999 }),
   city: () => faker.address.city(),
   address: () => faker.address.streetAddress(),
   email: () => faker.internet.email(),
