ce31ff3817044a4170e22fe1ba8ecb8820b6e3e7
Jonas Metzener <jonas.metzener@adfinis.com>
2021-05-06 14:29:30 +0200

    feat(be): add proper abilities and implement read only roles

---
+++ ember-camac-ng/app/abilities/audit.js
@@ -0,0 +1,26 @@
+import { inject as service } from "@ember/service";
+import { Ability } from "ember-can";
+
+export default class InstanceAbility extends Ability {
+  @service shoebox;
+
+  get canEditWorkItem() {
+    return (
+      !this.shoebox.isReadOnlyRole &&
+      this.model?.status === "READY" &&
+      this.model?.addressedGroups
+        .map((id) => parseInt(id))
+        .includes(parseInt(this.shoebox.content.serviceId))
+    );
+  }
+
+  get canEdit() {
+    return (
+      this.canEditWorkItem &&
+      parseInt(this.audit?._raw.createdByGroup) ===
+        parseInt(this.shoebox.content.serviceId) &&
+      parseInt(this.model?.caseData.instanceId) ===
+        parseInt(this.audit?.instanceId)
+    );
+  }
+}
