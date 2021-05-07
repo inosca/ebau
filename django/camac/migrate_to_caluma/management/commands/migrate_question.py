1cace6e6f36bbed10ebe916109902ca74c1b5e99
Jonas Lehmann <jonas.lehmann@adfinis.com>
2021-05-07 15:41:53 +0200

    fix(ur): cleanup migration command

--- django/camac/migrate_to_caluma/management/commands/migrate_question.py
+++ django/camac/migrate_to_caluma/management/commands/migrate_question.py
@@ -1,29 +1,12 @@
-import codecs
-import inspect
-import itertools
-import json
-import re
-from collections import defaultdict
-from datetime import datetime
 from logging import getLogger
-from pprint import pprint
 
 from caluma.caluma_form import models as form_models
 from caluma.caluma_workflow import models as workflow_models
-from caluma.caluma_workflow.jexl import GroupJexl
 from django.core.management.base import BaseCommand
-from django.core.validators import EMPTY_VALUES
 from django.db import transaction
-from django.db.models import F, Value
-from django.db.models.expressions import CombinedExpression, Func
-from django.utils.timezone import now
 
-from camac.constants import kt_uri as uri_constants
-from camac.core.models import Answer, ChapterPage
-from camac.instance.models import Instance, JournalEntry
-from camac.instance.serializers import generate_identifier
-from camac.migrate_to_caluma import question_map
-from camac.user.models import Location, User
+from camac.core.models import Answer
+from camac.instance.models import Instance
 
 log = getLogger(__name__)
 
@@ -69,17 +52,23 @@ class Command(BaseCommand):
             return
         caluma_case = workflow_models.Case.objects.filter(
             **{"meta__camac-instance-id": instance.instance_id}
+        ).first()
+        caluma_question = caluma_case.document.form.questions.filter(
+            slug="oereb-verfahren"
         )
-        caluma_question = caluma_case.document.form.questions.filter(slug="bezeichnung")
         if not caluma_question.exists():
             return
-        caluma_answer = caluma_case.document.answers.filter(slug="bezeichnung").first()
+        caluma_answer = caluma_case.document.answers.filter(
+            question_id="bezeichnung"
+        ).first()
         if caluma_answer:
             return
 
-        import pdb
-
-        pdb.set_trace()
+        form_models.Answer.objects.create(
+            document=caluma_case.document,
+            value=camac_answer.answer,
+            question_id="bezeichnung",
+        )
 
         if self._dry_run:
             raise DryRun()
