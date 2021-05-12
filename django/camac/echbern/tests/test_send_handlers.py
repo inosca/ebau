inosca/ebau

commit c869d778632ebf77e5204082d2ae6406ab7f3f84

fix(ech): remove leftovers of audit instance state
The instance state "Dossierprüfung" doesn't exist anymore since it was
merged with "Zirkulation initialisieren". This replaces permissions that
used the status "audit" with "circulation_init". This also removes
rejecting an instance state if the decision is negative since the
instance should then be finished - not rejected.

django/camac/constants/kt_bern.py
@@ -93,7 +93,6 @@ ECH_TASK_STELLUNGNAHME = "5200007"
 ECH_TASK_SB1_SUBMITTED = "5200008"
 ECH_TASK_SB2_SUBMITTED = "5200009"
 ECH_STATUS_NOTIFICATION_EBAU_NR_VERGEBEN = "5200030"
-ECH_STATUS_NOTIFICATION_PRUEFUNG_ABGESCHLOSSEN = "5200031"
 ECH_STATUS_NOTIFICATION_ZIRKULATION_GESTARTET = "5200032"
 ECH_STATUS_NOTIFICATION_SB1_AUSSTEHEND = "5200033"
 ECH_STATUS_NOTIFICATION_ABGESCHLOSSEN = "5200036"

django/camac/echbern/send_handlers.py
@@ -30,7 +30,7 @@ from camac.instance.models import Instance, InstanceState
 from camac.instance.serializers import CalumaInstanceChangeResponsibleServiceSerializer
 from camac.user.models import Service
 
-from .signals import accompanying_report_send, circulation_started, ruling, task_send
+from .signals import accompanying_report_send, circulation_started, task_send
 from .utils import judgement_to_decision
 
 
@@ -128,11 +128,11 @@ class NoticeRulingSendHandler(DocumentAccessibilityMixin, BaseSendHandler):
         if not super().has_permission()[0]:
             return False, None
 
-        if self.instance.instance_state.name == "audit":
+        if self.instance.instance_state.name == "circulation_init":
             if self.data.eventNotice.decisionRuling.judgement != 4:
                 return (
                     False,
-                    'For instances in the state "Dossierprüfung", only a NoticeRuling with judgement "4" is allowed.',
+                    'For instances in the state "Zirkulation initialisieren", only a NoticeRuling with judgement "4" is allowed.',
                 )
             return True, None
         if self.instance.instance_state.name not in ["coordination", "circulation"]:
@@ -169,31 +169,15 @@ class NoticeRulingSendHandler(DocumentAccessibilityMixin, BaseSendHandler):
             decision_type="UNKNOWN_ECH",
         )
 
-        if judgement in [3, 4]:
-            # reject instance
-            self.instance.instance_state = InstanceState.objects.get(name="rejected")
-            self.instance.save()
+        # we might have a running circulation, skip it
+        self.skip_work_item("circulation")
+        self.skip_work_item("start-decision")
+        # if we don't have one, skip the whole circulation
+        self.skip_work_item("skip-circulation")
 
-            # send ech event
-            ruling.send(
-                sender=self.__class__,
-                instance=self.instance,
-                user_pk=self.user.pk,
-                group_pk=self.group.pk,
-            )
-
-            # suspend case
-            workflow_api.suspend_case(case=self.get_case(), user=self.caluma_user)
-        else:
-            # we might have a running circulation, skip it
-            self.skip_work_item("circulation")
-            self.skip_work_item("start-decision")
-            # if we don't have one, skip the whole circulation
-            self.skip_work_item("skip-circulation")
-
-            # this handle status changes and assignment of the construction control
-            # for "normal" judgements
-            self.complete_work_item("decision")
+        # this handle status changes and assignment of the construction control
+        # for "normal" judgements
+        self.complete_work_item("decision")
 
         self.link_to_section(attachments)
 

django/camac/echbern/tests/test_send_handlers.py
@@ -54,8 +54,8 @@ def test_resolve_send_handler(xml_file, expected_send_handler):
 @pytest.mark.parametrize(
     "judgement,instance_state_name,has_permission,is_vorabklaerung,active,expected_state_name",
     [
-        (4, "audit", True, False, "leitbehoerde", "rejected"),
-        (3, "audit", False, False, "leitbehoerde", None),
+        (4, "circulation_init", True, False, "leitbehoerde", "finished"),
+        (3, "circulation_init", False, False, "leitbehoerde", None),
         (1, "coordination", True, False, "leitbehoerde", "sb1"),
         (1, "circulation", True, False, "leitbehoerde", "sb1"),
         (1, "circulation", True, False, "rsta", "sb1"),
@@ -157,7 +157,7 @@ def test_notice_ruling_send_handler(
     # put case in a realistic status
     skip_tasks = ["submit"]
 
-    if instance_state_name == "audit":
+    if instance_state_name == "circulation_init":
         skip_tasks.append("ebau-number")
     elif instance_state_name == "circulation":
         skip_tasks.extend(["ebau-number", "init-circulation"])
@@ -184,7 +184,7 @@ def test_notice_ruling_send_handler(
 
         expected_service = (
             active_service
-            if is_vorabklaerung or expected_state_name == "rejected"
+            if is_vorabklaerung or expected_state_name == "finished"
             else service_baukontrolle
         )
         assert ech_instance.responsible_service() == expected_service
@@ -193,9 +193,9 @@ def test_notice_ruling_send_handler(
 @pytest.mark.parametrize(
     "service_exists,instance_state_name,has_permission,success",
     [
-        (True, "audit", True, True),
+        (True, "circulation_init", True, True),
         (True, "sb1", False, False),
-        (False, "audit", True, False),
+        (False, "circulation_init", True, False),
     ],
 )
 def test_change_responsibility_send_handler(
@@ -487,7 +487,7 @@ def test_notice_kind_of_proceedings_send_handler(
     group.service = ech_instance.services.first()
     group.save()
 
-    state = instance_state_factory(name="audit")
+    state = instance_state_factory(name="subm")
     if has_permission:
         state = instance_state_factory(name="circulation_init")
     ech_instance.instance_state = state

django/camac/echbern/tests/test_views.py
@@ -217,6 +217,7 @@ def test_send(
     admin_client,
     admin_user,
     ech_instance,
+    ech_instance_case,
     mocker,
     instance_state_factory,
     role_factory,
@@ -252,21 +253,17 @@ def test_send(
         )
         attachment.attachment_sections.add(attachment_section_beteiligte_behoerden)
 
-    state = instance_state_factory(name="audit")
-    expected_state = instance_state_factory(name="rejected")
+    state = instance_state_factory(name="circulation_init")
+    expected_state = instance_state_factory(name="finished")
     ech_instance.instance_state = state
     ech_instance.save()
 
-    case = workflow_api.start_case(
-        workflow=caluma_workflow_models.Workflow.objects.get(slug="building-permit"),
-        form=caluma_form_models.Form.objects.get(slug="main-form"),
-        meta={"camac-instance-id": ech_instance.pk},
-        user=caluma_admin_user,
-    )
+    case = ech_instance_case()
 
-    workflow_api.complete_work_item(
-        work_item=case.work_items.get(task_id="submit"), user=caluma_admin_user
-    )
+    for task in ["submit", "ebau-number"]:
+        workflow_api.skip_work_item(
+            work_item=case.work_items.get(task_id=task), user=caluma_admin_user
+        )
 
     url = reverse("send")
     response = admin_client.post(
@@ -409,7 +406,7 @@ def test_send_404_attachment_missing(
     group.role = role_factory(name="support")
     group.save()
 
-    state = instance_state_factory(name="audit")
+    state = instance_state_factory(name="circulation_init")
     ech_instance.instance_state = state
     ech_instance.save()
 

django/camac/echbern/event_handlers.py
@@ -24,7 +24,6 @@ from camac.constants.kt_bern import (
     ECH_STATUS_NOTIFICATION_ABGESCHLOSSEN,
     ECH_STATUS_NOTIFICATION_EBAU_NR_VERGEBEN,
     ECH_STATUS_NOTIFICATION_IN_KOORDINATION,
-    ECH_STATUS_NOTIFICATION_PRUEFUNG_ABGESCHLOSSEN,
     ECH_STATUS_NOTIFICATION_SB1_AUSSTEHEND,
     ECH_STATUS_NOTIFICATION_ZIRKULATION_GESTARTET,
     ECH_STATUS_NOTIFICATION_ZURUECKGEWIESEN,
@@ -176,11 +175,6 @@ class StatusNotificationEventHandler(BaseEventHandler):
 
         if self.instance.previous_instance_state.name == "subm":
             message_type = ECH_STATUS_NOTIFICATION_EBAU_NR_VERGEBEN
-        elif (
-            self.instance.previous_instance_state.name == "audit"
-            and not self.instance.instance_state.name == "rejected"
-        ):  # pragma: no cover
-            message_type = ECH_STATUS_NOTIFICATION_PRUEFUNG_ABGESCHLOSSEN
         elif self.instance.instance_state.name == "circulation":
             message_type = ECH_STATUS_NOTIFICATION_ZIRKULATION_GESTARTET
         elif self.instance.instance_state.name == "sb1":  # pragma: no cover

django/camac/echbern/static/xml/get/status_notification_pruefung_abgeschlossen.xml
@@ -1,33 +1,0 @@
-<?xml version="1.0"?>
-<ns1:delivery xmlns:ns1="http://www.ech.ch/xmlns/eCH-0211/2" xmlns:ns2="http://www.ech.ch/xmlns/eCH-0058/5" xmlns:ns3="http://www.ech.ch/xmlns/eCH-0129/5">
-  <ns1:deliveryHeader>
-    <ns2:senderId>https://ebau.apps.be.ch</ns2:senderId>
-    <ns2:messageId>eb496caa-0a6c-4444-8607-87a0ca71df49</ns2:messageId>
-    <ns2:messageType>5200031</ns2:messageType>
-    <ns2:sendingApplication>
-      <ns2:manufacturer>Adfinis AG</ns2:manufacturer>
-      <ns2:product>camac</ns2:product>
-      <ns2:productVersion>0.0.0</ns2:productVersion>
-    </ns2:sendingApplication>
-    <ns2:subject>status notification</ns2:subject>
-    <ns2:messageDate>2019-11-28T06:45:55.188657Z</ns2:messageDate>
-    <ns2:action>1</ns2:action>
-    <ns2:testDeliveryFlag>true</ns2:testDeliveryFlag>
-  </ns1:deliveryHeader>
-  <ns1:eventStatusNotification>
-    <ns1:eventType>status notification</ns1:eventType>
-    <ns1:planningPermissionApplicationIdentification>
-      <ns1:localID>
-        <ns3:IdCategory>eBauNr</ns3:IdCategory>
-        <ns3:Id>2019-001</ns3:Id>
-      </ns1:localID>
-      <ns1:otherID>
-        <ns3:IdCategory>eBauNr</ns3:IdCategory>
-        <ns3:Id>2019-001</ns3:Id>
-      </ns1:otherID>
-      <ns1:dossierIdentification>2323</ns1:dossierIdentification>
-    </ns1:planningPermissionApplicationIdentification>
-    <ns1:status>in progress</ns1:status>
-    <ns1:remark>In Korrektur</ns1:remark>
-  </ns1:eventStatusNotification>
-</ns1:delivery>

django/camac/swagger/views.py
@@ -13,7 +13,6 @@ from camac.constants.kt_bern import (
     ECH_STATUS_NOTIFICATION_ABGESCHLOSSEN,
     ECH_STATUS_NOTIFICATION_EBAU_NR_VERGEBEN,
     ECH_STATUS_NOTIFICATION_IN_KOORDINATION,
-    ECH_STATUS_NOTIFICATION_PRUEFUNG_ABGESCHLOSSEN,
     ECH_STATUS_NOTIFICATION_SB1_AUSSTEHEND,
     ECH_STATUS_NOTIFICATION_ZIRKULATION_GESTARTET,
     ECH_STATUS_NOTIFICATION_ZURUECKGEWIESEN,
@@ -110,13 +109,6 @@ GET_TABLE_DATA = [
         "3.1",
         ECH_STATUS_NOTIFICATION_EBAU_NR_VERGEBEN,
         f"[status_notification_ebau_nr_vergeben]({static('xml/get/status_notification_ebau_nr_vergeben.xml')})",
-    ],
-    [
-        "StatusNotification",
-        "Prüfung abgeschlossen melden",
-        "3.1",
-        ECH_STATUS_NOTIFICATION_PRUEFUNG_ABGESCHLOSSEN,
-        f"[status_notification_pruefung_abgeschlossen]({static('xml/get/status_notification_pruefung_abgeschlossen.xml')})",
     ],
     [
         "StatusNotification",

commit 504434ca78351813c1462f00232f7c26231a7fd6

fix(ech): allow rejection via eCH send handler
This fixes a regression introduced in c869d778632ebf77e5204082d2ae6406ab7f3f84.
Rejection via eCH is possible and is now supported again.

django/camac/constants/kt_bern.py
@@ -98,6 +98,10 @@ ECH_STATUS_NOTIFICATION_SB1_AUSSTEHEND = "5200033"
 ECH_STATUS_NOTIFICATION_ABGESCHLOSSEN = "5200036"
 ECH_STATUS_NOTIFICATION_ZURUECKGEWIESEN = "5200037"
 ECH_STATUS_NOTIFICATION_IN_KOORDINATION = "5200038"
+ECH_JUDGEMENT_APPROVED = 1
+ECH_JUDGEMENT_APPROVED_WITH_RESERVATION = 2
+ECH_JUDGEMENT_WRITTEN_OFF = 3
+ECH_JUDGEMENT_DECLINED = 4
 
 # Notice Types
 NOTICE_TYPE_STELLUNGNAHME = 1
@@ -131,14 +135,14 @@ VORABKLAERUNG_DECISIONS_NEGATIVE = "negative"
 # 4 = abgelehnt
 DECISION_JUDGEMENT_MAP = {
     "building-permit": {
-        DECISIONS_BEWILLIGT: 1,
-        DECISIONS_ABGESCHRIEBEN: 3,
-        DECISIONS_ABGELEHNT: 4,
+        DECISIONS_BEWILLIGT: ECH_JUDGEMENT_APPROVED,
+        DECISIONS_ABGESCHRIEBEN: ECH_JUDGEMENT_WRITTEN_OFF,
+        DECISIONS_ABGELEHNT: ECH_JUDGEMENT_DECLINED,
     },
     "preliminary-clarification": {
-        VORABKLAERUNG_DECISIONS_BEWILLIGT: 1,
-        VORABKLAERUNG_DECISIONS_BEWILLIGT_MIT_VORBEHALT: 2,
-        VORABKLAERUNG_DECISIONS_NEGATIVE: 4,
+        VORABKLAERUNG_DECISIONS_BEWILLIGT: ECH_JUDGEMENT_APPROVED,
+        VORABKLAERUNG_DECISIONS_BEWILLIGT_MIT_VORBEHALT: ECH_JUDGEMENT_APPROVED_WITH_RESERVATION,
+        VORABKLAERUNG_DECISIONS_NEGATIVE: ECH_JUDGEMENT_DECLINED,
     },
 }
 

django/camac/echbern/send_handlers.py
@@ -10,6 +10,7 @@ from rest_framework.test import APIClient
 from camac.caluma.api import CalumaApi
 from camac.constants.kt_bern import (
     ATTACHMENT_SECTION_ALLE_BETEILIGTEN,
+    ECH_JUDGEMENT_DECLINED,
     INSTANCE_RESOURCE_ZIRKULATION,
     NOTICE_TYPE_NEBENBESTIMMUNG,
     NOTICE_TYPE_STELLUNGNAHME,
@@ -29,7 +30,7 @@ from camac.instance.models import Instance, InstanceState
 from camac.instance.serializers import CalumaInstanceChangeResponsibleServiceSerializer
 from camac.user.models import Service
 
-from .signals import accompanying_report_send, circulation_started, task_send
+from .signals import accompanying_report_send, circulation_started, ruling, task_send
 from .utils import judgement_to_decision
 
 
@@ -124,10 +125,10 @@ class NoticeRulingSendHandler(DocumentAccessibilityMixin, BaseSendHandler):
             return False, None
 
         if self.instance.instance_state.name == "circulation_init":
-            if self.data.eventNotice.decisionRuling.judgement != 4:
+            if self.data.eventNotice.decisionRuling.judgement != ECH_JUDGEMENT_DECLINED:
                 return (
                     False,
-                    'For instances in the state "Zirkulation initialisieren", only a NoticeRuling with judgement "4" is allowed.',
+                    f'For instances in the state "Zirkulation initialisieren", only a NoticeRuling with judgement "{ECH_JUDGEMENT_DECLINED}" is allowed.',
                 )
             return True, None
         if self.instance.instance_state.name not in ["coordination", "circulation"]:
@@ -146,7 +147,8 @@ class NoticeRulingSendHandler(DocumentAccessibilityMixin, BaseSendHandler):
                 status=403,
             )
 
-        workflow_slug = self.get_case().workflow_id
+        case = self.get_case()
+        workflow_slug = case.workflow_id
         judgement = self.data.eventNotice.decisionRuling.judgement
 
         try:
@@ -164,15 +166,31 @@ class NoticeRulingSendHandler(DocumentAccessibilityMixin, BaseSendHandler):
             decision_type="UNKNOWN_ECH",
         )
 
-        # we might have a running circulation, skip it
-        self.skip_work_item("circulation")
-        self.skip_work_item("start-decision")
-        # if we don't have one, skip the whole circulation
-        self.skip_work_item("skip-circulation")
+        if judgement == ECH_JUDGEMENT_DECLINED:
+            # reject instance
+            self.instance.instance_state = InstanceState.objects.get(name="rejected")
+            self.instance.save()
 
-        # this handle status changes and assignment of the construction control
-        # for "normal" judgements
-        self.complete_work_item("decision")
+            # send eCH event
+            ruling.send(
+                sender=self.__class__,
+                instance=self.instance,
+                user_pk=self.user.pk,
+                group_pk=self.group.pk,
+            )
+
+            # suspend case
+            workflow_api.suspend_case(case=case, user=self.caluma_user)
+        else:
+            # we might have a running circulation, skip it
+            self.skip_work_item("circulation")
+            self.skip_work_item("start-decision")
+            # if we don't have one, skip the whole circulation
+            self.skip_work_item("skip-circulation")
+
+            # this handle status changes and assignment of the construction control
+            # for "normal" judgements
+            self.complete_work_item("decision")
 
         self.link_to_section(attachments)
 

django/camac/echbern/tests/test_send_handlers.py
@@ -5,6 +5,9 @@ from camac.constants.kt_bern import (
     ATTACHMENT_SECTION_ALLE_BETEILIGTEN,
     ATTACHMENT_SECTION_BETEILIGTE_BEHOERDEN,
     DECISIONS_BEWILLIGT,
+    ECH_JUDGEMENT_APPROVED,
+    ECH_JUDGEMENT_DECLINED,
+    ECH_JUDGEMENT_WRITTEN_OFF,
     INSTANCE_RESOURCE_ZIRKULATION,
     NOTICE_TYPE_NEBENBESTIMMUNG,
     NOTICE_TYPE_STELLUNGNAHME,
@@ -54,13 +57,62 @@ def test_resolve_send_handler(xml_file, expected_send_handler):
 @pytest.mark.parametrize(
     "judgement,instance_state_name,has_permission,is_vorabklaerung,active,expected_state_name",
     [
-        (4, "circulation_init", True, False, "leitbehoerde", "finished"),
-        (3, "circulation_init", False, False, "leitbehoerde", None),
-        (1, "coordination", True, False, "leitbehoerde", "sb1"),
-        (1, "circulation", True, False, "leitbehoerde", "sb1"),
-        (1, "circulation", True, False, "rsta", "sb1"),
-        (1, "circulation", True, True, "leitbehoerde", "evaluated"),
-        (4, "subm", False, "leitbehoerde", False, None),
+        (
+            ECH_JUDGEMENT_DECLINED,
+            "circulation_init",
+            True,
+            False,
+            "leitbehoerde",
+            "rejected",
+        ),
+        (
+            ECH_JUDGEMENT_WRITTEN_OFF,
+            "circulation_init",
+            False,
+            False,
+            "leitbehoerde",
+            None,
+        ),
+        (
+            ECH_JUDGEMENT_APPROVED,
+            "coordination",
+            True,
+            False,
+            "leitbehoerde",
+            "sb1",
+        ),
+        (
+            ECH_JUDGEMENT_APPROVED,
+            "circulation",
+            True,
+            False,
+            "leitbehoerde",
+            "sb1",
+        ),
+        (
+            ECH_JUDGEMENT_APPROVED,
+            "circulation",
+            True,
+            False,
+            "rsta",
+            "sb1",
+        ),
+        (
+            ECH_JUDGEMENT_APPROVED,
+            "circulation",
+            True,
+            True,
+            "leitbehoerde",
+            "evaluated",
+        ),
+        (
+            ECH_JUDGEMENT_DECLINED,
+            "subm",
+            False,
+            "leitbehoerde",
+            False,
+            None,
+        ),
     ],
 )
 def test_notice_ruling_send_handler(
@@ -184,7 +236,7 @@ def test_notice_ruling_send_handler(
 
         expected_service = (
             active_service
-            if is_vorabklaerung or expected_state_name == "finished"
+            if is_vorabklaerung or expected_state_name == "rejected"
             else service_baukontrolle
         )
         assert ech_instance.responsible_service() == expected_service

django/camac/echbern/tests/test_views.py
@@ -12,6 +12,9 @@ from camac.constants.kt_bern import (
     ATTACHMENT_SECTION_BETEILIGTE_BEHOERDEN,
     DECISION_JUDGEMENT_MAP,
     DECISIONS_ABGESCHRIEBEN,
+    ECH_JUDGEMENT_APPROVED_WITH_RESERVATION,
+    ECH_JUDGEMENT_DECLINED,
+    ECH_JUDGEMENT_WRITTEN_OFF,
     VORABKLAERUNG_DECISIONS_BEWILLIGT_MIT_VORBEHALT,
 )
 from camac.core.models import (
@@ -254,7 +257,7 @@ def test_send(
         attachment.attachment_sections.add(attachment_section_beteiligte_behoerden)
 
     state = instance_state_factory(name="circulation_init")
-    expected_state = instance_state_factory(name="finished")
+    expected_state = instance_state_factory(name="rejected")
     ech_instance.instance_state = state
     ech_instance.save()
 
@@ -286,7 +289,13 @@ def test_send_400_no_data(admin_client):
     assert response.status_code == 400
 
 
-@pytest.mark.parametrize("is_vorabklaerung,judgement", [(False, 2), (True, 3)])
+@pytest.mark.parametrize(
+    "is_vorabklaerung,judgement",
+    [
+        (False, ECH_JUDGEMENT_APPROVED_WITH_RESERVATION),
+        (True, ECH_JUDGEMENT_WRITTEN_OFF),
+    ],
+)
 def test_send_400_invalid_judgement(
     is_vorabklaerung,
     judgement,
@@ -340,7 +349,7 @@ def test_send_400_invalid_judgement(
     response = admin_client.post(
         url,
         data=xml_data("notice_ruling").replace(
-            "<ns2:judgement>4</ns2:judgement>",
+            f"<ns2:judgement>{ECH_JUDGEMENT_DECLINED}</ns2:judgement>",
             f"<ns2:judgement>{judgement}</ns2:judgement>",
         ),
         content_type="application/xml",