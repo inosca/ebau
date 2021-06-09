d28cf37a225416f5af9e96aa37731e3a95908ba6
Jonas Lehmann <jonas.lehmann@adfinis.com>
2021-06-09 16:00:11 +0200

    fix(ur): fix side effect

--- django/camac/document/side_effects.py
+++ django/camac/document/side_effects.py
@@ -1,10 +1,29 @@
-from django.conf import settings
+from django.utils import timezone
+
+from camac.constants import kt_uri as uri_constants
+from camac.core.models import BuildingAuthorityButtonstate, WorkflowEntry
 
 
-def on_attachment_download(attachment, request):
-    if settings.APPLICATION_NAME != "kt_uri":
-        return
-    __import__("remote_pdb").RemotePdb("0.0.0.0", 5555).set_trace()
+def create_workflow_entry(attachment, request):
     # check if file is downloaded by gesuchsteller
-    # check if buttons are pressed
-    # write workflow entry for pressed buttons if it doesnt exist yet
+    if not request.group.role.pk == uri_constants.PORTAL_USER_ROLE:
+        return None
+    buttons = BuildingAuthorityButtonstate.objects.filter(
+        instance=attachment.instance,
+        building_authority_button_id__in=[
+            uri_constants.BUILDINGAUTHORITY_BUTTON_DECISION,
+            uri_constants.BUILDINGAUTHORITY_BUTTON_PRELIMINARY_DECISION,
+        ],
+        is_clicked=True,
+    )
+    mapping = {
+        uri_constants.BUILDINGAUTHORITY_BUTTON_DECISION: uri_constants.WORKFLOW_ENTRY_RECEIVED_DECISION,
+        uri_constants.BUILDINGAUTHORITY_BUTTON_PRELIMINARY_DECISION: uri_constants.WORKFLOW_ENTRY_RECEIVED_PRELIMINARY_DECISION,
+    }
+    for button in buttons:
+        WorkflowEntry.objects.get_or_create(
+            instance=attachment.instance,
+            workflow_item_id=mapping[button.building_authority_button_id],
+            group=1,
+            defaults={"workflow_date": timezone.now().replace(microsecond=0)},
+        )
