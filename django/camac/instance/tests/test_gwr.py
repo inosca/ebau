c07000dd3f1a3a1151829478f42acf3b65490a6e
Jonas Lehmann <jonas.lehmann@adfinis.com>
2021-08-04 11:28:49 +0200

    feat(ur): add type of construction to import

--- django/camac/instance/tests/test_gwr.py
+++ django/camac/instance/tests/test_gwr.py
@@ -4,11 +4,8 @@ from caluma.caluma_form import (
     models as caluma_form_models,
 )
 from caluma.caluma_workflow import api as workflow_api, models as caluma_workflow_models
-from django.conf import settings
 from django.urls import reverse
 from rest_framework import status
-
-from ..gwr_lookups import get_submit_date
 
 
 @pytest.mark.parametrize("role__name", ["Municipality"])
@@ -23,9 +20,13 @@ def test_instance_caluma_gwr_data_be(
     application_settings,
     workflow_item_factory,
     workflow_entry_factory,
-    gwr_config_be,
     question_factory,
 ):
+
+    application_settings["GWR"]["ANSWER_SLUGS"] = {
+        "projectAnnouncementDate": ("submit_date_from_task", "submit"),
+        "constructionProjectDescription": ("answer", None),
+    }
 
     case = workflow_api.start_case(
         workflow=caluma_workflow_models.Workflow.objects.get(pk="building-permit"),
@@ -37,59 +38,13 @@ def test_instance_caluma_gwr_data_be(
     workitem.closed_at = "2021-07-15 08:00:06+00"
     workitem.save()
 
-    building_cost_question = caluma_form_models.Question.objects.create(
-        slug="baukosten-in-chf", type=caluma_form_models.Question.TYPE_INTEGER
-    )
-    case.document.answers.create(question=building_cost_question, value="1200")
-    case.document.answers.create(
-        question_id="beschreibung-bauvorhaben", value="Bezeichnung"
-    )
-    table_answer = case.document.answers.create(
-        question_id="personalien-gesuchstellerin"
-    )
-    row = caluma_form_factories.DocumentFactory(form_id="personalien-tabelle")
-    table_answer.documents.add(row)
-    zip_question = caluma_form_models.Question.objects.create(
-        slug="plz-gesuchstellerin", type=caluma_form_models.Question.TYPE_INTEGER
-    )
-    city_question = caluma_form_models.Question.objects.create(
-        slug="ort-gesuchstellerin", type=caluma_form_models.Question.TYPE_TEXT
-    )
-    street_question = caluma_form_models.Question.objects.create(
-        slug="strasse-gesuchstellerin", type=caluma_form_models.Question.TYPE_TEXT
-    )
-    row.answers.create(question=city_question, value="Musterhausen")
-    row.answers.create(question=zip_question, value=1234)
-    row.answers.create(question=street_question, value="Musterstrasse")
-    row.answers.create(question_id="name-gesuchstellerin", value="Muster")
-    row.answers.create(question_id="vorname-gesuchstellerin", value="Max")
-
     url = reverse("instance-gwr-data", args=[instance.pk])
 
     response = admin_client.get(url)
     assert response.status_code == status.HTTP_200_OK
     data = response.json()["data"]
-    assert data["constructionProjectDescription"] == "Bezeichnung"
-    assert data["constructionLocalisation"]["municipalityName"] is None
-    assert data["client"]["address"]["town"] == "Musterhausen"
-    assert data["client"]["address"]["houseNumber"] is None
-    assert data["client"]["address"]["country"] is None
-    assert data["client"]["identification"]["isOrganisation"] == [None]
-    assert (
-        data["client"]["identification"]["organisationIdentification"][
-            "organisationName"
-        ]
-        is None
-    )
-    assert (
-        data["client"]["identification"]["personIdentification"]["officialName"]
-        == "Muster"
-    )
-    assert data["typeOfConstructionProject"] is None
-    assert data["officialConstructionProjectFileNo"] is None
-    assert data["realestateIdentification"]["EGRID"] is None
-    assert data["realestateIdentification"]["number"] is None
     assert data["projectAnnouncementDate"] == "2021-07-15T08:00:06Z"
+    assert data["constructionProjectDescription"] is None
 
 
 def test_instance_caluma_gwr_data_ur(
@@ -107,65 +62,62 @@ def test_instance_caluma_gwr_data_ur(
     location_factory,
 ):
     application_settings["GWR"]["ANSWER_SLUGS"] = {
-        "typeOfConstructionProject": "category",
+        "typeOfConstructionProject": (
+            "answer",
+            "category",
+            {
+                "category-hochbaute": 6011,
+            },
+        ),
+        "officialConstructionProjectFileNo": (
+            "case_meta",
+            "dossier-number",
+        ),
+        "client": (
+            "client",
+            "applicant",
+            {
+                "address_town": "city",
+                "address_swissZipCode": "zip",
+                "address_street": "street",
+                "address_houseNumber": "street-number",
+                "address_country": "country",
+                "identification_personIdentification_officialName": "last-name",
+                "identification_personIdentification_firstName": "first-name",
+                "identification_isOrganisation": (
+                    "is-juristic-person",
+                    {
+                        "is-juristic-person-no": False,
+                        "is-juristic-person-yes": True,
+                    },
+                ),
+                "identification_organisationIdentification_organisationName": "juristic-person-name",
+            },
+        ),
         "client_identification_isOrganisation": "is-juristic-person",
-        "constructionLocalisation_municipalityName": "municipality",
-    }
-    application_settings["GWR"]["ANSWER_MAPPING"] = {
-        "category-hochbaute": 6011,
+        "constructionLocalisation_municipalityName": ("location", "municipality"),
+        "projectAnnouncementDate": ("submit_date_from_workflow", [10, 12]),
+        "constructionProjectDescription": (
+            "answer",
+            [
+                "proposal-description",
+                "beschreibung-zu-mbv",
+                "bezeichnung",
+                "vorhaben-proposal-description",
+                "veranstaltung-beschrieb",
+            ],
+        ),
+        "realestateIdentification_EGRID": ("answer", "parcels.e-grid"),
+        "typeOfConstruction": (
+            "location",
+            None,
+        ),
     }
 
     case = workflow_api.start_case(
         workflow=caluma_workflow_models.Workflow.objects.get(pk="building-permit"),
         form=caluma_form_models.Form.objects.get(pk="main-form"),
         meta={"camac-instance-id": instance.pk, "dossier-number": "1200-00-00"},
-        user=caluma_admin_user,
-    )
-
-    category_question = caluma_form_models.Question.objects.create(
-        slug="category", type=caluma_form_models.Question.TYPE_MULTIPLE_CHOICE
-    )
-    case.document.answers.create(
-        question=category_question, value=["category-hochbaute"]
-    )
-    location_factory(pk=1, name="Musterhausen")
-    case.document.answers.create(question_id="municipality", value=1)
-    applicant_question = caluma_form_models.Question.objects.create(
-        slug="applicant", type=caluma_form_models.Question.TYPE_TABLE
-    )
-
-    table_answer = case.document.answers.create(question=applicant_question)
-    table_form = caluma_form_models.Form.objects.create(slug="personal-data-table")
-    row = caluma_form_factories.DocumentFactory(form=table_form)
-    table_answer.documents.add(row)
-    is_organisation_question = caluma_form_models.Question.objects.create(
-        slug="is-juristic-person", type=caluma_form_models.Question.TYPE_CHOICE
-    )
-    row.answers.create(
-        question=is_organisation_question, value="is-juristic-person-yes"
-    )
-    url = reverse("instance-gwr-data", args=[instance.pk])
-
-    response = admin_client.get(url)
-    assert response.status_code == status.HTTP_200_OK
-    data = response.json()["data"]
-    assert data["typeOfConstructionProject"] == 6011
-    assert data["constructionLocalisation"]["municipalityName"] == "Musterhausen"
-    assert data["officialConstructionProjectFileNo"] == "1200-00-00"
-
-
-def test_gwr_lookups_submit_date(
-    caluma_admin_user,
-    instance,
-    gwr_config_ur,
-    workflow_entry_factory,
-    workflow_item_factory,
-    caluma_workflow_config_ur,
-):
-    case = workflow_api.start_case(
-        workflow=caluma_workflow_models.Workflow.objects.get(pk="building-permit"),
-        form=caluma_form_models.Form.objects.get(pk="main-form"),
-        meta={"camac-instance-id": instance.pk, "dossier-number": "1200-00-000"},
         user=caluma_admin_user,
     )
     workflow_item_factory(pk=10, name="Dossiereingang")
@@ -176,6 +128,59 @@ def test_gwr_lookups_submit_date(
         group=1,
     )
 
-    answer_slugs = settings.APPLICATION["GWR"].get("ANSWER_SLUGS", {})
+    category_question = caluma_form_models.Question.objects.create(
+        slug="category", type=caluma_form_models.Question.TYPE_MULTIPLE_CHOICE
+    )
+    description_question = caluma_form_models.Question.objects.create(
+        slug="proposal-description", type=caluma_form_models.Question.TYPE_TEXT
+    )
+    case.document.answers.create(
+        question=category_question, value=["category-hochbaute"]
+    )
+    case.document.answers.create(question=description_question, value="Neues Haus")
+    location_factory(pk=1, name="Musterhausen")
+    case.document.answers.create(question_id="municipality", value=1)
 
-    assert get_submit_date(instance.pk, case, answer_slugs) == "2021-07-16"
+    applicant_question = caluma_form_models.Question.objects.create(
+        slug="applicant", type=caluma_form_models.Question.TYPE_TABLE
+    )
+    parcel_question = caluma_form_models.Question.objects.create(
+        slug="parcels", type=caluma_form_models.Question.TYPE_TABLE
+    )
+
+    table_answer_applicant = case.document.answers.create(question=applicant_question)
+    table_answer_parcel = case.document.answers.create(question=parcel_question)
+
+    table_form_applicant = caluma_form_models.Form.objects.create(
+        slug="personal-data-table"
+    )
+    table_form_parcel = caluma_form_models.Form.objects.create(slug="parcel-table")
+
+    applicant_row = caluma_form_factories.DocumentFactory(form=table_form_applicant)
+    parcel_row = caluma_form_factories.DocumentFactory(form=table_form_parcel)
+
+    table_answer_applicant.documents.add(applicant_row)
+    table_answer_parcel.documents.add(parcel_row)
+
+    is_organisation_question = caluma_form_models.Question.objects.create(
+        slug="is-juristic-person", type=caluma_form_models.Question.TYPE_CHOICE
+    )
+    egrid_nr_question = caluma_form_models.Question.objects.create(
+        slug="e-grid", type=caluma_form_models.Question.TYPE_TEXT
+    )
+
+    applicant_row.answers.create(
+        question=is_organisation_question, value="is-juristic-person-yes"
+    )
+    parcel_row.answers.create(question=egrid_nr_question, value="12345")
+    url = reverse("instance-gwr-data", args=[instance.pk])
+
+    response = admin_client.get(url)
+    assert response.status_code == status.HTTP_200_OK
+    data = response.json()["data"]
+    assert data["typeOfConstructionProject"] == 6011
+    assert data["constructionLocalisation"]["municipalityName"] == "Musterhausen"
+    assert data["officialConstructionProjectFileNo"] == "1200-00-00"
+    assert data["client"]["identification"]["isOrganisation"] is True
+    assert data["projectAnnouncementDate"] == "2021-07-16"
+    assert data["constructionProjectDescription"] == "Neues Haus"
