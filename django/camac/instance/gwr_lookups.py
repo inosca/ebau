4591bf9c47981fd4413130db85056780a9ac187d
Jonas Lehmann <jonas.lehmann@adfinis.com>
2021-08-04 11:28:49 +0200

    feat(ur): add type of construction to import

--- django/camac/instance/gwr_lookups.py
+++ django/camac/instance/gwr_lookups.py
@@ -1,50 +1,107 @@
+from caluma.caluma_form import models as form_models
 from django.conf import settings
 
 from camac.core.models import WorkflowEntry
 from camac.user.models import Location
 
 
-def get_answer_value(key, queryset):
-    answer_slugs = settings.APPLICATION["GWR"].get("ANSWER_SLUGS", {})
-    if not answer_slugs.get(key, None):
+def resolve(lookup_key, case):
+    config = settings.APPLICATION["GWR"].get("ANSWER_SLUGS", {}).get(lookup_key)
+    if config:
+        resolver = config[0]
+        return globals()[f"get_{resolver}"](*config[1:], case=case)
+
+    return None
+
+
+def get_answer(lookup, *args, case):
+    queryset = case.document.answers
+    if not lookup:
         return None
-    value = queryset.filter(question__slug=answer_slugs.get(key)).first().value
-    if isinstance(value, list):
-        return value[0]
-    return value
+    if type(lookup) == list:
+        value = queryset.filter(question__slug__in=lookup).first().value
+    else:
+        parts = lookup.split(".")
+        if len(parts) > 1:
+            queryset = form_models.Answer.objects.filter(
+                document=queryset.get(question__slug=parts[0]).documents.first()
+            )
+            value = queryset.filter(question__slug=parts[1]).first().value
+        else:
+            value = queryset.filter(question__slug=parts[0]).first().value
+
+    value = value[0] if isinstance(value, list) else value
+    mapping = args[0] if len(args) > 0 else None
+    return mapping.get(value) if mapping else value
 
 
-def get_answer_value_from_list(key, queryset):
-    answer_slugs = settings.APPLICATION["GWR"].get("ANSWER_SLUGS", {})
-    if not answer_slugs.get(key):
-        return None
-    return queryset.filter(question__slug__in=answer_slugs.get(key, "")).first().value
-
-
-def get_mapped_answer_value(key, queryset):
-    value = get_answer_value(key, queryset)
-    if not value:
-        return None
-    return settings.APPLICATION["GWR"].get("ANSWER_MAPPING", {}).get(value, None)
-
-
-def get_municipality_answer_value(key, queryset):
-    value = get_answer_value(key, queryset)
+def get_location(lookup, case):
+    value = get_answer(lookup, case=case)
     if not value:
         return None
     return Location.objects.get(pk=value).name
 
 
-def get_dossier_number(case):
-    if case.meta.get("dossier-number", None):
-        return case.meta["dossier-number"]
+def get_case_meta(lookup, case):
+    return case.meta.get(lookup)
 
 
-def get_submit_date(pk, case, answer_slugs):
-    submit_date = settings.APPLICATION["GWR"].get("SUBMIT_DATE", {})
-    if submit_date.get("TASK"):
-        return case.work_items.get(task_id=submit_date["TASK"]).closed_at
-    elif submit_date.get("WORKFLOW_ENTRY"):
-        return WorkflowEntry.objects.get(
-            instance_id=int(pk), workflow_item_id__in=submit_date["WORKFLOW_ENTRY"]
-        ).workflow_date.strftime("%Y-%m-%d")
+def get_submit_date_from_task(lookup, case):
+    return case.work_items.get(task_id="submit").closed_at
+
+
+def get_submit_date_from_workflow(workflow_item_ids, case):
+    return WorkflowEntry.objects.get(
+        instance_id=case.meta["camac-instance-id"],
+        workflow_item_id__in=workflow_item_ids,
+    ).workflow_date.strftime("%Y-%m-%d")
+
+
+def get_client(lookup, options, case):
+    try:
+
+        def find_answer(lookup_key):
+            config = options[lookup_key]
+
+            mapping = None
+            if type(config) is tuple:
+                slug, mapping = config
+            else:
+                slug = config
+
+            answer = client.filter(question__slug=slug).first()
+
+            value = answer.value if answer else None
+            return mapping.get(value) if mapping else value
+
+        client = form_models.Answer.objects.filter(
+            document=case.document.answers.get(question__slug=lookup).documents.first()
+        )
+
+        return {
+            "address": {
+                "town": find_answer("address_town"),
+                "swissZipCode": find_answer("address_swissZipCode"),
+                "street": find_answer("address_street"),
+                "houseNumber": find_answer("address_houseNumber"),
+                "country": find_answer("address_country"),
+            },
+            "identification": {
+                "personIdentification": {
+                    "officialName": find_answer(
+                        "identification_personIdentification_officialName"
+                    ),
+                    "firstName": find_answer(
+                        "identification_personIdentification_firstName"
+                    ),
+                },
+                "isOrganisation": find_answer("identification_isOrganisation"),
+                "organisationIdentification": {
+                    "organisationName": find_answer(
+                        "identification_organisationIdentification_organisationName"
+                    )
+                },
+            },
+        }
+    except form_models.Answer.DoesNotExist:  # pragma: no cover
+        return None
