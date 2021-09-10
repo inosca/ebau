3e2391a6a10a173a6eda2266d6bf4bd602ae1131
Jonas Metzener <jonas.metzener@adfinis.com>
2021-09-10 14:18:18 +0200

    fix(permissions): completely split public permissions from others
    Public instances can now only be accessed if either no authorization is
    done or the new "x-camac-public-access" header is given to avoid mixed
    permissions.

--- ember-caluma-portal/app/public-instances/route.js
+++ ember-caluma-portal/app/public-instances/route.js
@@ -1,3 +1,14 @@
 import Route from "@ember/routing/route";
+import { inject as service } from "@ember/service";
 
-export default class PublicInstancesRoute extends Route {}
+export default class PublicInstancesRoute extends Route {
+  @service session;
+
+  activate() {
+    this.session.enforcePublicAccess = true;
+  }
+
+  deactivate() {
+    this.session.enforcePublicAccess = false;
+  }
+}
