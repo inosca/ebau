83aadcbc477795cff1d43c38e4e467cab011893f
yelinz <yelin.zhang@adfinis.com>
2021-09-30 17:47:09 +0200

    feat(sz): create camac or caluma form in serializer

--- ember-camac-ng/app/ui/cases/new/route.js
+++ ember-camac-ng/app/ui/cases/new/route.js
@@ -1,4 +1,4 @@
-import Route from '@ember/routing/route';
+import Route from "@ember/routing/route";
 
 export default class CasesNewRoute extends Route {
   resetController(controller, isExiting) {
