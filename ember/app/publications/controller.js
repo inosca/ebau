de7c5dff2a7204b224d0b6015104051d5ce8ee54
yelinz <yelin.zhang@adfinis.com>
2021-10-05 16:37:35 +0200

    fix(ember): add paging to publication list

--- ember/app/publications/controller.js
+++ ember/app/publications/controller.js
@@ -1,31 +1,51 @@
 import Controller from "@ember/controller";
 import { inject as service } from "@ember/service";
-import {
-  dropTask,
-  restartableTask,
-  lastValue,
-} from "ember-concurrency-decorators";
+import { tracked } from "@glimmer/tracking";
+import { dropTask, restartableTask } from "ember-concurrency-decorators";
 
 export default class IndexController extends Controller {
   @service store;
   @service notification;
+  @service router;
 
-  @lastValue("getPublications") publications;
+  @tracked page = 1;
+  @tracked publications = [];
+
+  get hasNextPage() {
+    const pagination = this.fetchPublications.lastSuccessful?.value?.meta
+      .pagination;
+
+    return pagination && pagination.page < pagination.pages;
+  }
+
   @restartableTask
-  *getPublications() {
-    return yield this.store.query("publication-entry", {
+  *fetchPublications() {
+    const publications = yield this.store.query("publication-entry", {
       include: "instance,instance.location",
+      page: { number: this.page, size: 20 },
+      sort: "instance__location__name",
     });
+
+    this.publications = [...this.publications, ...publications.toArray()];
+
+    return publications;
   }
 
   @dropTask
   *navigate(publication) {
-    yield this.transitionToRoute(
+    yield this.router.transitionTo(
       "instances.edit",
       publication.instance.get("id"),
       {
         queryParams: { publication: publication.id },
       }
     );
+  }
+
+  @dropTask
+  *fetchMore() {
+    this.page++;
+
+    yield this.fetchPublications.perform();
   }
 }
