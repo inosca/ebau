bd8b6f64823b1216dab6c171c2555ec7fdc47a05
Jonas Metzener <jonas.metzener@adfinis.com>
2021-10-05 09:35:32 +0200

    feat(be): send reminder mails for activations with deadline yesterday

--- django/camac/notification/management/commands/send_activation_reminders.py
+++ django/camac/notification/management/commands/send_activation_reminders.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, timedelta
 
 from django.core.management.base import BaseCommand
 from django.db.models import Q
@@ -13,12 +13,12 @@ TEMPLATE_REMINDER_CIRCULATION = "05-meldung-fristuberschreitung-fachstelle"
 
 
 class Command(BaseCommand):
-    help = "Send reminders for all activations that exceeded their deadline today."
+    help = "Send reminders for all activations that exceeded their deadline yesterday."
 
     def handle(self, *args, **options):
         activations = Activation.objects.filter(
             ~Q(circulation_state__name="DONE"),
-            deadline_date__date=date.today(),
+            deadline_date__date=date.today() - timedelta(days=1),
             circulation__instance__instance_state__name="circulation",
         )
         instances = {a.circulation.instance for a in activations}
@@ -29,6 +29,6 @@ class Command(BaseCommand):
                 TEMPLATE_REMINDER_CIRCULATION,
                 {},
                 PermissionlessNotificationTemplateSendmailSerializer,
-                recipient_types=["activation_deadline_today"],
+                recipient_types=["activation_deadline_yesterday"],
                 instance={"id": instance.pk, "type": "instances"},
             )
