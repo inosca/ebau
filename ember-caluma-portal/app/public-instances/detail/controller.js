f611a4c1f51a3fed7558a4ab0e7bbc94686c7234
Jonas Metzener <jonas.metzener@adfinis.com>
2021-10-06 17:40:30 +0200

    feat(be): allow public instance access through secret key

--- ember-caluma-portal/app/public-instances/detail/controller.js
+++ ember-caluma-portal/app/public-instances/detail/controller.js
@@ -1,11 +1,16 @@
 import Controller from "@ember/controller";
 import { inject as service } from "@ember/service";
+import { tracked } from "@glimmer/tracking";
 import { dropTask, lastValue } from "ember-concurrency-decorators";
 
 export default class PublicInstancesDetailController extends Controller {
   @service store;
   @service notification;
   @service intl;
+
+  queryParams = ["key"];
+
+  @tracked key = null;
 
   @lastValue("fetchPublicInstance") publicInstance;
   @dropTask
