7a11214ac6e52a72a06aa23fe99ecdf98c2c8990
René Birrer <rene.birrer@adfinis.com>
2021-10-07 17:07:11 +0200

    feat: add manage command for importing dossiers from zip archive

--- django/camac/dossier_import/management/commands/import_dossiers.py
+++ django/camac/dossier_import/management/commands/import_dossiers.py
@@ -1,14 +1,24 @@
+from django.conf import settings
 from django.core.management.base import BaseCommand
-
-from camac.dossier_import.load_dossiers import perform_import
+from django.utils.module_loading import import_string
 
 
 class Command(BaseCommand):
     help = "Import instances and cases from zip package"
 
     def add_arguments(self, parser):
-        parser.add_arguments("path_to_file", nargs=1)
+        parser.add_argument(
+            "service_id",
+            type=int,
+            help="This identifies the 'Gemeinde' responsible"
+            " for the dossiers about to be imported.",
+        )
+        parser.add_argument("path_to_archive", type=str)
 
     def handle(self, *args, **options):
-        with open(options["path_to_file"], "r") as f:
-            perform_import(f)
+        importer_cls = import_string(
+            settings.APPLICATION["DOSSIER_IMPORT"]["XLSX_IMPORTER_CLASS"]
+        )
+        importer = importer_cls()
+        importer.initialize(options["service_id"], options["path_to_archive"])
+        importer.import_dossiers()
