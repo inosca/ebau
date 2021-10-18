20cff7073b9b5606d74ea5d95a1981e41f87659b
Jonas Metzener <jonas.metzener@adfinis.com>
2021-10-18 11:20:50 +0200

    chore(deps): upgrade dependencies of ember apps

--- ember-camac-ng/tests/integration/ui/components/case-dashboard/column/component-test.js
+++ ember-camac-ng/tests/integration/ui/components/case-dashboard/column/component-test.js
@@ -12,7 +12,7 @@ module("Integration | Component | case-dashboard/column", function (hooks) {
 
     await render(hbs`<CaseDashboard::Column />`);
 
-    assert.equal(this.element.textContent.trim(), "");
+    assert.dom(this.element).hasText("");
 
     // Template block usage:
     await render(hbs`
@@ -21,6 +21,6 @@ module("Integration | Component | case-dashboard/column", function (hooks) {
       </CaseDashboard::Column>
     `);
 
-    assert.equal(this.element.textContent.trim(), "template block text");
+    assert.dom(this.element).hasText("template block text");
   });
 });
--- ember-camac-ng/tests/unit/transforms/transform-test.js
+++ ember-camac-ng/tests/unit/transforms/transform-test.js
@@ -6,9 +6,9 @@ module("Unit | Transform | journal visibility", function (hooks) {
 
   test("it serializes", function (assert) {
     const transform = this.owner.lookup("transform:journal-visibility");
-    assert.equal(transform.serialize(true), "authorities");
-    assert.equal(transform.serialize(false), "own_organization");
-    assert.equal(transform.serialize(undefined), "own_organization");
+    assert.strictEqual(transform.serialize(true), "authorities");
+    assert.strictEqual(transform.serialize(false), "own_organization");
+    assert.strictEqual(transform.serialize(undefined), "own_organization");
   });
 
   test("it deserializes", function (assert) {
