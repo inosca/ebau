870256c17f5863433ae8f6a0ad7596516a91fdd8
Christian Zosel <christian@zosel.ch>
2021-11-01 17:04:22 +0100

    fix: refactor to use related field, cleanups

--- django/camac/instance/tests/snapshots/snap_test_instance.py
+++ django/camac/instance/tests/snapshots/snap_test_instance.py
@@ -18,29 +18,7 @@ snapshots['test_instance_list[Applicant-instance__user0-18-1-editable0-new] 1'] 
     'users'
 ]
 
-snapshots['test_instance_list[Applicant-instance__user0-18-editable0-new] 1'] = [
-    'circulations',
-    'forms',
-    'groups',
-    'instance-states',
-    'locations',
-    'services',
-    'services',
-    'users'
-]
-
 snapshots['test_instance_list[Canton-instance__user2-18-1-editable2-new] 1'] = [
-    'circulations',
-    'forms',
-    'groups',
-    'instance-states',
-    'locations',
-    'services',
-    'services',
-    'users'
-]
-
-snapshots['test_instance_list[Canton-instance__user2-18-editable2-new] 1'] = [
     'circulations',
     'forms',
     'groups',
@@ -62,17 +40,6 @@ snapshots['test_instance_list[Municipality-instance__user3-17-1-editable3-new] 1
     'users'
 ]
 
-snapshots['test_instance_list[Municipality-instance__user3-17-editable3-new] 1'] = [
-    'circulations',
-    'forms',
-    'groups',
-    'instance-states',
-    'locations',
-    'services',
-    'services',
-    'users'
-]
-
 snapshots['test_instance_list[Reader-instance__user1-18-1-editable1-new] 1'] = [
     'circulations',
     'forms',
@@ -84,29 +51,7 @@ snapshots['test_instance_list[Reader-instance__user1-18-1-editable1-new] 1'] = [
     'users'
 ]
 
-snapshots['test_instance_list[Reader-instance__user1-18-editable1-new] 1'] = [
-    'circulations',
-    'forms',
-    'groups',
-    'instance-states',
-    'locations',
-    'services',
-    'services',
-    'users'
-]
-
 snapshots['test_instance_list[Service-instance__user4-17-1-editable4-new] 1'] = [
-    'circulations',
-    'forms',
-    'groups',
-    'instance-states',
-    'locations',
-    'services',
-    'services',
-    'users'
-]
-
-snapshots['test_instance_list[Service-instance__user4-17-editable4-new] 1'] = [
     'circulations',
     'forms',
     'groups',
--- ember-camac-ng/app/models/instance-group.js
+++
@@ -1,5 +0,0 @@
-import Model, { attr } from "@ember-data/model";
-
-export default class InstanceGroupModel extends Model {
-  @attr name;
-}
