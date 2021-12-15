62ad2fcb90532d06c45f272b279a4eec523a6284
Christian Zosel <christian@zosel.ch>
2021-12-15 17:00:17 +0100

    fix: refactor fetch calls to ember-data (wip)

--- ember-camac-ng/app/ui/dossier-import/index/controller.js
+++ ember-camac-ng/app/ui/dossier-import/index/controller.js
@@ -16,42 +16,18 @@ export default class DossierImportIndexController extends Controller {
   @lastValue("fetchImports") imports;
   @dropTask
   *fetchImports() {
-    const response = yield this.fetch.fetch(
-      `/api/v1/dossier-imports?${new URLSearchParams({
-        "page[size]": 10,
+    try {
+      return yield this.store.findAll("dossier-import", {
         include: "user",
-      })}`,
-      {
-        method: "GET",
-      }
-    );
-
-    if (!response.ok) {
+        page: {
+          size: 10,
+        },
+      });
+    } catch (e) {
       this.notifications.error(
         this.intl.t("dossierImport.imports.fetchImportsError")
       );
-      return;
     }
-
-    const json = yield response.json();
-    const imports = json.data;
-    return imports.map((data) => {
-      const user = json.included.find(
-        (user) => user.id === data.relationships.user.data.id
-      ).attributes;
-      const createDate = new Date(data.attributes["created-at"]);
-      return {
-        id: data.id,
-        data: {
-          createDate: dateTime(createDate),
-          author: `${user.name} ${user.surname}`,
-          status: this.intl.t(
-            `dossierImport.imports.status.${data.attributes.status}`
-          ),
-          importDate: null, // TODO implement as soon as imports are possible
-        },
-      };
-    });
   }
 
   @action
