8f26d903c92b20a2dfa130cce772b651c4984e1c
Jonas Metzener <jonas.metzener@adfinis.com>
2022-01-06 13:28:40 +0100

    feat(be): refactor ebau-number form to caluma and add suggestion widget

--- django/camac/conftest.py
+++ django/camac/conftest.py
@@ -535,6 +535,7 @@ def caluma_forms_be(settings):
     caluma_form_models.Form.objects.create(slug="dossierpruefung")
     caluma_form_models.Form.objects.create(slug="publikation")
     caluma_form_models.Form.objects.create(slug="information-of-neighbors")
+    caluma_form_models.Form.objects.create(slug="ebau-number")
 
     # dynamic choice options get cached, so we clear them
     # to ensure the new "gemeinde" options will be valid
