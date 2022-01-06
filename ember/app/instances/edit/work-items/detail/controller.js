6384c8ac28be2fbcd4876083ecf4e5ab67773b21
yelinz <yelin.zhang@adfinis.com>
2022-01-06 10:08:23 +0100

    fix(sz): improve applicant work item list

--- ember/app/instances/edit/work-items/detail/controller.js
+++ ember/app/instances/edit/work-items/detail/controller.js
@@ -1,3 +1,14 @@
 import Controller from "@ember/controller";
+import { action } from "@ember/object";
+import { inject as service } from "@ember/service";
 
-export default class WorkItemsInstanceEditController extends Controller {}
+export default class WorkItemsInstanceEditController extends Controller {
+  @service router;
+
+  @action
+  async completeWorkItem() {
+    await this.model.completeWorkItem.perform();
+
+    await this.router.transitionTo("instance.edit.work-items.index");
+  }
+}
