95bb66b7df8fc17869790e8af67dcf7f6d8ecec9
Jonas Metzener <jonas.metzener@adfinis.com>
2022-01-19 10:36:03 +0100

    chore(release): v2.7.0-rc.8

--- django/camac/camac_metadata.py
+++ django/camac/camac_metadata.py
@@ -1,6 +1,6 @@
 __title__ = "camac"
 __author__ = "Adfinis AG"
-__version__ = "2.7.0-rc.7"
+__version__ = "2.7.0-rc.8"
 __email__ = "support@adfinis.com"
 __description__ = "Camac REST API"
 __url__ = "https://adfinis.com/"
