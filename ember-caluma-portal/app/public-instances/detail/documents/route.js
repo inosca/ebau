33e2004f65e1a8b10c573e7526ce285b7c31ce86
Jonas Metzener <jonas.metzener@adfinis.com>
2022-02-02 14:29:15 +0100

    feat(publication): add hint for public instances with same ebau number

--- ember-caluma-portal/app/public-instances/detail/documents/route.js
+++ ember-caluma-portal/app/public-instances/detail/documents/route.js
@@ -1,9 +1,3 @@
 import Route from "@ember/routing/route";
 
-export default class PublicInstancesDetailDocumentsRoute extends Route {
-  setupController(controller, model) {
-    super.setupController(controller, model);
-
-    controller.fetchPublicAttachments.perform();
-  }
-}
+export default class PublicInstancesDetailDocumentsRoute extends Route {}
