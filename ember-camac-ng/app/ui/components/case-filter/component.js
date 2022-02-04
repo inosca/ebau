48f011aa43a5d970696b2093dfeac64b273cd2f3
Alana Luyten <alana.luyten@adfinis.com>
2022-02-04 17:38:49 +0100

    refactor(cases): fetch filter select options conditionally
    Only fetch the select options for a certain select
    case filter if the case filter has been activated.

--- ember-camac-ng/app/ui/components/case-filter/component.js
+++ ember-camac-ng/app/ui/components/case-filter/component.js
@@ -6,11 +6,25 @@ import calumaQuery from "@projectcaluma/ember-core/caluma-query";
 import { allForms } from "@projectcaluma/ember-core/caluma-query/queries";
 import { queryManager } from "ember-apollo-client";
 import { restartableTask, lastValue } from "ember-concurrency";
-import { gql } from "graphql-tag";
 
 import caseFilters from "./filter-config";
 
 import config from "camac-ng/config/environment";
+import getBuildingPermitQuestion from "camac-ng/gql/queries/get-building-permit-question.graphql";
+
+const externalServiceGroupIds = [
+  "21",
+  "70",
+  "2",
+  "65",
+  "66",
+  "62",
+  "61",
+  "63",
+  "64",
+  "41",
+  "71",
+];
 
 export default class CaseFilterComponent extends Component {
   @queryManager apollo;
@@ -26,81 +40,58 @@ export default class CaseFilterComponent extends Component {
   @lastValue("fetchFilterData") filterData;
   @restartableTask
   *fetchFilterData() {
-    const municipalities = yield this.store.findAll("location");
-    const instanceStates = yield this.store.findAll("instance-state");
+    const optionQueries = {
+      municipalities: async () => await this.store.findAll("location"),
+      instanceStates: async () => await this.store.findAll("instance-state"),
+      services: async () =>
+        await this.store.query("service", {
+          service_group_id: externalServiceGroupIds.join(","),
+        }),
+      buildingPermitTypes: async () =>
+        (
+          await this.apollo.query(
+            {
+              query: getBuildingPermitQuestion,
+            },
+            "allQuestions.edges"
+          )
+        )
+          .map((edge) => edge.node.options.edges)[0]
+          ?.map((edge) => edge.node),
+      caseStatusOptions: () =>
+        this.caseFilters.caseStatus.optionValues.map((option) => ({
+          status: option,
+          label: this.intl.t(`cases.status.${option}`),
+        })),
+      formOptions: async () =>
+        (
+          await this.formsQuery.fetch({
+            filter: [
+              { isPublished: true },
+              { isArchived: false },
+              { orderBy: "NAME_ASC" },
+              {
+                metaValue: [{ key: "is_creatable", value: true }],
+              },
+            ],
+          })
+        ).allForms.edges.map((form) => form.node),
+    };
 
-    const externalServiceGroupIds = [
-      "21",
-      "70",
-      "2",
-      "65",
-      "66",
-      "62",
-      "61",
-      "63",
-      "64",
-      "41",
-      "71",
-    ];
-    const services = yield this.store.query("service", {
-      service_group_id: externalServiceGroupIds.join(","),
-    });
-
-    const buildingPermitTypes = (yield this.apollo.query(
-      {
-        query: gql`
-          query BuildingPermitQuestion {
-            allQuestions(slugs: ["form-type"]) {
-              edges {
-                node {
-                  ... on ChoiceQuestion {
-                    options(orderBy: LABEL_ASC, isArchived: false) {
-                      edges {
-                        node {
-                          slug
-                          label
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        `,
-      },
-      "allQuestions.edges"
-    ))
-      .map((edge) => edge.node.options.edges)[0]
-      ?.map((edge) => edge.node);
-
-    const caseStatusOptions = this.caseFilters.caseStatus.optionValues.map(
-      (option) => ({
-        status: option,
-        label: this.intl.t(`cases.status.${option}`),
-      })
+    const options = yield Promise.all(
+      Object.entries(optionQueries)
+        .filter(([key]) =>
+          this.activeCaseFilters
+            .map((filter) => this.caseFilters[filter].options)
+            .includes(key)
+        )
+        .map(async ([key, fn]) => [key, await fn()])
     );
 
-    const forms = yield this.formsQuery.fetch({
-      filter: [
-        { isPublished: true },
-        { isArchived: false },
-        { orderBy: "NAME_ASC" },
-        {
-          metaValue: [{ key: "is_creatable", value: true }],
-        },
-      ],
-    });
-    const formOptions = forms.allForms.edges.map((form) => form.node);
-
-    return {
-      municipalities,
-      buildingPermitTypes,
-      instanceStates,
-      services,
-      caseStatusOptions,
-      formOptions,
-    };
+    return options.reduce(
+      (obj, [key, value]) => ({ ...obj, [key]: value }),
+      {}
+    );
   }
 
   @action updateFilter(field, event) {
