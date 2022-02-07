1d9aba414398dc1578d28acb50635643c1f69e2b
Christian Zosel <christian@zosel.ch>
2022-02-07 16:33:37 +0100

    fix: minor test refactoring

--- django/camac/core/tests/test_migrate_workflow_entries_ur.py
+++ django/camac/core/tests/test_migrate_workflow_entries_ur.py
@@ -5,6 +5,11 @@ from caluma.caluma_workflow.models import Case
 from django.core.management import call_command
 
 from camac.core.models import WorkflowEntry
+from camac.core.management.commands.migrate_workflow_entries_ur import (
+    WORKFLOW_ITEM_EINGANG_POST,
+    WORKFLOW_ITEM_DOSSIER_IN_UREC_ERFASST,
+    WORKFLOW_ITEM_EINGANG_ONLINE,
+)
 
 
 @pytest.fixture
@@ -13,7 +18,7 @@ def workflow_entry_internal(db, ur_instance, workflow_entry_factory):
         instance=ur_instance,
         workflow_date="2022-01-14 12:49:34+00",
         group=1,
-        workflow_item__pk=12,
+        workflow_item__pk=WORKFLOW_ITEM_DOSSIER_IN_UREC_ERFASST,
     )
 
 
@@ -23,7 +28,7 @@ def workflow_entry_portal(db, ur_instance, workflow_entry_factory):
         instance_id=ur_instance.pk,
         workflow_date="2022-01-15 12:49:34+00",
         group=1,
-        workflow_item__pk=10,
+        workflow_item__pk=WORKFLOW_ITEM_EINGANG_POST,
     )
 
 
@@ -43,10 +48,12 @@ def test_migrate_portal_workflow_entries(workflow_entry_portal, ur_instance):
         stdout=open(os.devnull, "w"),
     )
 
-    workflow_entry = WorkflowEntry.objects.filter(
-        instance=ur_instance, workflow_item=12000000
-    )
+    assert WorkflowEntry.objects.filter(
+        instance=ur_instance, workflow_item=WORKFLOW_ITEM_EINGANG_ONLINE
+    ).exists()
+    assert not WorkflowEntry.objects.filter(
+        instance=ur_instance, workflow_item=10
+    ).exists()
 
     case = Case.objects.first()
     assert case.meta["submit-date"] == "2022-01-15T12:49:34+0000"
-    assert workflow_entry.first().workflow_item_id == 12000000
