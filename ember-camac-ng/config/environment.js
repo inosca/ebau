d552cf34df51d37b7fdc2318b3d67c4f2f18fe9f
Jonas Lehmann <jonas.lehmann@adfinis.com>
2022-02-08 14:21:18 +0100

    fix(dossier-list): make sort order configurable

--- ember-camac-ng/config/environment.js
+++ ember-camac-ng/config/environment.js
@@ -58,7 +58,7 @@ module.exports = function (environment) {
           cantonAbbreviation: "SZ",
           importModels: ["project", "building", "dwelling"],
         },
-
+        casesQueryOrder: [{ meta: "dossier-number" }],
         allowApplicantManualWorkItem: true,
         journalDefaultVisibility: false,
         newCase: {
@@ -72,6 +72,10 @@ module.exports = function (environment) {
           cantonAbbreviation: "UR",
           importModels: ["project", "building", "dwelling"],
         },
+        casesQueryOrder: [
+          { documentAnswer: "municipality" },
+          { meta: "dossier-number", direction: "DESC" },
+        ],
         allowApplicantManualWorkItem: false,
         journalDefaultVisibility: true,
         activeCirculationStates: [
