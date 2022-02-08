bba1f35b747083dd46a3b07acceb141e69126fa4
Jonas Lehmann <jonas.lehmann@adfinis.com>
2022-02-08 10:13:15 +0100

    fix(ur): run isort and fix tests

--- django/camac/core/tests/test_migrate_workflow_entries_ur.py
+++ django/camac/core/tests/test_migrate_workflow_entries_ur.py
@@ -4,31 +4,35 @@ import pytest
 from caluma.caluma_workflow.models import Case
 from django.core.management import call_command
 
-from camac.core.models import WorkflowEntry
 from camac.core.management.commands.migrate_workflow_entries_ur import (
-    WORKFLOW_ITEM_EINGANG_POST,
-    WORKFLOW_ITEM_DOSSIER_IN_UREC_ERFASST,
     WORKFLOW_ITEM_EINGANG_ONLINE,
 )
+from camac.core.models import WorkflowEntry
 
 
 @pytest.fixture
-def workflow_entry_internal(db, ur_instance, workflow_entry_factory):
-    workflow_entry_factory(
+def workflow_entry_internal(db, ur_instance, workflow_entry_factory, mocker):
+    workflow_entry_internal = workflow_entry_factory(
         instance=ur_instance,
         workflow_date="2022-01-14 12:49:34+00",
         group=1,
-        workflow_item__pk=WORKFLOW_ITEM_DOSSIER_IN_UREC_ERFASST,
+    )
+    mocker.patch(
+        "camac.core.management.commands.migrate_workflow_entries_ur.WORKFLOW_ITEM_DOSSIER_IN_UREC_ERFASST",
+        workflow_entry_internal.workflow_item.pk,
     )
 
 
 @pytest.fixture
-def workflow_entry_portal(db, ur_instance, workflow_entry_factory):
-    workflow_entry_factory(
+def workflow_entry_portal(db, ur_instance, workflow_entry_factory, mocker):
+    workflow_entry_portal = workflow_entry_factory(
         instance_id=ur_instance.pk,
         workflow_date="2022-01-15 12:49:34+00",
         group=1,
-        workflow_item__pk=WORKFLOW_ITEM_EINGANG_POST,
+    )
+    mocker.patch(
+        "camac.core.management.commands.migrate_workflow_entries_ur.WORKFLOW_ITEM_EINGANG_POST",
+        workflow_entry_portal.workflow_item.pk,
     )
 
 
