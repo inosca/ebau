95a2aed420f6a7f783968b72a45781e09c2b4d5d
Jonas Lehmann <jonas.lehmann@adfinis.com>
2022-02-17 13:50:47 +0100

    feat(ur): add the link-instance-button component

---
+++ ember-camac-ng/tests/integration/ui/components/link-instance-button/component-test.js
@@ -0,0 +1,17 @@
+import { render } from "@ember/test-helpers";
+import { hbs } from "ember-cli-htmlbars";
+import { setupRenderingTest } from "ember-qunit";
+import { module, test } from "qunit";
+
+module("Integration | Component | link-instance-button", function (hooks) {
+  setupRenderingTest(hooks);
+
+  test("it renders", async function (assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`<LinkInstanceButton />`);
+
+    assert.dom(this.element).hasText("");
+  });
+});
