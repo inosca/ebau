36bd4fb941ebcda083d9fa894f3656c452d399ee
Christian Zosel <christian@zosel.ch>
2022-02-21 16:06:48 +0100

    fix: make ur workflow entry migration more defensive

--- django/camac/core/management/commands/migrate_workflow_entries_ur.py
+++ django/camac/core/management/commands/migrate_workflow_entries_ur.py
@@ -30,37 +30,18 @@ class Command(BaseCommand):
         )
 
         for case in cases:
-
-            internal_workflow_entries = WorkflowEntry.objects.filter(
-                instance=case.instance,
-                workflow_item=WORKFLOW_ITEM_DOSSIER_IN_UREC_ERFASST,
-            )
-            if internal_workflow_entries:
-                # No workflow entry migration because the work-item "Dossiereingang" was renamed to "Eingang Post" and the id didn't change
-
-                if "submit-date" not in case.meta:
-                    case.meta[
-                        "submit-date"
-                    ] = internal_workflow_entries.first().workflow_date.strftime(
-                        SUBMIT_DATE_FORMAT
-                    )
-                    case.save()
-
-                    self.stdout.write(
-                        f"For the instance {case.instance.pk} the submit date {case.meta['submit-date']} was written to the case meta"
-                    )
-
-            else:
-                workflow_entries = WorkflowEntry.objects.filter(
+            try:
+                internal_workflow_entries = WorkflowEntry.objects.filter(
                     instance=case.instance,
-                    workflow_item=WORKFLOW_ITEM_EINGANG_POST,
+                    workflow_item=WORKFLOW_ITEM_DOSSIER_IN_UREC_ERFASST,
                 )
+                if internal_workflow_entries:
+                    # No workflow entry migration because the work-item "Dossiereingang" was renamed to "Eingang Post" and the id didn't change
 
-                if workflow_entries:
                     if "submit-date" not in case.meta:
                         case.meta[
                             "submit-date"
-                        ] = workflow_entries.first().workflow_date.strftime(
+                        ] = internal_workflow_entries.first().workflow_date.strftime(
                             SUBMIT_DATE_FORMAT
                         )
                         case.save()
@@ -69,11 +50,34 @@ class Command(BaseCommand):
                             f"For the instance {case.instance.pk} the submit date {case.meta['submit-date']} was written to the case meta"
                         )
 
-                    workflow_entries.update(workflow_item=WORKFLOW_ITEM_EINGANG_ONLINE)
-
-                    self.stdout.write(
-                        f"For the instance {case.instance.pk} the workflow entries {[workflow_entry.workflow_entry_id for workflow_entry in workflow_entries]} where chnanged from workflow item Nr. 10 to workflow item Nr. 12000000"
+                else:
+                    workflow_entries = WorkflowEntry.objects.filter(
+                        instance=case.instance,
+                        workflow_item=WORKFLOW_ITEM_EINGANG_POST,
                     )
+
+                    if workflow_entries:
+                        if "submit-date" not in case.meta:
+                            case.meta[
+                                "submit-date"
+                            ] = workflow_entries.first().workflow_date.strftime(
+                                SUBMIT_DATE_FORMAT
+                            )
+                            case.save()
+
+                            self.stdout.write(
+                                f"For the instance {case.instance.pk} the submit date {case.meta['submit-date']} was written to the case meta"
+                            )
+
+                        workflow_entries.update(
+                            workflow_item=WORKFLOW_ITEM_EINGANG_ONLINE
+                        )
+
+                        self.stdout.write(
+                            f"For the instance {case.instance.pk} the workflow entries {[workflow_entry.workflow_entry_id for workflow_entry in workflow_entries]} where chnanged from workflow item Nr. 10 to workflow item Nr. 12000000"
+                        )
+            except Exception as e:
+                self.stdout.write(f"Could not migrate case {case.id}: {e}")
 
         if options["dry"]:  # pragma: no cover
             transaction.savepoint_rollback(sid)
