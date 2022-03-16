bbe4fb8a2d2e681875ba337ec73f744e33f9babe
Christian Zosel <christian@zosel.ch>
2022-03-16 10:23:42 +0100

    fix: use stew renaming instead of embroider macros

---
+++ ember-camac-ng/app/config/case-table-sz.js
@@ -0,0 +1,13 @@
+export default {
+  columns: {
+    caluma: ["dossierNr", "caseDocumentFormName", "intent", "caseStatus"],
+    "camac-ng": [
+      "dossierNr",
+      "instanceFormDescription",
+      "locationSZ",
+      "builderSZ",
+      "intentSZ",
+      "instanceStateDescription",
+    ],
+  },
+};
