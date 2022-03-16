e27ac75c9e102c675e63e2b4d701c6253027f8e0
Jonas Lehmann <jonas.lehmann@adfinis.com>
2022-03-16 15:42:15 +0100

    fix(ur): change the intent question for the reklame form
    Add a new question to the `reklame` form and show its content as intent
    on the dossier list.

--- ember-camac-ng/app/config/case-model-ur.js
+++ ember-camac-ng/app/config/case-model-ur.js
@@ -6,23 +6,13 @@ const intentSlugs = [
   "bezeichnung",
   "vorhaben-proposal-description",
   "veranstaltung-beschrieb",
-  "reklamen",
+  "beschreibung-reklame",
 ];
 
 export default {
   intentSlugs,
   intent: (document) => {
     const answer = getAnswer(document, intentSlugs);
-
-    if (answer?.node.question.slug === "reklamen") {
-      const mapping = {
-        "art-der-reklame-neue": "Neue Reklame",
-        "art-der-reklame-aenderung": "Änderung bestehender Reklame",
-      };
-      return mapping[
-        getAnswer(answer?.node.value[0], "art-der-reklame")?.node.stringValue
-      ];
-    }
 
     return answer?.node.stringValue;
   },
