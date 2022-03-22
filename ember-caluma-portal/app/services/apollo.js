b31d9ddd02b630265536e337debbc53d5b072b80
Jonas Metzener <jonas.metzener@adfinis.com>
2022-03-22 14:05:48 +0100

    chore(deps): update dependencies

--- ember-caluma-portal/app/services/apollo.js
+++ ember-caluma-portal/app/services/apollo.js
@@ -1,29 +1,11 @@
-import { setContext } from "@apollo/client/link/context";
-import { onError } from "@apollo/client/link/error";
 import { inject as service } from "@ember/service";
 import CalumaApolloService from "@projectcaluma/ember-core/services/apollo";
+import { apolloMiddleware } from "ember-simple-auth-oidc";
 
 export default class CustomCalumaApolloService extends CalumaApolloService {
   @service session;
 
   link() {
-    const httpLink = super.link();
-
-    const middleware = setContext(async (_, context) => {
-      await this.session.refreshAuthentication.perform();
-
-      return {
-        ...context,
-        headers: { ...context.headers, ...this.session.headers },
-      };
-    });
-
-    const afterware = onError((error) => {
-      if (error.networkError && error.networkError.statusCode === 401) {
-        this.session.handleUnauthorized();
-      }
-    });
-
-    return middleware.concat(afterware).concat(httpLink);
+    return apolloMiddleware(super.link(), this.session);
   }
 }
