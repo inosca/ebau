53f07c22db2b107c2eb76cb3e1a8ca406073b6f2
Christian Zosel <christian@zosel.ch>
2022-03-22 15:29:06 +0100

    chore(refactor): only save ebau numbers on case

--- django/camac/core/tests/test_utils.py
+++ django/camac/core/tests/test_utils.py
@@ -1,80 +1,34 @@
 import pytest
 
-from camac.constants.kt_bern import CHAPTER_EBAU_NR, QUESTION_EBAU_NR
-from camac.core import models, utils
+from camac.core import utils
 
 
 @pytest.mark.freeze_time("2020-10-16")
-@pytest.mark.parametrize(
-    "question__question_id,chapter__chapter_id",
-    [
-        (QUESTION_EBAU_NR, CHAPTER_EBAU_NR),
-    ],  # noqa: E231
-)
-def test_max_ebau_nr(db, instance_factory, camac_answer_factory, question, chapter):
-    # no answer
+def test_max_ebau_nr(db, case_factory, question, chapter):
     assert utils.generate_ebau_nr(2020) == "2020-1"
 
-    # unrelated answer
-    camac_answer_factory(instance=instance_factory(), answer="foo")
+    case_factory(meta={"ebau-number": "2020-123"})
+    case_factory(meta={"ebau-number": "2020-99"})
+    assert utils.generate_ebau_nr(2020) == "2020-124"
 
-    assert utils.generate_ebau_nr(2020) == "2020-1"
-
-    camac_answer_factory(
-        instance=instance_factory(), question=question, chapter=chapter, answer="2020-1"
-    )
-    assert utils.generate_ebau_nr(2020) == "2020-2"
-
-    camac_answer_factory(
-        instance=instance_factory(),
-        question=question,
-        chapter=chapter,
-        answer="2020-10",
-    )
-    assert utils.generate_ebau_nr(2020) == "2020-11"
-
-    camac_answer_factory(
-        instance=instance_factory(),
-        question=question,
-        chapter=chapter,
-        answer="2020-10",
-    )
-    assert utils.generate_ebau_nr(2020) == "2020-11"
-
-    camac_answer_factory(
-        instance=instance_factory(),
-        question=question,
-        chapter=chapter,
-        answer="2019-100",
-    )
+    case_factory(meta={"ebau-number": "2019-100"})
     assert utils.generate_ebau_nr(2019) == "2019-101"
-    assert utils.generate_ebau_nr(2020) == "2020-11"
-
-    camac_answer_factory(
-        instance=instance_factory(),
-        question=question,
-        chapter=chapter,
-        answer="2020-99",
-    )
-    assert utils.generate_ebau_nr(2020) == "2020-100"
-
-    assert utils.generate_ebau_nr(2011) == "2011-1"
+    assert utils.generate_ebau_nr(2020) == "2020-124"
+    assert utils.generate_ebau_nr(2021) == "2021-1"
 
 
 @pytest.mark.freeze_time("2020-10-16")
-@pytest.mark.parametrize(
-    "question__question_id,chapter__chapter_id",
-    [
-        (QUESTION_EBAU_NR, CHAPTER_EBAU_NR),
-    ],  # noqa: E231
-)
-def test_assign_ebau_nr(db, camac_answer_factory, question, chapter, instance_factory):
-    inst = instance_factory()
+def test_assign_ebau_nr(
+    db,
+    question,
+    case_factory,
+    chapter,
+    instance_with_case,
+    instance_factory,
+    caluma_workflow_config_be,
+):
+    inst = instance_with_case(instance_factory())
     ebau_nr = utils.assign_ebau_nr(inst)
-
-    assert models.Answer.objects.filter(
-        instance=inst, question=question, chapter=chapter
-    ).exists()
 
     assert ebau_nr == "2020-1"
 
@@ -84,18 +38,12 @@ def test_assign_ebau_nr(db, camac_answer_factory, question, chapter, instance_fa
     # no change of year
     assert utils.assign_ebau_nr(inst, 2019) == "2020-1"
 
-    inst2 = instance_factory()
+    inst2 = instance_with_case(instance_factory())
     assert utils.assign_ebau_nr(inst2, 2020) == "2020-2"
 
-    inst3 = instance_factory()
+    inst3 = instance_with_case(instance_factory())
     assert utils.assign_ebau_nr(inst3, 2019) == "2019-1"
 
-    camac_answer_factory(
-        instance=instance_factory(),
-        question=question,
-        chapter=chapter,
-        answer="2017-420",
-    )
-
-    inst4 = instance_factory()
+    case_factory(meta={"ebau-number": "2017-420"})
+    inst4 = instance_with_case(instance_factory())
     assert utils.assign_ebau_nr(inst4, 2017) == "2017-421"
