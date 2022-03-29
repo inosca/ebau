a3383f401e4777bc885987f32aef5f7da31d2edd
René Birrer <rene.birrer@adfinis.com>
2022-03-29 15:43:29 +0200

    feat(ech0211): refactor swagger and ech0211 to separate configs for be and sz

--- django/camac/swagger/tests/test_swagger.py
+++ django/camac/swagger/tests/test_swagger.py
@@ -1,8 +1,12 @@
+import pytest
 from django.urls import reverse
 from rest_framework import status
 
 
-def test_swagger_schema(db, user, client, caplog):
-    response = client.get(reverse("schema-json", args=[".json"]))
-    assert response.status_code == status.HTTP_200_OK
-    assert not len(caplog.messages)
+@pytest.mark.parametrize("config", ["kt_bern", "kt_schwyz", "demo"])
+def test_swagger_schema(db, user, client, settings, config, caplog):
+    settings.APPLICATION = settings.APPLICATIONS[config]
+    if settings.APPLICATION["ECH0211"]["API_ACTIVE"]:
+        response = client.get(reverse("schema-json", args=[".json"]))
+        assert response.status_code == status.HTTP_200_OK
+        assert not len(caplog.messages)
