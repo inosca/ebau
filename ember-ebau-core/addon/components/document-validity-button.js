19619373c9dc3afb4b569030e99dfef6377ccbc5
Jonas Metzener <jonas.metzener@adfinis.com>
2022-04-06 13:53:18 +0200

    feat(be): add button for validating the audit forms

---
+++ ember-ebau-core/addon/components/document-validity-button.js
@@ -0,0 +1,20 @@
+import Component from "@glimmer/component";
+import { dropTask } from "ember-concurrency";
+
+export default class DocumentValidityButtonComponent extends Component {
+  validateOnEnter = false;
+  showLoadingHint = false;
+  type = "button";
+
+  get invalidFields() {
+    return this.args.field.document.fields.filter(
+      (field) => !field.hidden && field.isInvalid
+    );
+  }
+
+  @dropTask
+  *validate(validateFn) {
+    yield validateFn();
+    yield this.afterValidate?.perform();
+  }
+}
