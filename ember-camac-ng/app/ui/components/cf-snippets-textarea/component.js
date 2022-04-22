6cc9d9e275a68e0b31fdf5149717049608dd35c7
Jonas Metzener <jonas.metzener@adfinis.com>
2022-04-22 11:32:54 +0200

    chore(ember): rename textcomponents to snippets

--- ember-camac-ng/app/ui/components/cf-snippets-textarea/component.js
+++ ember-camac-ng/app/ui/components/cf-snippets-textarea/component.js
@@ -1,7 +1,7 @@
 import { action } from "@ember/object";
 import Component from "@glimmer/component";
 
-export default class CfTextareaTextcomponentComponent extends Component {
+export default class CfSnippetsTextareaComponent extends Component {
   @action
   save({ target: { value } }) {
     this.args.onSave(value);
--- ember-camac-ng/tests/integration/ui/components/cf-snippets-text/component-test.js
+++ ember-camac-ng/tests/integration/ui/components/cf-snippets-text/component-test.js
@@ -5,7 +5,7 @@ import { setupIntl } from "ember-intl/test-support";
 import { setupRenderingTest } from "ember-qunit";
 import { module, test } from "qunit";
 
-module("Integration | Component | cf-text-textcomponent", function (hooks) {
+module("Integration | Component | cf-snippets-text", function (hooks) {
   setupRenderingTest(hooks);
   setupMirage(hooks);
   setupIntl(hooks);
@@ -13,7 +13,7 @@ module("Integration | Component | cf-text-textcomponent", function (hooks) {
   test("it renders", async function (assert) {
     this.field = { answer: { value: "" } };
 
-    await render(hbs`<CfTextTextcomponent  @field={{@field}}/>`);
+    await render(hbs`<CfSnippetsText @field={{@field}}/>`);
 
     assert.dom(this.element).hasText("t:global.empty:()");
   });
