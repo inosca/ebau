b6b5ae30c54287baa1ddd1549b3bf6073c018233
Christian Zosel <christian@zosel.ch>
2022-04-28 08:23:46 +0200

    feat: move history to core and integrate in ember-ebau

--- ember-ebau-core/addon/components/history.js
+++ ember-ebau-core/addon/components/history.js
@@ -1,11 +1,11 @@
-import Controller from "@ember/controller";
 import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
+import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
 import { dropTask } from "ember-concurrency";
 import { useTask } from "ember-resources";
 
-export default class HistoryController extends Controller {
+export default class HistoryComponent extends Component {
   @service store;
   @service shoebox;
 
@@ -20,7 +20,7 @@ export default class HistoryController extends Controller {
     yield Promise.resolve();
 
     const entries = yield this.store.query("history-entry", {
-      instance: this.model.id,
+      instance: this.args.instanceId,
       include: "user",
       sort: "-created_at",
       "page[number]": page,
