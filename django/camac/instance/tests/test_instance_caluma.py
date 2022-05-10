0104ace1e381ab83ec79cd984cf05c8b2dd6f00c
Jonas Metzener <jonas.metzener@adfinis.com>
2022-05-10 14:22:57 +0200

    feat(distribution): move circulation suggestions to filter for services

--- django/camac/instance/tests/test_instance_caluma.py
+++ django/camac/instance/tests/test_instance_caluma.py
@@ -15,7 +15,7 @@ from rest_framework import status
 from camac.caluma.api import CalumaApi
 from camac.conftest import CALUMA_FORM_TYPES_SLUGS
 from camac.constants import kt_bern as be_constants, kt_uri as ur_constants
-from camac.core.models import Chapter, ProposalActivation, Question, QuestionType
+from camac.core.models import Chapter, Question, QuestionType
 from camac.ech0211 import event_handlers
 from camac.ech0211.data_preparation import DocumentParser
 from camac.ech0211.tests.caluma_document_data import baugesuch_data
@@ -1458,134 +1458,6 @@ def test_instance_delete(
 
     with pytest.raises(instance.DoesNotExist):
         instance.refresh_from_db()
-
-
-@pytest.mark.parametrize("service_group__name", ["municipality"])
-@pytest.mark.parametrize("instance_state__name", ["new"])
-@pytest.mark.parametrize(
-    "role__name,instance__user", [("Applicant", LazyFixture("admin_user"))]
-)
-@pytest.mark.parametrize("new_instance_state_name", ["subm"])
-@pytest.mark.parametrize(
-    "sugg_config,sugg_answer_values,expected_services",
-    [
-        ([], [], []),
-        (
-            [("non-existing-question", "foo", [0])],
-            [("baubeschrieb", ["baubeschrieb-erweiterung-anbau"])],
-            [],
-        ),
-        (
-            [("baubeschrieb", "baubeschrieb-erweiterung-anbau", [1234])],
-            [("baubeschrieb", ["baubeschrieb-um-ausbau"])],
-            [],
-        ),
-        (
-            [
-                ("baubeschrieb", "baubeschrieb-erweiterung-anbau", [1234]),
-                ("baubeschrieb", "baubeschrieb-um-ausbau", [5678]),
-                ("non-existing-question", "foo", [0]),
-            ],
-            [("baubeschrieb", ["baubeschrieb-erweiterung-anbau"])],
-            [1234],
-        ),
-        (
-            [
-                ("baubeschrieb", "baubeschrieb-erweiterung-anbau", [1234]),
-                ("art-versickerung-dach", "oberflaechengewaesser", [5678]),
-            ],
-            [
-                ("baubeschrieb", ["baubeschrieb-erweiterung-anbau"]),
-                ("art-versickerung-dach", "oberflaechengewaesser"),
-            ],
-            [1234, 5678],
-        ),
-        (
-            [
-                ("baubeschrieb", "baubeschrieb-erweiterung-anbau", [1234, 5678]),
-                ("art-versickerung-dach", "some value", [999]),
-                ("non-existing-question", "foo", [0]),
-            ],
-            [
-                (
-                    "baubeschrieb",
-                    ["baubeschrieb-erweiterung-anbau", "baubeschrieb-um-ausbau"],
-                ),
-                ("art-versickerung-dach", "some value"),
-            ],
-            [1234, 5678, 999],
-        ),
-    ],
-)
-def test_instance_submit_suggestions(
-    mocker,
-    admin_client,
-    role,
-    role_factory,
-    group_factory,
-    be_instance,
-    instance_state_factory,
-    service,
-    service_factory,
-    admin_user,
-    new_instance_state_name,
-    notification_template,
-    submit_date_question,
-    settings,
-    mock_public_status,
-    use_caluma_form,
-    multilang,
-    application_settings,
-    mock_nfd_permissions,
-    mock_generate_and_store_pdf,
-    ech_mandatory_answers_einfache_vorabklaerung,
-    caluma_workflow_config_be,
-    circulation_state_factory,
-    circulation_type_factory,
-    sugg_config,
-    sugg_answer_values,
-    expected_services,
-    caluma_admin_user,
-):
-    circulation_state_factory(
-        circulation_state_id=be_constants.CIRCULATION_STATE_WORKING
-    )
-    circulation_type_factory(circulation_type_id=be_constants.CIRCULATION_TYPE_STANDARD)
-    application_settings["NOTIFICATIONS"]["SUBMIT"] = [
-        {"template_slug": notification_template.slug, "recipient_types": ["applicant"]}
-    ]
-
-    be_instance.case.document.answers.create(
-        value=str(service.pk), question_id="gemeinde"
-    )
-
-    if sugg_config:
-        application_settings["SUGGESTIONS"] = sugg_config
-        for config in sugg_config:
-            for service_id in config[2]:
-                service_factory(pk=service_id)
-
-        for ans in sugg_answer_values:
-            be_instance.case.document.answers.create(question_id=ans[0], value=ans[1])
-
-    group_factory(role=role_factory(name="support"))
-    mocker.patch.object(
-        DocumentParser,
-        "parse_answers",
-        return_value=ech_mandatory_answers_einfache_vorabklaerung,
-    )
-    instance_state_factory(name=new_instance_state_name)
-
-    mocker.patch.object(event_handlers, "get_document", return_value=baugesuch_data)
-
-    response = admin_client.post(reverse("instance-submit", args=[be_instance.pk]))
-
-    assert response.status_code == status.HTTP_200_OK
-
-    assert (
-        list(ProposalActivation.objects.values_list("service_id", flat=True))
-        == expected_services
-    )
 
 
 @pytest.mark.parametrize("service_group__name", ["municipality"])
