ec4e82053cae03e5b72fc90d833aad2cc2be4306
Jonas Metzener <jonas.metzener@adfinis.com>
2022-05-11 14:59:08 +0200

    feat(distribution): configure various notifications
    - When distrubution started / ended
    - When inquiry was sent / answered

--- django/camac/caluma/extensions/events/distribution.py
+++ django/camac/caluma/extensions/events/distribution.py
@@ -7,11 +7,13 @@ from caluma.caluma_form.models import Form, Question
 from caluma.caluma_workflow.api import (
     cancel_work_item,
     complete_work_item,
+    skip_work_item,
     start_case,
     suspend_work_item,
 )
 from caluma.caluma_workflow.events import (
     post_complete_case,
+    post_complete_work_item,
     post_create_work_item,
     post_resume_work_item,
     pre_complete_work_item,
@@ -20,6 +22,25 @@ from caluma.caluma_workflow.models import Workflow, WorkItem
 from django.conf import settings
 from django.db import transaction
 from django.utils.timezone import now
+
+from camac.notification.utils import send_mail_without_request
+
+
+def send_inquiry_notification(settings_key, inquiry_work_item, user):
+    notification_config = settings.DISTRIBUTION["NOTIFICATIONS"].get(settings_key)
+
+    if notification_config:
+        send_mail_without_request(
+            notification_config["template_slug"],
+            user.username,
+            user.camac_group,
+            instance={
+                "id": inquiry_work_item.case.family.instance.pk,
+                "type": "instances",
+            },
+            inquiry={"id": inquiry_work_item.pk, "type": "work-items"},
+            recipient_types=notification_config["recipient_types"],
+        )
 
 
 def get_distribution_settings(settings_keys):
@@ -120,12 +141,30 @@ def post_resume_inquiry(sender, work_item, user, **kwargs):
     if init_work_item:
         complete_work_item(work_item=init_work_item, user=user)
 
+    # send notification to addressed service
+    send_inquiry_notification("INQUIRY_SENT", work_item, user)
+
+
+@on(post_complete_work_item, raise_exception=True)
+@filter_by_task("INQUIRY_TASK")
+@transaction.atomic
+def post_complete_inquiry(sender, work_item, user, **kwargs):
+    # send notification to controlling service
+    send_inquiry_notification("INQUIRY_ANSWERED", work_item, user)
+
 
 @on(pre_complete_work_item, raise_exception=True)
 @filter_by_task("DISTRIBUTION_COMPLETE_TASK")
 @transaction.atomic
 def pre_complete_distribution(sender, work_item, user, **kwargs):
     for work_item in work_item.case.work_items.filter(
+        task_id=settings.DISTRIBUTION["INQUIRY_TASK"], status=WorkItem.STATUS_READY
+    ):
+        # unanswered inquiries must be skipped
+        skip_work_item(work_item=work_item, user=user)
+
+    for work_item in work_item.case.work_items.filter(
         status__in=[WorkItem.STATUS_READY, WorkItem.STATUS_SUSPENDED]
     ):
+        # everything else canceled
         cancel_work_item(work_item=work_item, user=user)
