28eb5b2922ad538d618d113440d7a6ba35a71f2d
René Birrer <rene.birrer@adfinis.com>
2022-06-02 15:54:23 +0200

    fix(sz): add attachment download docs to swagger

--- django/camac/document/urls.py
+++ django/camac/document/urls.py
@@ -13,10 +13,6 @@ class UnswaggeredAttachmentSectionView(views.AttachmentSectionView):
     swagger_schema = None
 
 
-class UnswaggeredAttachmentDownloadView(views.AttachmentDownloadView):
-    swagger_schema = None
-
-
 r = SimpleRouter(trailing_slash=False)
 
 r.register(
@@ -37,16 +33,12 @@ r.register(r"attachment-download-history", views.AttachmentDownloadHistoryView)
 urlpatterns = [
     re_path(
         r"^(?P<path>attachments/files/.+\..+)$",
-        UnswaggeredAttachmentDownloadView.as_view({"get": "retrieve"})
-        if "document" in settings.APPLICATION.get("EXCLUDE_DOCS", [])
-        else views.AttachmentDownloadView.as_view({"get": "retrieve"}),
+        views.AttachmentDownloadView.as_view({"get": "retrieve"}),
         name="attachment-download",
     ),
     re_path(
         r"^attachments/files/",
-        UnswaggeredAttachmentDownloadView.as_view({"get": "list"})
-        if "document" in settings.APPLICATION.get("EXCLUDE_DOCS", [])
-        else views.AttachmentDownloadView.as_view({"get": "list"}),
+        views.AttachmentDownloadView.as_view({"get": "list"}),
         name="multi-attachment-download",
     ),
 ]
