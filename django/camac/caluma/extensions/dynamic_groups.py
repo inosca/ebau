60dc9334dbdeb4537234b76e230b1d1e31bc7d80
Jonas Metzener <jonas.metzener@adfinis.com>
2022-06-23 16:31:07 +0200

    fix(distribution): only one ready create-inquiry work item per service
    This makes sure that a service that already has a ready "create-inquiry"
    work item doesn't get another one.

--- django/camac/caluma/extensions/dynamic_groups.py
+++ django/camac/caluma/extensions/dynamic_groups.py
@@ -1,9 +1,12 @@
 import logging
+from itertools import chain
 
 from caluma.caluma_workflow.dynamic_groups import (
     BaseDynamicGroups,
     register_dynamic_group,
 )
+from caluma.caluma_workflow.models import WorkItem
+from django.conf import settings
 
 from camac.core.models import Activation, Circulation
 from camac.instance.models import Instance
@@ -93,15 +96,29 @@ class CustomDynamicGroups(BaseDynamicGroups):
         self, task, case, user, prev_work_item, context, **kwargs
     ):
         if context and context.get("addressed_groups") and prev_work_item:
+            # We should not create a new "create-inquiry" work item if there
+            # already is a ready one for that service.
+            existing_ids = set(
+                chain(
+                    *case.work_items.filter(
+                        task_id=settings.DISTRIBUTION["INQUIRY_CREATE_TASK"],
+                        status=WorkItem.STATUS_READY,
+                    ).values_list("addressed_groups", flat=True)
+                )
+            )
+
+            target_ids = {
+                # Target service for the inquiry
+                *context.get("addressed_groups", []),
+                # Service that created the inquiry
+                *prev_work_item.addressed_groups,
+            }
+
             services = Service.objects.filter(
-                pk__in=[
-                    # Target service for the inquiry
-                    *context.get("addressed_groups", []),
-                    # Service that created an inquiry
-                    *prev_work_item.addressed_groups,
-                ],
+                pk__in=target_ids - existing_ids,
                 service_parent__isnull=True,  # Subservices can't create any inquiries
             )
+
             return [str(pk) for pk in services.values_list("pk", flat=True)]
 
         # If no context is given it's the first "create-inquiry" work item in
