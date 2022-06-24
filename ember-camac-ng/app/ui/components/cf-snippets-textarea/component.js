07f0fc092d0476a6666ff7078fa1782c1f0b3dba
Jonas Metzener <jonas.metzener@adfinis.com>
2022-06-24 18:01:49 +0200

    fix(be): improve snippet input UX

--- ember-camac-ng/app/ui/components/cf-snippets-textarea/component.js
+++
@@ -1,9 +0,0 @@
-import { action } from "@ember/object";
-import Component from "@glimmer/component";
-
-export default class CfSnippetsTextareaComponent extends Component {
-  @action
-  applySnippet(content) {
-    this.args.onSave(`${this.args.field.answer.value ?? ""}${content}`, true);
-  }
-}
