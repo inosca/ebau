390575b3fde007fe050b2cd04834bef151e859bb
Jonas Metzener <jonas.metzener@adfinis.com>
2022-08-23 16:41:09 +0200

    feat(be): refactor organisation module to uikit

--- ember-camac-ng/app/ui/organisation/controller.js
+++ ember-camac-ng/app/ui/organisation/controller.js
@@ -1,39 +1,28 @@
 import Controller from "@ember/controller";
-import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
-import { dropTask, lastValue } from "ember-concurrency";
-import { loadingTask } from "ember-ebau-core/decorators";
+import { dropTask } from "ember-concurrency";
+import { findRecord } from "ember-data-resources";
 
 export default class OrganisationController extends Controller {
   @service intl;
   @service notifications;
   @service store;
+  @service shoebox;
 
-  @lastValue("fetchService") service;
-  @dropTask
-  @loadingTask
-  *fetchService() {
-    return yield this.store.findRecord("service", this.model);
-  }
+  service = findRecord(this, "service", () => [this.shoebox.content.serviceId]);
 
   @dropTask
-  @loadingTask
   *save(event) {
     event.preventDefault();
 
     try {
-      this.service.set("description", this.service.name);
+      this.service.record.set("description", this.service.record.name);
 
-      yield this.service.save();
+      yield this.service.record.save();
 
       this.notifications.success(this.intl.t("organisation.saveSuccess"));
     } catch (error) {
       this.notifications.error(this.intl.t("organisation.saveError"));
     }
-  }
-
-  @action
-  toggleNotifications() {
-    this.service.set("notification", this.service.notification);
   }
 }
