9491618f5243818b00627bbb22edbe92a5243eb4
Jonas Lehmann <jonas.lehmann@adfinis.com>
2022-09-05 08:21:21 +0200

    chore(ur): backport the old forms migration changes

--- django/camac/core/management/commands/migrate_old_forms_ur.py
+++ django/camac/core/management/commands/migrate_old_forms_ur.py
@@ -1,7 +1,8 @@
-from caluma.caluma_form.models import Answer, Question
+from caluma.caluma_form.models import Answer, DynamicOption, Question
 from django.core.management.base import BaseCommand
 from django.db import transaction
 
+from camac.caluma.extensions.data_sources import form_mapping_by_koor
 from camac.instance.models import Form, Instance
 
 FORM_ID_MAPPING = {
@@ -62,6 +63,9 @@ ANSWER_MITBERICHT_BUND_MAPPING = {
     250: "mbv-bund-type-pgv-seilbahn",
 }
 
+values = list(form_mapping_by_koor.values())
+flat = [item for sublist in values for item in sublist]
+
 
 class Command(BaseCommand):
     help = """Migrate the old forms to new ones."""
@@ -77,30 +81,41 @@ class Command(BaseCommand):
             form_id__in=list(FORM_ID_MAPPING.keys())
         )
 
+        self.stdout.write("INSTANCE_ID, OLD_FORM_ID, NEW_FORM_ID")
         for instance in instances_with_old_forms:
             new_form = Form.objects.get(pk=FORM_ID_MAPPING[instance.form.pk])
             old_form = instance.form
             instance.form = new_form
             instance.save()
-            self.stdout.write(
-                f"The form for instance {instance.instance_id} has changed from {old_form} to {new_form}"
-            )
+            self.stdout.write(f"{instance.instance_id}, {old_form.pk}, {new_form.pk}")
 
             if new_form.pk == 300:  # Mitberichtsverfahren Kanton
                 special_cases = {
                     43: 42,  # Int. Genehmigungsverf. -> Int. Mitbericht
                 }
                 value = special_cases.get(old_form.form_id, old_form.form_id)
-                Answer.objects.create(
-                    value=str(value),
+                _, created = Answer.objects.get_or_create(
                     document=instance.case.document,
                     question=Question.objects.get(slug="mbv-type"),
+                    defaults={"value": str(value)},
                 )
+
+                if created:
+                    pair = next(pair for pair in flat if pair[0] == value)
+                    DynamicOption.objects.get_or_create(
+                        document=instance.case.document,
+                        question=Question.objects.get(slug="mbv-type"),
+                        slug=str(value),
+                        defaults={"label": pair[1]},
+                    )
+
             elif new_form.pk == 292:  # Mitberichtsverfahren Bundesstelle
-                Answer.objects.create(
-                    value=ANSWER_MITBERICHT_BUND_MAPPING[old_form.form_id],
+                Answer.objects.get_or_create(
                     document=instance.case.document,
                     question=Question.objects.get(slug="mbv-bund-type"),
+                    defaults={
+                        "value": ANSWER_MITBERICHT_BUND_MAPPING[old_form.form_id]
+                    },
                 )
 
             try:
@@ -108,23 +123,15 @@ class Command(BaseCommand):
                     question_id="form-type"
                 ).first()
 
-                old_value = answer.value
-
                 answer.value = FORM_TYPE_MAPPING[new_form.pk]
 
                 answer.save()
-                self.stdout.write(
-                    f"Fixed form type of instance {instance.pk} (was {old_value})"
-                )
             except AttributeError:
 
                 Answer.objects.create(
                     value=FORM_TYPE_MAPPING[new_form.pk],
                     document=instance.case.document,
                     question=Question.objects.get(slug="form-type"),
-                )
-                self.stdout.write(
-                    f"There was no old form-type answer for the instance {instance.pk}. Therefore the new form-type {FORM_TYPE_MAPPING[new_form.pk]} was created"
                 )
 
         if options["dry"]:
