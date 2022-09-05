inosca/ebau

commit 6915bbeee2213fe9adabe12c4f6c12c84280d4a9

feat(be) new dossier overview site

ember-caluma-portal/app/instances/edit/template.hbs
@@ -1,5 +1,12 @@
+<h2>
+  {{this.instance.mainForm.name}} {{this.instance.typeDetail}}
+  ({{t "instances.instanceId"}} {{this.model}})
+</h2>
 {{#if (can "read instance" this.instance)}}
   <ul class="uk-tab">
+    <BeSubnavLink @route="instances.edit.index">
+      {{t "nav.overview"}}
+    </BeSubnavLink>
     {{#if this.instance.mainForm}}
       <BeSubnavLink
         @route="instances.edit.form"
@@ -16,13 +23,6 @@
         </BeSubnavLink>
       {{/if}}
     {{/each}}
-
-    {{#if (can "read feedback of instance" this.instance)}}
-      <BeSubnavLink @route="instances.edit.feedback">
-        {{t "nav.feedback"}}
-        {{#if this.feedback}}<UkBadge @label={{this.feedback.length}} class="uk-margin-small-left" />{{/if}}
-      </BeSubnavLink>
-    {{/if}}
 
     {{#if (can "manage applicants of instance" this.instance)}}
       <BeSubnavLink @route="instances.edit.applicants">

ember-caluma-portal/app/instances/edit/controller.js
@@ -31,7 +31,12 @@ export default class InstancesEditController extends Controller.extend(
   @dropTask
   *instanceTask() {
     const instance = yield this.store.findRecord("instance", this.model, {
-      include: "instance_state,involved_applicants,involved_applicants.invitee"
+      include: [
+        "instance_state",
+        "involved_applicants",
+        "involved_applicants.invitee",
+        "active_service"
+      ].join(",")
     });
 
     yield instance.getDocuments.perform();

ember-caluma-portal/app/instances/edit/index/controller.js
@@ -1,0 +1,92 @@
+import Controller, { inject as controller } from "@ember/controller";
+import { reads } from "@ember/object/computed";
+import { queryManager } from "ember-apollo-client";
+import getOverviewDocumentsQuery from "ember-caluma-portal/gql/queries/get-overview-documents";
+import { dropTask, lastValue } from "ember-concurrency-decorators";
+import QueryParams from "ember-parachute";
+
+const findAnswer = (answers, slug) => {
+  const answer = answers.find(answer => answer.question.slug === slug);
+
+  if (!answer) {
+    return null;
+  }
+
+  const key = Object.keys(answer).find(key => /Value$/.test(key));
+
+  return answer[key];
+};
+
+function getAddress(answers) {
+  const street = findAnswer(answers, "strasse-flurname");
+  const number = findAnswer(answers, "nr");
+  const city = findAnswer(answers, "ort-grundstueck");
+  return `${street} ${number}, ${city}`;
+}
+
+function getEbauNr(document) {
+  return document.node.meta["ebau-number"];
+}
+
+function getType(document) {
+  return document.node.form.name;
+}
+
+function getMunicipality(document) {
+  const answer = document.node.answers.edges.find(
+    answer => answer.node.question.slug === "gemeinde"
+  );
+  return (
+    answer &&
+    answer.node.question.options.edges.find(({ node: { slug } }) => {
+      return answer.node.stringValue === slug;
+    }).node.label
+  );
+}
+
+function getBuildingSpecification(answers) {
+  return findAnswer(answers, "beschreibung-bauvorhaben");
+}
+
+export default class InstancesEditIndexController extends Controller.extend(
+  new QueryParams().Mixin
+) {
+  @queryManager apollo;
+
+  @controller("instances.edit") editController;
+  @reads("editController.feedbackTask.isRunning") feedbackLoading;
+  @reads("editController.feedback") feedback;
+  @reads("editController.instance.instanceState.name") instanceState;
+  @reads("editController.instance.activeService.name") activeService;
+
+  setup() {
+    this.dataTask.perform(this.model);
+  }
+
+  @lastValue("dataTask") data;
+  @dropTask
+  *dataTask() {
+    const allDocuments = yield this.apollo.query(
+      {
+        fetchPolicy: "network-only",
+        query: getOverviewDocumentsQuery,
+        variables: {
+          instanceId: this.model
+        }
+      },
+      "allDocuments.edges"
+    );
+    const document = allDocuments.find(
+      doc => doc.node.form.meta["is-main-form"]
+    );
+    const answers = document.node.answers.edges.map(answer => answer.node);
+
+    return {
+      address: getAddress(answers),
+      ebauNr: getEbauNr(document),
+      type: getType(document),
+      municipality: getMunicipality(document),
+      buildingSpecification: getBuildingSpecification(answers)
+    };
+  }
+}

ember-caluma-portal/app/instances/edit/index/template.hbs
@@ -1,0 +1,75 @@
+<div class="uk-grid-collapse" uk-grid>
+  <h3 class="uk-width-1-3 uk-text-uppercase">{{t "instances.masterData"}}</h3>
+  <div class="uk-width-2-3">
+    <table class="uk-table">
+      <tbody>
+        <tr>
+          <th class="uk-text-bold">
+            {{t "instances.ebau"}}
+          </th>
+          <td>{{this.data.ebauNr}}</td>
+          <th class="uk-text-bold">
+            {{t "instances.address"}}
+          </th>
+          <td>{{this.data.address}}</td>
+        </tr>
+        <tr>
+          <th class="uk-text-bold">
+            {{t "instances.type"}}
+          </th>
+          <td>{{this.data.type}}</td>
+          <th class="uk-text-bold">
+            {{t "instances.municipality"}}
+          </th>
+          <td>{{this.data.municipality}}</td>
+        </tr>
+        <tr>
+          <th class="uk-text-bold">
+            {{t "instances.status.title"}}
+          </th>
+          <td>{{this.instanceState}}</td>
+          <th class="uk-text-bold">
+            {{t "instances.lead"}}
+          </th>
+          <td>{{this.activeService}}</td>
+        </tr>
+        <tr>
+          <th></th>
+          <td></td>
+          <th class="uk-text-bold">
+            {{t "instances.buildingSpecification"}}
+          </th>
+          <td>{{this.data.buildingSpecification}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
+<hr>
+<div class="uk-margin-top uk-grid-collapse" uk-grid>
+  <h3 class="uk-width-1-3 uk-text-uppercase">
+    {{t "instances.actions"}}
+  </h3>
+  <div class="uk-width-2-3 uk-padding-small uk-grid-small" uk-grid>
+    <h4 class="uk-width-2-3 uk-text-uppercase">{{t "instances.activateFurtherPersons"}}</h4>
+    <p class="uk-width-3-3">{{t "instances.activateFurtherPersonsText"}}</p>
+    <LinkTo @route="instances.edit.applicants" class="uk-button uk-button-default uk-width-1-3">
+      {{t "instances.managePermissions"}}
+    </LinkTo>
+  </div>
+</div>
+<hr>
+<div class="uk-margin-top uk-grid-collapse" uk-grid>
+  <h3 class="uk-width-1-3 uk-text-uppercase">
+    {{t "nav.feedback"}}
+    {{#if this.feedback}}<UkBadge @label={{this.feedback.length}} class="uk-margin-small-left" />{{/if}}
+  </h3>
+  <div class="uk-width-2-3 uk-padding-small uk-padding-remove-top">
+    {{#if this.feedback}}
+      {{be-attachment-list loading=this.feedbackLoading attachments=this.feedback}}
+      <hr>
+    {{else}}
+      <div>{{t "feedback.info"}}</div>
+    {{/if}}
+  </div>
+</div>

ember-caluma-portal/app/gql/queries/get-overview-documents.graphql
@@ -1,0 +1,82 @@
+query GetOverviewDocuments($instanceId: GenericScalar!) {
+  allDocuments(metaValue: [{ key: "camac-instance-id", value: $instanceId }]) {
+    edges {
+      node {
+        id
+        meta
+        form {
+          slug
+          name
+        }
+        answers(
+          questions: [
+            "name-gesuchstellerin-vorabklaerung"
+            "vorname-gesuchstellerin-vorabklaerung"
+            "anfrage-zur-vorabklaerung"
+            "strasse-gesuchstellerin"
+            "nummer-gesuchstellerin"
+            "plz-gesuchstellerin"
+            "ort-gesuchstellerin"
+            "strasse-flurname"
+            "nr"
+            "ort-grundstueck"
+            "beschreibung-bauvorhaben"
+            "beschreibung-projektaenderung"
+            "personalien-gesuchstellerin"
+            "gemeinde"
+            "parzelle"
+            "parzellennummer"
+          ]
+        ) {
+          edges {
+            node {
+              id
+              question {
+                slug
+                ... on DynamicChoiceQuestion {
+                  options {
+                    edges {
+                      node {
+                        slug
+                        label
+                      }
+                    }
+                  }
+                }
+              }
+              ... on StringAnswer {
+                stringValue: value
+              }
+              ... on IntegerAnswer {
+                integerValue: value
+              }
+              ... on TableAnswer {
+                tableValue: value {
+                  answers(
+                    questions: [
+                      "name-gesuchstellerin"
+                      "vorname-gesuchstellerin"
+                      "name-juristische-person-gesuchstellerin"
+                      "parzellennummer"
+                    ]
+                  ) {
+                    edges {
+                      node {
+                        question {
+                          slug
+                        }
+                        ... on StringAnswer {
+                          stringValue: value
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}

ember-caluma-portal/app/instances/edit/index/route.js
@@ -1,15 +1,23 @@
 import Route from "@ember/routing/route";
-import { next } from "@ember/runloop";
 
-export default Route.extend({
-  redirect() {
-    next(async () => {
-      const controller = this.controllerFor("instances.edit");
-      const instance = await controller.instanceTask.last;
-
-      if (instance && instance.mainForm) {
-        this.replaceWith("instances.edit.form", instance.mainForm.slug);
-      }
-    });
+export default class InstancesEditIndexRoute extends Route {
+  model() {
+    return this.modelFor("instances.edit");
   }
-});
+}
+
+// import Route from "@ember/routing/route";
+// import { next } from "@ember/runloop";
+
+// export default Route.extend({
+//   redirect() {
+//     next(async () => {
+//       const controller = this.controllerFor("instances.edit");
+//       const instance = await controller.instanceTask.last;
+
+//       if (instance && instance.mainForm) {
+//         this.replaceWith("instances.edit.form", instance.mainForm.slug);
+//       }
+//     });
+//   }
+// });

ember-caluma-portal/app/models/instance.js
@@ -20,6 +20,7 @@ export default class Instance extends Model {
   @attr("boolean") isPaper;
   @attr("boolean") isModification;
   @belongsTo("instance-state") instanceState;
+  @belongsTo("public-service") activeService;
   @hasMany("applicant", { inverse: "instance" }) involvedApplicants;
 
   @computed("intl.locale", "publicStatus")

ember-caluma-portal/mirage/factories/instance.js
@@ -1,5 +1,6 @@
 import { Factory, association } from "ember-cli-mirage";
 
 export default Factory.extend({
-  instanceState: association()
+  instanceState: association(),
+  activeService: association()
 });

commit d257620e3511c69f52b05bce6778671044cb1be4

fix(portal): re-add missing link to feedback route
This fixes a regression from 6915bbeee2213fe9adabe12c4f6c12c84280d4a9

ember-caluma-portal/app/instances/edit/feedback/controller.js
@@ -4,10 +4,10 @@ export default class InstancesEditFeedbackController extends Controller {
   @controller("instances.edit") editController;
 
   get loading() {
-    return this.editController.feeback.isRunning;
+    return this.editController.feeback?.isRunning;
   }
 
   get feedback() {
-    return this.editController.feedback.value;
+    return this.editController.feedback?.value;
   }
 }

ember-caluma-portal/app/instances/edit/template.hbs
@@ -36,6 +36,14 @@
           </Tab.item>
         {{/if}}
       {{/each}}
+      {{#if (can "read feedback of instance" this.instance)}}
+        <Tab.item @href={{concat "/instances/" @model "/feedback"}} as |active|>
+          <AriaHeading @enabled={{active}}>
+            {{t "nav.feedback"}}
+            {{#if this.feedback.value}}<UkBadge @label={{this.feedback.value.length}} class="uk-margin-small-left" />{{/if}}
+          </AriaHeading>
+        </Tab.item>
+      {{/if}}
       {{#if (can "read applicants of instance" this.instance.value)}}
         <Tab.item @href={{concat "/instances/" @model "/applicants"}} as |active|>
           <AriaHeading @enabled={{active}}>