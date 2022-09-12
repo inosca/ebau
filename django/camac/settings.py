999f830d0980ec40af94683a7d4972a1a9c2bc11
Christian Zosel <christian@zosel.ch>
2022-09-12 10:40:24 +0200

    fix: don't overwrite ENV with .env content when testing

--- django/camac/settings.py
+++ django/camac/settings.py
@@ -22,7 +22,9 @@ env = environ.Env()
 ROOT_DIR = environ.Path(__file__) - 2
 
 ENV_FILE = env.str("DJANGO_ENV_FILE", default=ROOT_DIR(".env"))
-if os.path.exists(ENV_FILE):  # pragma: no cover
+
+# overwrite ENV with contents of .env, except while running pytest
+if env.str("APPLICATION_ENV") != "ci" and os.path.exists(ENV_FILE):  # pragma: no cover
     environ.Env.read_env(ENV_FILE, True)
 
 # We need to import the caluma settings after we merge os.environ with our
