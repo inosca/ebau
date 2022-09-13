241d7b149fb73e0421add576e3d6683990507674
Jonas Metzener <jonas.metzener@adfinis.com>
2022-09-13 14:26:33 +0200

    chore(ember): use component colocation structure

---
+++ ember-camac-ng/app/components/loading-text.js
@@ -0,0 +1,24 @@
+import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
+import { dropTask, timeout } from "ember-concurrency";
+
+import config from "camac-ng/config/environment";
+
+export default class LoadingTextComponent extends Component {
+  @tracked dots = ".";
+
+  @dropTask
+  *animate() {
+    while (config.environment !== "test") {
+      yield timeout(500);
+
+      if (this.dots.length >= 3) {
+        this.dots = ".";
+      } else {
+        this.dots += ".";
+      }
+
+      this.animate.perform();
+    }
+  }
+}
