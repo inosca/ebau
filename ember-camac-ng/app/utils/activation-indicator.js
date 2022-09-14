8b4247dbd7fe07b828834dc8635dc67b55d130c4
Jonas Lehmann <jonas.lehmann@adfinis.com>
2022-09-14 10:19:51 +0200

    fix(ur): fix the displayed activation states of involved services

--- ember-camac-ng/app/utils/activation-indicator.js
+++ ember-camac-ng/app/utils/activation-indicator.js
@@ -8,14 +8,14 @@ export default function getActivationIndicator(activation) {
   const now = DateTime.now();
   if (activation.state === "NFD") {
     return "nfd";
+  } else if (activation.endDate || activation.suspensionDate) {
+    return "completed";
   } else if (DateTime.fromISO(activation.deadlineDate) < now) {
     return "expired";
   } else if (
     DateTime.fromISO(activation.deadlineDate).minus({ days: 5 }) < now
   ) {
     return "due-shortly";
-  } else if (activation.endDate || activation.suspensionDate) {
-    return "completed";
   }
 
   return null;
