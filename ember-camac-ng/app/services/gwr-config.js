3547825d337f273ed57d9f37d1b7bf9e00da09c3
Alana Luyten <alana.luyten@adfinis.com>
2022-09-16 16:07:48 +0200

    chore(gwr): update gwr dependencies and apply config changes

--- ember-camac-ng/app/services/gwr-config.js
+++ ember-camac-ng/app/services/gwr-config.js
@@ -1,3 +1,4 @@
+import { action } from "@ember/object";
 import Service, { inject as service } from "@ember/service";
 
 import ENV from "camac-ng/config/environment";
@@ -5,10 +6,15 @@ import ENV from "camac-ng/config/environment";
 export default class GwrConfigService extends Service {
   @service session;
   @service shoebox;
+  @service store;
 
   gwrAPI = "/housing-stat/regbl/api/ech0216/2";
   isTestEnvironment = ENV.appEnv !== "production";
   pageSize = 10;
+  projectSortColumn = "bau_modify_date";
+  projectSortDirection = "desc";
+  buildingSortColumn = "geb_modify_date";
+  buildingSortDirection = "desc";
 
   get modalContainer() {
     return ENV.APPLICATION.gwr.modalContainer ?? "#ember-camac-ng";
@@ -28,5 +34,18 @@ export default class GwrConfigService extends Service {
 
   get camacGroup() {
     return this.shoebox.content.groupId;
+  }
+
+  @action
+  async fetchInstanceLinks(localIds) {
+    const instances = await this.store.query("instance", {
+      instance_id: [...new Set(localIds)].join(","),
+    });
+
+    return instances.map(({ id, identifier }) => ({
+      localId: id,
+      identifier,
+      hostLink: `/index/redirect-to-instance-resource/instance-id/${id}?instance-resource-name=gwr&ember-hash=/gwr/${id}`,
+    }));
   }
 }
