8c732c3e629eaba4151c34a32f07b8a809cfe2c4
Jonas Metzener <jonas.metzener@adfinis.com>
2022-09-27 10:13:26 +0200

    chore(lint): lint ember ebau with adfinis eslint config

--- ember-ebau/app/ui/components/sub-navigation/component.js
+++ ember-ebau/app/ui/components/sub-navigation/component.js
@@ -1,5 +1,5 @@
-import Component from "@glimmer/component";
 import { inject as service } from "@ember/service";
+import Component from "@glimmer/component";
 import { dropTask } from "ember-concurrency";
 import { trackedTask } from "ember-resources/util/ember-concurrency";
 
--- ember-ebau/tests/unit/ui/cases/detail/work-items/index/controller-test.js
+++ ember-ebau/tests/unit/ui/cases/detail/work-items/index/controller-test.js
@@ -1,12 +1,12 @@
-import { module, test } from "qunit";
 import { setupTest } from "ember-qunit";
+import { module, test } from "qunit";
 
 module("Unit | Controller | cases/detail/work-items/index", function (hooks) {
   setupTest(hooks);
 
   // TODO: Replace this with your real tests.
   test("it exists", function (assert) {
-    let controller = this.owner.lookup(
+    const controller = this.owner.lookup(
       "controller:cases/detail/work-items/index"
     );
     assert.ok(controller);
