f2f62a5e951c6c267680fde820e387177ebe7725
Jonas Lehmann <jonas.lehmann@adfinis.com>
2022-10-03 09:32:46 +0200

    feat(django-admin): improve django admin
    * Remove the `PermissionsMixin` in the user model and grant users access
      to django-admin if they are in the admin group
    * Only show the models user, group and service in django-admin
    * Handle display of multilingual models in django-admin

---
+++ django/camac/user/admin/filters.py
@@ -0,0 +1,21 @@
+from django.contrib.admin import SimpleListFilter
+from django.utils.translation import gettext as _
+
+
+class DisabledFilter(SimpleListFilter):
+    title = _("Deaktiviert?")
+    parameter_name = "disabled"
+
+    def lookups(self, request, model_admin):
+        return (
+            (1, _("Yes")),
+            (0, _("No")),
+        )
+
+    def queryset(self, request, queryset):
+        value = self.value()
+
+        if value is not None:
+            return queryset.filter(disabled=value)
+
+        return queryset
