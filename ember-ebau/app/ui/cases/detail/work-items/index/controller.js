6bbbe060d81e313b6362a9542e7e8ed99aa43e5c
Jonas Metzener <jonas.metzener@adfinis.com>
2022-10-07 14:58:13 +0200

    chore(camac-ng): replace service only controllers with session-value helper

--- ember-ebau/app/ui/cases/detail/work-items/index/controller.js
+++ ember-ebau/app/ui/cases/detail/work-items/index/controller.js
@@ -1,12 +1,9 @@
 import Controller from "@ember/controller";
 import { action } from "@ember/object";
-import { inject as service } from "@ember/service";
 import { tracked } from "@glimmer/tracking";
 
 export default class CasesDetailWorkItemsIndexController extends Controller {
   queryParams = ["role"];
-
-  @service session;
 
   @tracked role = "active";
 
