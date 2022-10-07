6fbdb7c302097c8019e74d03555f26344cc57852
Jonas Metzener <jonas.metzener@adfinis.com>
2022-10-07 14:55:38 +0200

    chore(camac-ng): replace service only controllers with shoebox-value helper

--- ember-camac-ng/app/ui/work-items/instance/index/controller.js
+++ ember-camac-ng/app/ui/work-items/instance/index/controller.js
@@ -1,12 +1,9 @@
 import Controller from "@ember/controller";
 import { action } from "@ember/object";
-import { inject as service } from "@ember/service";
 import { tracked } from "@glimmer/tracking";
 
 export default class WorkItemsInstanceIndexController extends Controller {
   queryParams = ["role"];
-
-  @service shoebox;
 
   @tracked role = "active";
 
