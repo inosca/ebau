a3d0503c579135e4d972246ac6ff8e37adbb0763
Jonas Metzener <jonas.metzener@adfinis.com>
2022-10-11 17:17:58 +0200

    feat(django-admin): minor improvements
    - Fix various translations
    - Remove useless links in header
    - Automatically redirect to login page

--- django/camac/admin.py
+++ django/camac/admin.py
@@ -3,7 +3,6 @@ from django.utils.translation import gettext as _
 
 
 class DjangoAdminSite(AdminSite):
-    login_template = "login.html"
     site_header = _("eBau")
     site_title = _("eBau")
     index_title = _("Administration")
