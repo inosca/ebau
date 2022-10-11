49a14282d8e6e700b2d70666f389e9d048c3401b
Jonas Metzener <jonas.metzener@adfinis.com>
2022-10-11 08:39:13 +0200

    fix(be): migrate eBau number forms of cases without existing work item

--- django/camac/core/management/commands/migrate_ebau_number_form.py
+++ django/camac/core/management/commands/migrate_ebau_number_form.py
@@ -1,44 +1,15 @@
-import functools
-
-from caluma.caluma_form.models import Answer, Document, Question
-from caluma.caluma_workflow.models import WorkItem
+from caluma.caluma_form.models import Answer, Document
+from caluma.caluma_workflow.models import Case, WorkItem
 from django.core.management.base import BaseCommand
 from django.db import transaction
-from django.db.models import Q
-from simple_history.utils import bulk_create_with_history
+from django.db.models import Exists, OuterRef
+from tqdm import tqdm
 
 from camac.core.models import Answer as CamacAnswer
 
-# item, chapter, question
-ANSWER_MAPPING = {
-    "ebau-number-has-existing": (1, 20000, 20034),
-    "ebau-number-existing": (1, 20000, 20035),
-}
-
-
-def icq_dict(icq_tuple):
-    item, chapter, question = icq_tuple
-    return {"item": item, "chapter": chapter, "question": question}
-
-
-def get_answers_by_mapping(instance, mapping):
-    raw_filters = [Q(**icq_dict(icq_tuple)) for icq_tuple in mapping.values()]
-
-    filters = functools.reduce(lambda a, b: a | b, raw_filters)
-
-    return CamacAnswer.objects.filter(Q(instance=instance) & filters)
-
-
-def get_question_slug(mapping, question_id):
-    for slug, icq_tuple in mapping.items():
-        if icq_dict(icq_tuple).get("question") == question_id:
-            return slug
-
-    raise Exception(f"No slug for question '{question_id}' found")
-
 
 class Command(BaseCommand):
-    help = "Migrates the old camac form for 'ebau nummer vergeben' to a caluma form"
+    help = "Migrates the old camac form for 'eBau-Nummer vergeben' to a Caluma form"
 
     def add_arguments(self, parser):
         parser.add_argument("--dry", dest="dry", action="store_true", default=False)
@@ -47,54 +18,108 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         sid = transaction.savepoint()
 
-        ebau_number_work_items = WorkItem.objects.filter(task_id="ebau-number")
+        self.fix_case_meta()
 
-        count = ebau_number_work_items.count()
-
-        for i, work_item in enumerate(ebau_number_work_items):
-            instance = work_item.case.instance
-
-            assign_ebau_number_document = Document.objects.create(form_id="ebau-number")
-
-            work_item.document = assign_ebau_number_document
-            work_item.save()
-
-            self._migrate_assign_ebau_number(instance, assign_ebau_number_document)
-
-            self.stdout.write(
-                self.style.SUCCESS(
-                    f"Successfully migrated the form 'assign ebau number' of instance {instance.pk}\t({i+1} / {count})"
+        cases_without_ebau_number_document = (
+            Case.objects.filter(
+                workflow_id__in=[
+                    "building-permit",
+                    "preliminary-clarification",
+                    "internal",
+                    "migrated",
+                ]
+            )
+            .exclude(**{"meta__ebau-number__isnull": True})
+            .exclude(
+                Exists(
+                    WorkItem.objects.filter(
+                        case=OuterRef("pk"),
+                        task_id="ebau-number",
+                    )
                 )
             )
+        )
+
+        work_items = []
+        documents = []
+        answers = []
+
+        for case in tqdm(cases_without_ebau_number_document):
+            result = self.migrate_assign_ebau_number(case)
+
+            if result:
+                work_items.append(result[0])
+                documents.append(result[1])
+                answers.extend(result[2])
+
+        Document.objects.bulk_create(documents)
+        Answer.objects.bulk_create(answers)
+        WorkItem.objects.bulk_create(work_items)
+
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"Successfully created {len(documents)} documents, {len(answers)} answers and {len(work_items)} work items"
+            )
+        )
 
         if options["dry"]:
             transaction.savepoint_rollback(sid)
         else:
             transaction.savepoint_commit(sid)
 
-    def _migrate_assign_ebau_number(self, instance, document):
-        answers = get_answers_by_mapping(instance, ANSWER_MAPPING)
+    def fix_case_meta(self):
+        cases = Case.objects.filter(**{"meta__ebau-number": None})
+        count = cases.count()
 
-        if not answers.exists():
+        if not count:
             return
 
-        caluma_answers = []
+        # Remove meta["ebau-number"] where value is None
+        for case in cases:
+            del case.meta["ebau-number"]
+            case.save()
 
-        for answer in answers:
-            question = Question.objects.get(
-                pk=get_question_slug(ANSWER_MAPPING, answer.question_id)
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"Successfully removed empty eBau numbers on {count} cases"
             )
-            if answer.answer in ["yes", "no"]:
-                value = question.options.get(pk=f"{question.pk}-{answer.answer}").pk
-            else:
-                value = answer.answer
+        )
 
-            caluma_answers.append(
-                Answer(
-                    document=document,
-                    question=question,
-                    value=value,
+    def get_answers(self, case):
+        existing = CamacAnswer.objects.filter(
+            instance=case.instance, item=1, chapter=20000, question=20035
+        ).first()
+
+        if existing and existing.answer != case.meta["ebau-number"]:
+            tqdm.write(
+                self.style.WARNING(
+                    f"eBau number mismatch for instance {case.instance.pk}: {existing.answer} (form) != {case.meta['ebau-number']} (meta)"
                 )
             )
 
-        bulk_create_with_history(caluma_answers, Answer)
+        if not existing:
+            return {"ebau-number-has-existing": "ebau-number-has-existing-no"}
+
+        return {
+            "ebau-number-has-existing": "ebau-number-has-existing-yes",
+            "ebau-number-existing": case.meta["ebau-number"],
+        }
+
+    def migrate_assign_ebau_number(self, case):
+        camac_answers = self.get_answers(case)
+
+        document = Document(form_id="ebau-number")
+        work_item = WorkItem(
+            task_id="ebau-number",
+            status=WorkItem.STATUS_COMPLETED,
+            document=document,
+            case=case,
+        )
+        answers = []
+
+        for question_id, value in camac_answers.items():
+            answers.append(
+                Answer(document=document, question_id=question_id, value=value)
+            )
+
+        return (work_item, document, answers)
