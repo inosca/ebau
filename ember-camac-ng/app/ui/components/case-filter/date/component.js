4dc5d6a1e666895896c66fe487e6168a11d8d56c
Jonas Metzener <jonas.metzener@adfinis.com>
2022-10-21 13:00:28 +0200

    feat(date): replace pikaday with flatpickr

--- ember-camac-ng/app/ui/components/case-filter/date/component.js
+++ ember-camac-ng/app/ui/components/case-filter/date/component.js
@@ -5,35 +5,14 @@ import { DateTime } from "luxon";
 
 import template from "./template";
 
-const isEmpty = (val) => [undefined, null, ""].includes(val);
-
 export class CaseFilterDateComponent extends Component {
-  get value() {
-    return this.args.value;
-  }
-
-  get maxDate() {
-    return this.args.maxDate ? new Date(this.args.maxDate) : null;
-  }
-  get minDate() {
-    return this.args.minDate ? new Date(this.args.minDate) : null;
-  }
-
   @action
-  onChange(event) {
-    // Use onChange event handler instead of onSelect to support
-    // manual input and resetting of date
-    const date = event.target.value;
-    const newValue =
-      date && DateTime.fromFormat(date, "dd.MM.yyyy").toISODate();
-    if (
-      !(isEmpty(newValue) && isEmpty(this.value)) &&
-      newValue !== this.value
-    ) {
-      this.args.updateFilter({
-        target: { value: newValue },
-      });
-    }
+  onChange(value) {
+    const date = DateTime.fromJSDate(value);
+
+    this.args.updateFilter({
+      target: { value: date.isValid ? date.toISODate() : null },
+    });
   }
 }
 
