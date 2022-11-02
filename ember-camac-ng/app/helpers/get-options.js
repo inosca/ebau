e849132dec47c8fc564ecabe336649ffea112498
Jonas Metzener <jonas.metzener@adfinis.com>
2022-11-02 16:30:49 +0100

    feat(case-list): add async multiple select filter for tags

--- ember-camac-ng/app/helpers/get-options.js
+++ ember-camac-ng/app/helpers/get-options.js
@@ -1,5 +1,19 @@
-import { helper } from "@ember/component/helper";
+import Helper from "@ember/component/helper";
 
-export default helper(function getOptions([obj, key]) {
-  return obj[key]?.records ?? obj[key]?.value ?? obj[key] ?? [];
-});
+export default class GetOptions extends Helper {
+  _lastValue = [];
+
+  compute([obj, key]) {
+    const result = obj[key];
+
+    if (result?.isPending) {
+      return this._lastValue;
+    }
+
+    const value = result?.records ?? result?.value ?? result ?? [];
+
+    this._lastValue = value;
+
+    return value;
+  }
+}
