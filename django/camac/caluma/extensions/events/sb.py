50a4a93745e994912fbcfb0dc4b2f6a98e0487c8
Jonas Lehmann <jonas.lehmann@adfinis.com>
2022-11-24 17:08:54 +0100

    feat(be): create a custom workitem after sb1 state

--- django/camac/caluma/extensions/events/sb.py
+++ django/camac/caluma/extensions/events/sb.py
@@ -45,3 +45,38 @@ def custom_sb2_work_item(sender, work_item, user, context, **kwargs):
             user=user,
             context=context,
         )
+
+
+@on(post_complete_work_item, raise_exception=True)
+@transaction.atomic
+def custom_sb1_work_item(sender, work_item, user, context, **kwargs):
+    if (
+        work_item.task_id == get_caluma_setting("REPORT_TASK")
+        and Answer.objects.filter(
+            question_id="legal-submission-type",
+            value__contains="legal-submission-type-load-compensation-request",
+            document__family__work_item__case__family=work_item.case.family,
+        ).exists()
+    ):
+        construction_control = work_item.case.instance.responsible_service(
+            filter_type="construction_control"
+        ).pk
+
+        work_item = WorkItem.objects.create(
+            task_id=get_caluma_setting("MANUAL_WORK_ITEM_TASK"),
+            name=get_translations(gettext_noop("Informin burden compensation bodies")),
+            created_by_user=user.username,
+            created_by_group=user.group,
+            deadline=timezone.now() + timedelta(days=10),
+            case=work_item.case,
+            status=WorkItem.STATUS_READY,
+            addressed_groups=[str(construction_control)],
+        )
+
+        send_event(
+            post_create_work_item,
+            sender="report",
+            work_item=work_item,
+            user=user,
+            context=context,
+        )
