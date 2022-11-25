7a2ff4fa912a43f8bde3d7c28f0762b3d28b4593
Jonas Cosandey <jonas.cosandey@adfinis.com>
2022-11-25 12:44:12 +0100

    feat(com-mod:ember-ebau-core): setup models for communication module and first draft of topic list

--- ember-ebau-core/tests/dummy/mirage/factories/instance.js
+++ ember-ebau-core/tests/dummy/mirage/factories/instance.js
@@ -1,3 +1,14 @@
+import { faker } from "@faker-js/faker";
 import { Factory } from "miragejs";
 
-export default Factory.extend({});
+export default Factory.extend({
+  afterCreate(instance, server) {
+    server.createList(
+      "communications-topic",
+      faker.datatype.number({ max: 10 }),
+      {
+        instance,
+      }
+    );
+  },
+});
