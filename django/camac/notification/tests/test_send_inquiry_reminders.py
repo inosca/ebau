8fecc3710589a29b1377e1c2f943e47b322db2d6
Jonas Metzener <jonas.metzener@adfinis.com>
2022-11-30 17:11:18 +0100

    fix(be): fix inquiry overdue reminder command
    Currently we loop over all overdue inquiries and send a reminder to a
    recipient type which resolves to all services with an overdue inquiry on
    that instance. If e.g 3 services have an overdue inquiry on an instance,
    each of them will receive 3 notifications.
    
    Also, we add the automatic reminder to the metadata so it will be
    displayed in the frontend.

--- django/camac/notification/tests/test_send_inquiry_reminders.py
+++ django/camac/notification/tests/test_send_inquiry_reminders.py
@@ -5,6 +5,7 @@ from django.core.management import call_command
 from django.utils import timezone
 
 
+@pytest.mark.freeze_time("2022-11-30T17:00:00.123456+00:00")
 @pytest.mark.parametrize("instance_state__name", ["circulation"])
 def test_send_inquiry_reminders(
     db,
@@ -20,7 +21,17 @@ def test_send_inquiry_reminders(
         notification_template.slug,
     )
 
-    active_inquiry_factory(deadline=timezone.now() - timedelta(days=1))
+    inquiry = active_inquiry_factory(
+        deadline=timezone.now() - timedelta(days=1),
+        meta={"reminders": ["2022-11-30T16:00:00.000000+00:00"]},
+    )
 
     call_command("send_inquiry_reminders")
+
+    inquiry.refresh_from_db()
+
     assert len(mailoutbox) == 1
+    assert inquiry.meta["reminders"] == [
+        "2022-11-30T17:00:00.123456+00:00",
+        "2022-11-30T16:00:00.000000+00:00",
+    ]
