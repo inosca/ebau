a738be61c80712b55aac304f8a7d74556108b201
Alana Luyten <alana.luyten@adfinis.com>
2022-12-14 13:50:19 +0100

    fix(distribution): show work-item to remind users to complete distribution
    Add new work-item check-distribution to distribution
    workflow to remind users to complete distribution.
    Flow is handled through a dynamic task. Rewrite
    check-inquiries work-item to also be handled through
    the dynamic task.

--- django/camac/caluma/extensions/dynamic_tasks.py
+++ django/camac/caluma/extensions/dynamic_tasks.py
@@ -1,4 +1,5 @@
 from caluma.caluma_workflow.dynamic_tasks import BaseDynamicTasks, register_dynamic_task
+from caluma.caluma_workflow.models import WorkItem
 from django.conf import settings
 
 from camac.instance.utils import should_continue_after_decision
@@ -17,3 +18,50 @@ class CustomDynamicTasks(BaseDynamicTasks):
             ]
 
         return []
+
+    @register_dynamic_task("after-inquiries-completed")
+    def resolve_after_inquiries_completed(self, case, user, prev_work_item, context):
+        # Further work-items should only be created if there are no
+        # further ready sibling inquiries (i.e. within same distribution case)
+        # with the same controlling group as the previously completed inquiry.
+        pending_inquiries = case.work_items.filter(
+            task_id=settings.DISTRIBUTION["INQUIRY_TASK"],
+            status=WorkItem.STATUS_READY,
+            controlling_groups=prev_work_item.controlling_groups,
+        )
+
+        if pending_inquiries.exists():
+            return []
+
+        tasks = []
+
+        # If no check-inquiries work-item exists yet addressed to
+        # the controlling group of the previously completed inquiry,
+        # it should be created.
+        check_inquiries = case.work_items.filter(
+            task_id=settings.DISTRIBUTION["INQUIRY_CHECK_TASK"],
+            status=WorkItem.STATUS_READY,
+            addressed_groups=prev_work_item.controlling_groups,
+        )
+
+        if not check_inquiries.exists():
+            tasks.append(settings.DISTRIBUTION["INQUIRY_CHECK_TASK"])
+
+        # If no check-distribution work-item exists addressed to
+        # the lead authority, then it should be created if the
+        # controlling group of the previously completed inquiry is the
+        # lead authority itself.
+        check_distribution = case.work_items.filter(
+            task_id=settings.DISTRIBUTION["DISTRIBUTION_CHECK_TASK"],
+            status=WorkItem.STATUS_READY,
+            addressed_groups=case.parent_work_item.addressed_groups,
+        )
+
+        if (
+            not check_distribution.exists()
+            and prev_work_item.controlling_groups
+            == case.parent_work_item.addressed_groups
+        ):
+            tasks.append(settings.DISTRIBUTION["DISTRIBUTION_CHECK_TASK"])
+
+        return tasks
