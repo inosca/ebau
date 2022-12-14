86c450e7630fffbc728bf22c461b3486495d7832
Alana Luyten <alana.luyten@adfinis.com>
2022-12-14 13:50:19 +0100

    fix(distribution): show work-item to remind users to complete distribution
    Add new work-item check-distribution to distribution
    workflow to remind users to complete distribution.
    Flow is handled through a dynamic task. Rewrite
    check-inquiries work-item to also be handled through
    the dynamic task.

--- django/camac/caluma/tests/test_dynamic_tasks.py
+++ django/camac/caluma/tests/test_dynamic_tasks.py
@@ -1,8 +1,14 @@
 import pytest
-from caluma.caluma_form.models import DynamicOption
+from caluma.caluma_form.api import save_answer
+from caluma.caluma_form.models import DynamicOption, Question
 from caluma.caluma_workflow.api import complete_work_item, skip_work_item
-from caluma.caluma_workflow.models import Case
+from caluma.caluma_workflow.models import Case, WorkItem
 
+from camac.caluma.tests.test_distribution_workflow import (  # noqa: F401
+    distribution_case_be,
+    distribution_child_case_be,
+    inquiry_factory_be,
+)
 from camac.constants.kt_bern import (
     DECISION_TYPE_BAUBEWILLIGUNGSFREI,
     DECISION_TYPE_BUILDING_PERMIT,
@@ -144,3 +150,80 @@ def test_dynamic_task_after_decision(
     if case.status == Case.STATUS_RUNNING:
         assert case.work_items.filter(task_id="sb1").exists()
         assert case.instance.instance_state.name == "sb1"
+
+
+@pytest.mark.parametrize(
+    "is_lead_authority",
+    [False, True],
+)
+def test_dynamic_task_after_inquiries_completed(
+    db,
+    caluma_admin_user,
+    distribution_child_case_be,  # noqa: F811
+    be_distribution_settings,  # noqa: F811
+    inquiry_factory_be,  # noqa: F811
+    service,
+    service_factory,
+    is_lead_authority,
+):
+    invited_service = service_factory()
+    if is_lead_authority:
+        inquiry1 = inquiry_factory_be(sent=True)
+        inquiry2 = inquiry_factory_be(sent=True)
+    else:
+        inquiry_factory_be(to_service=invited_service, sent=True)
+        inquiry1 = inquiry_factory_be(from_service=invited_service, sent=True)
+        inquiry2 = inquiry_factory_be(from_service=invited_service, sent=True)
+
+    def answer_inquiry(inquiry):
+        save_answer(
+            question=Question.objects.get(pk="inquiry-answer-status"),
+            document=inquiry.child_case.document,
+            value="inquiry-answer-status-negative",
+            user=caluma_admin_user,
+        )
+
+        complete_work_item(
+            work_item=inquiry.child_case.work_items.first(), user=caluma_admin_user
+        )
+
+    check_inquiries_work_items = distribution_child_case_be.work_items.filter(
+        task_id=be_distribution_settings["INQUIRY_CHECK_TASK"],
+        status=WorkItem.STATUS_READY,
+        addressed_groups=[str(service.pk if is_lead_authority else invited_service.pk)],
+    )
+
+    check_distribution_work_items = distribution_child_case_be.work_items.filter(
+        task_id=be_distribution_settings["DISTRIBUTION_CHECK_TASK"],
+        status=WorkItem.STATUS_READY,
+        addressed_groups=[str(service.pk if is_lead_authority else invited_service.pk)],
+    )
+
+    answer_inquiry(inquiry1)
+
+    # No check-distribution or check-inquiries work-item should be created
+    # since there are pending controlling inquiries left.
+    assert check_inquiries_work_items.count() == 0
+    assert check_distribution_work_items.count() == 0
+
+    answer_inquiry(inquiry2)
+
+    # No pending controlling inquiries left, should create a
+    # check-inquiries work-item and an check-distribution
+    # (only for lead authority)
+    assert check_inquiries_work_items.count() == 1
+    assert check_distribution_work_items.count() == (1 if is_lead_authority else 0)
+
+    inquiry3 = inquiry_factory_be(sent=True)
+
+    # Check-distribution is canceled when new pending controlling
+    # work-items appear
+    assert check_inquiries_work_items.count() == 1
+    assert check_distribution_work_items.count() == 0
+
+    answer_inquiry(inquiry3)
+
+    # Should create check-distribution work-item and not create another
+    # check-inquiries work-item as there is already an existing one.
+    assert check_inquiries_work_items.count() == 1
+    assert check_distribution_work_items.count() == (1 if is_lead_authority else 0)
