9dedad11b1a5a30a88d896ca0e300c9666e9280f
Christian Zosel <christian@zosel.ch>
2023-03-23 14:55:08 +0100

    feat: run ember-ebau tests in CI

--- ember-ebau/tests/integration/components/main-navigation/component-test.js
+++ ember-ebau/tests/integration/components/main-navigation/component-test.js
@@ -1,26 +1,16 @@
 import { render } from "@ember/test-helpers";
 import { hbs } from "ember-cli-htmlbars";
+import { setupIntl } from "ember-intl/test-support";
 import { setupRenderingTest } from "ember-qunit";
 import { module, test } from "qunit";
 
 module("Integration | Component | main-navigation", function (hooks) {
   setupRenderingTest(hooks);
+  setupIntl(hooks);
 
   test("it renders", async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
     await render(hbs`<MainNavigation />`);
 
-    assert.dom(this.element).hasText("");
-
-    // Template block usage:
-    await render(hbs`
-      <MainNavigation>
-        template block text
-      </MainNavigation>
-    `);
-
-    assert.dom(this.element).hasText("template block text");
+    assert.dom(this.element).hasText("t:nav.login:()");
   });
 });
