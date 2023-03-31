31262afab3fa28861a043683159dc1709ec26920
yelinz <yelin.zhang@adfinis.com>
2023-03-31 16:16:16 +0200

    feat: add camac alexandria authentication

---
+++ alexandria/gr/extensions/authentication.py
@@ -0,0 +1,76 @@
+import functools
+import hashlib
+
+import requests
+from django.core.cache import cache
+from mozilla_django_oidc.auth import OIDCAuthenticationBackend
+from django.utils.encoding import force_bytes
+from alexandria.oidc_auth.models import BaseUser
+
+
+ADMIN_ROLE = "10000"
+
+class CamacUser(BaseUser):
+    def __init__(self, user_response):
+        super().__init__()
+
+        user = user_response["data"]
+        included = user_response["included"]
+
+        self.is_authenticated = True
+        self.username = user["id"]
+
+        # set groups to service?
+        self.groups = map(
+            lambda group: group["id"], user["relationships"]["groups"]["data"]
+        )
+        groups_data = {}
+        for inclusion in included:
+            if inclusion["type"] == "groups":
+                groups_data[inclusion["id"]] = inclusion
+        self.groups_data = groups_data
+
+        # group should be service in camac context
+        """
+        self.group = (
+            user["relationships"]["service"]["data"]["id"]
+            if user["relationships"]["service"]["data"] is not None
+            else "10001"
+        )
+        """
+        self.group = user["relationships"]["default-group"]["data"]["id"]
+        self.group_data = groups_data[self.group]
+        
+        self.is_superuser = self.group_data["relationships"]["role"]["data"]["id"] == ADMIN_ROLE
+
+
+class CamacAlexandriaAuthenticationBackend(OIDCAuthenticationBackend):
+    def get_or_create_user(self, access_token, id_token, payload):
+        user_response = self.cached_request(
+            self.get_camac_user, access_token, "camac_user"
+        )
+
+        return CamacUser(user_response)
+
+    def get_camac_user(self, access_token):
+        headers = {
+            "Authorization": f"Bearer {access_token}",
+            "Content-Type": "application/vnd.api+json",
+        }
+        response = requests.get(
+            "http://django/api/v1/me",
+            headers=headers,
+            params={"include": "groups,groups.role,groups.service"},
+        )
+        response.raise_for_status()
+        return response.json()
+
+    def cached_request(self, method, token, cache_prefix):
+        token_hash = hashlib.sha256(force_bytes(token)).hexdigest()
+
+        func = functools.partial(method, token)
+
+        return cache.get_or_set(
+            f"{cache_prefix}.{token_hash}",
+            func,
+        )
--- alexandria/gr/extensions/permissions.py
+++ alexandria/gr/extensions/permissions.py
@@ -3,24 +3,69 @@ from alexandria.core.permissions import (
     permission_for,
     object_permission_for,
 )
-from alexandria.core.models import BaseModel, Document
+from alexandria.core.models import BaseModel, Document, File, Tag, Category
+
+ADMIN_ROLE = "10000"
+APPLICANT_ROLE = "10001"
 
 
 class CustomPermission(BasePermission):
     @permission_for(BaseModel)
     def has_permission_default(self, request):
-        """
-        import logging
-        logging.error(request)
-        logging.error(vars(request.user))
+        if request.user.is_superuser:
+            return True
         return False
-        """
-        return True
 
+    @permission_for(File)
     @permission_for(Document)
     def has_permission_for_document(self, request):
+        """
+        portal users can create for their own instance
+        municipality users can create for their group's instance
+        """
+        print(request)
         return True
 
+    @object_permission_for(File)
     @object_permission_for(Document)
     def has_object_permission_for_document(self, request, instance):
-        return True
+        if request.user.is_superuser:
+            return True
+        elif (
+            request.user.group_data["relationships"]["role"]["data"]["id"]
+            == APPLICANT_ROLE
+        ):
+            if instance.created_by_user == request.user.username:
+                return True
+
+        if instance.created_by_group in request.user.groups:
+            return True
+
+        return False
+
+    @permission_for(Tag)
+    def has_permission_for_tag(self, request):
+        """
+        creating tags not allowed for portal users
+        """
+        self.has_permission_default(request)
+
+    @object_permission_for(Tag)
+    def has_object_permission_for_tag(self, request, instance):
+        """
+        portal users can add tags to their own document
+        municipality users can add tags to their group's documents
+        """
+        if request.user.is_superuser:
+            return True
+        elif (
+            request.user.group_data["relationships"]["role"]["data"]["id"]
+            == APPLICANT_ROLE
+        ):
+            if instance.created_by_user == request.user.username:
+                return True
+
+        if instance.created_by_group in request.user.groups:
+            return True
+
+        return False
