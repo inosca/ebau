11efa7ef8a55f350e99a6b42e6a54c06fa42c976
Jonas Metzener <jonas.metzener@adfinis.com>
2023-04-20 15:24:10 +0200

    feat(appeal): add custom work item for statement on appeal

--- django/camac/caluma/extensions/validations.py
+++ django/camac/caluma/extensions/validations.py
@@ -1,12 +1,22 @@
+from datetime import datetime
+
+import pytz
 from caluma.caluma_core.validations import BaseValidation, validation_for
 from caluma.caluma_form.models import Answer
-from caluma.caluma_form.schema import SaveDocumentDateAnswer, SaveDocumentStringAnswer
+from caluma.caluma_form.schema import (
+    SaveDocumentDateAnswer,
+    SaveDocumentStringAnswer,
+    SaveDocumentTableAnswer,
+)
 from caluma.caluma_workflow.models import WorkItem
 from caluma.caluma_workflow.schema import CompleteWorkItem
 from django.conf import settings
+from django.db.models import Q
+from django.utils.translation import gettext_noop
 from rest_framework import exceptions
 
 from camac.caluma.utils import CamacRequest, sync_inquiry_deadline
+from camac.core.translations import get_translations
 from camac.ech0211.signals import file_subsequently
 from camac.notification.utils import send_mail
 
@@ -20,6 +30,10 @@ NOTIFICATION_CLAIM_IN_PROGRESS_MUNICIPALITY = (
     "03-zusaetzliche-unterlagen-notwendig-gemeinde"
 )
 NOTIFICATION_CLAIM_ANSWERED = "03-nachforderung-beantwortet-leitbehorde"
+
+
+def date_to_deadline(date):
+    return pytz.utc.localize(datetime.combine(date, datetime.min.time()))
 
 
 class CustomValidation(BaseValidation):
@@ -102,7 +116,7 @@ class CustomValidation(BaseValidation):
         return data
 
     @validation_for(SaveDocumentDateAnswer)
-    def validate_inquiry_deadline(self, mutation, data, info):
+    def validate_date_answer(self, mutation, data, info):
         if (
             settings.DISTRIBUTION
             and data["question"].slug == settings.DISTRIBUTION["QUESTIONS"]["DEADLINE"]
@@ -110,5 +124,91 @@ class CustomValidation(BaseValidation):
             sync_inquiry_deadline(data["document"].work_item, data["date"])
 
             return data
+
+        if (
+            settings.APPEAL
+            and data["question"].slug == settings.APPEAL["QUESTIONS"]["DATE"]
+        ):
+            # Update potentially existing work items linked to this answer
+            WorkItem.objects.filter(
+                **{
+                    "task_id": settings.APPLICATION["CALUMA"]["MANUAL_WORK_ITEM_TASK"],
+                    "meta__is-appeal-statement-deadline": True,
+                    "meta__appeal-row-id": str(data["document"].pk),
+                }
+            ).update(deadline=date_to_deadline(data["date"]))
+
+        return data
+
+    @validation_for(SaveDocumentTableAnswer)
+    def validate_table_answer(self, mutation, data, info):
+        """Create a work item for a specific appeal row entry.
+
+        For appeal rows of the type deadline (Frist der Stellungnahme) and the
+        authority legal departement (Rechtsamt) we need to create a manual work
+        item for the lead authority. The deadline of the work item is the one
+        entered in the appeal row.
+        """
+
+        if (
+            settings.APPEAL
+            and data["question"].slug == settings.APPEAL["QUESTIONS"]["TABLE"]
+        ):
+            case = data["document"].work_item.case.family
+
+            deadline_rows = filter(
+                lambda row: row.answers.filter(
+                    Q(
+                        question_id=settings.APPEAL["QUESTIONS"]["AUTHORITY"],
+                        value=settings.APPEAL["ANSWERS"]["AUTHORITY"][
+                            "LEGAL_DEPARTEMENT"
+                        ],
+                    )
+                    | Q(
+                        question_id=settings.APPEAL["QUESTIONS"]["TYPE"],
+                        value=settings.APPEAL["ANSWERS"]["TYPE"]["DEADLINE"],
+                    )
+                ).count()
+                == 2,
+                data["documents"],
+            )
+
+            existing_work_items = []
+
+            for row in deadline_rows:
+                deadline = date_to_deadline(
+                    row.answers.filter(question_id=settings.APPEAL["QUESTIONS"]["DATE"])
+                    .values_list("date", flat=True)
+                    .first()
+                )
+
+                work_item, _ = WorkItem.objects.get_or_create(
+                    task_id=settings.APPLICATION["CALUMA"]["MANUAL_WORK_ITEM_TASK"],
+                    meta={
+                        "is-appeal-statement-deadline": True,
+                        "appeal-row-id": str(row.pk),
+                    },
+                    defaults={
+                        "name": get_translations(
+                            gettext_noop("Issue statement on appeal")
+                        ),
+                        "created_by_user": info.context.user.username,
+                        "created_by_group": info.context.user.group,
+                        "deadline": deadline,
+                        "case": case,
+                        "status": WorkItem.STATUS_READY,
+                        "addressed_groups": [str(info.context.user.group)],
+                    },
+                )
+
+                existing_work_items.append(work_item.pk)
+
+            # Delete work items for deadline rows that don't exist anymore
+            case.work_items.filter(
+                **{
+                    "task_id": settings.APPLICATION["CALUMA"]["MANUAL_WORK_ITEM_TASK"],
+                    "meta__is-appeal-statement-deadline": True,
+                }
+            ).exclude(pk__in=existing_work_items).delete()
 
         return data
---
+++ django/camac/settings_appeal.py
@@ -0,0 +1,17 @@
+APPEAL = {
+    "default": {},
+    "kt_bern": {
+        "ENABLED": True,
+        "ROW_FORM": "appeal-form",
+        "QUESTIONS": {
+            "TABLE": "appeal-table",
+            "TYPE": "appeal-type",
+            "AUTHORITY": "appeal-authority",
+            "DATE": "appeal-date",
+        },
+        "ANSWERS": {
+            "TYPE": {"DEADLINE": "appeal-type-frist-der-stellungnahme"},
+            "AUTHORITY": {"LEGAL_DEPARTEMENT": "appeal-authority-rechtsamt"},
+        },
+    },
+}
