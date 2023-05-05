0662a30c932ddf502e7f5ed4e2d6c44676cf6c60
Christian Zosel <christian@zosel.ch>
2023-05-05 07:45:44 +0200

    fix: remove KEYCLOAK_ADMIN from Dockerfile, docs

--- keycloak/custom-entrypoint.sh
+++ keycloak/custom-entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# load .env file if it is present. We don't use 'xargs' because it isn't installed
+# and installing packages is discouraged.
+# https://www.keycloak.org/server/containers#_installing_additional_rpm_packages
+# https://gist.github.com/mihow/9c7f559807069a03e302605691f85572?permalink_comment_id=4099982#gistcomment-4099982
 if [ $ENV_FILE ]; then
     export $(grep -vE "^(#.*|\s*)$" $ENV_FILE)
 fi
