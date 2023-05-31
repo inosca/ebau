cc043d51269283f4489f675a64f02a2b26dc7031
yelinz <yelin.zhang@adfinis.com>
2023-05-31 11:20:05 +0200

    feat(alexandria): remove old configuration

--- alexandria/extensions/settings.py
+++
@@ -1,8 +0,0 @@
-from alexandria.settings import *
-
-REST_FRAMEWORK["DEFAULT_AUTHENTICATION_CLASSES"] = [
-    "alexandria.extensions.authentication.CamacAlexandriaAuthentication"
-]
-OIDC_DRF_AUTH_BACKEND = (
-    "alexandria.oidc_auth.authentication.AlexandriaAuthenticationBackend"
-)
