532bbb7d43ec1bef5103fe6ed962f2cd2596a2dd
yelinz <yelin.zhang@adfinis.com>
2023-06-07 16:24:07 +0200

    feat(ember-ebau): display gr logo

--- ember-caluma-portal/config/locales.js
+++ ember-caluma-portal/config/locales.js
@@ -4,7 +4,7 @@ const LOCALES_MAP = {
   demo: ["de", "fr"],
   kt_bern: ["de", "fr"],
   kt_uri: ["de"],
-  kt_gr: ["de", "rm", "it"],
+  kt_gr: ["de", "it"],
 };
 
 module.exports = LOCALES_MAP[process.env.APPLICATION || "kt_bern"];
