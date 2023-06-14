7a8b2b6183647eb0d680540694986cd19ed5fa47
Jonas Metzener <jonas.metzener@adfinis.com>
2023-06-14 16:10:07 +0200

    feat(communications): only display attachment sections with upload permissions

--- ember-ebau-core/addon/components/communication/file-list/file.js
+++ ember-ebau-core/addon/components/communication/file-list/file.js
@@ -4,8 +4,4 @@ import Component from "@glimmer/component";
 export default class CommunicationFileListFileComponent extends Component {
   @service router;
   @service ebauModules;
-
-  get messageByApplicant() {
-    return this.args.file.get("message.createdBy.id") === "APPLICANT";
-  }
 }
--- ember-ebau-core/addon/components/link-attachments.js
+++ ember-ebau-core/addon/components/link-attachments.js
@@ -2,11 +2,8 @@ import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
 import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
-import { queryManager } from "ember-apollo-client";
 import { findRecord } from "ember-data-resources";
 import { trackedFunction } from "ember-resources/util/function";
-
-import hasRunningInquiriesQuery from "ember-ebau-core/gql/queries/has-running-inquiries.graphql";
 
 /**
  * `@projectcaluma/ember-form` custom widget component to link attachments from
@@ -19,8 +16,6 @@ export default class LinkAttachmentsComponent extends Component {
   @service notification;
   @service calumaOptions;
 
-  @queryManager apollo;
-
   @tracked showModal = false;
 
   instance = findRecord(this, "instance", () => [this.instanceId]);
@@ -28,21 +23,6 @@ export default class LinkAttachmentsComponent extends Component {
   attachmentSection = findRecord(this, "attachment-section", () => [
     this.args.field.question.raw.meta.attachmentSection,
   ]);
-
-  hasRunningInquiries = trackedFunction(this, async () => {
-    return (
-      (await this.apollo.query(
-        {
-          query: hasRunningInquiriesQuery,
-          variables: {
-            currentGroupId: this.calumaOptions.currentGroupId,
-            instanceId: this.instanceId,
-          },
-        },
-        "allWorkItems.totalCount"
-      )) > 0
-    );
-  });
 
   get instanceId() {
     return this.args.context.instanceId;
@@ -52,22 +32,12 @@ export default class LinkAttachmentsComponent extends Component {
     return this.instance.isLoading || this.attachmentSection.isLoading;
   }
 
-  get hasUploadPermission() {
-    switch (this.attachmentSection.record?.meta["permission-name"]) {
-      case "write":
-      case "admin":
-      case "admin-internal":
-      case "admin-service":
-        return true;
-      case "admin-service-before-decision":
-      case "admin-before-decision":
-        return !this.instance.record?.isAfterDecision;
-      case "admin-service-running-inquiry":
-        return this.hasRunningInquiries.value;
-      default:
-        return false;
-    }
-  }
+  hasUploadPermission = trackedFunction(this, async () => {
+    return await this.attachmentSection.record?.canUpload(
+      this.instanceId,
+      this.calumaOptions.currentGroupId
+    );
+  });
 
   @action
   async save(selected) {
