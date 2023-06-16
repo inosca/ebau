7210186d23d413c4670557e62c01864dc50ae084
Jonas Metzener <jonas.metzener@adfinis.com>
2023-06-16 13:32:05 +0200

    feat(be): scrub sensitive answers for publication

--- django/camac/caluma/conftest.py
+++ django/camac/caluma/conftest.py
@@ -9,15 +9,27 @@ from camac.caluma.utils import CamacRequest
 
 @pytest.fixture
 def caluma_admin_request(rf, caluma_admin_user):
-    request = rf.get("/graphql")
-    request.user = caluma_admin_user
-    Info = namedtuple("Info", "context")
-    request.camac_request = CamacRequest(Info(context=request)).request
-    return request
+    def wrapper(**kwargs):
+        request = rf.get("/graphql", **kwargs)
+        request.user = caluma_admin_user
+        Info = namedtuple("Info", "context")
+        request.camac_request = CamacRequest(Info(context=request)).request
+        return request
+
+    return wrapper
 
 
 @pytest.fixture
 def caluma_admin_schema_executor(caluma_admin_request):
     return functools.partial(
-        schema.execute, context_value=caluma_admin_request, middleware=[]
+        schema.execute, context_value=caluma_admin_request(), middleware=[]
+    )
+
+
+@pytest.fixture
+def caluma_admin_public_schema_executor(caluma_admin_request):
+    return functools.partial(
+        schema.execute,
+        context_value=caluma_admin_request(HTTP_X_CAMAC_PUBLIC_ACCESS=True),
+        middleware=[],
     )
