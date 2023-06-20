0458f9799c4de46c19f467e55a420189e3742391
Christian Zosel <christian@zosel.ch>
2023-06-20 16:15:57 +0200

    chore: fixes for Kt. UR

--- ember-ebau-core/addon/config/main-ur.js
+++ ember-ebau-core/addon/config/main-ur.js
@@ -20,4 +20,34 @@ export default {
     staticForestBoundaryMunicipality:
       "waldfeststellung-mit-statischen-waldgrenzen-gemeinde",
   },
+  instanceResourceRedirects: {
+    journal: {
+      3: 772, // KOOR BG
+      1101: 12000005, // KOOR BD
+      1127: 12000007, // KOOR AfE
+      1128: 12000009, // KOOR AFJ
+      1107: 12000006, // KOOR ALA
+      1061: 12000004, // KOOR NP
+      1129: 12000009, // KOOR SD
+      1106: 12000008, // KOOR AfU
+      6: 20001, // Municipality
+      1102: 12000001, // Gemeinde als Vernehmlassungsstelle
+      4: 12000001, // Vernehmlassungsstelle mit Koordinationsaufgaben
+      1021: 12000001, // Vernehmlassungsstelle ohne Koordinationsaufgaben
+    },
+    form: {
+      3: 4, // KOOR BG
+      1101: 605, // KOOR BD
+      1127: 723, // KOOR AfE
+      1128: 735, // KOOR AFJ
+      1107: 714, // KOOR ALA
+      1061: 445, // KOOR NP
+      1129: 735, // KOOR SD
+      1106: 699, // KOOR AfU
+      6: 771, // Municipality
+      1102: 2000000, // Gemeinde als Vernehmlassungsstelle
+      4: 2000000, // Vernehmlassungsstelle mit Koordinationsaufgaben
+      1021: 2000000, // Vernehmlassungsstelle ohne Koordinationsaufgaben
+    },
+  },
 };
