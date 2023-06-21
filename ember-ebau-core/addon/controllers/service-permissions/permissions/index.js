074077f043722eb997e416c6b4e3465cd2da5ac0
Jonas Metzener <jonas.metzener@adfinis.com>
2023-06-21 14:36:29 +0200

    fix(service-permission): fix reloading of lists after adding new entries

--- ember-ebau-core/addon/controllers/service-permissions/permissions/index.js
+++ ember-ebau-core/addon/controllers/service-permissions/permissions/index.js
@@ -1,16 +1,16 @@
 import Controller from "@ember/controller";
 import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
+import { tracked } from "@glimmer/tracking";
 import { dropTask, restartableTask, timeout } from "ember-concurrency";
-import { dedupeTracked } from "tracked-toolbox";
 
 import paginatedQuery from "ember-ebau-core/resources/paginated";
 
 export default class ServicePermissionsPermissionsIndexController extends Controller {
   @service store;
 
-  @dedupeTracked search = "";
-  @dedupeTracked page = 1;
+  @tracked search = "";
+  @tracked page = 1;
 
   queryParams = ["search"];
 
