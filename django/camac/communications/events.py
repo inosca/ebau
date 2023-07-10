0c4be3028f2ce54287e22b97689a118fb349752e
Jonas Metzener <jonas.metzener@adfinis.com>
2023-07-10 17:30:02 +0200

    fix(communications): only send notification if message was not created by applicant

--- django/camac/communications/events.py
+++ django/camac/communications/events.py
@@ -4,22 +4,21 @@ from camac.notification import utils as notification_utils
 
 
 def notify_receivers(message, context):
-    # Currently, we do not send notification to internal
-    # services.
+    # Currently, we do not send any notifications to internal services
     if "APPLICANT" in message.topic.involved_entities:
         _notify_applicants(message, context)
 
 
 def _notify_applicants(message, context):
+    # Don't notify applicants if they created the message to begin with
+    if message.created_by == "APPLICANT":
+        return
 
     instance = message.topic.instance
 
     notification_utils.send_mail(
         slug=settings.APPLICATION["COMMUNICATIONS"]["template_slug"],
         context=context,
-        recipient_types=["email_list"],
-        email_list=",".join(
-            [applicant.email for applicant in instance.involved_applicants.all()]
-        ),
+        recipient_types=["applicant"],
         instance={"id": instance.pk, "type": "instances"},
     )
