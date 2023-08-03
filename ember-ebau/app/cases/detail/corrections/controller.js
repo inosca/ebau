17dd5c5a20dd931263156bb847c4a2fcea444979
yelinz <yelin.zhang@adfinis.com>
2023-08-03 16:49:45 +0200

    fix(gr): enable editing in correction state

--- ember-ebau/app/cases/detail/corrections/controller.js
+++ ember-ebau/app/cases/detail/corrections/controller.js
@@ -2,28 +2,36 @@ import Controller from "@ember/controller";
 import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
 import { dropTask } from "ember-concurrency";
-import { confirmTask } from "ember-ebau-core/decorators";
 import { findRecord } from "ember-data-resources";
+import { confirmTask } from "ember-ebau-core/decorators";
 
 export default class CorrectionsController extends Controller {
   @service fetch;
   @service intl;
   @service notification;
 
+  instance = findRecord(this, "instance", () => this.model);
+
   @action
   toggleModal() {
     this.showModal = !this.showModal;
   }
 
-  instance = findRecord(this, "instance", () => this.model);
-
   @dropTask
   @confirmTask("corrections.document.confirm")
+  *startCorrection() {
+    yield this.documentCorrection.perform();
+  }
+
+  @dropTask
   *documentCorrection() {
     try {
       yield this.fetch.fetch(`/api/v1/instances/${this.model}/correction`, {
         method: "POST",
       });
+
+      // sadly we need this to have current data on the whole page
+      location.reload();
     } catch (error) {
       this.notification.danger(this.intl.t("corrections.document.error"));
     }
