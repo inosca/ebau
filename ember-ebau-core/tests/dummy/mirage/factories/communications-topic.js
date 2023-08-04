c6a0c6f2f0d70dc78b7a988025efd1c6ca005809
Jonas Metzener <jonas.metzener@adfinis.com>
2023-08-04 11:23:03 +0200

    chore(faker): fix faker deprecation

--- ember-ebau-core/tests/dummy/mirage/factories/communications-topic.js
+++ ember-ebau-core/tests/dummy/mirage/factories/communications-topic.js
@@ -1,14 +1,14 @@
 import { faker } from "@faker-js/faker";
 import { Factory, trait } from "miragejs";
 
-const randomInt = (max = 5) => faker.datatype.number({ max });
+const randomInt = (max = 5) => faker.number.int({ max });
 
 export default Factory.extend({
   subject: () => faker.lorem.sentence(),
   created: () => faker.date.recent(),
   hasUnread: () => faker.datatype.boolean(),
   dossierNumber: () =>
-    `${faker.datatype.datetime().getFullYear()}-${faker.datatype.number(99)}`,
+    `${faker.date.anytime().getFullYear()}-${faker.number.int()}`,
   afterCreate(communicationsTopic, server) {
     const initiatedBy = server.schema.users.first() ?? server.create("user");
     const initiatedByEntity = server.create("service", {
