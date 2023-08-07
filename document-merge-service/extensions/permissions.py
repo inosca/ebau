7df4d493295178ed4d66b8c978f659f12ccbc3dd
yelinz <yelin.zhang@adfinis.com>
2023-08-07 12:16:14 +0200

    fix(dms): fix delete permission check

--- document-merge-service/extensions/permissions.py
+++ document-merge-service/extensions/permissions.py
@@ -9,6 +9,16 @@ from document_merge_service.extensions.utils import get_services
 
 class CustomPermission:
     @permission_for(Template)
+    def has_permission_template(self, request):
+        # skip to object permissions when its delete
+        if request.method == "DELETE":
+            return True
+
+        raw_meta = request.data.get("meta")
+        meta = json.loads(raw_meta) if raw_meta else {}
+
+        return meta.get("service") in get_services(request)
+
     @object_permission_for(Template)
     def has_object_permission_template(self, request, template=None):
         view_name = resolve(request.get_full_path()).view_name
@@ -17,10 +27,4 @@ class CustomPermission:
         if view_name == "template-merge":
             return True
 
-        if template:
-            meta = template.meta
-        else:
-            raw_meta = request.data.get("meta")
-            meta = json.loads(raw_meta) if raw_meta else {}
-
-        return meta.get("service") in get_services(request)
+        return template.meta.get("service") in get_services(request)
