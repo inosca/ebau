d28e80bc4a5c8afab9d0f7e65da811e4b8ef5126
Alana Luyten <alana.luyten@adfinis.com>
2023-09-04 16:41:28 +0200

    fix(django-admin): fix back link to camac-admin

--- django/camac/core/tests/test_custom_template_tags.py
+++ django/camac/core/tests/test_custom_template_tags.py
@@ -3,13 +3,13 @@ from django.template import Context, Template
 
 def test_custom_template_tag_settings_value(settings, application_settings):
     settings.APPLICATION_NAME = "kt_gr"
-    application_settings["APPLICATION_NAME"] = "kt_gr"
-    context = Context({"APPLICATION_NAME": application_settings["APPLICATION_NAME"]})
+    application_settings["SHORT_NAME"] = settings.APPLICATIONS["kt_gr"]["SHORT_NAME"]
     template = Template(
         "{% load settings_value %}"
         "{% settings_value 'APPLICATION_NAME' as APPLICATION_NAME %}"
-        "Application name: {{APPLICATION_NAME}}"
+        "{% settings_value 'SHORT_NAME' from_application_settings=True as SHORT_NAME %}"
+        "{{APPLICATION_NAME}}, {{SHORT_NAME}}"
     )
 
-    rendered = template.render(context)
-    assert rendered == "Application name: kt_gr"
+    rendered = template.render(Context())
+    assert rendered == "kt_gr, gr"
