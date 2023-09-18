d2e5440ab41658dadb86c138235b6db6bd4a5eef
Jonas Metzener <jonas.metzener@adfinis.com>
2023-09-18 17:38:49 +0200

    feat(so): add watermark on test and dev systems

--- ember-caluma-portal/app/components/so-navbar.js
+++ ember-caluma-portal/app/components/so-navbar.js
@@ -1,3 +1,18 @@
+import { inject as service } from "@ember/service";
+import { isDevelopingApp } from "@embroider/macros";
+
 import BeNavbarComponent from "caluma-portal/components/be-navbar";
 
-export default class SoNavbarComponent extends BeNavbarComponent {}
+export default class SoNavbarComponent extends BeNavbarComponent {
+  @service intl;
+
+  get watermark() {
+    if (isDevelopingApp() || location.host === "ebau-portal.local") {
+      return this.intl.t("nav.watermark.dev");
+    } else if (location.host === "portal-ebau-t.so.ch") {
+      return this.intl.t("nav.watermark.test");
+    }
+
+    return null;
+  }
+}
