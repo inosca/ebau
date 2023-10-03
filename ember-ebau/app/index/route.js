66ae3051b6d9c9667e780dbb09b030179e6b7324
Jonas Metzener <jonas.metzener@adfinis.com>
2023-10-03 13:55:24 +0200

    feat(so): configure new internal design

--- ember-ebau/app/index/route.js
+++ ember-ebau/app/index/route.js
@@ -1,3 +1,20 @@
 import Route from "@ember/routing/route";
+import { inject as service } from "@ember/service";
 
-export default class IndexRoute extends Route {}
+export default class IndexRoute extends Route {
+  @service router;
+  @service store;
+
+  async redirect() {
+    const firstResource = (
+      await this.store.query("resource", {
+        "page[size]": 1,
+        "page[number]": 1,
+      })
+    )[0];
+
+    if (firstResource) {
+      return this.router.replaceWith(firstResource.link);
+    }
+  }
+}
