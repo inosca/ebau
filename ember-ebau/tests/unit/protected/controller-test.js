0215421d645fdeb3264250c54e36901a2303a2ff
Jonas Metzener <jonas.metzener@adfinis.com>
2023-10-03 13:55:24 +0200

    feat(so): configure new internal design

--- ember-ebau/tests/unit/protected/controller-test.js
+++ ember-ebau/tests/unit/protected/controller-test.js
@@ -2,12 +2,12 @@ import { module, test } from "qunit";
 
 import { setupTest } from "ebau/tests/helpers";
 
-module("Unit | Controller | application", function (hooks) {
+module("Unit | Controller | protected", function (hooks) {
   setupTest(hooks);
 
   // TODO: Replace this with your real tests.
   test("it exists", function (assert) {
-    const controller = this.owner.lookup("controller:application");
+    const controller = this.owner.lookup("controller:protected");
     assert.ok(controller);
   });
 });
