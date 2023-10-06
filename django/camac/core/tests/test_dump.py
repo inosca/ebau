45e0959482e9b04b044662a97ec620c78bee284b
Stephan Hug <stephan.hug@adfinis.com>
2023-10-06 15:05:00 +0200

    test(performance): improve performance of dump and load tests

--- django/camac/core/tests/test_dump.py
+++ django/camac/core/tests/test_dump.py
@@ -6,42 +6,44 @@ import pytest
 from django.conf import settings
 from django.core.management import call_command
 
+from camac.core.models import Resource
+
 
 @pytest.mark.parametrize("application", settings.APPLICATIONS.keys())
-@pytest.mark.parametrize("dump_type", ["data", "config"])
-def test_dump_and_load(db, settings, application, dump_type, tmp_path):
+def test_dump_and_load(db, settings, application, tmp_path, resource_factory):
     settings.APPLICATION_DIR = settings.ROOT_DIR.path(application)
     settings.APPLICATION = settings.APPLICATIONS[application]
     settings.APPLICATION_NAME = application
 
-    # load data including test data
+    resource_to_be_deleted = resource_factory()
+    assert Resource.objects.count() == 1
+
+    # load config including test data
     call_command(
         "camac_load",
         user="test-dummy@adfinis.com",
         stdout=open(os.devnull, "w"),
     )
 
-    # overwrite configuration
-    call_command(
-        "camac_load",
-        user="test-dummy@adfinis.com",
-        stdout=open(os.devnull, "w"),
-    )
-    outdir = tmp_path / dump_type
-    outdir.mkdir()
+    # make sure pure config models are flushed
+    assert not Resource.objects.filter(pk=resource_to_be_deleted.pk).exists()
 
-    call_command(
-        f"camac_dump_{dump_type}",
-        output_dir=str(outdir),
-        stdout=open(os.devnull, "w"),
-    )
+    for dump_type in ["config", "data"]:
+        outdir = tmp_path / dump_type
+        outdir.mkdir()
 
-    for filepath in glob(settings.APPLICATION_DIR(f"{dump_type}/*.json")):
-        filename = filepath.split("/")[-1]
-        test_filepath = outdir / filename
+        call_command(
+            f"camac_dump_{dump_type}",
+            output_dir=str(outdir),
+            stdout=open(os.devnull, "w"),
+        )
 
-        with open(test_filepath, "r") as test_dumped, open(filepath, "r") as dumped:
-            #  verify that dump is still the same
-            assert json.load(test_dumped) == json.load(
-                dumped
-            ), f"Dumped file '{filename}' does not match '{filepath}'"
+        for filepath in glob(settings.APPLICATION_DIR(f"{dump_type}/*.json")):
+            filename = filepath.split("/")[-1]
+            test_filepath = outdir / filename
+
+            with open(test_filepath, "r") as test_dumped, open(filepath, "r") as dumped:
+                #  verify that dump is still the same
+                assert json.load(test_dumped) == json.load(
+                    dumped
+                ), f"Dumped file '{filename}' does not match '{filepath}'"
