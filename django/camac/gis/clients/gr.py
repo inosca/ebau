b7ae4335e34c9a42950aa5c020d75c8cca7e40a4
Christian Zosel <christian@zosel.ch>
2023-10-09 13:55:21 +0200

    fix: refactor verbindlichkeit

--- django/camac/gis/clients/gr.py
+++ django/camac/gis/clients/gr.py
@@ -178,10 +178,13 @@ class GrGisClient(GISBaseClient):
 
             for layer in layers:
                 for data in self.get_xml(resonse_content, layer):
-                    if find(data, "Bezeichnung"):
-                        result[question] += [
-                            f"{find(data, 'Bezeichnung')}{'('+ find(data, 'Verbindlichkeit')+')' if find(data, 'Verbindlichkeit') and find(data, 'Verbindlichkeit')!='Nutzungsplanfestlegung' else ''}"
-                        ]
+                    if bezeichnung := find(data, "Bezeichnung"):
+                        bracket = ""
+                        if verbindlichkeit := find(data, "Verbindlichkeit"):
+                            if verbindlichkeit != "Nutzungsplanfestlegung":
+                                bracket = " (" + verbindlichkeit + ")"
+
+                        result[question] += [bezeichnung + bracket]
 
         return {
             question: ", ".join(value) for question, value in result.items() if value
