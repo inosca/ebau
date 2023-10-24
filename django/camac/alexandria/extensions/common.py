99c0c1499b003da9ace8e58a37af2f425e4c04e7
Christian Zosel <christian@zosel.ch>
2023-10-24 18:20:54 +0200

    fix(alexandria): applicant access

--- django/camac/alexandria/extensions/common.py
+++ django/camac/alexandria/extensions/common.py
@@ -8,7 +8,7 @@ from camac.user.models import Service
 
 def get_role(user):
     # TODO: tests set group even when its public
-    if user.group is None:  # pragma: no cover
+    if user.camac_group is None:  # pragma: no cover
         return "public"
 
     role = user.camac_role
