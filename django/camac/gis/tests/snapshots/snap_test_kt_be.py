3d7d27c5c2a98e51fd2d8f5d6911773f0b3cb2dc
Maria Vatasoiu <maria.vatasoiu@adfinis.com>
2023-10-26 17:29:01 +0200

    feat(be) GIS migration to new client

---
+++ django/camac/gis/tests/snapshots/snap_test_kt_be.py
@@ -0,0 +1,196 @@
+# -*- coding: utf-8 -*-
+# snapshottest: v1 - https://goo.gl/zC4yUc
+from __future__ import unicode_literals
+
+from snapshottest import Snapshot
+
+snapshots = Snapshot()
+
+snapshots['test_be_client[CH643546955207] 1'] = {
+    'data': {
+        'belasteter-standort': {
+            'displayValue': 'ja',
+            'hidden': False,
+            'label': 'belasteter-standort',
+            'value': 'belasteter-standort-ja'
+        },
+        'gebiet-mit-archaeologischen-objekten': {
+            'displayValue': 'nein',
+            'hidden': False,
+            'label': 'gebiet-mit-archaeologischen-objekten',
+            'value': 'gebiet-mit-archaeologischen-objekten-nein'
+        },
+        'gebiet-mit-naturgefahren': {
+            'displayValue': 'nein',
+            'hidden': False,
+            'label': 'gebiet-mit-naturgefahren',
+            'value': 'gebiet-mit-naturgefahren-nein'
+        },
+        'gewaesserschutzbereich-v2': {
+            'hidden': False,
+            'label': 'gewaesserschutzbereich-v2',
+            'value': [
+                {
+                    'displayValue': 'ueb',
+                    'value': 'gewaesserschutzbereich-v2-ueb'
+                },
+                {
+                    'displayValue': 'ao',
+                    'value': 'gewaesserschutzbereich-v2-ao'
+                }
+            ]
+        },
+        'handelt-es-sich-um-ein-baudenkmal': {
+            'displayValue': 'ja',
+            'hidden': False,
+            'label': 'handelt-es-sich-um-ein-baudenkmal',
+            'value': 'handelt-es-sich-um-ein-baudenkmal-ja'
+        },
+        'naturschutz': {
+            'displayValue': 'ja',
+            'hidden': False,
+            'label': 'naturschutz',
+            'value': 'naturschutz-ja'
+        },
+        'objekt-des-besonderen-landschaftsschutzes': {
+            'displayValue': 'nein',
+            'hidden': False,
+            'label': 'objekt-des-besonderen-landschaftsschutzes',
+            'value': 'objekt-des-besonderen-landschaftsschutzes-nein'
+        }
+    }
+}
+
+snapshots['test_be_client[CH673533354667] 1'] = {
+    'data': {
+        'belasteter-standort': {
+            'displayValue': 'ja',
+            'hidden': False,
+            'label': 'belasteter-standort',
+            'value': 'belasteter-standort-ja'
+        },
+        'gebiet-mit-archaeologischen-objekten': {
+            'displayValue': 'ja',
+            'hidden': False,
+            'label': 'gebiet-mit-archaeologischen-objekten',
+            'value': 'gebiet-mit-archaeologischen-objekten-ja'
+        },
+        'gebiet-mit-naturgefahren': {
+            'displayValue': 'ja',
+            'hidden': False,
+            'label': 'gebiet-mit-naturgefahren',
+            'value': 'gebiet-mit-naturgefahren-ja'
+        },
+        'gewaesserschutzbereich-v2': {
+            'hidden': False,
+            'label': 'gewaesserschutzbereich-v2',
+            'value': [
+                {
+                    'displayValue': 'au',
+                    'value': 'gewaesserschutzbereich-v2-au'
+                }
+            ]
+        },
+        'handelt-es-sich-um-ein-baudenkmal': {
+            'displayValue': 'nein',
+            'hidden': False,
+            'label': 'handelt-es-sich-um-ein-baudenkmal',
+            'value': 'handelt-es-sich-um-ein-baudenkmal-nein'
+        },
+        'naturschutz': {
+            'displayValue': 'nein',
+            'hidden': False,
+            'label': 'naturschutz',
+            'value': 'naturschutz-nein'
+        },
+        'nutzungszone': {
+            'hidden': False,
+            'label': 'nutzungszone',
+            'value': 'Gewerbezone_G2, Grünzone_GrZ, Zone_für_öffentliche_Nutzung, Zone_für_öffentliche_Nutzung_B, Zone_für_öffentliche_Nutzung_C, Zone_für_öffentliche_Nutzung_D'
+        },
+        'objekt-des-besonderen-landschaftsschutzes': {
+            'displayValue': 'nein',
+            'hidden': False,
+            'label': 'objekt-des-besonderen-landschaftsschutzes',
+            'value': 'objekt-des-besonderen-landschaftsschutzes-nein'
+        },
+        'ueberbauungsordnung': {
+            'hidden': False,
+            'label': 'ueberbauungsordnung',
+            'value': 'ZPP_2_"Aarolina", ZPP_5_"Chräjeninsel", ÜO_"Arolina", ÜO_"Chräjeninsel"'
+        }
+    }
+}
+
+snapshots['test_be_client[CH851446093521] 1'] = {
+    'data': {
+        'belasteter-standort': {
+            'displayValue': 'nein',
+            'hidden': False,
+            'label': 'belasteter-standort',
+            'value': 'belasteter-standort-nein'
+        },
+        'gebiet-mit-archaeologischen-objekten': {
+            'displayValue': 'ja',
+            'hidden': False,
+            'label': 'gebiet-mit-archaeologischen-objekten',
+            'value': 'gebiet-mit-archaeologischen-objekten-ja'
+        },
+        'gebiet-mit-naturgefahren': {
+            'displayValue': 'nein',
+            'hidden': False,
+            'label': 'gebiet-mit-naturgefahren',
+            'value': 'gebiet-mit-naturgefahren-nein'
+        },
+        'gewaesserschutzbereich-v2': {
+            'hidden': False,
+            'label': 'gewaesserschutzbereich-v2',
+            'value': [
+                {
+                    'displayValue': 'ao',
+                    'value': 'gewaesserschutzbereich-v2-ao'
+                },
+                {
+                    'displayValue': 'au',
+                    'value': 'gewaesserschutzbereich-v2-au'
+                }
+            ]
+        },
+        'handelt-es-sich-um-ein-baudenkmal': {
+            'displayValue': 'ja',
+            'hidden': False,
+            'label': 'handelt-es-sich-um-ein-baudenkmal',
+            'value': 'handelt-es-sich-um-ein-baudenkmal-ja'
+        },
+        'naturschutz': {
+            'displayValue': 'nein',
+            'hidden': False,
+            'label': 'naturschutz',
+            'value': 'naturschutz-nein'
+        },
+        'nutzungszone': {
+            'hidden': False,
+            'label': 'nutzungszone',
+            'value': 'Wohnzone 1 E1, Wohnzone 2 E2, Zone für öffentliche Nutzung ZöN 7'
+        },
+        'objekt-des-besonderen-landschaftsschutzes': {
+            'displayValue': 'nein',
+            'hidden': False,
+            'label': 'objekt-des-besonderen-landschaftsschutzes',
+            'value': 'objekt-des-besonderen-landschaftsschutzes-nein'
+        }
+    }
+}
+
+
+snapshots['test_be_client_error[doesntexist] 1'] = [
+    'No polygon found'
+]
+
+snapshots['test_be_client_error[emptygis] 1'] = [
+    'No polygon found'
+]
+
+snapshots['test_be_client_error[emptypolygon] 1'] = [
+    'No polygon found'
+]
