77f8dc449cd1630289e943a047162f150e79132d
yelinz <yelin.zhang@adfinis.com>
2023-10-26 10:38:54 +0200

    fix(django): alexandria search param

--- django/camac/alexandria/apps.py
+++ django/camac/alexandria/apps.py
@@ -2,19 +2,10 @@ from importlib import import_module
 
 from django.apps import AppConfig
 
-from camac.user.permissions import DefaultPermission, PublicationPermission
-
 
 class AlexandriaConfig(AppConfig):
     name = "camac.alexandria"
 
     def ready(self):
+        # load signals
         import_module("camac.alexandria.extensions.events")
-        from alexandria.core import views
-
-        views.DocumentViewSet.permission_classes = [
-            DefaultPermission | PublicationPermission
-        ]
-        views.FileViewSet.permission_classes = [
-            DefaultPermission | PublicationPermission
-        ]
---
+++ django/camac/alexandria/views.py
@@ -0,0 +1,27 @@
+from alexandria.core import views
+from rest_framework.filters import OrderingFilter
+from rest_framework_json_api.django_filters import DjangoFilterBackend
+
+from camac.filters import MultilingualSearchFilter
+from camac.user.permissions import DefaultPermission, PublicationPermission
+
+
+class PatchedDjangoFilterBackend(DjangoFilterBackend):
+    search_param = "filter[search]"
+
+
+class PatchedSearch(MultilingualSearchFilter):
+    search_param = "filter[search]"
+
+
+class PatchedDocumentViewSet(views.DocumentViewSet):
+    permission_classes = [DefaultPermission | PublicationPermission]
+    filter_backends = [
+        PatchedSearch,
+        OrderingFilter,
+        PatchedDjangoFilterBackend,
+    ]
+
+
+class PatchedFileViewSet(views.FileViewSet):
+    permission_classes = [DefaultPermission | PublicationPermission]
