acdcb15b8e559ce52ed047632851b5685e713e13
Jonas Metzener <jonas.metzener@adfinis.com>
2023-11-01 17:10:34 +0100

    fix(ember-ebau): reset case filters properly
    We need to reset the filters of the case list when exiting the route.
    The case filter component will propagate the correct filters from the
    local storage. But if we don't clear them, we'll always use the previous
    filters even if we switch group which we don't want because it will
    result in weird behaviour.

--- ember-ebau/app/cases/index/route.js
+++ ember-ebau/app/cases/index/route.js
@@ -1,3 +1,11 @@
 import Route from "@ember/routing/route";
 
-export default class CasesIndexRoute extends Route {}
+export default class CasesIndexRoute extends Route {
+  resetController(controller, isExiting, transition) {
+    super.resetController(controller, isExiting, transition);
+
+    if (isExiting) {
+      controller.caseFilter = {};
+    }
+  }
+}
