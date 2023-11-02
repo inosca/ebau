115d1b5d5cdf9830f7b35da1a7d142a95fb37a11
Jonas Metzener <jonas.metzener@adfinis.com>
2023-11-02 13:42:24 +0100

    feat(dev): add command to automatically generate IR ACLs

---
+++ django/camac/core/management/commands/assign_acls.py
@@ -0,0 +1,81 @@
+import json
+import os
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+from camac.core.models import InstanceResource, IrRoleAcl
+from camac.instance.models import InstanceState
+from camac.user.models import Role
+
+
+class Command(BaseCommand):
+    help = "Assign instance resource ACLs"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.filename = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            f"acls/{settings.APPLICATION_NAME}.json",
+        )
+
+    def add_arguments(self, parser):
+        parser.add_argument("-d", "--dump", dest="dump", action="store_true")
+
+    def handle(self, *app_labels, **options):
+        if options.get("dump"):
+            self.dump()
+        else:
+            self.assign()
+
+    def dump(self):
+        data = {}
+
+        for ir in InstanceResource.objects.all().order_by("pk"):
+            acls = {}
+
+            for role in ir.role_acls.values_list("role__name", flat=True).order_by(
+                "role__name"
+            ):
+                acls[role] = list(
+                    ir.role_acls.filter(role__name=role)
+                    .order_by("instance_state__name")
+                    .values_list("instance_state__name", flat=True)
+                )
+
+            data[ir.pk] = {
+                "label": ir.get_name(),
+                "acls": acls,
+            }
+
+        with open(self.filename, "w") as file:
+            json.dump(data, file, indent=2, ensure_ascii=False)
+
+    def assign(self):
+        with open(self.filename, "r") as file:
+            data = json.load(file)
+
+            for ir_pk, config in data.items():
+                instance_resource = InstanceResource.objects.get(pk=ir_pk)
+
+                for role_name, instance_states in config["acls"].items():
+                    role = Role.objects.get(name=role_name)
+
+                    for instance_state_name in instance_states:
+                        instance_state = InstanceState.objects.get(
+                            name=instance_state_name
+                        )
+
+                        _, created = IrRoleAcl.objects.get_or_create(
+                            instance_resource=instance_resource,
+                            role=role,
+                            instance_state=instance_state,
+                        )
+
+                        if created:
+                            self.stdout.write(
+                                self.style.SUCCESS(
+                                    f"New ACL: {role_name}, {instance_state_name} on IR {instance_resource.get_name()}"
+                                )
+                            )
