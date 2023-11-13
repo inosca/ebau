27f097aadcd2192f16108c3025ea4003952d4ac2
Jonas Metzener <jonas.metzener@adfinis.com>
2023-11-13 13:51:22 +0100

    feat(so): add new category for services and their children

--- django/camac/alexandria/extensions/common.py
+++ django/camac/alexandria/extensions/common.py
@@ -1,4 +1,6 @@
 from functools import lru_cache
+from itertools import chain
+from typing import List, Union
 
 from django.conf import settings
 
@@ -30,3 +32,21 @@ def get_kt_gr_mapped_role(user, role):
     service_group = get_service_group(user.group)
 
     return KT_GR_MAPPING.get(service_group, role)
+
+
+@lru_cache
+def get_service_parent_and_children(service_id: Union[int, str]) -> List[str]:
+    ids = set(
+        chain(
+            *Service.objects.filter(pk=service_id).values_list(
+                "pk",
+                # Passed service is parent service
+                "service_children__pk",
+                # Passed service is child service
+                "service_parent_id",
+                "service_parent__service_children__pk",
+            )
+        )
+    )
+
+    return [str(id) for id in ids if id is not None]
--- django/camac/alexandria/tests/test_visibilities.py
+++ django/camac/alexandria/tests/test_visibilities.py
@@ -5,8 +5,8 @@ from alexandria.core.factories import (
     FileFactory,
     TagFactory,
 )
+from alexandria.core.models import Document
 from django.urls import reverse
-from pytest_factoryboy import LazyFixture
 from rest_framework.status import HTTP_200_OK
 
 from camac.instance.tests.test_instance_public import (  # noqa: F401
@@ -14,160 +14,173 @@ from camac.instance.tests.test_instance_public import (  # noqa: F401
 )
 
 
-@pytest.mark.parametrize("instance__user", [LazyFixture("user")])
-@pytest.mark.parametrize(
-    "role__name,is_nested_category,expected",
-    [
-        ("applicant", False, ["municipality", "invitee", "applicant"]),
-        ("municipality", False, ["municipality"]),
-        ("municipality", True, ["municipality"]),
-        ("service", False, ["service"]),
-        ("public", False, ["public"]),
-    ],
-)
-def test_document_visibility(
+@pytest.fixture
+def alexandria_setup(
     db,
-    role,
-    caluma_admin_user,
-    admin_client,
-    instance,
-    instance_factory,
-    instance_with_case,
-    publication_settings,
-    applicant_factory,
     create_caluma_publication,  # noqa: F811
-    is_nested_category,
-    expected,
+    instance_with_case,
+    instance,
+    minio_mock,
+    mocker,
+    publication_settings,
+    role,
+    service_factory,
+    service,
 ):
-    applicant_factory(invitee=admin_client.user, instance=instance)
+    mocker.patch(
+        "camac.alexandria.extensions.visibilities.CustomVisibility._all_visible_instances",
+        return_value=[instance.pk],
+    )
 
-    # directly readble
+    instance = instance_with_case(instance)
+    other_service = service_factory()
+
+    if role.name == "subservice":
+        parent_service = service_factory()
+        service.service_parent = parent_service
+        service.save()
+        subservice_1 = service
+    else:
+        parent_service = service
+        subservice_1 = service_factory(service_parent=parent_service)
+
+    subservice_2 = service_factory(service_parent=parent_service)
+
     applicant_category = CategoryFactory(
         metainfo={"access": {"applicant": {"visibility": "all"}}}
     )
+    applicant_nested_category = CategoryFactory(parent=applicant_category)
     municipality_category = CategoryFactory(
         metainfo={"access": {"municipality": {"visibility": "all"}}}
     )
     service_category = CategoryFactory(
         metainfo={"access": {"service": {"visibility": "service"}}}
     )
+    service_and_subservice_category = CategoryFactory(
+        metainfo={
+            "access": {
+                "service": {"visibility": "service-and-subservice"},
+                "subservice": {"visibility": "service-and-subservice"},
+            }
+        }
+    )
 
-    if is_nested_category:
-        municipality_category = CategoryFactory(parent=municipality_category)
-
+    # applicant documents
     DocumentFactory(
         category=applicant_category,
         metainfo={"camac-instance-id": instance.pk},
         title="applicant",
     )
-    document = DocumentFactory(
-        category=municipality_category,
+    DocumentFactory(
+        category=applicant_nested_category,
         metainfo={"camac-instance-id": instance.pk},
-        title="municipality",
+        title="applicant nested",
     )
-    # decision document
-    document.tags.add(TagFactory(slug="decision"))
 
-    # readable from service
+    # service documents
     DocumentFactory(
         category=service_category,
-        created_by_group=caluma_admin_user.group,
+        created_by_group=str(service.pk),
         metainfo={"camac-instance-id": instance.pk},
         title="service",
     )
     DocumentFactory(
         category=service_category,
-        created_by_group=caluma_admin_user.group + 1,
+        created_by_group=str(other_service.pk),
         metainfo={"camac-instance-id": instance.pk},
         title="service 2",
     )
 
-    # readable as invitee
+    # service and subservice documents
     DocumentFactory(
-        category=applicant_category,
+        category=service_and_subservice_category,
+        created_by_group=str(parent_service.pk),
         metainfo={"camac-instance-id": instance.pk},
-        title="invitee",
+        title="subservice shared 1",
+    )
+    DocumentFactory(
+        category=service_and_subservice_category,
+        created_by_group=str(subservice_1.pk),
+        metainfo={"camac-instance-id": instance.pk},
+        title="subservice shared 2",
+    )
+    DocumentFactory(
+        category=service_and_subservice_category,
+        created_by_group=str(subservice_2.pk),
+        metainfo={"camac-instance-id": instance.pk},
+        title="subservice shared 3",
     )
 
-    # published instance
-    public_instance = instance_with_case(instance_factory())
-    create_caluma_publication(public_instance)
-    DocumentFactory(metainfo={"camac-instance-id": public_instance.pk}, title="hidden")
+    # decision document
+    document = DocumentFactory(
+        category=municipality_category,
+        metainfo={"camac-instance-id": instance.pk},
+        title="decision",
+    )
+    document.tags.add(TagFactory(slug="decision"))
+
+    # publication document
+    create_caluma_publication(instance)
+    DocumentFactory(metainfo={"camac-instance-id": instance.pk}, title="hidden")
     public = DocumentFactory(
-        metainfo={"camac-instance-id": public_instance.pk}, title="public"
+        metainfo={"camac-instance-id": instance.pk}, title="publication"
     )
     public.tags.add(TagFactory(slug="publication"))
 
-    url = reverse("document-list")
-    if role.name == "public":
-        response = admin_client.get(url, HTTP_X_CAMAC_PUBLIC_ACCESS=True)
-    else:
-        response = admin_client.get(url)
-
-    assert response.status_code == HTTP_200_OK
-    json = response.json()
-    assert set([obj["attributes"]["title"]["de"] for obj in json["data"]]) == set(
-        expected
-    )
+    for document in Document.objects.all():
+        FileFactory(document=document)
 
 
-@pytest.mark.parametrize("is_nested_category", [True, False])
+@pytest.mark.parametrize("type", ["document", "file"])
 @pytest.mark.parametrize(
-    "role__name,expected_count",
+    "role__name,expected",
     [
-        ("applicant", 3),
-        ("municipality", 2),
-        ("service", 1),
+        ("applicant", ["applicant", "applicant nested", "decision"]),
+        ("municipality", ["decision"]),
+        (
+            "service",
+            [
+                "service",
+                "subservice shared 1",
+                "subservice shared 2",
+                "subservice shared 3",
+            ],
+        ),
+        (
+            "subservice",
+            ["subservice shared 1", "subservice shared 2", "subservice shared 3"],
+        ),
+        ("public", ["publication"]),
     ],
 )
-def test_file_visibility(
+def test_document_and_file_visibility(
     db,
-    minio_mock,
-    applicant_factory,
-    caluma_admin_user,
-    instance,
     admin_client,
+    alexandria_setup,
+    expected,
     role,
-    expected_count,
-    is_nested_category,
+    type,
 ):
-    applicant_factory(invitee=admin_client.user, instance=instance)
-    applicant_category = CategoryFactory(
-        metainfo={"access": {"applicant": {"visibility": "all"}}}
-    )
-    municipality_category = CategoryFactory(
-        metainfo={"access": {"municipality": {"visibility": "all"}}}
-    )
-    service_category = CategoryFactory(
-        metainfo={"access": {"service": {"visibility": "service"}}}
-    )
+    url = reverse(f"{type}-list")
 
-    if is_nested_category:
-        applicant_category = CategoryFactory(parent=applicant_category)
-        municipality_category = CategoryFactory(parent=municipality_category)
-        service_category = CategoryFactory(parent=service_category)
+    if type == "file":
+        data = {"include": "document"}
+    else:
+        data = {}
 
-    applicant_document = DocumentFactory(
-        category=applicant_category, metainfo={"camac-instance-id": instance.pk}
-    )
-    municipality_document = DocumentFactory(
-        category=municipality_category, metainfo={"camac-instance-id": instance.pk}
-    )
-    service_document = DocumentFactory(
-        category=service_category,
-        created_by_group=caluma_admin_user.group,
-        metainfo={"camac-instance-id": instance.pk},
-    )
-    FileFactory.create_batch(3, document=applicant_document)
-    FileFactory.create_batch(2, document=municipality_document)
-    FileFactory.create_batch(1, document=service_document)
-
-    url = reverse("file-list")
-    response = admin_client.get(url)
+    if role.name == "public":
+        response = admin_client.get(url, data=data, HTTP_X_CAMAC_PUBLIC_ACCESS=True)
+    else:
+        response = admin_client.get(url, data=data)
 
     assert response.status_code == HTTP_200_OK
     json = response.json()
-    assert len(json["data"]) == expected_count
+    assert set(
+        [
+            obj["attributes"]["title"]["de"]
+            for obj in json["included" if type == "file" else "data"]
+        ]
+    ) == set(expected)
 
 
 @pytest.mark.parametrize(
