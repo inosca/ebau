83a092e4fca22192de8f25053014bd7379b05364
Jonas Metzener <jonas.metzener@adfinis.com>
2023-11-17 14:36:37 +0100

    chore(swagger): disable swagger for alexandria views

--- django/camac/alexandria/urls.py
+++ django/camac/alexandria/urls.py
@@ -2,17 +2,17 @@ from alexandria.core import views as alexandria_views
 from django.urls import path
 from rest_framework.routers import SimpleRouter
 
-from .views import PatchedDocumentViewSet, PatchedFileViewSet, PatchedTagViewSet
+from camac.alexandria import views
 
 r = SimpleRouter(trailing_slash=False)
 
 # override the default viewset with our patched versions
 # this needs to be updated when new urls are added in alexandria
-r.register(r"categories", alexandria_views.CategoryViewSet)
-r.register(r"documents", PatchedDocumentViewSet)
-r.register(r"files", PatchedFileViewSet)
-r.register(r"tags", PatchedTagViewSet)
-r.register(r"tagsynonymgroups", alexandria_views.TagSynonymGroupViewSet)
+r.register(r"categories", views.PatchedCategoryViewSet)
+r.register(r"documents", views.PatchedDocumentViewSet)
+r.register(r"files", views.PatchedFileViewSet)
+r.register(r"tags", views.PatchedTagViewSet)
+r.register(r"tagsynonymgroups", views.PatchedTagSynonymGroupViewSet)
 
 urlpatterns = r.urls
 
