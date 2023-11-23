cdc215363baa5f14431f28eca3748bd77cbf038a
Falk <falk.neumann@adfinis.com>
2023-11-23 12:48:00 +0100

    chore(deps): update ember-power-select to v7.2.0

--- ember-ebau-core/addon/models/instance-acl.js
+++ ember-ebau-core/addon/models/instance-acl.js
@@ -1,23 +1,27 @@
+import { inject as service } from "@ember/service";
 import Model, { attr, belongsTo } from "@ember-data/model";
 import { DateTime } from "luxon";
 
 export const AVAILABLE_GRANT_TYPES = [
-  "anonymous-public",
-  "authenticated-public",
-  "service",
-  "token",
-  "user",
+  "ANONYMOUS_PUBLIC",
+  "AUTHENTICATED_PUBLIC",
+  "SERVICE",
+  "TOKEN",
+  "USER",
 ];
 
 export default class InstanceAclModel extends Model {
+  @service store;
+  @service fetch;
+
   @attr grantType;
   @attr createdByEvent;
   @attr revokedByEvent;
 
-  @attr("date") createdAt;
-  @attr("date") revokedAt;
-  @attr("date") startTime;
-  @attr("date") endTime;
+  @attr createdAt;
+  @attr revokedAt;
+  @attr startTime;
+  @attr endTime;
 
   // only one of those can be set.
   @attr token;
@@ -29,7 +33,7 @@ export default class InstanceAclModel extends Model {
   @belongsTo("user", { inverse: null, async: true }) revokedByUser;
   @belongsTo("service", { inverse: null, async: true }) createdByService;
   @belongsTo("service", { inverse: null, async: true }) revokedByService;
-  @belongsTo("access-level", { inverse: null, asnyc: true }) accessLevel;
+  @belongsTo("access-level", { inverse: null, async: true }) accessLevel;
 
   get createdByName() {
     return this.createdByUser
@@ -55,23 +59,44 @@ export default class InstanceAclModel extends Model {
   }
 
   get entityName() {
+    const placeholder = "-";
     switch (this.grantType) {
-      case "user":
-        return this.user.get("fullName");
-      case "service":
-        return this.service.get("name");
-      case "authenticated-public":
-        // TODO: translate
-        return "Öffentlicher Zugriff (nicht registriert)";
-      case "anonymous-public":
+      case "USER":
+        return this.user.get("fullName") ?? placeholder;
+      case "SERVICE":
+        return this.service.get("name") ?? placeholder;
+      case "AUTHENTICATED_PUBLIC":
         // TODO: translate
         return "Öffentlicher Zugriff (registriert)";
-      case "token":
+      case "ANONYMOUS_PUBLIC":
+        // TODO: translate
+        return "Öffentlicher Zugriff (nicht registriert)";
+      case "TOKEN":
         // TODO: translate
         return "Via Zugangscode";
       default:
-        // TODO: find a better fallback message
-        return "-";
+        return placeholder;
     }
+  }
+
+  get revokeable() {
+    return (
+      this.createdByEvent === "manual-creation" && this.status === "active"
+    );
+  }
+
+  async revoke() {
+    const modelName = "instance-acl";
+    const adapter = this.store.adapterFor(modelName);
+
+    const url = adapter.buildURL(modelName, this.id);
+    const body = JSON.stringify(adapter.serialize(this));
+
+    const response = await this.fetch.fetch(`${url}/revoke`, {
+      method: "POST",
+      body,
+    });
+    const json = await response.json();
+    this.store.pushPayload(json);
   }
 }
