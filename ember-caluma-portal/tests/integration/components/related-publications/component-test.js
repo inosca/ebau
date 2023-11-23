009ce801f74e2bd063fc1ceb0c944cd302522804
Jonas Metzener <jonas.metzener@adfinis.com>
2023-11-23 15:57:47 +0100

    test(ember): use skip instead of todo for empty tests
    `testem-gitlab-reporter` counts tests that are marked as `todo` as
    failures.

--- ember-caluma-portal/tests/integration/components/related-publications/component-test.js
+++ ember-caluma-portal/tests/integration/components/related-publications/component-test.js
@@ -1,13 +1,13 @@
 import { render } from "@ember/test-helpers";
 import { hbs } from "ember-cli-htmlbars";
-import { module, todo } from "qunit";
+import { module, skip } from "qunit";
 
 import { setupRenderingTest } from "caluma-portal/tests/helpers";
 
 module("Integration | Component | related-publications", function (hooks) {
   setupRenderingTest(hooks);
 
-  todo("it renders", async function (assert) {
+  skip("it renders", async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
