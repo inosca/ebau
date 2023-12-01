c3774a13433ffe74aca630f6cddbdf9b1acee6c4
Jonas Metzener <jonas.metzener@adfinis.com>
2023-12-01 13:19:38 +0100

    feat(django-admin): show version in django admin

--- django/camac/core/tests/test_custom_template_tags.py
+++ django/camac/core/tests/test_custom_template_tags.py
@@ -1,3 +1,4 @@
+import pytest
 from django.template import Context, Template
 
 
@@ -13,3 +14,25 @@ def test_custom_template_tag_settings_value(settings, application_settings):
 
     rendered = template.render(Context())
     assert rendered == "kt_gr, gr"
+
+
+@pytest.mark.parametrize(
+    "use_tagged_releases,env,expected",
+    [
+        (True, "production", "1.2.3"),
+        (True, "development", "development"),
+        (False, "production", None),
+    ],
+)
+def test_custom_template_tag_app_version(
+    settings, application_settings, mocker, use_tagged_releases, env, expected
+):
+    mocker.patch("camac.camac_metadata.__version__", "1.2.3")
+
+    settings.ENV = env
+    application_settings["TAGGED_RELEASES"] = use_tagged_releases
+
+    template = Template("{% load app_version %}{% app_version as VERSION %}{{VERSION}}")
+
+    rendered = template.render(Context())
+    assert rendered == str(expected)
