ddb7fac5ce0adf610a4081027869d2ce7b60f024
Falk <falk.neumann@adfinis.com>
2023-12-04 13:37:54 +0100

    fix(permissions): add confirm dialog before revoking acls

--- ember-ebau-core/addon/components/permissions/acl-table.js
+++ ember-ebau-core/addon/components/permissions/acl-table.js
@@ -4,6 +4,7 @@ import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
 import { findRecord, findAll } from "ember-data-resources";
 import { dedupeTracked } from "tracked-toolbox";
+import { confirm } from "ember-uikit";
 
 import paginatedQuery from "ember-ebau-core/resources/paginated";
 
@@ -78,6 +79,7 @@ export default class AclTable extends Component {
   @action
   async revokeAcl(acl) {
     try {
+      await confirm(this.intl.t("permissions.confirmRevoke"));
       await acl.revoke();
     } catch (error) {
       console.error(error);
