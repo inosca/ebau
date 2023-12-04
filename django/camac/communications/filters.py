7bb31327e29ada8dd3c5b8ac73e80f93f0f168ba
Maria Vatasoiu <maria.vatasoiu@adfinis.com>
2023-12-04 14:45:05 +0100

    feat(be): Add responsible_service_users to TopicSerializer and add responsible_service_user filter field to TopicFilterSet

--- django/camac/communications/filters.py
+++ django/camac/communications/filters.py
@@ -1,16 +1,47 @@
-from django_filters.rest_framework import BooleanFilter, FilterSet, NumberFilter
+from django.core.validators import EMPTY_VALUES
+from django_filters.rest_framework import (
+    BooleanFilter,
+    CharFilter,
+    FilterSet,
+    NumberFilter,
+)
 
 from camac.filters import CharMultiValueFilter
 
+from ..responsible import models as responsible_models
 from . import models
+
+
+class TopicResponsibleServiceUserFilter(CharFilter):
+    def filter(self, qs, value):
+        if value in EMPTY_VALUES:
+            return qs
+
+        # restrict to service from request header
+        current_service = self.parent.request.group.service
+
+        if value.lower() == "nobody":
+            return qs.exclude(
+                # exclude all topics with instances which have responsible services
+                instance__pk__in=responsible_models.ResponsibleService.objects.filter(
+                    service=current_service
+                ).values("instance")
+            )
+
+        return qs.filter(
+            # restrict to current service
+            instance__responsible_services__service=current_service,
+            instance__responsible_services__responsible_user=value,
+        )
 
 
 class TopicFilterSet(FilterSet):
     has_unread = BooleanFilter()
+    responsible_service_user = TopicResponsibleServiceUserFilter()
 
     class Meta:
         model = models.CommunicationsTopic
-        fields = ("subject", "has_unread", "instance")
+        fields = ("subject", "has_unread", "instance", "responsible_service_user")
 
 
 class IsReadFilter(BooleanFilter):
