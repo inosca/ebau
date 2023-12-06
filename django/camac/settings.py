31d2613f4b2c90f2f63c584e96159c713d095a83
David Vogt <david.vogt@adfinis.com>
2023-12-06 14:29:19 +0100

    fix(settings): do not crash if django_extensions is not installed
    If DEBUG is set, but we don't have a dev build running, we shouldn't
    just crash on startup

--- django/camac/settings.py
+++ django/camac/settings.py
@@ -140,8 +140,14 @@ INSTALLED_APPS = [
     "django_json_widget",
 ]
 
-if DEBUG:
-    INSTALLED_APPS.append("django_extensions")
+if DEBUG:  # pragma: no cover
+    try:
+        __import__("django_extensions")
+        INSTALLED_APPS.append("django_extensions")
+    except ImportError:
+        # Nothing bad, just won't have django-extensions niceties installed
+        # (Most likely the container was built without dev dependencies)
+        pass
 
 MIDDLEWARE = [
     "django.contrib.sessions.middleware.SessionMiddleware",
