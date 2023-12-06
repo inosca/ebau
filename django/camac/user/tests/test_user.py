c94a1cf8515778cae1d4b399bd213d81d161684e
Alana Luyten <alana.luyten@adfinis.com>
2023-12-06 13:48:27 +0100

    fix(geometer): provide permissions for responsible services to geometer role

--- django/camac/user/tests/test_user.py
+++ django/camac/user/tests/test_user.py
@@ -70,6 +70,7 @@ def test_me_service_municipality(
         ("Canton", 1),
         ("Municipality", 1),
         ("Coordination", 1),
+        ("Geometer", 1),
     ],
 )
 def test_user_list(admin_client, size):
