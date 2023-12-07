7a8221cf53c953632ec937e15aba2fa6b76e9a02
yelinz <yelin.zhang@adfinis.com>
2023-12-07 16:02:15 +0100

    feat(ember-ebau): add 404 handling for case detail

--- ember-ebau/app/cases/detail/route.js
+++ ember-ebau/app/cases/detail/route.js
@@ -5,15 +5,21 @@ export default class CasesDetailRoute extends Route {
   @service alexandriaConfig;
   @service ebauModules;
   @service store;
+  @service router;
 
-  model({ instance_id }) {
+  async model({ instance_id }) {
     this.alexandriaConfig.instanceId = parseInt(instance_id);
     this.ebauModules.instanceId = parseInt(instance_id);
 
-    // fetch instance to allow reloading after state changes
-    // from ebau-modules.js (redirectToWorkItems)
-    return this.store.findRecord("instance", instance_id, {
-      include: "instance_state,responsible_service_users",
-    });
+    try {
+      // fetch instance to allow reloading after state changes
+      // from ebau-modules.js (redirectToWorkItems)
+      return await this.store.findRecord("instance", instance_id, {
+        include: "instance_state,responsible_service_users",
+      });
+    } catch (error) {
+      console.error(error);
+      this.router.transitionTo("cases.not-found");
+    }
   }
 }
