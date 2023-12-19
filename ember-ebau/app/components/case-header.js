0a376d37f404a2f8a53604bc39acf497179a9502
Christian Zosel <christian@zosel.ch>
2023-12-19 15:58:39 +0100

    feat(gr): add "download receipt" action in portal
    This also consolidates the various places where we offer PDF exports
    into a common service.

--- ember-ebau/app/components/case-header.js
+++ ember-ebau/app/components/case-header.js
@@ -3,12 +3,14 @@ import { inject as service } from "@ember/service";
 import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
 import { dropTask } from "ember-concurrency";
-import { saveAs } from "file-saver";
 
 const LOCAL_STORAGE_KEY = "ebau-hide-master-data";
 
 export default class CaseHeaderComponent extends Component {
   @service fetch;
+  @service dms;
+  @service notification;
+  @service intl;
 
   @tracked hideHeader =
     JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) ?? false;
@@ -23,13 +25,11 @@ export default class CaseHeaderComponent extends Component {
 
   @dropTask
   *downloadPdf() {
-    const response = yield this.fetch.fetch(
-      `/api/v1/instances/${this.args.case.instanceId}/generate-pdf`,
-    );
-    const filename = response.headers
-      .get("content-disposition")
-      .match(/filename="(.*)"/)[1];
-
-    saveAs(yield response.blob(), filename);
+    try {
+      yield this.dms.generatePdf(this.args.case.instanceId);
+    } catch (e) {
+      console.error(e);
+      this.notification.danger(this.intl.t("dms.download-export-error"));
+    }
   }
 }
