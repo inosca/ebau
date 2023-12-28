f59c489d88c3099c2de75f73f33aa89e9e731aa4
Falk <falk.neumann@adfinis.com>
2023-12-28 15:48:10 +0100

    feat(ember-ebau): visual environment indicator

--- ember-ebau/ember-cli-build.js
+++ ember-ebau/ember-cli-build.js
@@ -32,6 +32,7 @@ module.exports = function (defaults) {
       },
       setOwnConfig: {
         application: ENV,
+        enableHeaderWatermark: [ENV_MAP.kt_gr].includes(ENV),
       },
     },
     fingerprint: {
