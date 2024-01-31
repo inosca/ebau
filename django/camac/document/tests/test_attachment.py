ac2df451a7e9974fb4018f2b12d094e865fa654b
David Vogt <david.vogt@adfinis.com>
2024-01-31 17:55:12 +0100

    refactor: move some test functionality to own file to reduce module size

--- django/camac/document/tests/test_attachment.py
+++ django/camac/document/tests/test_attachment.py
@@ -1,4 +1,3 @@
-import copy
 import io
 import json
 from datetime import timedelta
@@ -17,14 +16,6 @@ from camac.permissions.conditions import Always
 from camac.utils import build_url
 
 from .data import django_file
-
-
-@pytest.fixture
-def document_permissions():
-    before = permissions.PERMISSIONS
-    permissions.PERMISSIONS = copy.deepcopy(before)
-    yield permissions.PERMISSIONS
-    permissions.PERMISSIONS = before
 
 
 def _configure_geometer_access(
@@ -1770,78 +1761,3 @@ def test_convert_docx_to_word(
     response = admin_client.post(url)
 
     assert response.status_code == status.HTTP_201_CREATED
-
-
-@pytest.mark.parametrize("role__name", ["municipality-lead"])
-@pytest.mark.parametrize(
-    "is_active, is_involved, expect_success",
-    # Not all of the combinations make sense, but we
-    # wanna test exhaustively nonetheless
-    [
-        (False, False, False),
-        (False, True, True),
-        (True, False, False),  # makes no sense, just for completeness
-        (True, True, True),
-    ],
-)
-def test_set_geometer_flag(
-    # fixtures
-    db,
-    be_instance,
-    set_application_be,
-    attachment_factory,
-    instance_service_factory,
-    instance_factory,
-    service_factory,
-    admin_client,
-    document_permissions,
-    attachment_section_factory,
-    # params
-    is_active,
-    is_involved,
-    expect_success,
-):
-    # Parametrisation
-    if is_involved:
-        # TODO it seems there's allready a correct InstanceService
-        # here, no need to add it again. But need to verify
-        instance_service_factory(
-            instance=be_instance,
-            active=is_active,
-            service=admin_client.user.get_default_group().service,
-        )
-    else:
-        # move the instances to a new, unrelated service.
-        # We shouldn't drop them, as this may cause fallbacks leading
-        # to "our" service again, defeating the purpose
-        be_instance.instance_services.all().update(service=service_factory())
-
-    # Data setup
-    section = attachment_section_factory()
-    attachment = attachment_factory(instance=be_instance)
-    attachment.attachment_sections.set([section])
-
-    # Patching Env to make settings work
-    document_permissions["kt_bern"]["municipality-lead"][
-        permissions.AdminServiceBeforeDecisionPermission
-    ].append(section.pk)
-
-    url = reverse("attachment-detail", args=[attachment.pk])
-    resp = admin_client.patch(
-        f"{url}?instance={be_instance.pk}",
-        data={
-            "data": {
-                "id": str(attachment.pk),
-                "type": "attachments",
-                "attributes": {"context": {"for_geometer": True}},
-            },
-        },
-    )
-
-    if expect_success:
-        assert resp.status_code == status.HTTP_200_OK, resp.json()
-        assert resp.json()["data"]["attributes"]["context"]["for_geometer"]
-    else:
-        # response must be in HTTP/40x range
-        assert resp.status_code >= status.HTTP_400_BAD_REQUEST
-        assert resp.status_code < status.HTTP_500_INTERNAL_SERVER_ERROR
