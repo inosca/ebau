1af3d36abd43279f9539fa7c469607dbff92f2bd
David Vogt <david.vogt@adfinis.com>
2024-02-13 16:16:59 +0100

    feat(kt-bern/permissions): introduce "applicant" access level
    This access level is used to define the permissions that an applicant has
    on the dossier. It will certainly be extended in the future, this is WIP,
    at least during this merge request.
    
    Also implement the event handlers for dealing with adding/removing
    applicants: If an applicant is added or removed, grant or revoke the
    corresponding ACLs.
    
    The applicant event methods are in a new "common" module, so other
    cantons can easily reuse them. Goal is to have as much event handler
    code in the "common" module as possible.

---
+++ django/camac/permissions/config/common.py
@@ -0,0 +1,44 @@
+from logging import getLogger
+
+from camac.permissions import api as permissions_api
+from camac.permissions.events import EmptyEventHandler
+from camac.permissions.models import InstanceACL
+
+log = getLogger(__name__)
+
+
+class ApplicantsEventHandlerMixin(EmptyEventHandler):
+    def applicant_added(self, instance, applicant):
+        # Add an applicant ACL when an applicant is added to the
+        # instance (instance.involved_applicants).
+        if not applicant.invitee:
+            # Non-user applicant, can't do anything right now.
+            # This will be called again once the invitee logs in
+            return
+        self.manager.grant(
+            instance,
+            grant_type=permissions_api.GRANT_CHOICES.USER.value,
+            access_level="applicant",
+            user=applicant.invitee,
+            event_name="applicant_added",
+        )
+
+    def applicant_removed(self, instance, applicant):
+        # See if our applicant has a permission, then revoke if
+        # it exists
+        acls = InstanceACL.objects.filter(
+            instance=instance, user=applicant.invitee, access_level="applicant"
+        )
+        if not acls and getattr(applicant, "invitee"):  # pragma: no cover
+            # applicant (with invitee, so not an email invite) didn't have an
+            # ACL - this is no good
+            log.warning(
+                f"Applicant on instance {instance.pk} ({applicant.invitee}) "
+                "removed, but no matching ACL found. This should never happen!"
+            )
+            return
+        for acl in acls:
+            # Note: There should never be multiple acls for the same
+            # applicant, but as there's no DB constraint enforcing this,
+            # we loop to make sure we get all the affected ACLs
+            self.manager.revoke(acl, event_name="applicant_removed")
