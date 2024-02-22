inosca/ebau

commit a47c77413377e0e4d4a953a00be899b46dafcb08

chore: refactor eCH-0211

django/camac/conftest.py
@@ -1,5 +1,4 @@
 import copy
-import glob
 import inspect
 import logging
 import os
@@ -350,86 +349,6 @@ def use_caluma_form(application_settings):
         "USE_LOCATION": False,
         "GENERATE_IDENTIFIER": False,
     }
-
-
-@pytest.fixture
-def ech_mandatory_answers_baugesuch():
-    return {
-        "ech-subject": "Baugesuch",
-        "caluma-workflow-slug": "building-permit",
-        "gemeinde": "Testgemeinde",
-        "parzelle": [{"parzellennummer": "1586"}],
-        "personalien-gesuchstellerin": [
-            {
-                "name-gesuchstellerin": "Testname",
-                "ort-gesuchstellerin": "Testort",
-                "plz-gesuchstellerin": 2323,
-                "strasse-gesuchstellerin": "Teststrasse",
-                "vorname-gesuchstellerin": "Testvorname",
-                "juristische-person-gesuchstellerin": "Nein",
-            }
-        ],
-    }
-
-
-@pytest.fixture
-def ech_mandatory_answers_einfache_vorabklaerung():
-    return {
-        "ech-subject": "Einfache Vorabklärung",
-        "caluma-workflow-slug": "preliminary-clarification",
-        "personalien-gesuchstellerin": [
-            {
-                "name-gesuchstellerin": "Testname",
-                "ort-gesuchstellerin": "Testort",
-                "plz-gesuchstellerin": 232,  # non standard swiss zip
-                "strasse-gesuchstellerin": "Teststrasse",
-                "vorname-gesuchstellerin": "Testvorname",
-                "juristische-person-gesuchstellerin": "Nein",
-                "telefon-oder-mobile-gesuchstellerin": int("0311234567"),
-                "e-mail-gesuchstellerin": "a@b.ch",
-            }
-        ],
-        "gemeinde": "Testgemeinde",
-        "parzelle": [
-            {
-                "parzellennummer": "1586",
-                "lagekoordinaten-ost": 2480000,
-                "lagekoordinaten-nord": 1070000,
-            }
-        ],
-        "beschreibung-bauvorhaben": "Testvorhaben",
-    }
-
-
-@pytest.fixture
-def ech_mandatory_answers_vollstaendige_vorabklaerung():
-    return {
-        "ech-subject": "Vollständige Vorabklärung",
-        "caluma-workflow-slug": "preliminary-clarification",
-        "gemeinde": "Testgemeinde",
-        "personalien-gesuchstellerin": [
-            {
-                "name-gesuchstellerin": "Testname",
-                "ort-gesuchstellerin": "Testort",
-                "plz-gesuchstellerin": 232,  # non standard swiss zip
-                "strasse-gesuchstellerin": "Teststrasse",
-                "vorname-gesuchstellerin": "Testvorname",
-                "juristische-person-gesuchstellerin": "Nein",
-            }
-        ],
-    }
-
-
-@pytest.fixture
-def caluma_config_bern(db):
-    """
-    Load the caluma config for kt_bern.
-
-    Execution of this fixture takes some time. Only use if really necessary.
-    """
-    call_command(
-        "loaddata", *glob.glob(settings.ROOT_DIR("kt_bern/config/caluma_*.json"))
-    )
 
 
 @pytest.fixture
@@ -1401,6 +1320,7 @@ def minio_mock(mocker):
 @pytest.fixture
 def enable_ech(application_settings):
     application_settings["ECH0211"]["API_ACTIVE"] = True
+    application_settings["ECH0211"]["API_LEVEL"] = "full"
     application_settings["DOSSIER_IMPORT"]["PROD_URL"] = "ebau.local"
 
 
@@ -1471,10 +1391,283 @@ def configure_custom_notification_types(
         )
 
 
-@pytest.fixture(autouse=True)
-def _default_file_storage_backend(settings):
-    # This is needed that alexandria file factories don't try to upload
-    # something to a possibly non-existent minio container in tests. Also, we
-    # explicitly disable encryption.
-    settings.ALEXANDRIA_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
-    settings.ALEXANDRIA_ENABLE_AT_REST_ENCRYPTION = False
+class Utils:
+    @staticmethod
+    def _question(slug, type, label=None, options=None):
+        question = caluma_form_models.Question.objects.filter(pk=slug).first()
+        if question is None:
+            question = caluma_form_factories.QuestionFactory(
+                pk=slug,
+                type=type,
+                **({"label": label} if label else {}),
+            )
+        if options:
+            for option in options:
+                caluma_form_factories.QuestionOptionFactory(
+                    question=question,
+                    option__slug=option,
+                    option__label=option.capitalize(),
+                )
+        return question
+
+    @staticmethod
+    def _get_question_type(value, options):
+        if options:
+            if isinstance(value, list):  # pragma: no cover
+                return caluma_form_models.Question.TYPE_MULTIPLE_CHOICE
+            return caluma_form_models.Question.TYPE_CHOICE
+        if isinstance(value, date):
+            return caluma_form_models.Question.TYPE_DATE
+        if isinstance(value, int):
+            return caluma_form_models.Question.TYPE_INTEGER
+        return caluma_form_models.Question.TYPE_TEXT
+
+    @staticmethod
+    def add_answer(
+        document,
+        question,
+        value,
+        label=None,
+        question_label=None,
+        options=None,
+    ):
+
+        question_type = Utils._get_question_type(value, options)
+        value_key = (
+            "date"
+            if question_type == caluma_form_models.Question.TYPE_DATE
+            else "value"
+        )
+
+        answer = caluma_form_factories.AnswerFactory(
+            document=document,
+            question=Utils._question(question, question_type, question_label, options),
+            **{value_key: value},
+        )
+
+        if label:
+            if not isinstance(label, list):
+                label = [label]
+
+            if not isinstance(value, list):
+                value = [value]
+
+            for val, lab in zip(value, label):
+                if not isinstance(lab, dict):
+                    lab = {"de": lab, "fr": lab}
+
+                caluma_form_factories.QuestionOptionFactory(
+                    question_id=question, option__slug=val, option__label=lab
+                )
+
+        return answer
+
+    @staticmethod
+    def add_table_answer(document, question, rows, table_answer=None, row_form_id=None):
+        answer = (
+            Utils.add_answer(document, question, None)
+            if not table_answer
+            else table_answer
+        )
+
+        for i, row in enumerate(reversed(rows)):
+            row_args = {"form_id": row_form_id} if row_form_id else {}
+            row_document = caluma_form_factories.DocumentFactory(
+                family=document.family, **row_args
+            )
+            for column, value in row.items():
+                options = None
+                if type(value) == dict:
+                    options = value["options"]
+                    value = value["value"]
+                Utils.add_answer(row_document, column, value, options=options)
+
+            caluma_form_factories.AnswerDocumentFactory(
+                document=row_document, answer=answer, sort=i
+            )
+
+        return answer
+
+
+@pytest.fixture
+def utils():
+    return Utils
+
+
+@pytest.fixture
+def ur_master_data_case(
+    db,
+    ur_instance,
+    workflow_entry_factory,
+    camac_answer_factory,
+    master_data_is_visible_mock,
+    utils,
+):
+    ur_instance.case.meta = {"dossier-number": "1201-21-003"}
+    ur_instance.case.save()
+
+    document = ur_instance.case.document
+
+    # Simple data
+    utils.add_answer(document, "proposal-description", "Grosses Haus")
+    utils.add_answer(document, "parcel-street", "Musterstrasse")
+    utils.add_answer(document, "parcel-street-number", 4)
+    utils.add_answer(document, "construction-cost", 129000)
+    utils.add_answer(document, "parcel-city", "Musterdorf")
+    utils.add_answer(document, "category", ["category-hochbaute", "category-tiefbaute"])
+    utils.add_answer(document, "veranstaltung-art", ["veranstaltung-art-umbau"])
+    utils.add_answer(document, "oereb-thema", ["oereb-thema-knp"])
+    utils.add_answer(document, "form-type", ["form-type-oereb"])
+    utils.add_answer(document, "typ-des-verfahrens", ["typ-des-verfahrens-genehmigung"])
+
+    # Municipality
+    utils.add_answer(document, "municipality", "1")
+    caluma_form_factories.DynamicOptionFactory(
+        question_id="municipality",
+        document=ur_instance.case.document,
+        slug="1",
+        label={"de": "Altdorf"},
+    )
+
+    # Authority
+    utils.add_answer(document, "leitbehoerde", "1")
+    caluma_form_factories.DynamicOptionFactory(
+        question_id="leitbehoerde",
+        document=ur_instance.case.document,
+        slug="1",
+        label={"de": "Leitbehörde Altdorf"},
+    )
+
+    # Plot
+    utils.add_table_answer(
+        document,
+        "parcels",
+        [
+            {
+                "parcel-number": 123456789,
+                "e-grid": "CH123456789",
+                "coordinates-east": 2690970.9,
+                "coordinates-north": 1192891.9,
+            }
+        ],
+    )
+
+    # Applicant
+    utils.add_table_answer(
+        document,
+        "applicant",
+        [
+            {
+                "first-name": "Max",
+                "last-name": "Mustermann",
+                "is-juristic-person": "is-juristic-person-yes",
+                "juristic-person-name": "ACME AG",
+                "street": "Teststrasse",
+                "street-number": 123,
+                "zip": 1233,
+                "city": "Musterdorf",
+                "country": "Schweiz",
+            }
+        ],
+    )
+
+    # Submit date
+    workflow_entry_factory(
+        instance=ur_instance,
+        workflow_date="2021-07-16 08:00:06+00",
+        group=1,
+        workflow_item__pk=12,
+    )
+
+    # Decision date
+    workflow_entry_factory(
+        instance=ur_instance,
+        workflow_date="2021-07-20 08:00:06+00",
+        group=1,
+        workflow_item__pk=47,
+    )
+
+    # Construction start date
+    workflow_entry_factory(
+        instance=ur_instance,
+        workflow_date="2021-07-25 08:00:06+00",
+        group=1,
+        workflow_item__pk=55,
+    )
+
+    # Construction end date
+    workflow_entry_factory(
+        instance=ur_instance,
+        workflow_date="2021-07-30 08:00:06+00",
+        group=1,
+        workflow_item__pk=67,
+    )
+
+    # Approval reason
+    camac_answer_factory(answer=5031, question__question_id=264, instance=ur_instance)
+
+    # Type of applicant
+    camac_answer_factory(
+        answer=6141,
+        question__question_id=267,
+        instance=ur_instance,
+    )
+
+    # Buildings
+    utils.add_table_answer(
+        document,
+        "gebaeude",
+        [
+            {
+                "art-der-hochbaute": "art-der-hochbaute-parkhaus",
+                "gebaeudenummer-bezeichnung": "Villa",
+                "proposal": ["proposal-neubau"],
+                "gebaeudekategorie": "gebaeudekategorie-ohne-wohnnutzung",
+            }
+        ],
+    )
+
+    # Dwellings
+    utils.add_table_answer(
+        document,
+        "wohnungen",
+        [
+            {
+                "zugehoerigkeit": "Villa",
+                "stockwerktyp": "stockwerktyp-obergeschoss",
+                "stockwerknummer": "2",
+                "lage": "Süd",
+                "wohnungsgroesse": "20",
+                "kocheinrichtung": "kocheinrichtung-kochnische-greater-4-m2",
+                "flaeche-in-m2": "420",
+                "mehrgeschossige-wohnung": "mehrgeschossige-wohnung-ja",
+                "zwg": "zwg-keine",
+            },
+            {
+                "zugehoerigkeit": "Villa",
+                "stockwerktyp": "stockwerktyp-parterre",
+                "lage": "Nord",
+                "wohnungsgroesse": "10",
+                "kocheinrichtung": "kocheinrichtung-keine-kocheinrichtung",
+                "flaeche-in-m2": "72",
+                "mehrgeschossige-wohnung": "mehrgeschossige-wohnung-nein",
+                "zwg": "zwg-erstwohnung",
+            },
+        ],
+    )
+
+    # Energy devices
+    utils.add_table_answer(
+        document,
+        "haustechnik-tabelle",
+        [
+            {
+                "gehoert-zu-gebaeudenummer": "Villa",
+                "anlagetyp": "anlagetyp-hauptheizung",
+                "heizsystem-art": "-hauptheizung",
+                "hauptheizungsanlage": "hauptheizungsanlage-sonne-thermisch",
+            },
+        ],
+    )
+
+    return ur_instance.case

django/camac/instance/tests/test_document_merge_service.py
@@ -16,7 +16,6 @@ from rest_framework import exceptions, status
 from camac.utils import build_url
 
 from ..document_merge_service import DMSClient, DMSHandler, DMSVisitor
-from .test_master_data import add_answer, add_table_answer
 
 
 @pytest.fixture
@@ -131,6 +130,7 @@ def test_document_merge_service_cover_sheet_with_header_values(
     master_data_is_visible_mock,
     freezer,
     be_master_data_settings,
+    utils,
 ):
     be_instance.case.meta = {
         "camac-instance-id": be_instance.pk,
@@ -149,7 +149,7 @@ def test_document_merge_service_cover_sheet_with_header_values(
     group.save()
 
     # Prepare plot answer
-    add_table_answer(
+    utils.add_table_answer(
         be_instance.case.document,
         "parzelle",
         [
@@ -164,7 +164,7 @@ def test_document_merge_service_cover_sheet_with_header_values(
     )
 
     # Prepare applicant answer
-    add_table_answer(
+    utils.add_table_answer(
         be_instance.case.document,
         "personalien-gesuchstellerin",
         [
@@ -178,9 +178,9 @@ def test_document_merge_service_cover_sheet_with_header_values(
     )
 
     # Prepare plot address
-    add_answer(be_instance.case.document, "strasse-flurname", "Bahnhofstrasse")
-    add_answer(be_instance.case.document, "nr", "2")
-    add_answer(be_instance.case.document, "ort-grundstueck", "Testhausen")
+    utils.add_answer(be_instance.case.document, "strasse-flurname", "Bahnhofstrasse")
+    utils.add_answer(be_instance.case.document, "nr", "2")
+    utils.add_answer(be_instance.case.document, "ort-grundstueck", "Testhausen")
 
     # Prepare tags
     tag_factory(name="some tag", instance=be_instance, service=municipality)
@@ -196,15 +196,17 @@ def test_document_merge_service_cover_sheet_with_header_values(
     )
 
     # Prepare modification
-    add_answer(be_instance.case.document, "beschreibung-projektaenderung", "Anbau Haus")
+    utils.add_answer(
+        be_instance.case.document, "beschreibung-projektaenderung", "Anbau Haus"
+    )
 
     # Prepare proposal
-    add_answer(
+    utils.add_answer(
         be_instance.case.document, "beschreibung-bauvorhaben", "Bau Einfamilienhaus"
     )
 
     # Municipality
-    add_answer(be_instance.case.document, "gemeinde", "1")
+    utils.add_answer(be_instance.case.document, "gemeinde", "1")
     DynamicOption.objects.create(
         document=be_instance.case.document,
         question_id="gemeinde",
@@ -254,7 +256,6 @@ def test_document_merge_service_cover_sheet_without_header_values(
 @pytest.mark.django_db(transaction=True, reset_sequences=True)
 def test_eingabebestaetigung_gr(
     db,
-    minio_mock,
     gr_dms_settings,
     settings,
     gr_instance,
@@ -264,7 +265,9 @@ def test_eingabebestaetigung_gr(
     freezer,
     application_settings,
     master_data_is_visible_mock,
+    utils,
     gr_master_data_settings,
+    minio_mock,
 ):
     settings.APPLICATION_NAME = "kt_gr"
     application_settings["DOCUMENT_BACKEND"] = "alexandria"
@@ -300,7 +303,7 @@ def test_eingabebestaetigung_gr(
     )
 
     # Prepare plot answer
-    add_table_answer(
+    utils.add_table_answer(
         gr_instance.case.document,
         "parzelle",
         [
@@ -315,7 +318,7 @@ def test_eingabebestaetigung_gr(
     )
 
     # Prepare applicant answer
-    add_table_answer(
+    utils.add_table_answer(
         gr_instance.case.document,
         "personalien-gesuchstellerin",
         [
@@ -329,7 +332,7 @@ def test_eingabebestaetigung_gr(
     )
 
     # Prepare landowner answer
-    add_table_answer(
+    utils.add_table_answer(
         gr_instance.case.document,
         "personalien-grundeigentumerin",
         [
@@ -343,7 +346,7 @@ def test_eingabebestaetigung_gr(
     )
 
     # Prepare project author answer
-    add_table_answer(
+    utils.add_table_answer(
         gr_instance.case.document,
         "personalien-projektverfasserin",
         [
@@ -357,26 +360,30 @@ def test_eingabebestaetigung_gr(
     )
 
     # Prepare project modification
-    add_answer(gr_instance.case.document, "projektaenderung", "projektaenderung-ja")
-    add_answer(
+    utils.add_answer(
+        gr_instance.case.document, "projektaenderung", "projektaenderung-ja"
+    )
+    utils.add_answer(
         gr_instance.case.document, "beschreibung-projektaenderung", "Projekt Änderung"
     )
 
     # Prepare plot address
-    add_answer(gr_instance.case.document, "street-and-housenumber", "Bahnhofstrasse 2")
-    add_answer(gr_instance.case.document, "ort-grundstueck", "Testhausen")
+    utils.add_answer(
+        gr_instance.case.document, "street-and-housenumber", "Bahnhofstrasse 2"
+    )
+    utils.add_answer(gr_instance.case.document, "ort-grundstueck", "Testhausen")
 
     # Prepare authority
     gr_instance.instance_services.all().delete()
     gr_instance.instance_services.create(service=municipality, active=1)
 
     # Prepare proposal
-    add_answer(
+    utils.add_answer(
         gr_instance.case.document, "beschreibung-bauvorhaben", "Bau Einfamilienhaus"
     )
 
     # Municipality
-    add_answer(gr_instance.case.document, "gemeinde", "1")
+    utils.add_answer(gr_instance.case.document, "gemeinde", "1")
     DynamicOption.objects.create(
         document=gr_instance.case.document,
         question_id="gemeinde",

commit 669029f55d9e8f0479ccaadaa9778b83a721f52d

test(minio): always use file system as storage in tests
This reverts a regression from a47c77413377e0e4d4a953a00be899b46dafcb08

django/camac/communications/serializers.py
@@ -473,7 +473,7 @@ class ConvertToDocumentSerializer(serializers.ModelSerializer):
             )
             try:
                 file.create_thumbnail()
-            except DjangoCoreValidationError:
+            except DjangoCoreValidationError:  # pragma: no cover
                 # thumbnail could not be generated because of an unsupported
                 # mime type
                 pass

django/camac/conftest.py
@@ -1670,3 +1670,13 @@ def ur_master_data_case(
     )
 
     return ur_instance.case
+
+
+@pytest.fixture(autouse=True)
+def _default_file_storage_backend(settings):
+    # This is needed that alexandria file factories don't try to upload
+    # something to a possibly non-existent minio container in tests. Also, we
+    # explicitly disable encryption.
+    settings.DEFAULT_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
+    settings.ALEXANDRIA_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
+    settings.ALEXANDRIA_ENABLE_AT_REST_ENCRYPTION = False

django/camac/alexandria/tests/test_permissions.py
@@ -354,9 +354,7 @@ def test_document_permission(
         ),
     ],
 )
-def test_file_permission(
-    db, role, minio_mock, admin_client, instance, access, method, status_code
-):
+def test_file_permission(db, role, admin_client, instance, access, method, status_code):
     alexandria_category = CategoryFactory(metainfo={"access": access})
     doc = DocumentFactory(
         title="Foo",
@@ -518,7 +516,6 @@ def test_nested_permission(db, role, applicant_factory, admin_client, instance):
 def test_patch_fields(
     db,
     role,
-    minio_mock,
     caluma_admin_user,
     applicant_factory,
     admin_client,

django/camac/alexandria/tests/test_visibilities.py
@@ -21,7 +21,6 @@ def alexandria_setup(
     create_caluma_publication,  # noqa: F811
     instance_with_case,
     instance,
-    minio_mock,
     mocker,
     publication_settings,
     role,
@@ -315,7 +314,6 @@ def test_mark_visibility(db, admin_client, visible_marks):
 def test_detail_visibility(
     db,
     instance,
-    minio_mock,
     admin_client,
     mocker,
     caluma_admin_user,
@@ -357,7 +355,7 @@ def test_detail_visibility(
 
 
 @pytest.mark.parametrize("role__name", ["applicant"])
-def test_file_download(db, minio_mock, alexandria_setup, admin_client, client):
+def test_file_download(db, alexandria_setup, admin_client, client):
     response = admin_client.get(reverse("file-list"))
     url = response.json()["data"][0]["attributes"]["download-url"]
 

django/camac/communications/tests/test_models.py
@@ -27,7 +27,7 @@ def document_attachment(db, file_attachment, attachment):
 
 
 @pytest.fixture
-def alexandria_file(db, minio_mock):
+def alexandria_file(db):
     file = FileFactory(
         name="mydocument.docx",
         document__title="My Document",

django/camac/instance/tests/test_document_merge_service.py
@@ -267,7 +267,6 @@ def test_eingabebestaetigung_gr(
     master_data_is_visible_mock,
     utils,
     gr_master_data_settings,
-    minio_mock,
 ):
     settings.APPLICATION_NAME = "kt_gr"
     application_settings["DOCUMENT_BACKEND"] = "alexandria"

django/camac/instance/tests/test_domain_logic_create.py
@@ -16,7 +16,6 @@ def test_copy_attachments(
     db,
     instance_factory,
     application_settings,
-    minio_mock,
     instance_with_case,
     caluma_workflow_config_gr,
     skip_exported_form_attachment,
@@ -45,19 +44,22 @@ def test_copy_attachments(
     files = [FileFactory(document=doc) for doc in docs]
 
     assert Document.objects.count() == 2
-    assert File.objects.count() == 2
+    assert File.objects.filter(variant=File.Variant.ORIGINAL).count() == 2
 
     CreateInstanceLogic.copy_attachments(
         source_instance, target_instance, skip_exported_form_attachment
     )
 
     assert Document.objects.count() == 2 + expected_copies
-    assert File.objects.count() == 2 + expected_copies
+    assert (
+        File.objects.filter(variant=File.Variant.ORIGINAL).count()
+        == 2 + expected_copies
+    )
 
     new_document = (
         Document.objects.filter(title=docs[0].title).order_by("-created_at").first()
     )
-    new_file = new_document.files.first()
+    new_file = new_document.get_latest_original()
     old_file = files[0]
 
     assert new_document.metainfo["camac-instance-id"] == str(target_instance.pk)

django/camac/instance/tests/test_instance_caluma.py
@@ -1708,7 +1708,6 @@ def test_generate_and_store_pdf_in_alexandria(
     dms_settings,
     gr_instance,
     group,
-    minio_mock,
     mocker,
 ):
     alexandria_category = CategoryFactory()

django/camac/instance/tests/test_placeholders.py
@@ -107,7 +107,6 @@ def test_dms_placeholders_gr(
     responsible_service_factory,
     utils,
     gr_master_data_settings,
-    minio_mock,
 ):
     settings.DISTRIBUTION["QUESTIONS"]["STATEMENT"] = "inquiry-answer-statement"
     application_settings["MUNICIPALITY_DATA_SHEET"] = settings.ROOT_DIR(
@@ -287,7 +286,6 @@ def test_dms_placeholders_so(
     mocker,
     multilang,
     utils,
-    minio_mock,
 ):
     # Authority
     authority = service_factory(