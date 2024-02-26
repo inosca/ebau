88e11f82f877f70cfe69e89c70cd4c193edeacf8
Falk <falk.neumann@adfinis.com>
2024-02-26 13:50:36 +0100

    test(gwr): parametrise canton specific fixtures

--- django/camac/instance/tests/test_gwr.py
+++ django/camac/instance/tests/test_gwr.py
@@ -1,8 +1,8 @@
 import pytest
 from django.urls import reverse
+from pytest_lazyfixture import lazy_fixture
 from rest_framework import status
 
-from camac.conftest import sz_master_data_case  # noqa
 from camac.instance.tests.test_master_data import (
     gr_master_data_case,  # noqa
     sz_master_data_case_gwr,  # noqa
@@ -10,197 +10,105 @@ from camac.instance.tests.test_master_data import (
 )
 
 
+@pytest.mark.parametrize(
+    "canton_name,canton_short_name,instance_for,master_data_case",
+    [
+        (
+            "kt_so",
+            "so",
+            lazy_fixture("so_instance"),
+            lazy_fixture("so_master_data_case"),
+        ),
+        (
+            "kt_uri",
+            "ur",
+            lazy_fixture("ur_instance"),
+            lazy_fixture("ur_master_data_case"),
+        ),
+        (
+            "kt_gr",
+            "gr",
+            lazy_fixture("gr_instance"),
+            lazy_fixture("gr_master_data_case"),
+        ),
+        (
+            "kt_schwyz",
+            "sz",
+            lazy_fixture("sz_instance"),
+            lazy_fixture("sz_master_data_case_gwr"),
+        ),
+        (
+            "kt_schwyz",
+            "sz",
+            lazy_fixture("sz_instance"),
+            lazy_fixture("sz_master_data_case_gwr_v2"),
+        ),
+    ],
+)
 @pytest.mark.freeze_time("2021-10-07")
-def test_gwr_data_so(
+def test_gwr_data(
     admin_client,
     user,
     instance,
+    instance_for,
+    canton_name,
+    canton_short_name,
     use_caluma_form,
-    so_instance,
-    caluma_forms_so,
     caluma_admin_user,
     application_settings,
+    master_data_case,
     settings,
     workflow_entry_factory,
     snapshot,
-    so_master_data_case,
-    so_master_data_settings,
     utils,
 ):
-    settings.APPLICATION_NAME = "kt_so"
-    settings.APPLICATION["SHORT_NAME"] = "so"
+    settings.APPLICATION_NAME = canton_name
+    settings.APPLICATION["SHORT_NAME"] = canton_short_name
 
-    so_instance.case.meta = {"dossier-number": "1201-21-003"}
-    so_instance.case.save()
+    if canton_name in ["kt_uri", "kt_so"]:
+        instance_for.case.meta = {"dossier-number": "1201-21-003"}
+        instance_for.case.save()
 
-    document = so_instance.case.document
+        document = instance_for.case.document
 
-    # Completed date
-    # Assert that workflow entry of last group (phase) is selected
-    workflow_entry = next(
-        filter(
-            lambda entry: entry.workflow_item_id == 67,
-            so_instance.workflowentry_set.all(),
-        ),
-        None,
-    )
+        # Completed date
+        # Assert that workflow entry of last group (phase) is selected
+        workflow_entry = next(
+            filter(
+                lambda entry: entry.workflow_item_id == 67,
+                instance_for.workflowentry_set.all(),
+            ),
+            None,
+        )
 
-    workflow_entry_factory(
-        instance=so_instance,
-        workflow_date="2021-08-05 08:00:06+00",
-        group=2,
-        workflow_item=workflow_entry.workflow_item,
-    )
+        workflow_entry_factory(
+            instance=instance_for,
+            workflow_date="2021-08-05 08:00:06+00",
+            group=2,
+            workflow_item=workflow_entry.workflow_item,
+        )
 
-    # Energy devices
-    # Check logic for heating / warmwater devices and
-    # primary / secondary devices
-    table_answer = document.answers.filter(question_id="haustechnik-tabelle").first()
-    utils.add_table_answer(
-        document,
-        "haustechnik-tabelle",
-        [
-            {
-                "gehoert-zu-gebaeudenummer": "Villa",
-                "anlagetyp": "anlagetyp-warmwasser",
-                "heizsystem-art": "-zusatzheizung",
-                "hauptheizungsanlage": "hauptheizungsanlage-gas",
-            }
-        ],
-        table_answer,
-    )
-
-    url = reverse("instance-gwr-data", args=[instance.pk])
-
-    response = admin_client.get(url)
-    assert response.status_code == status.HTTP_200_OK
-
-    snapshot.assert_match(response.json())
-
-
-@pytest.mark.freeze_time("2021-10-07")
-def test_gwr_data_ur(
-    admin_client,
-    user,
-    instance,
-    use_caluma_form,
-    ur_instance,
-    caluma_forms_ur,
-    caluma_admin_user,
-    application_settings,
-    settings,
-    workflow_entry_factory,
-    snapshot,
-    ur_master_data_case,
-    ur_master_data_settings,
-    utils,
-):
-    settings.APPLICATION_NAME = "kt_uri"
-    settings.APPLICATION["SHORT_NAME"] = "ur"
-
-    ur_instance.case.meta = {"dossier-number": "1201-21-003"}
-    ur_instance.case.save()
-
-    document = ur_instance.case.document
-
-    # Completed date
-    # Assert that workflow entry of last group (phase) is selected
-    workflow_entry = next(
-        filter(
-            lambda entry: entry.workflow_item_id == 67,
-            ur_instance.workflowentry_set.all(),
-        ),
-        None,
-    )
-
-    workflow_entry_factory(
-        instance=ur_instance,
-        workflow_date="2021-08-05 08:00:06+00",
-        group=2,
-        workflow_item=workflow_entry.workflow_item,
-    )
-
-    # Energy devices
-    # Check logic for heating / warmwater devices and
-    # primary / secondary devices
-    table_answer = document.answers.filter(question_id="haustechnik-tabelle").first()
-    utils.add_table_answer(
-        document,
-        "haustechnik-tabelle",
-        [
-            {
-                "gehoert-zu-gebaeudenummer": "Villa",
-                "anlagetyp": "anlagetyp-warmwasser",
-                "heizsystem-art": "-zusatzheizung",
-                "hauptheizungsanlage": "hauptheizungsanlage-gas",
-            }
-        ],
-        table_answer,
-    )
+        # Energy devices
+        # Check logic for heating / warmwater devices and
+        # primary / secondary devices
+        table_answer = document.answers.filter(
+            question_id="haustechnik-tabelle"
+        ).first()
+        utils.add_table_answer(
+            document,
+            "haustechnik-tabelle",
+            [
+                {
+                    "gehoert-zu-gebaeudenummer": "Villa",
+                    "anlagetyp": "anlagetyp-warmwasser",
+                    "heizsystem-art": "-zusatzheizung",
+                    "hauptheizungsanlage": "hauptheizungsanlage-gas",
+                }
+            ],
+            table_answer,
+        )
 
     url = reverse("instance-gwr-data", args=[instance.pk])
-
-    response = admin_client.get(url)
-    assert response.status_code == status.HTTP_200_OK
-
-    snapshot.assert_match(response.json())
-
-
-def test_instance_gwr_data_sz_gwr(
-    admin_client,
-    user,
-    sz_instance,
-    application_settings,
-    settings,
-    snapshot,
-    sz_master_data_case_gwr,  # noqa
-    sz_master_data_settings,
-):
-    settings.APPLICATION_NAME = "kt_schwyz"
-    settings.APPLICATION["SHORT_NAME"] = "sz"
-
-    url = reverse("instance-gwr-data", args=[sz_instance.pk])
-
-    response = admin_client.get(url)
-    assert response.status_code == status.HTTP_200_OK
-
-    snapshot.assert_match(response.json())
-
-
-def test_instance_gwr_data_sz_gwr_v2(
-    admin_client,
-    user,
-    sz_instance,
-    application_settings,
-    settings,
-    snapshot,
-    sz_master_data_case_gwr_v2,  # noqa
-    sz_master_data_settings,
-):
-    settings.APPLICATION_NAME = "kt_schwyz"
-    settings.APPLICATION["SHORT_NAME"] = "sz"
-
-    url = reverse("instance-gwr-data", args=[sz_instance.pk])
-
-    response = admin_client.get(url)
-    assert response.status_code == status.HTTP_200_OK
-
-    snapshot.assert_match(response.json())
-
-
-def test_instance_gwr_data_gr_gwr(
-    admin_client,
-    user,
-    gr_instance,
-    application_settings,
-    settings,
-    snapshot,
-    gr_master_data_case,  # noqa
-):
-    settings.APPLICATION_NAME = "kt_gr"
-    settings.APPLICATION["SHORT_NAME"] = "gr"
-
-    url = reverse("instance-gwr-data", args=[gr_instance.pk])
 
     response = admin_client.get(url)
     assert response.status_code == status.HTTP_200_OK
