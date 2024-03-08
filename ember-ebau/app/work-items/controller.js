347508797bc76e5685b7cccc3e1a1f2c15e3610c
Jonas Lehmann <jonas.lehmann@adfinis.com>
2024-03-08 13:02:36 +0100

    fix(workitems): fix workitem-list for GR and SO

--- ember-ebau/app/work-items/controller.js
+++ ember-ebau/app/work-items/controller.js
@@ -2,9 +2,12 @@ import Controller from "@ember/controller";
 import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
 import { tracked } from "@glimmer/tracking";
+import { trackedFunction } from "ember-resources/util/function";
 
 export default class WorkItemsController extends Controller {
   @service session;
+  @service intl;
+  @service store;
 
   queryParams = ["order", "responsible", "type", "status", "role", "task"];
 
@@ -16,8 +19,33 @@ export default class WorkItemsController extends Controller {
   @tracked status = "READY";
   @tracked task = "all";
 
+  allResponsibles = trackedFunction(this, async () => {
+    const users = await this.store.query("user", {
+      sort: "name",
+    });
+    return [
+      { value: "all", label: this.intl.t("workItems.filters.all") },
+      { value: "own", label: this.intl.t("workItems.filters.own") },
+      ...users.map((u) => ({
+        label: `${u.name} ${u.surname}`,
+        value: u.username,
+      })),
+    ];
+  });
+
+  get selectedResponsible() {
+    return this.allResponsibles.value?.find(
+      (r) => r.value === this.responsible,
+    );
+  }
+
   @action
   setFilter(filter, value) {
     this[filter] = value;
+  }
+
+  @action
+  setResponsible(person) {
+    this.responsible = person.value;
   }
 }
