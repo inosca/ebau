5c7a721db819ed399005b502e8f99795ff7816a1
Jonas Metzener <jonas.metzener@adfinis.com>
2024-04-04 16:52:05 +0200

    fix(ember): fix configuration and styling of watermark component

--- ember-caluma-portal/ember-cli-build.js
+++ ember-caluma-portal/ember-cli-build.js
@@ -15,7 +15,6 @@ const ENV_MAP = {
 const ENVS = Object.values(ENV_MAP);
 const ENV = ENV_MAP[process.env.APPLICATION] || ENVS[0];
 const UNUSED_ENVS = ENVS.filter((e) => e !== ENV).join("|");
-const APP_ENV = process.env.APP_ENV ?? "dev";
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
@@ -35,9 +34,6 @@ module.exports = function (defaults) {
       setConfig: {
         "@ember-data/store": {
           polyfillUUID: true,
-        },
-        "ember-ebau-core": {
-          appEnv: APP_ENV,
         },
       },
     },
