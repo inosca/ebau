393e3b075f12b024a2e788b4257f1511a021016a
Jonas Metzener <jonas.metzener@adfinis.com>
2024-04-16 09:43:30 +0200

    feat(so): add link to internal application in portal

--- ember-caluma-portal/config/icons.js
+++ ember-caluma-portal/config/icons.js
@@ -1,5 +1,10 @@
 module.exports = function () {
   return {
-    "free-solid-svg-icons": ["circle-question", "power-off", "caret-down"],
+    "free-solid-svg-icons": [
+      "circle-question",
+      "power-off",
+      "caret-down",
+      "arrow-up-right-from-square",
+    ],
   };
 };
