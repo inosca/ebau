53d820e9a5a717b08d7ccec7b9d91c6f0f9603fc
Christian Zosel <christian@zosel.ch>
2024-05-07 14:27:53 +0200

    fix(gr): add ".dwg" so that browser suggests it in upload dialog

--- ember-ebau-core/addon/config/attachments-gr.js
+++ ember-ebau-core/addon/config/attachments-gr.js
@@ -2,7 +2,11 @@ export default {
   allowedMimetypes: [
     "image/png",
     "image/jpeg",
+    "image/gif",
     "application/pdf",
     "image/vnd.dwg",
+    // for dwg files, just the mimetype is not enough to list the files
+    // by default in file upload dialog, so allow extension as well
+    ".dwg",
   ],
 };
