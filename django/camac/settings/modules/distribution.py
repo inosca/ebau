9b65a2b1a2649734e306c3876c9e278caea230b3
Stephan Hug <stephan.hug@adfinis.com>
2024-05-10 11:24:27 +0200

    feat(django-ur): enable distribution suggestions based on gis and location data

--- django/camac/settings/modules/distribution.py
+++ django/camac/settings/modules/distribution.py
@@ -705,6 +705,170 @@ DISTRIBUTION = {
             "ANCILLARY_CLAUSES": "inquiry-answer-ancillary-clauses",
         },
         "DEFAULT_SUGGESTIONS": [1],  # KOOR BG
+        "SUGGESTIONS": {
+            "QUESTIONS": {
+                (
+                    "purpose",
+                    "purpose-industrie",
+                ): [3],
+                (
+                    "purpose",
+                    "purpose-gewerbe",
+                ): [3],
+                (
+                    "purpose",
+                    "purpose-dienstleistung",
+                ): [82],
+                (
+                    "purpose",
+                    "purpose-verkauf",
+                ): [82],
+                (
+                    "purpose",
+                    "purpose-landwirtschaft",
+                ): [525],
+                (
+                    "purpose",
+                    "purpose-forstwirtschaft",
+                ): [546],
+                (
+                    "purpose",
+                    "purpose-gastgewerbe",
+                ): [3],
+                (
+                    "purpose",
+                    "purpose-oeffentlich-zugaenglich",
+                ): [82],
+                (
+                    "grundnutzung",
+                    "Gewerbezone",
+                ): [3],
+                (
+                    "grundnutzung",
+                    "Industriezone",
+                ): [3],
+                (
+                    "grundnutzung",
+                    "Kernzone",
+                ): [241],
+                (
+                    "grundnutzung",
+                    "Zone für öffentliche Bauten und Anlagen",
+                ): [82],
+                (
+                    "grundnutzung",
+                    "Landwirtschaftszone",
+                ): [96, 88, 261, 262],
+                (
+                    "grundnutzung",
+                    "Naturschutzzone",
+                ): [98],
+                (
+                    "grundnutzung",
+                    "Uferbereich",
+                ): [567, 568],
+                (
+                    "grundnutzung",
+                    "Gewässer",
+                ): [567, 568, 41, 88, 261, 262],
+                (
+                    "grundnutzung",
+                    "Verkehrsfläche ausserhalb Bauzone",
+                ): [88, 261, 262, 41],
+                (
+                    "grundnutzung",
+                    "Reservezone",
+                ): [88, 261, 262],
+                (
+                    "grundnutzung",
+                    "Wald",
+                ): [2, 88, 261, 262],
+                (
+                    "grundnutzung",
+                    "Deponiezone",
+                ): [567, 568],
+                (
+                    "grundnutzung",
+                    "Abbauzone",
+                ): [567, 568],
+                (
+                    "municipality",
+                    "1201",
+                ): [88],  # BaB 1
+                (
+                    "municipality",
+                    "1216",
+                ): [88],
+                (
+                    "municipality",
+                    "1209",
+                ): [88],
+                (
+                    "municipality",
+                    "1220",
+                ): [88],
+                (
+                    "municipality",
+                    "1208",
+                ): [88],
+                (
+                    "municipality",
+                    "1202",
+                ): [88],
+                (
+                    "municipality",
+                    "1210",
+                ): [88],
+                (
+                    "municipality",
+                    "1212",
+                ): [88],
+                (
+                    "municipality",
+                    "1217",
+                ): [261],  # BaB 2
+                (
+                    "municipality",
+                    "1207",
+                ): [261],
+                (
+                    "municipality",
+                    "1218",
+                ): [261],
+                (
+                    "municipality",
+                    "1219",
+                ): [261],
+                (
+                    "municipality",
+                    "1213",
+                ): [262],  # Bab 3
+                (
+                    "municipality",
+                    "1214",
+                ): [262],
+                (
+                    "municipality",
+                    "1204",
+                ): [262],
+                (
+                    "municipality",
+                    "1211",
+                ): [262],
+                (
+                    "municipality",
+                    "1215",
+                ): [262],
+                (
+                    "municipality",
+                    "1205",
+                ): [262],
+                (
+                    "municipality",
+                    "1203",
+                ): [262],
+            },
+        },
     },
     "demo": {"ENABLED": True},
 }
