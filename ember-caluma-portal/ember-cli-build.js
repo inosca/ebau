7dc50275299aff994af3cff1eef92e840c8b3c6b
Falk <falk.neumann@adfinis.com>
2024-05-28 18:01:38 +0200

    fix: effectively replace env vars
    The config is stored in meta as long as you don't specify it
    diffrently. That's why the replacement didnt work in the first place.

--- ember-caluma-portal/ember-cli-build.js
+++ ember-caluma-portal/ember-cli-build.js
@@ -18,7 +18,7 @@ const UNUSED_ENVS = ENVS.filter((e) => e !== ENV).join("|");
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
-    storeConfigInMeta: ENV !== "so",
+    storeConfigInMeta: !["be", "so"].includes(ENV),
     "@embroider/macros": {
       setOwnConfig: {
         application: ENV,
