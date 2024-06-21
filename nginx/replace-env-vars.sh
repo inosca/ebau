a9694d5bb2ba9a31dfa38d06c25d975b4998e937
René Birrer <rene.birrer@adfinis.com>
2024-06-21 12:23:46 +0200

    fix(build): escape variables where escaping is needed

--- nginx/replace-env-vars.sh
+++ nginx/replace-env-vars.sh
@@ -1,11 +1,19 @@
 #/bin/sh
 
-vars=$1
+varnames=$1
 path=$2
+
+# prefix each variable name with $ and
+# concatenate them in a string
+for var in $varnames
+do
+  variables=${variables:+$variables}" \$$var"
+done
+
 
 for file in $(find $path -iname "*.js");
 do
   tmpfile="/tmp/$(basename $file)"
-  envsubst $vars < $file > $tmpfile;
+  envsubst $variables < $file > $tmpfile;
   mv $tmpfile $file
 done
