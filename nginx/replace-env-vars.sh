d2d0635cc80d454a786168e4ead1320c7893aaa6
Jonas Metzener <jonas.metzener@adfinis.com>
2024-06-27 10:51:20 +0200

    fix(docker): fix replacing of env variables in the ember build

--- nginx/replace-env-vars.sh
+++ nginx/replace-env-vars.sh
@@ -1,15 +1,15 @@
 #/bin/sh
 
+# WARNING: Please be aware that this is a shell script, not a bash script! If
+# you change the code of this script, you must make sure to test it in sh, not
+# bash.
+
 varnames=$1
 path=$2
 
-# prefix each variable name with $ and
-# concatenate them in a string
-for var in $varnames
-do
-  variables=${variables:+$variables}" \$$var"
-done
-
+# Prefix each variable name with a dollar sign. This converts a value of
+# "FOO,BAR,BAZ" to "$FOO,$BAR,$BAZ" which is required for envsubst to work
+variables=$(echo $varnames | sed 's/\([^,]*\)/$\1/g')
 
 for file in $(find $path -iname "*.js");
 do
