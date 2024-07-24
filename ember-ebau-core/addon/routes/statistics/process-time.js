e2dcc949ebe550f7f9fd28e6131503e36191eea1
Jonas Metzener <jonas.metzener@adfinis.com>
2024-07-24 15:02:35 +0200

    chore(ember): replace deprecated usage of can service with abilities

--- ember-ebau-core/addon/routes/statistics/process-time.js
+++ ember-ebau-core/addon/routes/statistics/process-time.js
@@ -2,11 +2,11 @@ import Route from "@ember/routing/route";
 import { inject as service } from "@ember/service";
 
 export default class StatisticsProcessTimeRoute extends Route {
-  @service can;
+  @service abilities;
   @service router;
 
   redirect() {
-    if (this.can.cannot("view statistics", "process-time")) {
+    if (this.abilities.cannot("view statistics", "process-time")) {
       this.router.replaceWith("statistics.index");
     }
   }
