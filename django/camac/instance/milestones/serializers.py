4a06bebbfe7973088eff538f902982cf03ec9e91
Stephan Hug <stephan.hug@adfinis.com>
2024-08-08 09:26:01 +0200

    feat(ur): finalise milestones

--- django/camac/instance/milestones/serializers.py
+++ django/camac/instance/milestones/serializers.py
@@ -11,7 +11,10 @@ from ..master_data import MasterData
 from . import fields
 
 
-def _is_controlled_by_a_coordination_service():
+# This doesn't have coverage because the fields that needed this were
+# removed recently. It is therefore not part of the snapshot tests anymore.
+# If Uri decides that they will never use this, it can be removed.
+def _is_controlled_by_a_coordination_service():  # pragma: no cover
     if settings.APPLICATION.get("COORDINATION_SERVICE_IDS"):
         return Q(
             controlling_groups__overlap=settings.APPLICATION["COORDINATION_SERVICE_IDS"]
@@ -37,33 +40,44 @@ class MilestonesSerializer(serializers.Serializer):
         super().__init__(instance, *args, **kwargs)
 
         instance._master_data = MasterData(instance.case)
-        instance._child_cases = Case.objects.filter(family=instance.case)
-        instance._work_items = WorkItem.objects.filter(case=instance.case)
-        instance._all_work_items = WorkItem.objects.filter(
-            case__in=instance._child_cases,
+        instance._child_cases = list(Case.objects.filter(family=instance.case))
+        instance._work_items = list(
+            WorkItem.objects.filter(case=instance.case).prefetch_related(
+                "document__answers"
+            )
+        )
+        instance._all_work_items = list(
+            WorkItem.objects.filter(case__in=instance._child_cases).prefetch_related(
+                "document__answers"
+            )
         )
 
     def get_paper_submission(self, instance):
         if CalumaApi().is_paper(instance):
-            if submit_task := instance._work_items.filter(
-                task__in=settings.APPLICATION["CALUMA"]["SUBMIT_TASKS"]
-            ).first():
-                return submit_task.closed_at if submit_task else []
+            submit_work_item = next(
+                (
+                    wi
+                    for wi in instance._work_items
+                    if wi.task_id in settings.APPLICATION["CALUMA"]["SUBMIT_TASKS"]
+                ),
+                None,
+            )
+            return submit_work_item.closed_at if submit_work_item else []
 
         return []  # pragma: no cover
 
     def get_instance_submitted(self, instance):
         if not CalumaApi().is_paper(instance):
-            submit_work_item = instance._work_items.filter(task="submit").first()
+            submit_work_item = next(
+                (
+                    wi
+                    for wi in instance._work_items
+                    if wi.task_id in settings.APPLICATION["CALUMA"]["SUBMIT_TASKS"]
+                ),
+                None,
+            )
+
             return submit_work_item.closed_at if submit_work_item else []
-
-        return []  # pragma: no cover
-
-    def get_building_permit_valid_until(self, instance):
-        decision_work_item = instance._work_items.filter(task="decision").first()
-
-        if decision_work_item and decision_work_item.closed_at:
-            return decision_work_item.closed_at + relativedelta.relativedelta(years=1)
 
         return []  # pragma: no cover
 
@@ -84,9 +98,15 @@ class UrMilestonesSerializer(MilestonesSerializer):
                     fields.WorkItemsField(
                         slug="additional-demand",
                         label=_("Additional demand"),
-                        task="additional-demand",
+                        task="send-additional-demand",
                         status=WorkItem.STATUS_COMPLETED,
                         field="created_at",
+                    ),
+                    fields.WorkItemsField(
+                        slug="check-completed",
+                        label=_("Check completed"),
+                        task="complete-check",
+                        field="closed_at",
                     ),
                     fields.MethodField(
                         slug="instance-complete", label=_("Instance complete")
@@ -123,7 +143,7 @@ class UrMilestonesSerializer(MilestonesSerializer):
                     fields.WorkItemsField(
                         slug="decision",
                         label=_("Decision made"),
-                        task="distribution",
+                        task="decision",
                         field="closed_at",
                         status=WorkItem.STATUS_COMPLETED,
                     ),
@@ -139,38 +159,6 @@ class UrMilestonesSerializer(MilestonesSerializer):
                         task="gebaeudeschaetzung",
                         field="created_at",
                     ),
-                    # I added this to get test code coverage.
-                    # I will replace this with a more meaningful field once Uri
-                    # uses the new workflow and actually has meaningful date questions
-                    fields.AnswerField(
-                        slug="is-paper", label=_("is paper"), document="building-permit"
-                    ),
-                    fields.WorkItemsField(
-                        slug="check-completed",
-                        label=_("Check completed"),
-                        task="complete-check",
-                        field="closed_at",
-                    ),
-                    fields.WorkItemsField(
-                        slug="forwarded-to-koor",
-                        label=_("Forwarded to coordination service"),
-                        task="fill-inquiry",
-                        filter=lambda instance: _is_controlled_by_a_coordination_service(),
-                    ),
-                    fields.WorkItemsField(
-                        slug="start-cantonal-distribution",
-                        label=_("Start of cantonal distribution"),
-                        task="inquiry",
-                        filter=lambda instance: _is_controlled_by_a_coordination_service(),
-                    ),
-                    fields.WorkItemsField(
-                        slug="returned-to-municipality",
-                        label=_("Returned to municipality"),
-                        task="check-inquiries",
-                        field="closed_at",
-                        status=WorkItem.STATUS_COMPLETED,
-                        filter=lambda instance: _is_controlled_by_a_coordination_service(),
-                    ),
                     fields.MethodField(
                         slug="building-permit-valid-until",
                         label=_("Building permit valid until"),
@@ -181,16 +169,81 @@ class UrMilestonesSerializer(MilestonesSerializer):
     )
 
     def get_instance_complete(self, instance):
-        # todo: fix this once the additional-demand setup is working with the nested child cases etc.
-        return None
         # In Uri "Dossier vollständig" means that all required information is available
         # to continue with the instance.
-        # if complete_check_work_item := instance.case.work_items.filter(task_id="complete-check", status=WorkItem.STATUS_COMPLETED):
-        #     complete_check_answer = complete_check_work_item.document.answers.get("complete-check-vollstaendigkeitspruefung").value
-        #     if complete_check_answer == "complete-check-vollstaendigkeitspruefung-complete":
-        #         # Dossier is "vollständig"
-        #         return complete_check_work_item.closed_at
 
-        #     if complete_check_answer in ["complete-check-vollstaendigkeitspruefung-incomplete", "complete-check-vollstaendigkeitspruefung-incomplete-wait"]:
-        #         # Dossier was incomplete during the check and additional-demands were required
-        #         return None
+        complete_check_work_item = next(
+            (
+                wi
+                for wi in instance._all_work_items
+                if wi.task_id == "complete-check"
+                and wi.status == WorkItem.STATUS_COMPLETED
+            ),
+            None,
+        )
+
+        if complete_check_work_item:
+            if not complete_check_work_item.document.answers.exists():
+                # for migrated dossiers in Uri there is no "complete-check"
+                return None  # pragma: no cover
+
+            complete_check_answer = complete_check_work_item.document.answers.get(
+                question_id="complete-check-vollstaendigkeitspruefung"
+            ).value
+
+            if (
+                complete_check_answer
+                == "complete-check-vollstaendigkeitspruefung-complete"
+            ):
+                # Dossier is "vollständig"
+                return complete_check_work_item.closed_at
+
+            if complete_check_answer in [
+                "complete-check-vollstaendigkeitspruefung-incomplete",
+                "complete-check-vollstaendigkeitspruefung-incomplete-wait",
+            ]:
+                # Dossier was incomplete during the check and additional-demands were required
+                open_additional_demand_work_items = [
+                    wi
+                    for wi in instance._all_work_items
+                    if (
+                        wi.task_id
+                        in [
+                            "send-additional-demand",
+                            "fill-additional-demand",
+                            "check-additional-demand",
+                        ]
+                        and wi.status == WorkItem.STATUS_READY
+                    )
+                ]
+
+                if len(open_additional_demand_work_items):
+                    # There are open additional-demands
+                    return None
+                else:
+                    completed_check_additional_demand_work_items_closed_at = [
+                        wi.closed_at
+                        for wi in instance._all_work_items
+                        if (
+                            wi.task_id == "check-additional-demand"
+                            and wi.status == WorkItem.STATUS_COMPLETED
+                        )
+                    ]
+                    return max(completed_check_additional_demand_work_items_closed_at)
+
+        return None  # pragma: no cover
+
+    def get_building_permit_valid_until(self, instance):
+        decision_work_item = next(
+            (
+                wi
+                for wi in instance._work_items
+                if wi.task_id == "decision" and wi.status == WorkItem.STATUS_COMPLETED
+            ),
+            None,
+        )
+
+        if decision_work_item:
+            return decision_work_item.closed_at + relativedelta.relativedelta(years=1)
+
+        return []  # pragma: no cover
--- django/camac/instance/tests/test_milestones.py
+++ django/camac/instance/tests/test_milestones.py
@@ -13,10 +13,18 @@ from camac.constants import kt_uri as uri_constants
 @pytest.mark.parametrize(
     "role__name",
     [
-        ("Municipality"),
+        ("Sekretariat der Gemeindebaubehörde"),
     ],
 )
-@pytest.mark.parametrize("is_paper_answer", ["is-paper-no", "is-paper-yes"])
+@pytest.mark.parametrize(
+    "is_paper_answer,complete_check_outcome,open_additional_demands",
+    [
+        ("is-paper-no", "complete-check-vollstaendigkeitspruefung-complete", False),
+        ("is-paper-yes", "complete-check-vollstaendigkeitspruefung-complete", False),
+        ("is-paper-no", "complete-check-vollstaendigkeitspruefung-incomplete", True),
+        ("is-paper-no", "complete-check-vollstaendigkeitspruefung-incomplete", False),
+    ],
+)
 @pytest.mark.freeze_time("2023-12-31")
 @pytest.mark.django_db(
     transaction=True, reset_sequences=True
@@ -29,15 +37,14 @@ def test_milestones_ur(
     settings,
     application_settings,
     snapshot,
-    is_paper_answer,
     answer_factory,
+    document_factory,
+    set_application_ur,
+    #
+    is_paper_answer,
+    complete_check_outcome,
+    open_additional_demands,
 ):
-    application_settings["CALUMA"]["SUBMIT_TASKS"] = ["submit"]
-    application_settings["COORDINATION_SERVICE_IDS"] = [
-        uri_constants.KOOR_BG_SERVICE_ID
-    ]
-    settings.APPLICATION_NAME = "kt_uri"
-
     answer_factory(
         document=ur_instance.case.document,
         question_id="is-paper",
@@ -46,15 +53,35 @@ def test_milestones_ur(
 
     submit_task = ur_instance.case.work_items.get(task_id="submit")
     submit_task.closed_at = make_aware(datetime(2023, 1, 1, 20, 0, 0))
+    submit_task.status = WorkItem.STATUS_COMPLETED
     submit_task.save()
 
-    for task_slug in ["check-permit", "decision"]:
-        WorkItemFactory(
-            task_id=task_slug,
-            case=ur_instance.case,
-            closed_at=make_aware(datetime(2023, 1, 1, 20, 0, 0)),
-            status=WorkItem.STATUS_COMPLETED,
-        )
+    complete_check_work_item = WorkItemFactory(
+        task_id="complete-check",
+        case=ur_instance.case,
+        closed_at=make_aware(datetime(2023, 1, 1, 20, 0, 0)),
+        status=WorkItem.STATUS_COMPLETED,
+        document=document_factory(form_id="complete-check"),
+    )
+    answer_factory(
+        document=complete_check_work_item.document,
+        question__slug="complete-check-vollstaendigkeitspruefung",
+        value=complete_check_outcome,
+    )
+    WorkItemFactory(
+        task_id="check-additional-demand",
+        case=ur_instance.case,
+        closed_at=make_aware(datetime(2023, 1, 1, 20, 0, 0)),
+        status=WorkItem.STATUS_READY
+        if open_additional_demands
+        else WorkItem.STATUS_COMPLETED,
+    )
+    WorkItemFactory(
+        task_id="decision",
+        case=ur_instance.case,
+        closed_at=make_aware(datetime(2023, 1, 1, 20, 0, 0)),
+        status=WorkItem.STATUS_COMPLETED,
+    )
 
     # top level circulation (Gemeinde -> KOOR)
     WorkItemFactory(
