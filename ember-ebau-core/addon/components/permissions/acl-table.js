efe57dec206522c7949708c9c19d17959465e31f
Jonas Metzener <jonas.metzener@adfinis.com>
2024-08-14 17:24:47 +0200

    feat(permissions): show entity type in UI and remove flat rate label

--- ember-ebau-core/addon/components/permissions/acl-table.js
+++ ember-ebau-core/addon/components/permissions/acl-table.js
@@ -40,7 +40,7 @@ export default class AclTable extends Component {
 
   acls = paginatedQuery(this, "instance-acl", () => ({
     instance: this.args.instanceId,
-    include: "user,service",
+    include: "user,service,role",
     filter: {
       ...(this.statusFilter === "all" ? {} : { status: this.statusFilter }),
       ...(this.accessLevelFilter
