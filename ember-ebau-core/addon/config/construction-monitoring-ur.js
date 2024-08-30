6254e11e5e355041c6b03b0a1c9168b74c6904f9
Stephan Hug <stephan.hug@adfinis.com>
2024-08-30 12:55:28 +0200

    feat(ur): move gwr state stuff to construction monitoring

---
+++ ember-ebau-core/addon/config/construction-monitoring-ur.js
@@ -0,0 +1,15 @@
+export default {
+  constructionSteps: [
+    "construction-step-plan-construction-stage",
+    "construction-step-update-gwr-state-begin",
+    "construction-step-baufreigabe",
+    "construction-step-baubeginn",
+    "construction-step-kanalisationsabnahme",
+    "construction-step-schnurgeruestabnahme",
+    "construction-step-rohbauabnahme",
+    "construction-step-zwischenkontrolle",
+    "construction-step-schlussabnahme-gebaeude",
+    "construction-step-schlussabnahme-projekt",
+    "construction-step-update-gwr-state-completion",
+  ],
+};
