7183e9338f4638aeaafb76beb08915897a400564
yelinz <yelin.zhang@adfinis.com>
2024-09-13 14:40:28 +0200

    build: unify django and celery entrypoint

--- celery/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-wait-for-it db:5432 -- wait-for-it redis:6379;
-
-if [ "$ENV" = "dev" ]; then
-    watchmedo auto-restart -d . --recursive -p '*.py' -- celery -A camca worker -l INFO -E -O fair;
-else
-    celery -A camac worker -l INFO -E -O fair;
-fi
