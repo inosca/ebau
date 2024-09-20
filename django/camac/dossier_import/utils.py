a3785c9dd59ed592ff90607c177ff48e2c929166
Jonas Metzener <jonas.metzener@adfinis.com>
2024-09-20 15:02:52 +0200

    fix(dossier-import): ignore all whitespace chars in headings

--- django/camac/dossier_import/utils.py
+++ django/camac/dossier_import/utils.py
@@ -1,13 +1,17 @@
-from typing import List, Tuple
+import re
+from typing import Any, List, Tuple
 
 import pyexcel_xlsx
 
 
-def clean_value(value):
-    if isinstance(value, str):
-        value = value.strip()
+def clean_heading(value: str) -> str:
+    """Remove all whitespace characters from headings."""
+    return re.sub(r"\s", "", value).strip()
 
-    return value
+
+def clean_value(value: Any) -> Any:
+    """Remove leading and trailing spaces from string values."""
+    return value.strip() if isinstance(value, str) else value
 
 
 def get_worksheet_headings_and_rows(file) -> Tuple[List[str], List[dict]]:
@@ -16,7 +20,7 @@ def get_worksheet_headings_and_rows(file) -> Tuple[List[str], List[dict]]:
     workbook = pyexcel_xlsx.get_data(file)
     worksheet = workbook[list(workbook.keys())[0]]
 
-    headings = [clean_value(heading) for heading in worksheet[0]]
+    headings = [clean_heading(heading) for heading in worksheet[0]]
     rows = [
         dict(zip(headings, [clean_value(cell) for cell in row]))
         for row in worksheet[1:]
