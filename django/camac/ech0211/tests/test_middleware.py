fb6c37377de220d8fa4edadeb81948d77df45278
Christian Zosel <christian@zosel.ch>
2024-10-31 22:20:03 +0100

    fix(ech): geofencing for all ech endpoints

--- django/camac/ech0211/tests/test_middleware.py
+++ django/camac/ech0211/tests/test_middleware.py
@@ -1,60 +1,59 @@
 import pytest
-from django.core.exceptions import PermissionDenied
 from django.http import HttpRequest
 
 from camac.ech0211.middleware import GeofenceMiddleware
 
 
 @pytest.mark.vcr()
-def test_geofencing(mocker, vcr, gr_ech0211_settings):
-    get_response = mocker.stub()
+def test_geofencing(vcr, gr_ech0211_settings):
     request = HttpRequest()
     request.META["REMOTE_ADDR"] = "2a02:168:a856:0:618:9d26:5d4c:cc0c"  # Switzerland
-    GeofenceMiddleware(get_response=get_response)(request)
+    view = type("View", (), {"request": request})
+    response = GeofenceMiddleware(None).process_view(view, None, None, None)
 
-    get_response.assert_called_once()
     assert vcr.play_count == 1
+    assert response is None
 
 
 @pytest.mark.vcr()
-def test_geofencing_disabled(mocker, vcr, gr_ech0211_settings):
+def test_geofencing_disabled(vcr, gr_ech0211_settings):
     gr_ech0211_settings["GEOFENCE"]["ENABLE"] = False
-    get_response = mocker.stub()
     request = HttpRequest()
     request.META["REMOTE_ADDR"] = "2a02:168:a856:0:618:9d26:5d4c:cc0c"  # Switzerland
-    GeofenceMiddleware(get_response=get_response)(request)
+    view = type("View", (), {"request": request})
+    response = GeofenceMiddleware(None).process_view(view, None, None, None)
 
-    get_response.assert_called_once()
     assert vcr.play_count == 0
+    assert response is None
     gr_ech0211_settings["GEOFENCE"]["ENABLE"] = True
 
 
 def test_geofencing_no_ip(mocker, gr_ech0211_settings):
     mocker.patch("ipware.get_client_ip", return_value=(None, False))
     request = HttpRequest()
-    with pytest.raises(PermissionDenied) as exc:
-        GeofenceMiddleware()(request)
-    assert "Unable to get the client's IP address" in exc.value.args[0]
+    view = type("View", (), {"request": request})
+    response = GeofenceMiddleware(None).process_view(view, None, None, None)
+    assert "Unable to get the client's IP address" in response.content.decode()
 
 
 @pytest.mark.vcr()
 def test_geofencing_wrong_region(vcr, gr_ech0211_settings):
     request = HttpRequest()
     request.META["REMOTE_ADDR"] = "85.214.132.117"  # Germany
-    with pytest.raises(PermissionDenied) as exc:
-        GeofenceMiddleware()(request)
-    assert "Outside of allowed region" in exc.value.args[0]
+    view = type("View", (), {"request": request})
+    response = GeofenceMiddleware(None).process_view(view, None, None, None)
+    assert "Outside of allowed region" in response.content.decode()
     assert vcr.play_count == 1
 
 
 @pytest.mark.vcr()
-def test_geofencing_no_api(vcr, mocker, caplog, gr_ech0211_settings):
-    get_response = mocker.stub()
+def test_geofencing_no_api(vcr, caplog, gr_ech0211_settings):
     request = HttpRequest()
     request.META["REMOTE_ADDR"] = "85.195.233.243"
-    GeofenceMiddleware(get_response=get_response)(request)
+    view = type("View", (), {"request": request})
+    response = GeofenceMiddleware(None).process_view(view, None, None, None)
 
-    get_response.assert_called_once()
+    assert response is None
     assert vcr.play_count == 1
     assert (
         "500 Server Error: OK for url: http://www.geoplugin.net/json.gp?ip=85.195.233.243"
@@ -65,6 +64,6 @@ def test_geofencing_no_api(vcr, mocker, caplog, gr_ech0211_settings):
 def test_geofencing_malformed_ip(gr_ech0211_settings):
     request = HttpRequest()
     request.META["REMOTE_ADDR"] = "85,195,233,243"
-    with pytest.raises(PermissionDenied) as exc:
-        GeofenceMiddleware()(request)
-    assert "Unable to get the client's IP address" in exc.value.args[0]
+    view = type("View", (), {"request": request})
+    response = GeofenceMiddleware(None).process_view(view, None, None, None)
+    assert "Unable to get the client's IP address" in response.content.decode()
