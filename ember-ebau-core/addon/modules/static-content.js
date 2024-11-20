048ab4d80d115c4b1ea0a5953e31053d96be8386
Jonas Metzener <jonas.metzener@adfinis.com>
2024-11-20 15:08:21 +0100

    fix(token-exchange): allow access to static content for public users

--- ember-ebau-core/addon/modules/static-content.js
+++ ember-ebau-core/addon/modules/static-content.js
@@ -2,10 +2,14 @@ import { registerModule } from "ember-ebau-core/modules";
 import StaticContentRoute from "ember-ebau-core/routes/static-content";
 import StaticContentTemplate from "ember-ebau-core/templates/static-content";
 
-export default function register(router, options = {}) {
+export default function register(
+  router,
+  options = {},
+  moduleName = "static-content",
+) {
   router.route("static-content", { ...options, path: "/static-content/:type" });
 
-  registerModule("static-content", router.parent, options.resetNamespace, {
+  registerModule(moduleName, router.parent, options.resetNamespace, {
     routes: { "static-content": StaticContentRoute },
     controllers: {},
     templates: { "static-content": StaticContentTemplate },
