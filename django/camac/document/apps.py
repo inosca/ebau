13f5b1691a38f463705bfd24966f961b89e85f30
David Vogt <david.vogt@adfinis.com>
2024-11-21 12:06:52 +0100

    fix(documents/performance): greatly improve thumbnailing speed
    Our thumbnailing engine uses imagemagick behind the scene, which
    by default looks at the whole document. It has a performance issue
    that is dependent on the physical size of the document. SORL
    therefore only looks at the first page for calculating thumbnails,
    but to detect the page orientation, this tiny suffix is missing.
    
    Therefore, documents with a huge amount of pages will take a significant
    time to thumbnail, all due to the fact that it looks at all the pages
    instead of just the first one.

--- django/camac/document/apps.py
+++ django/camac/document/apps.py
@@ -1,3 +1,5 @@
+import subprocess
+
 from django.apps import AppConfig
 
 
@@ -6,3 +8,30 @@ class DefaultConfig(AppConfig):
 
     def ready(self):
         import camac.document.signals  # noqa
+
+        _monkeypatch_sorl()
+
+
+def _monkeypatch_sorl():
+    # Can be removed if/when this PR is merged and released:
+    # https://github.com/jazzband/sorl-thumbnail/pull/778
+    from sorl.thumbnail.conf import settings
+    from sorl.thumbnail.engines.convert_engine import Engine
+
+    def _get_exif_orientation(self, image):
+        # This is a straight-up copy of the original function, but it appends '[0]'
+        # to the file name. This will tell `identify` to only look at the first
+        # page. The other convert calls already do this, but for some reason,
+        # it was not done in the _get_exif_orientation() method. Speeds up
+        # multi-page calls significantly
+        args = settings.THUMBNAIL_IDENTIFY.split()
+        args.extend(["-format", "%[exif:orientation]", f'{image["source"]}[0]'])
+        p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        p.wait()
+        result = p.stdout.read().strip()
+        try:
+            return int(result)
+        except ValueError:
+            return None
+
+    Engine._get_exif_orientation = _get_exif_orientation
