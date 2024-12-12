42d12de2c5add1cf363bf3463e784069b22e0f2c
Jonas Metzener <jonas.metzener@adfinis.com>
2024-12-12 14:40:03 +0100

    feat(so): add signatures PDF export for additional demands
    Related to kt-so/ebauso-coordination#281

--- ember-caluma-portal/app/components/be-download-pdf.js
+++ ember-caluma-portal/app/components/be-download-pdf.js
@@ -14,12 +14,22 @@ export default class BeDownloadPdfComponent extends Component {
     try {
       const params = {};
 
-      if (!this.args.field.document.rootForm.raw.meta["is-main-form"]) {
+      const isMainForm =
+        this.args.field.document.rootForm.raw.meta["is-main-form"] ?? false;
+      const forAdditionalDemand =
+        this.args.field.question.raw.meta.forAdditionalDemand ?? false;
+      const template = this.args.field.question.raw.meta.template ?? null;
+
+      if (!isMainForm && !forAdditionalDemand) {
         params["form-slug"] = this.args.field.document.rootForm.slug;
       }
 
-      if (this.args.field.question.raw.meta.template) {
-        params.template = this.args.field.question.raw.meta.template;
+      if (template) {
+        params.template = template;
+      }
+
+      if (forAdditionalDemand) {
+        params["for-additional-demand"] = this.args.field.document.uuid;
       }
 
       yield this.dms.generatePdf(this.args.context.instanceId, params);
