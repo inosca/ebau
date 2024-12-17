207c8527f66c663c0857392c0b13fac94f9c1fc8
Tilo Stoppe <tilo.stoppe@adfinis.com>
2024-12-17 11:25:31 +0100

    feat(ag): add ag styling & logo in ebau, add ag configs

---
+++ ember-ebau-core/addon/config/work-item-list-ag.js
@@ -0,0 +1,31 @@
+export default {
+  showTaskFilter: true,
+  availableTasks: {
+    roles: {
+      municipality: [
+        "formal-exam",
+        "publication",
+        "init-distribution",
+        "check-inquiries",
+        "decision",
+        "construction-acceptance",
+      ],
+    },
+    services: {
+      1: ["inquiry", "check-inquiries", "check-additional-demand"],
+    },
+    default: ["inquiry"],
+  },
+  columns(status, role) {
+    return [
+      "task",
+      "instance",
+      role === "municipality" ? "applicants" : null,
+      ...(role === "service" ? ["municipality", "applicants"] : []),
+      "description",
+      ...(status === "COMPLETED"
+        ? ["closedAt", "closedBy"]
+        : ["deadline", "responsible"]),
+    ].filter((value) => value !== null);
+  },
+};
