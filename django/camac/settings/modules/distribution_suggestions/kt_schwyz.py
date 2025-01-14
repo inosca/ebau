dfa1d39d219f49034155028b83a7eba8d37fb080
Jonas Metzener <jonas.metzener@adfinis.com>
2025-01-14 14:24:58 +0100

    refactor(distribution): move distribution suggestions to separate files

---
+++ django/camac/settings/modules/distribution_suggestions/kt_schwyz.py
@@ -0,0 +1,21 @@
+SZ_DISTRIBUTION_SUGGESIONS = {
+    # For the suggestions services ids or slugs can be used but they shouldn't be mixed.
+    "SUBMODULES": [
+        ("fachthemen.landwirtschaft", [9]),
+        ("fachthemen.wald", [15]),
+        ("fachthemen.naturgefahren", [15]),
+        ("fachthemen.arbeitssicherheit-und-gesundheitsschutz", [4]),
+        ("fachthemen.zivilschutz", [79]),
+        ("fachthemen.zivilschutz-v2", [79]),
+        ("fachthemen.gewasserschutz", [267, 13]),
+        ("fachthemen.reklamen", [18]),
+        ("fachthemen.denkmalschutz-und-archaeologie", [8]),
+        ("fachthemen.fischerei", [267]),
+    ],
+    "QUESTIONS": [
+        (
+            "'Ja' in 'lebensmittel-umgehen'|value || 'Ja' in 'offentlich-duschanlage-oder-bad'|value",
+            [22],
+        )
+    ],
+}
