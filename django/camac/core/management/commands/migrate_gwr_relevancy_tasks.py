95b9fbeb72364b68324817e9aa2257bc6690d960
Stephan Hug <stephan.hug@adfinis.com>
2025-01-30 17:35:02 +0100

    fix(ur): add missing "AV projeziert" task

--- django/camac/core/management/commands/migrate_gwr_relevancy_tasks.py
+++ django/camac/core/management/commands/migrate_gwr_relevancy_tasks.py
@@ -1,9 +1,12 @@
 import datetime
+from datetime import timedelta
 
 from caluma.caluma_user.models import AnonymousUser
 from caluma.caluma_workflow.models import Task, WorkItem
 from django.core.management.base import BaseCommand
 from django.db import transaction
+from django.db.models import Q
+from django.utils import timezone
 from django.utils.timezone import now
 
 from camac.instance.models import Instance
@@ -20,16 +23,33 @@ class Command(BaseCommand):
         sid = transaction.savepoint()
 
         due_date = datetime.datetime(
-            2025, 1, 13, 0, 0, 0, 0, tzinfo=datetime.timezone.utc
+            2024, 1, 13, 0, 0, 0, 0, tzinfo=datetime.timezone.utc
         )
 
-        instances = Instance.objects.filter(creation_date__gte=due_date)
+        instances = Instance.objects.filter(
+            Q(creation_date__gte=due_date, case__document__form_id="building-permit")
+            & ~Q(instance_state__name__in=["new", "arch", "old", "del", "rejected"])
+        )
 
         check_gwr_relevancy_task = Task.objects.get(pk="check-gwr-relevancy")
 
         user = AnonymousUser()
 
+        counter = 0
         for instance in instances:
+            if instance.instance_state == "new":
+                continue
+            if instance.case.work_items.filter(
+                task_id="complete-check", status="ready"
+            ).exists():
+                continue
+
+            if instance.case.work_items.filter(
+                task_id="complete-construction-monitoring",
+                status__in=["completed", "skipped"],
+            ).exists():
+                continue
+
             gwr_relevancy_work_item = instance.case.work_items.filter(
                 task_id="check-gwr-relevancy"
             ).first()
@@ -39,7 +59,7 @@ class Command(BaseCommand):
                     task=check_gwr_relevancy_task,
                     name=check_gwr_relevancy_task.name,
                     status=WorkItem.STATUS_READY,
-                    addressed_groups=[],
+                    addressed_groups=[instance.group.service.pk],
                     controlling_groups=[],
                     created_at=now(),
                     created_by_user=user,
@@ -48,12 +68,16 @@ class Command(BaseCommand):
                         task_id="complete-check"
                     ).first(),
                     document=instance.case.document,
+                    deadline=timezone.now() + timedelta(days=28),
                 )
                 self.stdout.write(
                     f"A work-item of type 'check-gwr-relevancy' was created for the instance {instance.pk}"
                 )
+                counter += 1
             else:
                 self.stdout.write("No work-items were created")
+
+        self.stdout.write(f"{counter} work-items were created")
 
         if options["dry"]:
             transaction.savepoint_rollback(sid)
