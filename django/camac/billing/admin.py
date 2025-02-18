edf60ada47b89663ecdf635f9e2e0c7d73b77e87
Jonas Metzener <jonas.metzener@adfinis.com>
2025-02-18 18:19:37 +0100

    feat(billing): improve billing templates
    - Migrate ID to UUID
    - Remove `service` relation in favor of optional `services` and
      `service_groups` to allow templates for multiple services, service
      groups and also for everyone (both is empty)
    - Add translations for the admin and remove unused form fields
    - Add a base set of templates for the cantonal services to the config
    - Allow searching for a template in the creation mask

--- django/camac/billing/admin.py
+++ django/camac/billing/admin.py
@@ -1,4 +1,5 @@
 from django.contrib.admin import ModelAdmin, display, register
+from django.forms import ModelForm
 from django.utils.translation import gettext as _
 from localized_fields.admin import LocalizedFieldsAdminMixin
 
@@ -6,12 +7,56 @@ from camac.admin import EbauAdminMixin
 from camac.billing.models import BillingV2EntryTemplate
 
 
+class BillingV2EntryTemplateForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Mark certain fields as required in the admin even though they are not
+        # required in the DB
+        self.fields["calculation"].required = True
+        self.fields["tax_mode"].required = True
+
+    class Meta:
+        model = BillingV2EntryTemplate
+        fields = (
+            "services",
+            "service_groups",
+            "name",
+            "hint",
+            "text",
+            "calculation",
+            "hours",
+            "hourly_rate",
+            "percentage",
+            "total_cost",
+            "tax_mode",
+            "tax_rate",
+        )
+
+
 @register(BillingV2EntryTemplate)
 class BillingV2EntryTemplateAdmin(
     EbauAdminMixin, LocalizedFieldsAdminMixin, ModelAdmin
 ):
-    list_display = ["name", "get_service_name"]
+    list_display = [
+        "name",
+        "text",
+        "calculation",
+        "tax_mode",
+        "get_service_names",
+        "get_service_group_names",
+    ]
+    autocomplete_fields = ["services", "service_groups"]
+    search_fields = ["name"]
+    ordering = ["name"]
+    form = BillingV2EntryTemplateForm
 
-    @display(description=_("Service"))
-    def get_service_name(self, obj):
-        return obj.service.get_name()
+    @display(description=_("Services"))
+    def get_service_names(self, obj):
+        return ", ".join([service.get_name() for service in obj.services.all()])
+
+    @display(description=_("Service groups"))
+    def get_service_group_names(self, obj):
+        return ", ".join(
+            [service_group.get_name() for service_group in obj.service_groups.all()]
+        )
