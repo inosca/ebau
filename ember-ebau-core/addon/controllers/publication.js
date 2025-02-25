3ef5d7dc19f78916479de10be7223e78afce6e5e
Jonas Metzener <jonas.metzener@adfinis.com>
2025-02-25 13:39:47 +0100

    feat(publication): allow displaying of multiple date ranges
    This feature enables the date ranges in the navigation for cantons that
    have multiple ranges in one publication form. Also, the dates are now
    shown on the right hand side of the navigation.

--- ember-ebau-core/addon/controllers/publication.js
+++ ember-ebau-core/addon/controllers/publication.js
@@ -7,6 +7,7 @@ import { trackedTask } from "reactiveweb/ember-concurrency";
 
 import mainConfig from "ember-ebau-core/config/main";
 import getPublications from "ember-ebau-core/gql/queries/get-publications.graphql";
+import { getAnswer } from "ember-ebau-core/utils/get-answer";
 
 export default class PublicationController extends Controller {
   @service notification;
@@ -19,16 +20,17 @@ export default class PublicationController extends Controller {
     this.variables,
   ]);
 
+  get #config() {
+    return mainConfig.publication[this.model.type];
+  }
+
   get variables() {
-    const { task, startQuestion, endQuestion } =
-      mainConfig.publication[this.model.type];
+    const { task, dateRanges } = this.#config;
 
     return {
       instanceId: this.ebauModules.instanceId,
       task,
-      startQuestion,
-      endQuestion,
-      fetchDates: Boolean(startQuestion && endQuestion),
+      dateQuestions: dateRanges.flat(),
     };
   }
 
@@ -44,24 +46,46 @@ export default class PublicationController extends Controller {
     }
   }
 
-  dateStatus = (startDate, endDate) => {
-    if (!startDate || !endDate) {
+  #getDateRanges(document) {
+    const rawDates = this.#config.dateRanges
+      .map((questions) => {
+        return questions.map((slug) => getAnswer(document, slug)?.node.value);
+      })
+      .filter((dates) => dates.every(Boolean));
+
+    const uniqueDates = [
+      ...new Set(rawDates.map((dates) => dates.join(";"))),
+    ].map((dates) => dates.split(";"));
+
+    return uniqueDates.map((dates) => dates.map(DateTime.fromISO));
+  }
+
+  dateRanges = (document) => {
+    return this.#getDateRanges(document)
+      .map(([start, end]) => {
+        return `${this.intl.formatDate(start, { format: "date" })} - ${this.intl.formatDate(end, { format: "date" })}`;
+      })
+      .join(", ");
+  };
+
+  dateStatus = (document) => {
+    const ranges = this.#getDateRanges(document);
+
+    if (!ranges) {
       return null;
     }
 
-    const interval = Interval.fromDateTimes(
-      DateTime.fromISO(startDate),
-      DateTime.fromISO(endDate),
-    );
     const now = DateTime.now();
+    const intervals = ranges.map((dates) => Interval.fromDateTimes(...dates));
 
-    const status = interval.contains(now)
-      ? "active"
-      : interval.isAfter(now)
-        ? "future"
-        : interval.isBefore(now)
-          ? "past"
-          : null;
+    let status = null;
+    if (intervals.some((i) => i.contains(now))) {
+      status = "active";
+    } else if (intervals.some((i) => i.isAfter(now))) {
+      status = "future";
+    } else if (intervals.some((i) => i.isBefore(now))) {
+      status = "past";
+    }
 
     return status ? this.intl.t(`publication.${status}`) : null;
   };
