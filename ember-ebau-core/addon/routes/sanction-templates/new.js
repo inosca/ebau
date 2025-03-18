73d952204365861e5dd9d1b2ea267834e4ccdfd3
Martin Weber <martin.weber@adfinis.com>
2025-03-18 10:42:59 +0100

    fix(sanctions): do not set createdByService on template creation
    There appears to be a timing issue that causes peekRecord() to return
    null on staging, which in turn causes the backend to reject the
    parameter (it accepts any service associated with the request user, but
    not null).
    
    But really, there is no need to pass this from the frontend, as we're
    setting it in the backend anyway, so we remove it altogether.

--- ember-ebau-core/addon/routes/sanction-templates/new.js
+++ ember-ebau-core/addon/routes/sanction-templates/new.js
@@ -3,15 +3,8 @@ import { service } from "@ember/service";
 
 export default class SanctionTemplatesNewRoute extends Route {
   @service store;
-  @service ebauModules;
 
   model() {
-    const service = this.store.peekRecord(
-      "service",
-      this.ebauModules.serviceId,
-    );
-    return this.store.createRecord("sanctionTemplate", {
-      createdByService: service,
-    });
+    return this.store.createRecord("sanctionTemplate");
   }
 }
