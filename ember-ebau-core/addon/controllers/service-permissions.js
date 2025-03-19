c6ec8f4eed104fdc4bbcd3ec04f32b123728797b
Mitan Omar <mitan.omar@adfinis.com>
2025-03-19 09:32:26 +0100

    feat(ember-ebau-core): allow defining the sub-service that can be load with...

---
+++ ember-ebau-core/addon/controllers/service-permissions.js
@@ -0,0 +1,19 @@
+import Controller from "@ember/controller";
+
+import mainConfig from "ember-ebau-core/config/main";
+
+export default class ServicePermissionsController extends Controller {
+  get hasMoreThenOneRoute() {
+    return this.includeSubRoutes.length > 1;
+  }
+
+  get includeSubRoutes() {
+    return (
+      mainConfig.servicePermissions?.includeSubRoutes || [
+        "permissions",
+        "organisation",
+        "sub-services",
+      ]
+    );
+  }
+}
