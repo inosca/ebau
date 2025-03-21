12dd70cbcb8e8160e81cc08c4630db76d9ccbdfd
Falk <falk.neumann@adfinis.com>
2025-03-21 16:53:50 +0100

    feat(django): separate webdav entrypoint

---
+++ django/camac/wsgi_gunicorn.py
@@ -0,0 +1,19 @@
+"""
+WSGI config for camac-ng project without integrated dav.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import locale
+import os
+
+from django.conf import settings
+from django.core.wsgi import get_wsgi_application
+
+locale.setlocale(locale.LC_ALL, f"{settings.DEFAULT_LOCALE_CODE}.UTF-8")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "camac.settings")
+
+application = get_wsgi_application()
