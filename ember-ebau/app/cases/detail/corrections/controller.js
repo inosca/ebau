f906048b45707b8b8ee94308b8702a846ec3c3a2
Yelin Zhang <yelin.zhang@adfinis.com>
2025-03-26 09:15:54 +0100

    fix(ember-ebau): check document validity before correction finish

--- ember-ebau/app/cases/detail/corrections/controller.js
+++ ember-ebau/app/cases/detail/corrections/controller.js
@@ -1,5 +1,4 @@
 import Controller from "@ember/controller";
-import { action } from "@ember/object";
 import { service } from "@ember/service";
 import { dropTask } from "ember-concurrency";
 import { findRecord } from "ember-data-resources";
@@ -12,41 +11,6 @@ export default class CorrectionsController extends Controller {
   @service ebauModules;
 
   instance = findRecord(this, "instance", () => this.model.id);
-
-  @action
-  toggleModal() {
-    this.showModal = !this.showModal;
-  }
-
-  @dropTask
-  @confirmTask("corrections.document.confirm")
-  *startCorrection() {
-    yield this.documentCorrection.perform();
-  }
-
-  @dropTask
-  @confirmTask("corrections.document.confirm-finish")
-  *finishCorrection() {
-    yield this.documentCorrection.perform();
-  }
-
-  @dropTask
-  *documentCorrection() {
-    try {
-      yield this.fetch.fetch(`/api/v1/instances/${this.model.id}/correction`, {
-        method: "POST",
-      });
-
-      // sadly we need this to have current data on the whole page
-      location.reload();
-    } catch (error) {
-      if (error.cause) {
-        this.notification.danger(error.cause.map((e) => e.detail).join("<br>"));
-      } else {
-        this.notification.danger(this.intl.t("corrections.document.error"));
-      }
-    }
-  }
 
   @dropTask
   @confirmTask("corrections.withdraw.confirm")
