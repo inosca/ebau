6c94dbb8d10183c6f478e4a454191523a8be6eb9
Jonas Metzener <jonas.metzener@adfinis.com>
2025-05-09 15:05:10 +0200

    fix(ag): add frontend validation to responsible user rule form

--- ember-ebau-core/addon/components/rulesets/responsible-user-rule/edit.js
+++ ember-ebau-core/addon/components/rulesets/responsible-user-rule/edit.js
@@ -1,11 +1,30 @@
 import { action } from "@ember/object";
 import { service } from "@ember/service";
 import Component from "@glimmer/component";
+import { validatePresence } from "ember-changeset-validations/validators";
 import { task } from "ember-concurrency";
 import { findAll, query } from "ember-data-resources";
 import { localCopy } from "tracked-toolbox";
 
+import InputErrorComponent from "ember-ebau-core/components/input-error";
+
 export default class RulesetsResponsibleUserRuleEditComponent extends Component {
+  errorComponent = InputErrorComponent;
+  validations = {
+    municipalities: this.validatePresenceForType("municipalities"),
+    applicationTypes: this.validatePresenceForType("application-types"),
+    responsibleUser: validatePresence(true),
+  };
+
+  validatePresenceForType(type) {
+    return (...args) => {
+      const shouldBePresent = this.type === type;
+
+      // Only require presence if `this.type` has a given value
+      return validatePresence(shouldBePresent)(...args);
+    };
+  }
+
   @service intl;
   @service router;
   @service ebauModules;
