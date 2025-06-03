12a563f9a31ea6acd89aa8fc6dec7457c344b19f
David Vogt <david.vogt@adfinis.com>
2025-06-03 17:50:34 +0200

    feat(be-gever): sync documents after decision
    When decision is decreed, the GEVER interface should re-sync all the
    documents.

--- django/camac/gever/events.py
+++ django/camac/gever/events.py
@@ -9,5 +9,12 @@ Public entrypoints for the events that have a GEVER-related effect.
 """
 
 
+def decision_decreed(instance):
+    """Perform GEVER sync operations after decision has been decreed."""
+    # Spec: "Nach dem Bauentscheid durch die Leitbehörde werden nochmals die
+    # Dokumente im BE-GEVER aktualisiert."
+    return async_task(tasks.sync_documents, instance)
+
+
 def sync_button_pressed(instance):
     return async_task(tasks.sync_full, instance)
--- django/camac/gever/tasks.py
+++ django/camac/gever/tasks.py
@@ -11,6 +11,16 @@ is stored, so the serialisation / deserialisation will not cause problems.
 """
 
 
+def sync_documents(instance):
+    """Perform GEVER sync operations after decision has been decreed."""
+    # Spec: "Nach dem Bauentscheid durch die Leitbehörde werden nochmals die
+    # Dokumente im BE-GEVER aktualisiert."
+    api = GeverAPI(instance)
+    if api.get_gever_geschaeft():
+        # If there was no sync before, we're not syncing the documents either
+        api.sync_documents()
+
+
 def sync_full(instance):
     api = GeverAPI(instance)
     api.sync_full()
