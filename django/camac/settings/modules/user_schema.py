de6619b85c60078fec876366791e1ba0364d8060
Falk <falk.neumann@adfinis.com>
2025-07-02 10:50:28 +0200

    fix(user-config-schema): default to empty list/dict
    Defaulting to an empty list/dict makes the app code more clean
    and falling back to None only makes sense to state that a
    feature is turned 'off', which we would do with an explicit
    flag.

--- django/camac/settings/modules/user_schema.py
+++ django/camac/settings/modules/user_schema.py
@@ -10,11 +10,11 @@ class UserConfig(ModuleApplicationConfig):
     This module should be enabled in combination with the user profile view.
     """
 
-    question_user_attributes_mapping: dict[str, str | list[str]] | None = Field(
+    question_user_attributes_mapping: dict[str, str | list[str]] = Field(
         description="Mapping of question related OIDC attributes to the user model.",
-        default=None,
+        default={},
     )
-    allowed_write_attributes: list[str] | None = Field(
+    allowed_write_attributes: list[str] = Field(
         description="List of attributes which should be writeable by the user in the user profile.",
-        default=None,
+        default=[],
     )
