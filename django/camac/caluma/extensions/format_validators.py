79d20be360b12ef0bad9feb451c89a6542b9c20f
Tilo Stoppe <tilo.stoppe@adfinis.com>
2025-07-18 17:33:30 +0200

    feat(ag): add ag even project number validator and form field

--- django/camac/caluma/extensions/format_validators.py
+++ django/camac/caluma/extensions/format_validators.py
@@ -12,3 +12,16 @@ class IntegerListFormatValidator(BaseFormatValidator):
         "en": "Only comma separated intergers are permited",
         "de": "Nur Komma separierte Integers sind erlaubt",
     }
+
+
+class EvenProjectNumberFormatValidator(BaseFormatValidator):
+    slug = "even-project-number"
+    name = {
+        "en": "EVEN project number format",
+        "de": "Format der EVEN-Projektnummer",
+    }
+    regex = r"^[A-Z]{2}-[A-Z0-9]{5}$"
+    error_msg = {
+        "en": "The marking must consist of two capital letters for the canton abbreviation, a hyphen and five letters/numbers.",
+        "de": "Die Kennzeichnung muss aus zwei Grossbuchstaben für das Kantonskürzel, einen Bindestrich und fünf Buchstaben/Zahlen bestehen.",
+    }
