22a438a6117f6a5ae7e9821b5910bc01a6e9037f
Martin Weber <martin.weber@adfinis.com>
2025-09-03 13:32:22 +0200

    fix(tools/bump-version): properly quote VERSION variable
    Without quotes, this may misbehave if $VERSION has a value that expands
    to a valid arguments sequence for test(1p) (for example `= -z`).
    
    For other cases, this did the right thing (though in some cases with
    shell error messages). To keep it correct, let's quote $VERSION
    correctly here. :-)

--- tools/bump-version.sh
+++ tools/bump-version.sh
@@ -2,12 +2,12 @@
 
 VERSION=$1
 
-if [ -z $VERSION ]
+if [ -z "$VERSION" ]
   then echo "Please pass a version"
   exit 1
 fi
 
-echo $VERSION > VERSION.txt;
+echo "$VERSION" > VERSION.txt;
 sed -i -e "s/\"version\": \".*\",/\"version\": \"$VERSION\",/g" ember-camac-ng/package.json
 sed -i -e "s/\"version\": \".*\",/\"version\": \"$VERSION\",/g" ember-caluma-portal/package.json
 sed -i -e "s/__version__ = \".*\"/__version__ = \"$VERSION\"/g" django/camac/camac_metadata.py
