8d93a9887c64911c5f5b4075130396c29a31df44
Tilo Stoppe <tilo.stoppe@adfinis.com>
2025-09-12 09:59:50 +0200

    chore(ag): add minDate for publication

--- ember-ebau-core/addon/components/publication-fill-end-date.js
+++ ember-ebau-core/addon/components/publication-fill-end-date.js
@@ -7,6 +7,26 @@ export default class PublicationFillEndDate extends Component {
     return this.args.field.raw.question.meta["fill-end-date"] ?? {};
   }
 
+  get minDate() {
+    if (
+      this.#config.minDateFromNow &&
+      typeof this.#config.minDateFromNow === "number"
+    ) {
+      return new Date().fp_incr(this.#config.minDateFromNow);
+    }
+    return undefined;
+  }
+
+  get maxDate() {
+    if (
+      this.#config.maxDateFromNow &&
+      typeof this.#config.maxDateFromNow === "number"
+    ) {
+      return new Date().fp_incr(this.#config.maxDateFromNow);
+    }
+    return undefined;
+  }
+
   saveAndFill = task({ drop: true }, async (value) => {
     const endQuestion = this.args.field.document.findField(
       this.#config.question,
