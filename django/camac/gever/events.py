18d3d658ae2753c3f05c31c0076d101aa1839e16
David Vogt <david.vogt@adfinis.com>
2025-09-22 09:49:28 +0200

    fix(be-gever): do not trigger background tasks if gever disabled
    GEVER is not really active (and likely never will be). Code from that
    module shouldn't be run if the module is not actually active.
    
    Currently this is not a big problem, but it causes django-q tasks to
    run that shouldn't be, which spams the logs with stack traces

--- django/camac/gever/events.py
+++ django/camac/gever/events.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django_q.tasks import async_task
 
 from . import tasks
@@ -11,10 +12,16 @@ Public entrypoints for the events that have a GEVER-related effect.
 
 def decision_decreed(instance):
     """Perform GEVER sync operations after decision has been decreed."""
+    if not settings.GEVER or not settings.GEVER["ENABLED"]:
+        # GEVER not active - don't do anything
+        return False
     # Spec: "Nach dem Bauentscheid durch die Leitbehörde werden nochmals die
     # Dokumente im BE-GEVER aktualisiert."
     return async_task(tasks.sync_documents, instance)
 
 
 def sync_button_pressed(instance):
+    if not settings.GEVER or not settings.GEVER["ENABLED"]:
+        # GEVER not active - don't do anything
+        return False
     return async_task(tasks.sync_full, instance)
