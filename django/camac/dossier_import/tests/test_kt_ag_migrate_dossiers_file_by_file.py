842cde42ba4998c5caefa652f87a3428aac84ede
Martin Huber <martin.huber@karakun.com>
2025-09-26 14:44:33 +0200

    fix test failures

--- django/camac/dossier_import/tests/test_kt_ag_migrate_dossiers_file_by_file.py
+++ django/camac/dossier_import/tests/test_kt_ag_migrate_dossiers_file_by_file.py
@@ -1,7 +1,6 @@
 import os
 
 import pytest
-import pytz
 
 from camac.dossier_import.tests.test_kt_ag_migrate_dossiers import (
     _migrate_from_file_and_assert,
@@ -16,6 +15,6 @@ from camac.dossier_import.tests.test_kt_ag_migrate_dossiers import (
 )
 @pytest.mark.order(1)  # Slow tests should run first
 @pytest.mark.freeze_time("2025-07-28 12:00:00")
-@pytest.mark.timezone(pytz.FixedOffset(120))
+@pytest.mark.django_db(transaction=True)
 def test_migrate_single_json_file(input_file, db, setup_dossier_import_ag, snapshot):
     _migrate_from_file_and_assert(input_file, snapshot)
