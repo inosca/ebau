566db1e5bf7e91d6f4d4b7d41e7aca04198db60d
Jonas Metzener <jonas.metzener@adfinis.com>
2025-10-02 16:18:29 +0200

    feat(responsible): order users in dropdown by full name
    Related to camac-ng/kanton-schwyz-projektmanagement#45

--- ember-ebau-core/addon/controllers/responsible.js
+++ ember-ebau-core/addon/controllers/responsible.js
@@ -14,6 +14,7 @@ export default class ResponsibleController extends Controller {
 
   users = query(this, "user", () => ({
     service: this.ebauModules.serviceId,
+    sort: "name,surname",
   }));
 
   responsibilities = query(this, "responsible-service", () => ({
