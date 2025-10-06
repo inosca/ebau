92c4677b05cc954d64b8db1a0cc819968bd296b7
Martin Huber <martin.huber@karakun.com>
2025-10-06 11:18:29 +0200

    address comment to last MR

--- django/camac/dossier_import/tasks.py
+++ django/camac/dossier_import/tasks.py
@@ -41,7 +41,7 @@ if settings.APPLICATION_NAME == "kt_ag":  # pragma: no cover
 
     @shared_task(
         soft_time_limit=60 * 60 * 10
-    )  # 6 hours timeout to complete with all dossiers from one municipality
+    )  # timeout to complete with all dossiers from one municipality
     def import_s3_docs_task(
         municipality: str, dossier_ids: List[str], segment_name: str, start_time: str
     ):
