bd506f5c131cf56c42934219cf375429691494bf
René Birrer <rene.birrer@adfinis.com>
2025-10-07 18:40:00 +0200

    fix(sz/coordinates): migrate coordinates to correct data format
    related to 0b5bf692fc

---
+++ django/camac/instance/tests/test_sz_migrations.py
@@ -0,0 +1,34 @@
+import importlib
+
+from camac.instance.models import FormField
+
+
+def test_fix_sz_false_coordinate_values(
+    db, instance_factory, form_field_factory, set_application_sz
+):
+    field_name = "punkte"
+    fix_me = instance_factory()
+    bad_field_value = [
+        {"lat": 47.175669937318816, "lng": 8.8984885140077},
+        {"lat": 47.175669937318810, "lng": 8.8984885140066},
+    ]
+    form_field_factory(instance=fix_me, name=field_name, value=bad_field_value)
+    good_instance = instance_factory()
+    good_instance_field_value = [
+        [
+            {"lat": 47.175669937318816, "lng": 8.8984885140077},
+            {"lat": 47.175669937318810, "lng": 8.8984885140066},
+        ],
+        [{"lat": 47.175669937318816, "lng": 8.8984885140077}],
+    ]
+    form_field_factory(
+        instance=good_instance, name=field_name, value=good_instance_field_value
+    )
+    migration = importlib.import_module(
+        "camac.instance.migrations.0042_data_fix_sz_punkte_values"
+    )
+    migration.fix_field_values(FormField)
+    assert fix_me.fields.get(name=field_name).value == [
+        [obj] for obj in bad_field_value
+    ]
+    assert good_instance.fields.get(name=field_name).value == good_instance_field_value
