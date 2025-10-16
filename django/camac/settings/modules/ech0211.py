779e3c7f4b5a0602733bea245f37fa523ecb7576
Christian Zosel <christian@zosel.ch>
2025-10-16 16:27:05 +0200

    fix(ag): allow eCH clients to upload files to "Alle Beteiligten"

--- django/camac/settings/modules/ech0211.py
+++ django/camac/settings/modules/ech0211.py
@@ -705,7 +705,12 @@ ECH0211 = {
             r"claim/<int:instance_id>/": "/cases/%(instance_id)i/additional-demand",
             r"dossier-check/<int:instance_id>/": "/cases/%(instance_id)i/task-form/formal-exam",
         },
-        "ALLOWED_CATEGORIES": ["beteiligte-behörden", "intern", "beilagen-zum-gesuch"],
+        "ALLOWED_CATEGORIES": [
+            "beteiligte-behörden",
+            "intern",
+            "beilagen-zum-gesuch",
+            "alle-beteiligten",
+        ],
         "NOTICE_RULING": {
             "ALLOWED_STATES": ["decision", "circulation"],
             "ONLY_DECLINE": ["distribution-init"],
