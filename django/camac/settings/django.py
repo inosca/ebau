279a12045b79a5a2643056f254c96fba615b6b7d
Christian Zosel <christian@zosel.ch>
2025-11-06 16:14:44 +0100

    chore: add config for celery transport options
    We need this to set the "visibility timeout" to enable long-running
    tasks in a migration.

--- django/camac/settings/django.py
+++ django/camac/settings/django.py
@@ -3774,6 +3774,9 @@ CELERY_TASK_ACKS_LATE = env.bool(
     "CELERY_TASK_ACKS_LATE",
     default=True,
 )
+CELERY_BROKER_TRANSPORT_OPTIONS = env.dict(
+    "CELERY_BROKER_TRANSPORT_OPTIONS", default={}
+)
 CELERY_TASK_SOFT_TIME_LIMIT = env.int("CELERY_TASK_SOFT_TIME_LIMIT", default=60)
 # if unspecified, celery starts one worker process per CPU.
 CELERY_WORKER_CONCURRENCY = env.int("CELERY_WORKER_CONCURRENCY", default=None)
