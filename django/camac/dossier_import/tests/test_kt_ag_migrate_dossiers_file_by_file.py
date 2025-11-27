42a2d72497bbfccd207fc1ab5e515da99bf512ed
Martin Huber <martin.huber@karakun.com>
2025-11-27 14:15:29 +0100

    chore(ag): skip non-main-path tests

--- django/camac/dossier_import/tests/test_kt_ag_migrate_dossiers_file_by_file.py
+++ django/camac/dossier_import/tests/test_kt_ag_migrate_dossiers_file_by_file.py
@@ -13,8 +13,11 @@ from camac.dossier_import.tests.test_kt_ag_migrate_dossiers import (
     get_test_files(),
     ids=lambda p: os.path.basename(p),
 )
+@pytest.mark.skip(reason="not productive execution that is tested")
 @pytest.mark.order(1)  # Slow tests should run first
 @pytest.mark.freeze_time("2025-07-28 12:00:00")
 @pytest.mark.django_db(transaction=True)
-def test_migrate_single_json_file(input_file, db, setup_dossier_import_ag, snapshot):
+def test_migrate_single_json_file(
+    input_file, db, setup_dossier_import_ag, snapshot
+):  # pragma: no cover
     _migrate_from_file_and_assert(input_file, snapshot)
