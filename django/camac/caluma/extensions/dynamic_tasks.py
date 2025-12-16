ce20339056ac8b2c33ebf0e08861a34f3790924d
Tilo Stoppe <tilo.stoppe@adfinis.com>
2025-12-16 09:39:47 +0100

    fix(ag): workflow check-pa task for afb only for active inquiries

--- django/camac/caluma/extensions/dynamic_tasks.py
+++ django/camac/caluma/extensions/dynamic_tasks.py
@@ -179,7 +179,7 @@ class CustomDynamicTasks(BaseDynamicTasks):
         if (
             Inquiry.objects.for_root_case(case)
             .addressed_to(Service.objects.get(slug="afb"))
-            .exists()
+            .only_active()
         ):
             tasks.append("check-pa")
 
