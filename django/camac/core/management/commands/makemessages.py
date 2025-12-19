c0e288fcc373b9a3e780b7d56174fd686c4530e0
Jonas Metzener <jonas.metzener@adfinis.com>
2025-12-19 13:21:23 +0100

    chore(i18n): disable fuzzy matches in translations
    Fuzzy matches confuse more than they help. As we don't work with
    translators that expect them, we disable it entirely.

--- django/camac/core/management/commands/makemessages.py
+++ django/camac/core/management/commands/makemessages.py
@@ -2,6 +2,12 @@ from django.core.management.commands import makemessages
 
 
 class Command(makemessages.Command):
+    msgmerge_options = makemessages.Command.msgmerge_options + ["--no-fuzzy-matching"]
+    msgattrib_options = makemessages.Command.msgattrib_options + [
+        "--clear-fuzzy",
+        "--empty",
+    ]
+
     def handle(self, *args, **options):
         options["add_location"] = "file"
         options["no_obsolete"] = True
