5bcf3e8a539ecdaf3cf987eab35a54fd1963301f
Stephan Hug <stephan.hug@adfinis.com>
2026-01-19 11:46:55 +0100

    feat(elixir): update dockerfile to multistage

--- elixir-ebau/entrypoint.sh
+++ elixir-ebau/entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 set -e
 
+if [ ! -d "/app/assets/node_modules" ]; then
+    if [ ! -d "/opt/node_modules" ]; then
+        echo "/opt/node_modules does not exist. Exiting."
+        exit 1
+    fi
+    cp -r  /opt/node_modules/ /app/assets/node_modules/
+fi
+
 mix deps.get
-mix deps.compile
 
 mix phx.server
