06fe17cd8f011b50f5d2d1ad2dd76c65aed565d7
René Birrer <rene.birrer@adfinis.com>
2026-01-21 15:28:48 +0100

    feat(sz|document): add command for migrating templates to dms

--- django/camac/document/management/commands/legacy_templates.py
+++ django/camac/document/management/commands/legacy_templates.py
@@ -1,6 +1,10 @@
 import importlib
+import json
+from pathlib import Path
 
+from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
+from django.utils.text import slugify
 from docxtpl import DocxTemplate
 from jinja2.exceptions import TemplateSyntaxError
 
@@ -37,6 +41,25 @@ class Command(BaseCommand):
         )
         extract_used_parser.set_defaults(method=self.extract_used)
 
+        # migrate templates to DMS
+        export_templates_parser = subparsers.add_parser(
+            "export_templates", help="Dump templates to JSON for loading in DMS."
+        )
+        export_templates_parser.set_defaults(method=self.export_templates)
+        export_templates_parser.add_argument(
+            "--out_file",
+            help="Absolute path of the resulting fixture file",
+            default="dms_templates_fixture.json",
+        )
+
+        # cleanup dangling files in templates directory
+        remove_dangling_parser = subparsers.add_parser(
+            "remove_dangling",
+            help="Remove unreferenced template files from upload dir.",
+        )
+        remove_dangling_parser.add_argument("--run", action="store_true", default=False)
+        remove_dangling_parser.set_defaults(method=self.remove_dangling)
+
     def handle(self, *args, method, **options):
         method(*args, **options)
 
@@ -67,3 +90,71 @@ class Command(BaseCommand):
 
         for placeholder in sorted(self.all_placeholders):
             self.stdout.write(f"\t- {placeholder}")
+
+    def export_templates(self, *args, **options):
+        """Create a JSON dump of all templates ready for loading to DMS."""
+        target_file = Path(options["out_file"])
+
+        templates = []
+        for templ in self.template_class.objects.all():
+            template = Path(templ.path.name).name
+            templ_slug = (
+                slugify(templ.name)
+                if len(slugify(templ.name)) < 45
+                else f"{slugify(templ.name)[:45]}{templ.pk:04}"
+            )
+
+            # make a fixture dump for templates for loading in document-merge-service
+            dms_template = {
+                "model": "api.template",
+                "pk": templ_slug,
+                "fields": {
+                    "description": f"{templ.name}",
+                    "template": template,
+                    "engine": "docx-template",
+                    "meta": {
+                        # "service_group" expects the group SLUG for evaluation of template's visibility
+                        "service_group": templ.group.service.slug or "",
+                        # service id is expected to be a string
+                        "service": str(templ.service_id or ""),
+                    },
+                },
+            }
+            templates.append(dms_template)
+        json.dump(templates, target_file.open("w"), indent=4)
+        self.stdout.write(
+            f"Dumped {len(templates)} template entries to {target_file.absolute()}."
+        )
+
+    def remove_dangling(self, *args, **options):
+        referenced = self.template_class.objects.values_list("path", flat=True)
+        match = "**/templates/*"
+        all_files = Path(settings.MEDIA_ROOT).glob(match)
+        self.stdout.write(
+            self.style.NOTICE(
+                f"\n{len(list(all_files))} total number of files in $MEDIA_ROOT/templates/**"
+            )
+        )
+        dangling = []
+        for any_file in Path(settings.MEDIA_ROOT).glob(match):
+            if "/".join(any_file.parts[-2:]) not in referenced:
+                dangling.append(any_file)
+                if options.get("run") is True:
+                    any_file.unlink(missing_ok=True)
+        self.stdout.write(
+            self.style.NOTICE(
+                f"\n{'Removed' if options.get('run') else 'Would remove'} {len(dangling)} dangling files from $MEDIA_ROOT/templates/ dir."
+            )
+        )
+        self.stdout.write(self.style.WARNING("\n=== LEFTOVER FILES ==="))
+        self.stdout.write(
+            self.style.WARNING("\n".join(str(f.absolute()) for f in dangling))
+        )
+        self.stdout.write(
+            self.style.SUCCESS("\n=== FILES REFERENCED BY TEMPLATE ===")
+            if options.get("verbosity") > 1
+            else ""
+        )
+        self.stdout.write(
+            self.style.SUCCESS("\n".join(ref for ref in referenced))
+        ) if options.get("verbosity") > 1 else ""
