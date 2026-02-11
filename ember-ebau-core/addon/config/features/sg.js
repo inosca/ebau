4ae51e6d64a959816c61d0fc8072146f06b1d8b1
Jonas Metzener <jonas.metzener@adfinis.com>
2026-02-11 10:35:37 +0100

    feat(sg): enable common features
    Enables roles for applicants and the v2 work item list so it won't be
    forgotten.

--- ember-ebau-core/addon/config/features/sg.js
+++ ember-ebau-core/addon/config/features/sg.js
@@ -2,4 +2,10 @@ import { getOwnConfig, isDevelopingApp } from "@embroider/macros";
 
 export default {
   watermark: isDevelopingApp() || getOwnConfig().enableWatermark,
+  permissions: {
+    applicantRoles: true,
+  },
+  workItems: {
+    v2: true,
+  },
 };
