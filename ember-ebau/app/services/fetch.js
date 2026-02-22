3d541f5a081609164a795a615651384f66fe27c0
Christian Zosel <christian@zosel.ch>
2026-02-22 12:30:20 +0100

    fix(ember-ebau): refresh auth in fetch service

--- ember-ebau/app/services/fetch.js
+++ ember-ebau/app/services/fetch.js
@@ -10,6 +10,8 @@ export default class FetchService extends Service {
   @service session;
 
   async fetch(resource, init = {}) {
+    await this.session.refreshAuthentication.perform();
+
     init.headers = cleanObject({
       ...this.session.headers,
       accept: CONTENT_TYPE,
