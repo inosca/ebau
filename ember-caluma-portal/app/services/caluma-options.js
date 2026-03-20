375f55201ffc75a05fac7e70d9c1fdbca57251f4
Niels Verbeek <niels.verbeek@adfinis.com>
2026-03-20 14:23:18 +0100

    feat(construction-monitoring): show addressed groups on work items

--- ember-caluma-portal/app/services/caluma-options.js
+++ ember-caluma-portal/app/services/caluma-options.js
@@ -1,8 +1,25 @@
+import { service } from "@ember/service";
 import CalumaOptionsService from "@projectcaluma/ember-core/services/caluma-options";
 import { hasFeature } from "ember-ebau-core/helpers/has-feature";
+import fetchIfNotCached from "ember-ebau-core/utils/fetch-if-not-cached";
 
 export default class CustomCalumaOptionsService extends CalumaOptionsService {
+  @service store;
+
   useNumberSeparatorWidgetAsDefault = hasFeature(
     "caluma.useNumberSeparatorWidgetAsDefault",
   );
+
+  resolveUsers(identifiers) {
+    return fetchIfNotCached("public-user", "username", identifiers, this.store);
+  }
+
+  resolveGroups(identifiers) {
+    return fetchIfNotCached(
+      "public-service",
+      "service_id",
+      identifiers,
+      this.store,
+    );
+  }
 }
