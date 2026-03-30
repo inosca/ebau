65d1710975e6365ef1601a6725df7ab67639593d
David Vogt <david.vogt@adfinis.com>
2026-03-30 15:16:27 +0200

    fix(be-gis): merge data in correct order
    Threading is nice and all, but we need to merge the data in a defined
    order, otherwise things will be overwritten in an unexpected way
    
    This is still problematic, as apparently we get conflicting data, which
    is the actual problem here. But at least the tests can be green again now...

--- django/camac/gis/clients/begis.py
+++ django/camac/gis/clients/begis.py
@@ -154,6 +154,7 @@ class BeGisClient(GISBaseClient):
                 for egrid in egrids
             }
 
+            results = {}
             for future in concurrent.futures.as_completed(futures):
                 egrid = futures[future]
                 try:
@@ -167,10 +168,16 @@ class BeGisClient(GISBaseClient):
                         xml_data=xml_data,
                         et=et,
                     )
-                    self.merge_data_dict(data, new_data, special_layers)
+                    results[egrid] = new_data
 
                 except RuntimeError as e:  # pragma: no cover
                     exception_messages.add(f"Error for {egrid}: {str(e)}")
+
+            # Threads may return out-of-order, but we need to process the data
+            # in specified order to avoid races
+            for egrid in egrids:
+                new_data = results[egrid]
+                self.merge_data_dict(data, new_data, special_layers)
 
         if exception_messages:  # pragma: no cover
             # We raise a single RuntimeError per GIS datasource;
