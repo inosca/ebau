fac4b6a83c9655b2c82fba525cd1fc168da38ebb
David Vogt <david.vogt@adfinis.com>
2026-04-02 17:25:45 +0200

    refactor(tests): fix translate test so it doesn't write persistent data file
    The test modifies it's own test data, then tries to restore it. It's
    much better to make a temp copy and let the code to-be-tested run on
    that instead.

--- django/camac/core/tests/test_translate_caluma.py
+++ django/camac/core/tests/test_translate_caluma.py
@@ -3,26 +3,22 @@ import json
 from django.core.management import call_command
 
 
-def test_translate(db):
-    with open("camac/core/tests/untranslated_config.json", "r+") as file:
-        original_data = json.load(file)
+def test_translate(db, tmp_path):
+    ORIG_FILE = "camac/core/tests/untranslated_config.json"
 
-        call_command("translate_caluma", "camac/core/tests/untranslated_config.json")
+    work_file = tmp_path / "test_config.json"
 
-    with open("camac/core/tests/untranslated_config.json", "r+") as file:
+    with open(ORIG_FILE, "rb") as fh_in, open(work_file, "wb") as fh_out:
+        fh_out.write(fh_in.read())
+
+    call_command("translate_caluma", work_file)
+
+    with open(work_file, "r+") as file:
         data = json.load(file)
 
-        item = next(
-            (item for item in data if item["pk"] == "werden-siloanlagen-erstellt")
-        )
-        untranslated_item = next((item for item in data if item["pk"] == "Test"))
+    item = next((item for item in data if item["pk"] == "werden-siloanlagen-erstellt"))
+    untranslated_item = next((item for item in data if item["pk"] == "Test"))
 
-        # reset file content
-        file.seek(0)
-        json.dump(original_data, file, ensure_ascii=False, indent=2)
-
-        assert (
-            json.loads(item["fields"]["label"])["de"] == "Werden Siloanlagen erstellt?"
-        )
-        assert json.loads(item["fields"]["label"])["fr"] == "Des silos sont-ils prévus?"
-        assert json.loads(untranslated_item["fields"]["label"])["de"] == "Test abc"
+    assert json.loads(item["fields"]["label"])["de"] == "Werden Siloanlagen erstellt?"
+    assert json.loads(item["fields"]["label"])["fr"] == "Des silos sont-ils prévus?"
+    assert json.loads(untranslated_item["fields"]["label"])["de"] == "Test abc"
