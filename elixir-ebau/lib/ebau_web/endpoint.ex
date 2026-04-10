017943ae8ab9fb49083ba24d27b937adc288b8df
Jonas Cosandey <jonas.cosandey@adfinis.com>
2026-04-10 13:02:01 +0200

    fix: use custom code reloader

--- elixir-ebau/lib/ebau_web/endpoint.ex
+++ elixir-ebau/lib/ebau_web/endpoint.ex
@@ -32,7 +32,7 @@ defmodule EbauWeb.Endpoint do
   if code_reloading? do
     socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
     plug Phoenix.LiveReloader
-    plug Phoenix.CodeReloader
+    plug EbauWeb.Plugs.SelectiveCodeReloader
     plug Phoenix.Ecto.CheckRepoStatus, otp_app: :ebau
   end
 
---
+++ elixir-ebau/lib/ebau_web/plugs/selective_code_reloader.ex
@@ -0,0 +1,35 @@
+defmodule EbauWeb.Plugs.SelectiveCodeReloader do
+  @moduledoc """
+  Wraps `Phoenix.CodeReloader` to only invoke `mix compile` when source
+  files have actually changed, avoiding the ~1s noop overhead on every
+  request.
+
+  Checks whether any `.ex`, `.heex`, or `.po` file in `lib/` or
+  `priv/gettext/` is newer than the Elixir compiler manifest. If nothing
+  is stale the plug is a no-op (microseconds). If something changed it
+  delegates to `Phoenix.CodeReloader` as normal.
+  """
+
+  @behaviour Plug
+
+  def init(opts), do: Phoenix.CodeReloader.init(opts)
+
+  def call(conn, opts) do
+    if stale?() do
+      Phoenix.CodeReloader.call(conn, opts)
+    else
+      conn
+    end
+  end
+
+  defp stale? do
+    sources =
+      Path.wildcard("lib/**/*.ex") ++
+        Path.wildcard("lib/**/*.heex") ++
+        Path.wildcard("priv/gettext/**/*.po")
+
+    manifests = Mix.Tasks.Compile.Elixir.manifests()
+
+    Mix.Utils.stale?(sources, manifests)
+  end
+end
