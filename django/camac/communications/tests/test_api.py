3ce8bc6d30952ea6b04c4f2b419bbbf9c3d18330
David Vogt <david.vogt@adfinis.com>
2026-04-16 12:17:15 +0200

    fix(be/documents): file validation for zip uploads
    Some applications / browsers may submit expired or incorrect MIME types
    for ZIP archives. These should also be accepted, even if our detection
    yields application/zip.
    
    Introduce an ALTERNATE_MIME_TYPES definition in the validation routine
    so we can add other mime type groups as well in the future if needed.

--- django/camac/communications/tests/test_api.py
+++ django/camac/communications/tests/test_api.py
@@ -8,12 +8,14 @@ the corresponding test modules.
 import io
 import json
 import logging
+from contextlib import nullcontext as no_exception
 from mimetypes import guess_extension
 
 import pytest
 from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.urls import reverse
 from rest_framework import status
+from rest_framework.exceptions import ValidationError
 
 from camac.communications.models import (
     CommunicationsAttachment,
@@ -557,3 +559,37 @@ def test_validation_of_display_name_by_message_creation(
         pk=resp.json()["data"]["id"]
     ).attachments.all():
         assert attachment.file_attachment.name.endswith(corrected_display_name)
+
+
+@pytest.mark.parametrize(
+    "content_type, expectation",
+    [
+        ("application/x-zip-compressed", no_exception()),
+        ("application/zip-compressed", no_exception()),
+        ("application/x-zip", no_exception()),
+        ("application/zip", no_exception()),
+        ("application/x-not-a-zip-archive", pytest.raises(ValidationError)),
+    ],
+)
+def test_validate_mime_type_alternative_mime_for_zip(
+    caplog,
+    content_type,
+    expectation,
+    mocker,
+    settings,
+):
+
+    raw_file = django_file("simple-archive.zip")
+    filename = "testfile.zip"
+
+    file = InMemoryUploadedFile(
+        file=raw_file,
+        name=filename,
+        content_type=content_type,
+        size=raw_file.size,
+        charset="utf8",
+        field_name="irrelevant",
+    )
+
+    with expectation:
+        validate_mime_type(file)
