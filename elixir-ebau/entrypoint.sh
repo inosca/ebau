db55e2c443f8f3036bd6bec18c9ab85ce1120dd1
Stephan Hug <stephan.hug@adfinis.com>
2026-04-27 09:55:39 +0200

    fix(elixir): add missing entrypoint.sh file

---
+++ elixir-ebau/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+echo "==> Installing dependencies..."
+mix deps.get
+
+echo "==> Compiling dependencies (this takes a few minutes on first run)..."
+mix deps.compile
+
+echo "==> Compiling application..."
+mix compile
+
+echo "==> Running migrations..."
+mix ecto.migrate
+
+echo "==> Starting Phoenix server..."
+exec mix phx.server
