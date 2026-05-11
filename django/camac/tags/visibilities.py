9bd31700f6f97673d6d6bd2d19c3913b1ccb5a3c
Jonas Cosandey <jonas.cosandey@adfinis.com>
2026-05-11 11:12:55 +0200

    fix(static-keywords): visibility should include child services

--- django/camac/tags/visibilities.py
+++ django/camac/tags/visibilities.py
@@ -6,12 +6,25 @@ from camac.user.permissions import get_role_name
 
 class TagsVisibility:
     @filter_queryset_for(Keyword)
-    @filter_queryset_for(StaticKeyword)
     def filter_keywords(self, queryset, request):
         role = get_role_name(request.group)
 
         if role in {"municipality", "service"}:
             return queryset.filter(service=request.group.service)
+        elif role == "support":
+            return queryset
+
+        return queryset.none()
+
+    @filter_queryset_for(StaticKeyword)
+    def filter_static_keywords(self, queryset, request):
+        role = get_role_name(request.group)
+        service = request.group.service
+
+        if role in {"municipality", "service"}:
+            return queryset.filter(
+                service=service.service_parent or service,
+            )
         elif role == "support":
             return queryset
 
