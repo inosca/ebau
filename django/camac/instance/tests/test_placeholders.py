08fb32354f95aff2277af7c7ee79e32f81bcda39
René Birrer <rene.birrer@adfinis.com>
2026-05-13 12:19:04 +0200

    test(sz|placeholders): add fake request to serializer

--- django/camac/instance/tests/test_placeholders.py
+++ django/camac/instance/tests/test_placeholders.py
@@ -1189,9 +1189,12 @@ def test_publication_journal_number_override_sz(
     publication_entry,
     admin_client,
     sz_instance,
+    fake_request,
     expected_output,
 ):
-    serializer = SzDMSPlaceholdersSerializer(sz_instance)
+    serializer = SzDMSPlaceholdersSerializer(
+        sz_instance, context={"request": fake_request}
+    )
     assert serializer.data["PUBLICATIONS"][0]["JOURNAL_NUMBER"] == expected_output
 
 
