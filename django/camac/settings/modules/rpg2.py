553649878a01791d5718f42a5afdeb461eef7465
Maria Vatasoiu <maria.vatasoiu@adfinis.com>
2026-06-10 17:30:55 +0200

    feat(be/rpg2): enable RPG2 module for kt. Bern and update allowed forms

--- django/camac/settings/modules/rpg2.py
+++ django/camac/settings/modules/rpg2.py
@@ -4,9 +4,14 @@ from camac.settings.modules.rpg2_schema import RPG2Config
 RPG2 = ModuleConfig[RPG2Config](
     default=RPG2Config(),
     kt_bern=RPG2Config(
-        enabled=False,  # TODO: Enable
+        enabled=True,
         service_slugs=["agr-bauen", "agr-kantonsplanung"],
-        allowed_forms=["baugesuch", "baugesuch-generell", "baugesuch-mit-uvp"],
+        allowed_forms=[
+            "baugesuch",
+            "baugesuch-generell",
+            "baugesuch-mit-uvp",
+            "verlaengerung-geltungsdauer",
+        ],
     ),
     kt_ag=RPG2Config(
         enabled=False,  # TODO: Enable
