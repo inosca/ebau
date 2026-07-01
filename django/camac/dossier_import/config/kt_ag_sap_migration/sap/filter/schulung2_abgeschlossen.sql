b782f6e3fe414c66a847f6053776d33f2f61db8d
Jonas Metzener <jonas.metzener@adfinis.com>
2026-07-01 14:45:17 +0200

    chore(ag): cleanup code for SAP migration
    The migration was successful, we don't need the code anymore.

--- django/camac/dossier_import/config/kt_ag_sap_migration/sap/filter/schulung2_abgeschlossen.sql
+++
@@ -1,5 +0,0 @@
-t.TXT30 in ('Gesuch archiviert', 'Gesuch storniert', 'Gesuch abgeschrieben', 'Gesuch zurückgezogen')
-  and city.CITY in
-        ('Fischbach-Göslikon', 'Aarburg', 'Möhlin', 'Zuzgen', 'Tägerig', 'Biberstein', 'Arni (AG)', 'Wallbach',
-        'Tegerfelden', 'Lengnau (AG)', 'Würenlingen', 'Riniken')
-  and g.DIVERSES_KNZ != 'X' and g.VORABKL_KNZ != 'X'
